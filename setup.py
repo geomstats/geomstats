8a25f92620d86a7283bc7731580073c69c1571ff
Nina Miolane <ninamio78@gmail.com>
2018-01-09 11:52:17 +0100

    Add setup.py and requirements

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+version = '1.0'
+
+setup(name='geomstats',
+      version=version,
+      author='Nina Miolane',
+      py_modules=['rotations', 'rigid_transformations', 'visualization'])
