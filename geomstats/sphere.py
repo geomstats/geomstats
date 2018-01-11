9b36e81b185649637f94d16f21fa5146a85e1a91
Nina Miolane <ninamio78@gmail.com>
2018-01-11 19:28:05 +0100

    Address CR

--- geomstats/sphere.py
+++ geomstats/sphere.py
@@ -8,6 +8,25 @@ import math
 
 EPSILON = 1e-6
 
+SIN_TAYLOR_COEFFS = [0., 1.,
+                     0., - 1 / math.factorial(3),
+                     0., + 1 / math.factorial(5),
+                     0., - 1 / math.factorial(7),
+                     0., + 1 / math.factorial(9)]
+COS_TAYLOR_COEFFS = [1., 0.,
+                     - 1 / math.factorial(2), 0.,
+                     + 1 / math.factorial(4), 0.,
+                     - 1 / math.factorial(6), 0.,
+                     + 1 / math.factorial(8), 0.]
+INV_SIN_TAYLOR_COEFFS = [0., 1. / 6.,
+                         0., 7. / 360.,
+                         0., 31. / 15120.,
+                         0., 127. / 604800.]
+INV_TAN_TAYLOR_COEFFS = [0., - 1. / 3.,
+                         0., - 1. / 45.,
+                         0., - 2. / 945.,
+                         0., -1. / 4725.]
+
 
 def projection_to_tangent_space(ref_point, vector):
     """
@@ -30,20 +49,20 @@ def riemannian_exp(ref_point, vector, epsilon=EPSILON):
 
     :param ref_point: a point on the n-dimensional sphere
     :param vector: (n+1)-dimensional vector
-    :returns riem_exp: a point on the n-dimensional sphere
+    :return riem_exp: a point on the n-dimensional sphere
     """
     tangent_vec = projection_to_tangent_space(ref_point, vector)
     norm_tangent_vec = np.linalg.norm(tangent_vec)
 
     if norm_tangent_vec < epsilon:
-        coef_1 = (1. - norm_tangent_vec ** 2 / math.factorial(2)
-                  + norm_tangent_vec ** 4 / math.factorial(4)
-                  - norm_tangent_vec ** 6 / math.factorial(6)
-                  + norm_tangent_vec ** 8 / math.factorial(8))
-        coef_2 = (1. - norm_tangent_vec ** 2 / math.factorial(3)
-                  + norm_tangent_vec ** 4 / math.factorial(5)
-                  - norm_tangent_vec ** 6 / math.factorial(7)
-                  + norm_tangent_vec ** 8 / math.factorial(9))
+        coef_1 = (1. + COS_TAYLOR_COEFFS[2] * norm_tangent_vec ** 2
+                  + COS_TAYLOR_COEFFS[4] * norm_tangent_vec ** 4
+                  + COS_TAYLOR_COEFFS[6] * norm_tangent_vec ** 6
+                  + COS_TAYLOR_COEFFS[8] * norm_tangent_vec ** 8)
+        coef_2 = (1. + SIN_TAYLOR_COEFFS[3] * norm_tangent_vec ** 2
+                  + SIN_TAYLOR_COEFFS[5] * norm_tangent_vec ** 4
+                  + SIN_TAYLOR_COEFFS[7] * norm_tangent_vec ** 6
+                  + SIN_TAYLOR_COEFFS[9] * norm_tangent_vec ** 8)
     else:
         coef_1 = np.cos(norm_tangent_vec)
         coef_2 = np.sin(norm_tangent_vec) / norm_tangent_vec
@@ -64,7 +83,7 @@ def riemannian_log(ref_point, point, epsilon=EPSILON):
 
     :param ref_point: point on the n-dimensional sphere
     :param point: point on the n-dimensional sphere
-    :returns riem_log: tangent vector at ref_point
+    :return riem_log: tangent vector at ref_point
     """
     norm_ref_point = np.linalg.norm(ref_point)
     norm_point = np.linalg.norm(point)
@@ -76,14 +95,14 @@ def riemannian_log(ref_point, point, epsilon=EPSILON):
         angle = np.arccos(cos_angle)
 
     if angle < epsilon:
-        coef_1 = (1. + angle ** 2 / 6.
-                  + (7./360.) * angle ** 4
-                  + (31./15120.) * angle ** 6
-                  + (127./604800.) * angle ** 8)
-        coef_2 = (1. - angle ** 2 / 3.
-                  - angle ** 4 / 45.
-                  - (2./945.) * angle ** 6
-                  - angle ** 8 / 4725)
+        coef_1 = (1. + INV_SIN_TAYLOR_COEFFS[1] * angle ** 2
+                  + INV_SIN_TAYLOR_COEFFS[3] * angle ** 4
+                  + INV_SIN_TAYLOR_COEFFS[5] * angle ** 6
+                  + INV_SIN_TAYLOR_COEFFS[7] * angle ** 8)
+        coef_2 = (1. + INV_TAN_TAYLOR_COEFFS[1] * angle ** 2
+                  + INV_TAN_TAYLOR_COEFFS[3] * angle ** 4
+                  + INV_TAN_TAYLOR_COEFFS[5] * angle ** 6
+                  + INV_TAN_TAYLOR_COEFFS[7] * angle ** 8)
     else:
         coef_1 = angle / np.sin(angle)
         coef_2 = angle / np.tan(angle)
@@ -100,7 +119,7 @@ def riemannian_dist(point_a, point_b):
     embedding of the n-dimensional sphere
     in the (n+1)-dimensional euclidean space.
     """
-    # TODO(nina): np.dot(unit_vec, unit_vec) != 1 ??
+    # TODO(nina): case np.dot(unit_vec, unit_vec) != 1
     if np.all(point_a == point_b):
         return 0.
 
