db6c7869d0b8701387f0b7cefe4bd5a40d6f53cb
Nina Miolane <ninamio78@gmail.com>
2018-03-21 18:15:50 +0100

    Testing examples without showing the plots

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,16 +1,28 @@
 """Unit tests for the examples."""
 
-import os
 import unittest
 
+import matplotlib
+matplotlib.use('agg')  # noqa
+
+import examples.plot_geodesics_h2 as plot_geodesics_h2
+import examples.plot_geodesics_s2 as plot_geodesics_s2
 import examples.plot_geodesics_se3 as plot_geodesics_se3
+import examples.plot_geodesics_so3 as plot_geodesics_so3
 
 
 class TestEuclideanSpaceMethods(unittest.TestCase):
+    def test_plot_geodesics_h2(self):
+        plot_geodesics_h2.main()
+
+    def test_plot_geodesics_s2(self):
+        plot_geodesics_s2.main()
+
     def test_plot_geodesics_se3(self):
         plot_geodesics_se3.main()
-        #os.system('python3 examples/plot_geodesics_se3.py')
 
+    def test_plot_geodesics_so3(self):
+        plot_geodesics_so3.main()
 
 if __name__ == '__main__':
         unittest.main()
