b08d21417499a4a39e619dc1372758b86325b7b7
Nina Miolane <ninamio78@gmail.com>
2018-04-12 10:59:27 -0700

    Examples with two types of geodesics on H2

--- examples/plot_geodesics_h2.py
+++ examples/plot_geodesics_h2.py
@@ -15,19 +15,29 @@ METRIC = H2.metric
 
 def main():
     initial_point = H2.intrinsic_to_extrinsic_coords(np.array([0, 0.]))
+    end_point = H2.intrinsic_to_extrinsic_coords(np.array([1.5, 1.5]))
     assert H2.belongs(initial_point)
+    assert H2.belongs(end_point)
     initial_tangent_vec = H2.projection_to_tangent_space(
                                         vector=np.array([3.5, 0.6, 0]),
                                         base_point=initial_point)
-    geodesic = METRIC.geodesic(initial_point=initial_point,
+
+    geodesic_with_initial_tangent_vec = METRIC.geodesic(
+                               initial_point=initial_point,
                                initial_tangent_vec=initial_tangent_vec)
 
+    geodesic_between_two_points = METRIC.geodesic(
+                               initial_point=initial_point,
+                               end_point=end_point)
     n_steps = 10
     t = np.linspace(0, 1, n_steps)
 
-    points = geodesic(t)
-
+    points = geodesic_with_initial_tangent_vec(t)
     visualization.plot(points, space='H2')
+
+    points = geodesic_between_two_points(t)
+    visualization.plot(points, space='H2')
+
     plt.show()
 
 
