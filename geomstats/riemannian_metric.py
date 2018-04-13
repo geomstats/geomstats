27a8a6e2b443aec559e2b6b89a94b4b51854fe6a
Nina Miolane <ninamio78@gmail.com>
2018-04-13 09:30:28 -0700

    Address vectorization bug in geodesics

--- geomstats/riemannian_metric.py
+++ geomstats/riemannian_metric.py
@@ -190,14 +190,18 @@ class RiemannianMetric(object):
         for manifolds whose points are represented by matrices or higher
         dimensional tensors.
         """
+        initial_point = vectorization.to_ndarray(initial_point, to_ndim=2)
+
         if end_point is None and initial_tangent_vec is None:
             raise ValueError('Specify an end point or an initial tangent '
                              'vector to define the geodesic.')
         if end_point is not None:
+            end_point = vectorization.to_ndarray(end_point, to_ndim=2)
             shooting_tangent_vec = self.log(point=end_point, base_point=initial_point)
             if initial_tangent_vec is not None:
-                assert shooting_tangent_vec == initial_tangent_vec
+                assert np.allclose(shooting_tangent_vec, initial_tangent_vec)
             initial_tangent_vec = shooting_tangent_vec
+        initial_tangent_vec = vectorization.to_ndarray(initial_tangent_vec, to_ndim=2)
 
         def point_on_geodesic(t):
             t = vectorization.to_ndarray(t, to_ndim=1)
