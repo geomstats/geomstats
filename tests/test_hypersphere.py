e9e6b183d61ed205f043cdc6c6565f1393ed75a3
Nina Miolane <ninamio78@gmail.com>
2018-05-09 16:03:51 -0700

    Fix bugs in hypersphere and minkowski

--- tests/test_hypersphere.py
+++ tests/test_hypersphere.py
@@ -257,16 +257,16 @@ class TestHypersphereMethods(unittest.TestCase):
         n_points_b = self.space.random_uniform(n_samples=n_samples)
 
         result = self.metric.squared_dist(one_point_a, one_point_b)
-        self.assertTrue(gs.allclose(result.shape, (1, 1)))
+        self.assertTrue(gs.allclose(result.shape, ()))
 
         result = self.metric.squared_dist(n_points_a, one_point_b)
-        self.assertTrue(gs.allclose(result.shape, (n_samples, 1)))
+        self.assertTrue(gs.allclose(result.shape, (n_samples,)))
 
         result = self.metric.squared_dist(one_point_a, n_points_b)
-        self.assertTrue(gs.allclose(result.shape, (n_samples, 1)))
+        self.assertTrue(gs.allclose(result.shape, (n_samples,)))
 
         result = self.metric.squared_dist(n_points_a, n_points_b)
-        self.assertTrue(gs.allclose(result.shape, (n_samples, 1)))
+        self.assertTrue(gs.allclose(result.shape, (n_samples,)))
 
     def test_norm_and_dist(self):
         """
@@ -289,16 +289,16 @@ class TestHypersphereMethods(unittest.TestCase):
         n_points_b = self.space.random_uniform(n_samples=n_samples)
 
         result = self.metric.dist(one_point_a, one_point_b)
-        self.assertTrue(gs.allclose(result.shape, (1, 1)))
+        self.assertTrue(gs.allclose(result.shape, ()))
 
         result = self.metric.dist(n_points_a, one_point_b)
-        self.assertTrue(gs.allclose(result.shape, (n_samples, 1)))
+        self.assertTrue(gs.allclose(result.shape, (n_samples,)))
 
         result = self.metric.dist(one_point_a, n_points_b)
-        self.assertTrue(gs.allclose(result.shape, (n_samples, 1)))
+        self.assertTrue(gs.allclose(result.shape, (n_samples,)))
 
         result = self.metric.dist(n_points_a, n_points_b)
-        self.assertTrue(gs.allclose(result.shape, (n_samples, 1)))
+        self.assertTrue(gs.allclose(result.shape, (n_samples,)))
 
     def test_dist_point_and_itself(self):
         # Distance between a point and itself is 0.
