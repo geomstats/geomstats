fix gradient descent tests

examples/gradient_descent_s2.py
@@ -32,7 +32,7 @@ def gradient_descent(start,
                      loss,
                      grad,
                      manifold,
-                     lr=0.1,
+                     lr=0.5,
                      max_iter=128,
                      precision=1e-5):
     """Operate a gradient descent on a given manifold until either max_iter or
@@ -43,7 +43,7 @@ def gradient_descent(start,
         euclidean_grad = - lr * grad(x)
         tangent_vec = manifold.projection_to_tangent_space(
                 vector=euclidean_grad, base_point=x)
-        x = manifold.metric.exp(base_point=x, tangent_vec=tangent_vec)[0]
+        x = manifold.metric.exp(base_point=x, tangent_vec=tangent_vec)
         if np.abs(loss(x) - loss(x_prev)) <= precision:
             print('x: %s' % x)
             print('reached precision %s' % precision)
@@ -78,13 +78,13 @@ def plot_and_save_video(geodesics,
 
 
 def generate_well_behaved_matrix():
-    """Generate a matrix with real eig."""
+    """Generate a matrix with real eigenvalues."""
     matrix = 2 * SPDMatricesSpace(n=3).random_uniform()[0]
     assert np.linalg.det(matrix) > 0
     return matrix
 
 
-def main():
+def main(video_file='out.mp4', max_iter=128):
     A = generate_well_behaved_matrix()
     loss = lambda x: np.matmul(x.T, np.matmul(A, x))  # NOQA
     grad = lambda x: 2 * np.matmul(A, x)  # NOQA
@@ -94,7 +94,11 @@ def main():
     n_steps = 20
     # TODO(johmathe): auto differentiation
     # TODO(johmathe): gpu implementation
-    for x, fx in gradient_descent(initial_point, loss, grad, manifold=SPHERE2):
+    for x, fx in gradient_descent(initial_point,
+                                  loss,
+                                  grad,
+                                  max_iter=max_iter,
+                                  manifold=SPHERE2):
         initial_tangent_vec = METRIC.log(point=x, base_point=previous_x)
         geodesic = METRIC.geodesic(initial_point=previous_x,
                                    initial_tangent_vec=initial_tangent_vec)
@@ -102,7 +106,7 @@ def main():
         t = np.linspace(0, 1, n_steps)
         geodesics.append(geodesic(t))
         previous_x = x
-    plot_and_save_video(geodesics, loss)
+    plot_and_save_video(geodesics, loss, out=video_file, )
     eig, _ = np.linalg.eig(A)
     np.testing.assert_almost_equal(loss(x), np.min(eig), decimal=2)
 