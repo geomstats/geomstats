65d30346f125a7817d79c46c90fc3dd9fd708ef1
Johan Mathe <johan.mathe@gmail.com>
2018-05-18 00:17:16 -0700

    version 1.9

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as fp:
+        install_requires = fp.read()
+
 setup(name='geomstats',
-      version='1.8',
+      version='1.9',
+      install_requires=install_requires,
       description='Geometric statistics on manifolds',
       url='http://github.com/ninamiolane/geomstats',
       author='Nina Miolane',
