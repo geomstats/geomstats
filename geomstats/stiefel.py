9241e6f3ad91712eda65c9250baffa494bc81520
Oleg Kachan <15810673+oleg-kachan@users.noreply.github.com>
2018-08-16 02:33:53 +0300

    Vectorized exp, log, random_uniform, inner_product methods

--- geomstats/stiefel.py
+++ geomstats/stiefel.py
@@ -15,6 +15,19 @@ from geomstats.riemannian_metric import RiemannianMetric
 TOLERANCE = 1e-6
 EPSILON = 1e-6
 
+def matrix_f(sq_mat, f):
+    
+    sq_mat = gs.to_ndarray(sq_mat, to_ndim=3)
+    
+    [eigenvalues, vectors] = gs.linalg.eig(sq_mat)
+    
+    exp_eigenvalues = f(eigenvalues)
+    
+    aux = gs.einsum('ijk,ik->ijk', vectors, exp_eigenvalues)
+    exp_mat = gs.einsum('ijk,ikl->ijl', aux, gs.linalg.inv(vectors))
+    
+    return exp_mat.real
+
 class Stiefel(EmbeddedManifold):
 
     def __init__(self, p, n):
@@ -43,156 +56,224 @@ class Stiefel(EmbeddedManifold):
         pass
 
     def random_uniform(self, n_samples=1):
-        pass
+        """
+        Sample on St(p,n) with the uniform distribution.
+
+        If Z(p,n) ~ N(0,1), then St(p,n) ~ U, according to Haar measure:
+        St(p,n) := Z(Z^TZ)^{-1/2}
+        """
+        Z = gs.normal(size=(n_samples, self.n, self.p))
+        return gs.matmul(Z, gs.linalg.inv(matrix_f(gs.matmul(gs.transpose(Z, axes=(0, 2, 1)), Z), gs.sqrt)))
 
 class StiefelMetric(RiemannianMetric):
 
     def __init__(self, dimension):
         self.dimension = dimension
 
-    def inner_product(self, tangent_vec_a, tangent_vec_b, base_point):
+    def inner_product(self, tangent_vec_a, tangent_vec_b, base_point=None):
         """
-        Compute the inner product of tangent_vec_a and tangent_vec_b
-        at point base_point using the canonical Riemannian metric on St(p,n).
+        Compute the Frobenius inner product of tangent_vec_a and tangent_vec_b
+        at base_point using the canonical Riemannian metric on St(p,n).
         """
-        pass
+        tangent_vec_a = gs.to_ndarray(tangent_vec_a, to_ndim=3)
+        n_tangent_vecs_a, _, _ = tangent_vec_a.shape
+
+        tangent_vec_b = gs.to_ndarray(tangent_vec_a, to_ndim=3)
+        n_tangent_vecs_b, _, _ = tangent_vec_b.shape
+
+        assert(n_tangent_vecs_a == n_tangent_vecs_b)
+        
+        return gs.einsum("zij,zij->z", tangent_vec_a, tangent_vec_b)
 
     def norm(self, tangent_vec_a, tangent_vec_b):
         pass
 
-    def exp(self):
-        raise NotImplementedError('Exponential mapping is not implemented.')
-
-    def log(self):
-        raise NotImplementedError('Logarithmic mapping is not implemented.')
-
-    def mean(self, points, epsilon=EPSILON):
+    def exp(self, tangent_vec, base_point):
         """
-        Compute Riemannian mean on St(p,n), by applying retraction to
-        the arithmetic average of the lifted points from the manifold to tangent space.
+        Riemannian exponential of a tangent vector wrt to a base point.
         """
-        X = gs.asarray(points)
+        tangent_vec = gs.to_ndarray(tangent_vec, to_ndim=3)
+        n_tangent_vecs, _, _ = tangent_vec.shape
 
-        i = 0
-        converged = False
+        base_point = gs.to_ndarray(base_point, to_ndim=3)
+        n_base_points, n, p = base_point.shape
 
-        # set random element as initial mean
-        random_id = random.randrange(int(gs.shape(X)[0]))
-        X_mean = X[random_id]
+        assert(n_tangent_vecs == n_base_points
+            or n_tangent_vecs == 1
+            or n_base_points == 1)
         
-        while not converged:
-            i += 1
-            X_mean_prev = gs.copy(X_mean)
+        A = gs.matmul(gs.transpose(base_point, axes=(0, 2, 1)), tangent_vec)
+        K = tangent_vec - gs.matmul(base_point, A)
+        
+        Q = gs.zeros((K.shape))
+        R = gs.zeros((K.shape[0], K.shape[2], K.shape[2]))
+        for i, k in enumerate(K):
+            Q[i], R[i] = gs.linalg.qr(k)
+        
+        AR = gs.concatenate([A, -gs.transpose(R, axes=(0, 2, 1))], axis=2)
+        RZ = gs.concatenate([R, gs.zeros((n_base_points,p,p))], axis=2)
+        block = gs.concatenate([AR, RZ], axis=1)
+        MNe = matrix_f(block, gs.exp)
+        
+        return gs.matmul(gs.concatenate([base_point, Q], axis=2), MNe[:, :, 0:p])
 
-            X_mean = self.mean_iteration(X_mean, X)
-
-            if gs.linalg.norm(X_mean - X_mean_prev) <= epsilon:
-                converged = True
-
-        return X_mean
-
-    def mean_iteration(self, X, Qs):
+    def log(self, point, base_point, max_iter=100, tol=1e-6):
         """
-        Iteration of a fixed point algorithm
-        for computing Riemannian mean on St(p,n).
+        Riemannian logarithm of a point wrt a base point.
 
         Based on:
-        Kaneko, Tetsuya, Simone G. O. Fiori and Toshihisa Tanaka. 
-        "Empirical Arithmetic Averaging Over the Compact Stiefel Manifold."
-        IEEE Transactions on Signal Processing 61 (2013): 883-894.
+        Zimmermann, Ralf
+        "A Matrix-Algebraic Algorithm for the Riemannian Logarithm
+        on the Stiefel Manifold under the Canonical Metric"
+        SIAM J. Matrix Anal. & Appl., 38(2), 322–342.
         """
+        point = gs.to_ndarray(point, to_ndim=3)
+        n_points, _, _ = point.shape
 
-        # lift points to tangent space
-        V = []
-        for i, Q in enumerate(Qs):
-            try:
-                v = self.lifting(X, Q)
-                V.append(v)
-            except Exception as error:
-                pass
-                print("Q does not belong to the domain of the lifting map P_X^{-1}, because of {}".format(error))
-                
-        if len(V) <= 0:
-            raise Exception("Nothing to average. Q do(es) not belong to the domain of the lifting map at point X.")
-                
-        V = gs.array(V)
+        base_point = gs.to_ndarray(base_point, to_ndim=3)
+        n_base_points, n, p = base_point.shape
+
+        assert (n_points == n_base_points
+                or n_points == 1
+                or n_base_points == 1)
         
-        # average in tangent space
-        V = gs.mean(V, axis=0)
+        M = gs.matmul(gs.transpose(base_point, (0, 2, 1)), point)
+        
+        # QR of normal component of a point
+        K = point - gs.matmul(base_point, M)
+        
+        Q = gs.zeros((K.shape))
+        N = gs.zeros((K.shape[0], K.shape[2], K.shape[2]))
+        for i, k in enumerate(K):
+            Q[i], N[i] = gs.linalg.qr(k)
+        
+        # orthogonal completion
+        W = gs.concatenate([M, N], axis=1)
+        
+        V = gs.zeros((W.shape[0], max(W.shape[1], W.shape[2]), max(W.shape[1], W.shape[2])))
 
-        # retract computed mean to manifold
-        X_mean = self.retraction(X, V)
+        for i, w in enumerate(W):
+            V[i], _ = gs.linalg.qr(w, mode="complete")
+        
+        # Procrustes preprocessing
+        [D, S, R] = gs.linalg.svd(V[:, p:2*p, p:2*p])
+        
+        V[:, :, p:2*p] = gs.matmul(V[:, :, p:2*p], gs.matmul(R, gs.transpose(D, axes=(0, 2, 1))))
+        V = gs.concatenate([gs.concatenate([M, N], axis=1), V[:, :, p:2*p]], axis=2)
+        
+        for k in range(max_iter):
+            
+            LV = matrix_f(V, gs.log)
+            
+            C = LV[:, p:2*p, p:2*p]
+            normC = gs.linalg.norm(C, ord=2, axis=(1, 2))
+            
+            if normC < tol:
+                #print("Converged in {} iterations".format(k+1))
+                break
+                
+            Phi = matrix_f(-C, gs.exp)
+            V[:, :, p:2*p] = gs.matmul(V[:, :, p:2*p], Phi)
+            
+        return gs.matmul(base_point, LV[:, 0:p, 0:p]) + gs.matmul(Q, LV[:, p:2*p, 0:p])
 
-        return X_mean
-
-    def retraction(self, X, V):
+    def retraction(self, tangent_vec, base_point):
         """
         Retraction map, based on QR-decomposion:
         P_x(V) = qf(X + V)
         """
-        Q, R = gs.linalg.qr(X + V)
+        tangent_vec = gs.to_ndarray(tangent_vec, to_ndim=3)
+        n_tangent_vecs, _, _ = tangent_vec.shape
+
+        base_point = gs.to_ndarray(base_point, to_ndim=3)
+        n_base_points, n, p = base_point.shape
+
+        assert(n_tangent_vecs == n_base_points
+            or n_tangent_vecs == 1
+            or n_base_points == 1)
+
+        # Q, R = gs.linalg.qr(base_point + tangent_vec)
+        # TODO: remove cycle, when qr will be vectorized
+        Q = gs.zeros((base_point.shape))
+        R = gs.zeros((base_point.shape[0], base_point.shape[2], base_point.shape[2]))
+        for i, k in enumerate(base_point + tangent_vec):
+            Q[i], R[i] = gs.linalg.qr(k)
         
         # flipping signs
-        Q = gs.matmul(Q, gs.diag(gs.sign(gs.sign(gs.diagonal(R)) + 0.5)))
-        
-        return Q
+        # Q = gs.matmul(Q, gs.diag(gs.sign(gs.sign(gs.diagonal(R)) + 0.5)))
+        # TODO: remove cycle, when diag, diangonal will be vectorized
+        result = gs.zeros((base_point.shape))
+        for i, _ in enumerate(R):
+            result[i] = gs.matmul(Q, gs.diag(gs.sign(gs.sign(gs.diagonal(R[i])) + 0.5)))
 
-    def lifting(self, X, Q):
+        return result
+
+    def lifting(self, point, base_point):
         """
         Lifting map, based on QR-decomposion:
         P_x^{-1}(Q) = QR - X
         """
-        p, n = gs.shape(X)
-        
-        R = gs.zeros((n, n))
-        r = []
-        
-        M = gs.matmul(X.T, Q)
-        
-        # construct r_0
-        r.append(make_r(0, M))
-            
-        for i in range(1, n):
-            
-            # get principal minor
-            M_i = make_minor(i, M)
+        point = gs.to_ndarray(point, to_ndim=3)
+        n_points, _, _ = point.shape
 
-            if (gs.linalg.det(M_i) != 0):
-                b_i = make_b(i, M, r)
-                r_i = gs.matmul(gs.linalg.inv(M_i), b_i)
-                
-                if r_i[i] <= 0:
-                    raise Exception("(r_i)_i <= 0")
+        base_point = gs.to_ndarray(base_point, to_ndim=3)
+        n_base_points, p, n = base_point.shape
+
+        assert (n_points == n_base_points
+                or n_points == 1
+                or n_base_points == 1)
+
+        def make_minor(i, M):
+            return M[:i+1,:i+1]
+
+        def make_r(i, M):
+            if i == 0:
+                if (M[0,0] > 0):
+                    return gs.array([1. / M[0,0]])
                 else:
-                    r.append(r_i)
+                    raise Exception("M[0,0] <= 0")
             else:
-                raise Exception("det(M_i) == 0, not invertible")
+                return M[:i+1,i]
+
+        def make_b(i, M, r):
+
+            b = gs.ones(i+1)
+
+            for j in range(i):
+                b[j] = - gs.matmul(M[i,:j+1], r[j])
+
+            return b
         
-        for i, item in enumerate(r):
-            R[:len(item),i] = gs.array(item)
+        R = gs.zeros((n_base_points, n, n))
+        M = gs.matmul(gs.transpose(base_point, axes=(0, 2, 1)), point)
+
+        for k in range(n_base_points):
+            r = []
             
-        return gs.matmul(Q, R) - X
+            try:
+                # construct r_0
+                r.append(make_r(0, M[k]))
+                    
+                for i in range(1, n):
+                    
+                    # get principal minor
+                    M_i = make_minor(i, M[k])
+
+                    if (gs.linalg.det(M_i) != 0):
+                        b_i = make_b(i, M[k], r)
+                        r_i = gs.matmul(gs.linalg.inv(M_i), b_i)
+                        
+                        if r_i[i] <= 0:
+                            raise Exception("(r_i)_i <= 0")
+                        else:
+                            r.append(r_i)
+                    else:
+                        raise Exception("det(M_i) == 0, not invertible")
+                
+                for i, item in enumerate(r):
+                    R[k, :len(item), i] = gs.array(item)
+            
+        return gs.matmul(point, R) - base_point
 
     def dist(self):
         raise NotImplementedError('Geodesic distance is not implemented.')
-
-def make_minor(i, M):
-    return M[:i+1,:i+1]
-
-def make_r(i, M):
-    if i == 0:
-        if (M[0,0] > 0):
-            return gs.array([1. / M[0,0]])
-        else:
-            raise Exception("M[0,0] <= 0")
-    else:
-        return M[:i+1,i]
-
-def make_b(i, M, r):
-
-    b = gs.ones(i+1)
-
-    for j in range(i):
-        b[j] = - gs.matmul(M[i,:j+1], r[j])
-
-    return b
