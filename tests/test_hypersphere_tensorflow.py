cbe6875ea4b42393c62893490d1fd618413aecc9
Nina Miolane <ninamio78@gmail.com>
2018-09-12 14:55:36 -0700

    Debug log hypersphere TF for real

--- tests/test_hypersphere_tensorflow.py
+++ tests/test_hypersphere_tensorflow.py
@@ -168,6 +168,74 @@ class TestHypersphereOnTensorFlow(tf.test.TestCase):
 
         gs.testing.assert_allclose(result, expected)
 
+    def test_exp_vectorization(self):
+        n_samples = self.n_samples
+        dim = self.dimension + 1
+
+        one_vec = self.space.random_uniform()
+        one_base_point = self.space.random_uniform()
+        n_vecs = self.space.random_uniform(n_samples=n_samples)
+        n_base_points = self.space.random_uniform(n_samples=n_samples)
+
+        one_tangent_vec = self.space.projection_to_tangent_space(
+            one_vec, base_point=one_base_point)
+        result = self.metric.exp(one_tangent_vec, one_base_point)
+        gs.testing.assert_allclose(result.shape, (1, dim))
+
+        n_tangent_vecs = self.space.projection_to_tangent_space(
+            n_vecs, base_point=one_base_point)
+        result = self.metric.exp(n_tangent_vecs, one_base_point)
+        gs.testing.assert_allclose(result.shape, (n_samples, dim))
+
+        expected = gs.zeros((n_samples, dim))
+        for i in range(n_samples):
+            expected[i] = self.metric.exp(n_tangent_vecs[i], one_base_point)
+        expected = helper.to_vector(expected)
+        gs.testing.assert_allclose(result, expected)
+
+        one_tangent_vec = self.space.projection_to_tangent_space(
+            one_vec, base_point=n_base_points)
+        result = self.metric.exp(one_tangent_vec, n_base_points)
+        gs.testing.assert_allclose(result.shape, (n_samples, dim))
+
+        expected = gs.zeros((n_samples, dim))
+        for i in range(n_samples):
+            expected[i] = self.metric.exp(one_tangent_vec[i], n_base_points[i])
+        expected = helper.to_vector(expected)
+        gs.testing.assert_allclose(result, expected)
+
+        n_tangent_vecs = self.space.projection_to_tangent_space(
+            n_vecs, base_point=n_base_points)
+        result = self.metric.exp(n_tangent_vecs, n_base_points)
+        gs.testing.assert_allclose(result.shape, (n_samples, dim))
+
+        expected = gs.zeros((n_samples, dim))
+        for i in range(n_samples):
+            expected[i] = self.metric.exp(n_tangent_vecs[i], n_base_points[i])
+        expected = helper.to_vector(expected)
+        gs.testing.assert_allclose(result, expected)
+
+    def test_log_vectorization(self):
+        n_samples = self.n_samples
+        dim = self.dimension + 1
+
+        one_base_point = self.space.random_uniform()
+        one_point = self.space.random_uniform()
+        n_points = self.space.random_uniform(n_samples=n_samples)
+        n_base_points = self.space.random_uniform(n_samples=n_samples)
+
+        result = self.metric.log(one_point, one_base_point)
+        gs.testing.assert_allclose(result.shape, (1, dim))
+
+        result = self.metric.log(n_points, one_base_point)
+        gs.testing.assert_allclose(result.shape, (n_samples, dim))
+
+        result = self.metric.log(one_point, n_base_points)
+        gs.testing.assert_allclose(result.shape, (n_samples, dim))
+
+        result = self.metric.log(n_points, n_base_points)
+        gs.testing.assert_allclose(result.shape, (n_samples, dim))
+
     def test_exp_and_dist_and_projection_to_tangent_space(self):
         with self.test_session():
             base_point = gs.array([16., -2., -2.5, 84., 3.])
