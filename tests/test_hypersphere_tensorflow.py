a7eccb2bc5deb0fbec0056f6c87a76cbf2bff06c
Nina Miolane <ninamio78@gmail.com>
2018-09-13 14:30:18 -0700

    Test enable eager execution at the start

--- tests/test_hypersphere_tensorflow.py
+++ tests/test_hypersphere_tensorflow.py
@@ -12,11 +12,14 @@ import tests.helper as helper
 
 from geomstats.hypersphere import Hypersphere
 
+tf.enable_eager_execution()
+
 
 class TestHypersphereOnTensorFlow(tf.test.TestCase):
     _multiprocess_can_split_ = True
 
     def setUp(self):
+        #tf.enable_eager_execution()
         gs.random.seed(1234)
         self.dimension = 4
         self.space = Hypersphere(dimension=self.dimension)
@@ -26,7 +29,7 @@ class TestHypersphereOnTensorFlow(tf.test.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        tf.enable_eager_execution()
+        #tf.enable_eager_execution()
         os.environ['GEOMSTATS_BACKEND'] = 'tensorflow'
         importlib.reload(gs)
 
