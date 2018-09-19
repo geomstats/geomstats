a3314e3de6c5f380f731a8b8eff78b049da66a97
Nina Miolane <ninamio78@gmail.com>
2018-09-18 23:24:28 -0700

    lalala

--- tests/test_backend_tensorflow.py
+++ tests/test_backend_tensorflow.py
@@ -442,30 +442,31 @@ class TestBackendTensorFlow(tf.test.TestCase):
             expected = helper.to_scalar(expected)
             self.assertAllClose(gs.eval(result), gs.eval(expected))
 
-    # def test_exp_and_dist_and_projection_to_tangent_space_vec(self):
-    #     with self.test_session():
+    def test_exp_and_dist_and_projection_to_tangent_space_vec(self):
+        with self.test_session():
 
-    #         base_point = gs.array([[16., -2., -2.5, 84., 3.],
-    #                                [16., -2., -2.5, 84., 3.]])
+            base_point = tf.convert_to_tensor([
+                [16., -2., -2.5, 84., 3.],
+                [16., -2., -2.5, 84., 3.]])
 
-    #         base_single_point = gs.array([16., -2., -2.5, 84., 3.])
-    #         scalar_norm = gs.linalg.norm(base_single_point)
+            base_single_point = tf.convert_to_tensor([16., -2., -2.5, 84., 3.])
+            scalar_norm = gs.linalg.norm(base_single_point)
 
-    #         base_point = base_point / scalar_norm
-    #         vector = gs.array([[9., 0., -1., -2., 1.], [9., 0., -1., -2., 1]])
+            base_point = base_point / scalar_norm
+            vector = tf.convert_to_tensor([[9., 0., -1., -2., 1.], [9., 0., -1., -2., 1]])
 
-    #         tangent_vec = self.space.projection_to_tangent_space(
-    #                 vector=vector,
-    #                 base_point=base_point)
+            tangent_vec = self.space.projection_to_tangent_space(
+                    vector=vector,
+                    base_point=base_point)
 
-    #         exp = self.metric.exp(tangent_vec=tangent_vec,
-    #                               base_point=base_point)
+            exp = self.metric.exp(tangent_vec=tangent_vec,
+                                  base_point=base_point)
 
-    #         result = self.metric.dist(base_point, exp)
-    #         expected = gs.linalg.norm(tangent_vec, axis=-1) % (2 * gs.pi)
+            result = self.metric.dist(base_point, exp)
+            expected = gs.linalg.norm(tangent_vec, axis=-1) % (2 * gs.pi)
 
-    #         expected = helper.to_scalar(expected)
-    #         self.assertAllClose(gs.eval(result), gs.eval(expected))
+            expected = helper.to_scalar(expected)
+            self.assertAllClose(gs.eval(result), gs.eval(expected))
 
     # def test_geodesic_and_belongs(self):
     #     initial_point = self.space.random_uniform()
