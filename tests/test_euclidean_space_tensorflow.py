9a666515610e071964994d864b01b93c30429599
Nina Miolane <ninamio78@gmail.com>
2018-09-25 16:42:04 -0700

    More steps for Euclidean space in TF

--- tests/test_euclidean_space_tensorflow.py
+++ tests/test_euclidean_space_tensorflow.py
@@ -93,18 +93,87 @@ class TestEuclideanSpaceMethodsTensorFlow(tf.test.TestCase):
 
         result = self.metric.inner_product(n_points_a, one_point_b)
         point_numpy = np.random.uniform(size=(n_samples, 1))
+        # TODO(nina): Fix that this test fails with assertShapeEqual
         with self.test_session():
-            self.assertShapeEqual(point_numpy, result)
+            self.assertAllClose(point_numpy.shape, gs.eval(gs.shape(result)))
 
         result = self.metric.inner_product(one_point_a, n_points_b)
         point_numpy = np.random.uniform(size=(n_samples, 1))
+        # TODO(nina): Fix that this test fails with assertShapeEqual
         with self.test_session():
-            self.assertShapeEqual(point_numpy, result)
+            self.assertAllClose(point_numpy.shape, gs.eval(gs.shape(result)))
 
         result = self.metric.inner_product(n_points_a, n_points_b)
         point_numpy = np.random.uniform(size=(n_samples, 1))
+        # TODO(nina): Fix that this test fails with assertShapeEqual
         with self.test_session():
-            self.assertShapeEqual(point_numpy, result)
+            self.assertAllClose(point_numpy.shape, gs.eval(gs.shape(result)))
+
+    def test_squared_norm(self):
+        point = tf.convert_to_tensor([-2., 4.])
+
+        result = self.metric.squared_norm(point)
+        expected = gs.linalg.norm(point) ** 2
+        expected = helper.to_scalar(expected)
+
+        with self.test_session():
+            self.assertAllClose(gs.eval(result), gs.eval(expected))
+
+    def test_norm(self):
+        point = tf.convert_to_tensor([-2., 4.])
+
+        result = self.metric.norm(point)
+        expected = gs.linalg.norm(point)
+        expected = helper.to_scalar(expected)
+
+        with self.test_session():
+            self.assertAllClose(gs.eval(result), gs.eval(expected))
+
+    def test_exp(self):
+        base_point = tf.convert_to_tensor([0., 1.])
+        vector = tf.convert_to_tensor([2., 10.])
+
+        result = self.metric.exp(tangent_vec=vector,
+                                 base_point=base_point)
+        expected = base_point + vector
+        expected = helper.to_vector(expected)
+
+        with self.test_session():
+            self.assertAllClose(gs.eval(result), gs.eval(expected))
+
+    def test_log(self):
+        base_point = tf.convert_to_tensor([0., 1.])
+        point = tf.convert_to_tensor([2., 10.])
+
+        result = self.metric.log(point=point, base_point=base_point)
+        expected = point - base_point
+        expected = helper.to_vector(expected)
+
+        with self.test_session():
+            self.assertAllClose(gs.eval(result), gs.eval(expected))
+
+    def test_squared_dist(self):
+        point_a = tf.convert_to_tensor([-1., 4.])
+        point_b = tf.convert_to_tensor([1., 1.])
+
+        result = self.metric.squared_dist(point_a, point_b)
+        vec = point_b - point_a
+        expected = gs.dot(vec, vec)
+        expected = helper.to_scalar(expected)
+
+        with self.test_session():
+            self.assertAllClose(gs.eval(result), gs.eval(expected))
+
+    def test_dist(self):
+        point_a = tf.convert_to_tensor([0., 1.])
+        point_b = tf.convert_to_tensor([2., 10.])
+
+        result = self.metric.dist(point_a, point_b)
+        expected = gs.linalg.norm(point_b - point_a)
+        expected = helper.to_scalar(expected)
+
+        with self.test_session():
+            self.assertAllClose(gs.eval(result), gs.eval(expected))
 
 
 if __name__ == '__main__':
