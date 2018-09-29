a892fba9c025397f6d3b9f2bb6a4aa8bedb69935
Nina Miolane <ninamio78@gmail.com>
2018-09-28 22:52:03 -0700

    without eager mode

--- tests/test_general_linear_group_tensorflow.py
+++ tests/test_general_linear_group_tensorflow.py
@@ -27,7 +27,6 @@ class TestGeneralLinearGroupTensorFlow(tf.test.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        tf.enable_eager_execution()
         os.environ['GEOMSTATS_BACKEND'] = 'tensorflow'
         importlib.reload(gs)
 
@@ -41,7 +40,7 @@ class TestGeneralLinearGroupTensorFlow(tf.test.TestCase):
         A rotation matrix belongs to the matrix Lie group
         of invertible matrices.
         """
-        rot_vec = self.so3_group.random_uniform()
+        rot_vec = tf.convert_to_tensor([0.2, -0.1, 0.1])
         rot_mat = self.so3_group.matrix_from_rotation_vector(rot_vec)
         result = self.group.belongs(rot_mat)
         expected = tf.convert_to_tensor([True])
@@ -52,34 +51,31 @@ class TestGeneralLinearGroupTensorFlow(tf.test.TestCase):
     def test_compose(self):
         # 1. Composition by identity, on the right
         # Expect the original transformation
-        rot_vec = self.so3_group.random_uniform()
+        rot_vec = tf.convert_to_tensor([0.2, -0.1, 0.1])
         mat = self.so3_group.matrix_from_rotation_vector(rot_vec)
 
         result = self.group.compose(mat, self.group.identity)
         expected = mat
+        expected = helper.to_matrix(mat)
 
         with self.test_session():
             self.assertAllClose(gs.eval(result), gs.eval(expected))
 
         # 2. Composition by identity, on the left
         # Expect the original transformation
-        rot_vec = self.so3_group.random_uniform()
+        rot_vec = tf.convert_to_tensor([0.2, 0.1, -0.1])
         mat = self.so3_group.matrix_from_rotation_vector(rot_vec)
 
         result = self.group.compose(self.group.identity, mat)
         expected = mat
 
-        norm = gs.linalg.norm(expected)
-        atol = RTOL
-        if norm != 0:
-            atol = RTOL * norm
         with self.test_session():
-            self.assertAllClose(gs.eval(result), gs.eval(expected), atol=atol)
+            self.assertAllClose(gs.eval(result), gs.eval(expected))
 
     def test_compose_and_inverse(self):
         # 1. Compose transformation by its inverse on the right
         # Expect the group identity
-        rot_vec = self.so3_group.random_uniform()
+        rot_vec = tf.convert_to_tensor([0.2, 0.1, 0.1])
         mat = self.so3_group.matrix_from_rotation_vector(rot_vec)
         inv_mat = self.group.inverse(mat)
 
@@ -87,17 +83,12 @@ class TestGeneralLinearGroupTensorFlow(tf.test.TestCase):
         expected = self.group.identity
         expected = helper.to_matrix(expected)
 
-        norm = gs.linalg.norm(expected)
-        atol = RTOL
-        if norm != 0:
-            atol = RTOL * norm
-
         with self.test_session():
-            self.assertAllClose(gs.eval(result), gs.eval(expected), atol=atol)
+            self.assertAllClose(gs.eval(result), gs.eval(expected))
 
         # 2. Compose transformation by its inverse on the left
         # Expect the group identity
-        rot_vec = self.so3_group.random_uniform()
+        rot_vec = tf.convert_to_tensor([0.7, 0.1, 0.1])
         mat = self.so3_group.matrix_from_rotation_vector(rot_vec)
         inv_mat = self.group.inverse(mat)
 
@@ -105,13 +96,8 @@ class TestGeneralLinearGroupTensorFlow(tf.test.TestCase):
         expected = self.group.identity
         expected = helper.to_matrix(expected)
 
-        norm = gs.linalg.norm(expected)
-        atol = RTOL
-        if norm != 0:
-            atol = RTOL * norm
-
         with self.test_session():
-            self.assertAllClose(gs.eval(result), gs.eval(expected), atol=atol)
+            self.assertAllClose(gs.eval(result), gs.eval(expected))
 
 
 if __name__ == '__main__':
