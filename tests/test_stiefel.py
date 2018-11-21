77658db30f8d149644a23e30fbd9a3f0fead112a
Nina Miolane <ninamio78@gmail.com>
2018-11-21 12:17:13 -0800

    Add stiefel unit test inner product

--- tests/test_stiefel.py
+++ tests/test_stiefel.py
@@ -14,6 +14,10 @@ class TestStiefelMethods(unittest.TestCase):
     _multiprocess_can_split_ = True
 
     def setUp(self):
+        """
+        Tangent vectors constructed following:
+        http://noodle.med.yale.edu/hdtag/notes/steifel_notes.pdf
+        """
         warnings.simplefilter('ignore', category=ImportWarning)
 
         gs.random.seed(1234)
@@ -24,6 +28,44 @@ class TestStiefelMethods(unittest.TestCase):
         self.n_samples = 10
         self.dimension = int(
             self.p * self.n - (self.p * (self.p + 1) / 2))
+
+        self.point = gs.array([
+            [1., 0., 0.],
+            [0., 1., 0.],
+            [0., 0., 1.],
+            [0., 0., 0.]])
+
+        point_perp = gs.array([
+            [0.],
+            [0.],
+            [0.],
+            [1.]])
+
+        matrix_a_1 = gs.array([
+            [0., 2., -5.],
+            [-2., 0., -1.],
+            [5., 1., 0.]])
+
+        matrix_b_1 = gs.array([
+            [-2., 1., 4.]])
+
+        matrix_a_2 = gs.array([
+            [0., 2., -5.],
+            [-2., 0., -1.],
+            [5., 1., 0.]])
+
+        matrix_b_2 = gs.array([
+            [-2., 1., 4.]])
+
+        self.tangent_vector_1 = (
+            gs.matmul(self.point, matrix_a_1)
+            + gs.matmul(point_perp, matrix_b_1))
+
+        self.tangent_vector_2 = (
+            gs.matmul(self.point, matrix_a_2)
+            + gs.matmul(point_perp, matrix_b_2))
+
+        self.metric = self.space.euclidean_metric
 
     def test_belongs(self):
         point = self.space.random_uniform()
@@ -44,20 +86,19 @@ class TestStiefelMethods(unittest.TestCase):
         gs.testing.assert_allclose(point.shape, (1, self.n, self.p))
 
     def test_inner_product(self):
-        point_a = gs.array([
-            [1., 0., 0.],
-            [0., 1., 0.],
-            [0., 0., 1.],
-            [0., 0., 0.]])
+        base_point = self.point
 
-        point_b = gs.array([
-            [1., 0., 0.],
-            [0., 1., 0.],
-            [0., 0., 1.],
-            [1., 0., 0.]])
+        result = self.metric.inner_product(
+            self.tangent_vector_1,
+            self.tangent_vector_2,
+            base_point=base_point)
 
-        result = self.metric.inner_product(point_a, point_b)
+        expected = gs.trace(
+            gs.matmul(
+                self.tangent_vector_1.T,
+                self.tangent_vector_2))
 
+        gs.testing.assert_allclose(result, expected)
 
 
 if __name__ == '__main__':
