984f70aa92cf51070853b790976c741a431a725f
Nina Miolane <ninamio78@gmail.com>
2018-12-27 14:50:36 +0100

    One more test

--- tests/test_discretized_curves_space.py
+++ tests/test_discretized_curves_space.py
@@ -36,7 +36,7 @@ class TestDiscretizedCurvesSpaceMethods(geomstats.tests.TestCase):
 
         self.n_discretized_curves = 5
         self.times = gs.linspace(0., 1., self.n_discretized_curves)
-        self.atol = 1e-8
+        self.atol = 1e-6
         gs.random.seed(1234)
         self.space_curves_in_euclidean_3d = DiscretizedCurvesSpace(
                 embedding_manifold=r3)
@@ -69,7 +69,6 @@ class TestDiscretizedCurvesSpaceMethods(geomstats.tests.TestCase):
 
         self.assertAllClose(result, expected)
 
-    @geomstats.tests.np_only
     def test_l2_metric_log_and_exp(self):
         """
         Test that exp and log are inverse maps.
@@ -80,7 +79,7 @@ class TestDiscretizedCurvesSpaceMethods(geomstats.tests.TestCase):
                                        base_curve=self.curve_a)
         expected = self.curve_b
 
-        gs.testing.assert_allclose(result, expected, atol=self.atol)
+        self.assertAllClose(result, expected, atol=self.atol)
 
     @geomstats.tests.np_only
     def test_l2_metric_inner_product_vectorization(self):
