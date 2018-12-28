d9e332862d826908af180fb1e2445effca6a7ea4
Nina Miolane <ninamio78@gmail.com>
2018-12-28 12:41:24 +0100

    Add numpy backend for examples with visualizations

--- examples/plot_geodesics_se3.py
+++ examples/plot_geodesics_se3.py
@@ -5,6 +5,9 @@ with its left-invariant canonical METRIC.
 
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+# Examples with visualizations are only implemented with numpy
+os.environ['GEOMSTATS_BACKEND'] = 'numpy'  # NOQA
 
 import geomstats.visualization as visualization
 
