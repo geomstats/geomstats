ed71d01cf84731ba06a864c153ce9cf4f036c90f
Nina Miolane <ninamio78@gmail.com>
2018-12-29 16:28:21 +0100

    Gln tests with TF

--- geomstats/general_linear_group.py
+++ geomstats/general_linear_group.py
@@ -67,21 +67,59 @@ class GeneralLinearGroup(LieGroup, MatricesSpace):
     def group_exp_from_identity(self, tangent_vec, point_type=None):
         """
         Group exponential of the Lie group of
-        all invertible matrices has a straight-forward
-        computation for symmetric positive definite matrices.
+        all invertible matrices at the identity.
         """
         tangent_vec = gs.to_ndarray(tangent_vec, to_ndim=3)
         group_exp = gs.linalg.expm(tangent_vec)
 
         return gs.real(group_exp)
 
+    def group_exp_not_from_identity(
+            self, tangent_vec, base_point, point_type=None):
+        """
+        Group exponential of the Lie group of
+        all invertible matrices.
+        """
+        tangent_vec = gs.to_ndarray(tangent_vec, to_ndim=3)
+        base_point = gs.to_ndarray(base_point, to_ndim=3)
+
+        tangent_vec_at_identity = self.compose(
+            self.inverse(base_point), tangent_vec)
+
+        group_exp_from_identity = self.group_exp_from_identity(
+                tangent_vec_at_identity)
+
+        group_exp = self.compose(
+            base_point, group_exp_from_identity)
+
+        return group_exp
+
     def group_log_from_identity(self, point, point_type=None):
         """
         Group logarithm of the Lie group of
-        all invertible matrices has a straight-forward
-        computation for symmetric positive definite matrices.
+        all invertible matrices at the identity.
         """
         point = gs.to_ndarray(point, to_ndim=3)
         group_log = gs.linalg.logm(point)
 
         return gs.real(group_log)
+
+    def group_log_not_from_identity(
+            self, point, base_point, point_type=None):
+        """
+        Group logarithm of the Lie group of
+        all invertible matrices.
+        """
+        point = gs.to_ndarray(point, to_ndim=3)
+        base_point = gs.to_ndarray(base_point, to_ndim=3)
+
+        point_near_identity = self.compose(
+            self.inverse(base_point), point)
+
+        group_log_from_identity = self.group_log_from_identity(
+            point_near_identity)
+
+        group_log = self.compose(
+            base_point, group_log_from_identity)
+
+        return group_log
