c277fc18990ef03ab23f0f74f666b87c513977ab
Nina Miolane <ninamio78@gmail.com>
2018-12-31 10:04:37 +0100

    Comment failing examples in TF

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -41,9 +41,11 @@ class TestExamples(geomstats.tests.TestCase):
     def test_gradient_descent_s2(self):
         gradient_descent_s2.main(max_iter=32, output_file=None)
 
+    @geomstats.tests.np_only
     def test_loss_and_gradient_so3(self):
         loss_and_gradient_so3.main()
 
+    @geomstats.tests.np_only
     def test_loss_and_gradient_se3(self):
         loss_and_gradient_se3.main()
 
