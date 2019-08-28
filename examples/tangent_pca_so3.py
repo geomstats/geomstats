6fa78eebb531215d2fb5f5955a9ff353b6b8ddf7
Nina Miolane <ninamio78@gmail.com>
2019-08-28 11:32:00 +0200

    Replace tangent pca example

--- examples/tangent_pca_so3.py
+++ examples/tangent_pca_so3.py
@@ -8,12 +8,15 @@ import numpy as np
 
 import geomstats.visualization as visualization
 
+from geomstats.learning.pca import TangentPCA
+
 from geomstats.special_orthogonal_group import SpecialOrthogonalGroup
 
 SO3_GROUP = SpecialOrthogonalGroup(n=3)
 METRIC = SO3_GROUP.bi_invariant_metric
 
-N_SAMPLES = 20
+N_SAMPLES = 10
+N_COMPONENTS = 2
 
 
 def main():
@@ -22,32 +25,33 @@ def main():
     data = SO3_GROUP.random_uniform(n_samples=N_SAMPLES)
     mean = METRIC.mean(data)
 
-    eigenvalues, tangent_eigenvecs = METRIC.tangent_pca(data, base_point=mean)
-    n_eigenvalues = min(N_SAMPLES, SO3_GROUP.dimension)
+    tpca = TangentPCA(metric=METRIC, n_components=N_COMPONENTS)
+    tpca = tpca.fit(data, base_point=mean)
+    tangent_projected_data = tpca.transform(data)
+    print(
+        'Coordinates of the Log of the first 5 data points at the mean, '
+        'projected on the principal components:')
+    print(tangent_projected_data[:5])
 
-    explained_variances = np.cumsum(eigenvalues)
-    explained_variances = explained_variances / explained_variances[-1]
-
-    ax_eig = fig.add_subplot(131)
-    xticks = np.arange(0, n_eigenvalues, 1)
-    ax_eig.xaxis.set_ticks(xticks)
-    ax_eig.set_title('Eigenvalues in decreasing order')
-    ax_eig.plot(xticks, eigenvalues)
-
-    ax_var = fig.add_subplot(132)
-    xticks = np.arange(0, n_eigenvalues, 1)
+    ax_var = fig.add_subplot(121)
+    xticks = np.arange(1, N_COMPONENTS+1, 1)
     ax_var.xaxis.set_ticks(xticks)
     ax_var.set_title('Explained variance')
     ax_var.set_xlabel('Number of Principal Components')
-    ax_var.plot(xticks, explained_variances)
+    ax_var.set_ylim((0, 1))
+    ax_var.plot(xticks, tpca.explained_variance_ratio_)
 
-    ax = fig.add_subplot(133, projection="3d")
+    ax = fig.add_subplot(122, projection="3d")
     plt.setp(ax,
-             xlim=(-1, 1), ylim=(-1, 1), zlim=(-1, 1),
+             xlim=(-3, 3), ylim=(-3, 3), zlim=(-3, 3),
              xlabel="X", ylabel="Y", zlabel="Z")
 
+    ax.set_title('Data in SO3 (black) and Frechet mean (color)')
     visualization.plot(data, ax, space='SO3_GROUP', color='black')
     visualization.plot(mean, ax, space='SO3_GROUP', linewidth=3)
+    ax.set_xlim((-2, 2))
+    ax.set_ylim((-2, 2))
+    ax.set_zlim((-2, 2))
     plt.show()
 
 
