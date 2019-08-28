4e4cc2af98c014adefc8dada45eb6a32806ea29d
Nina Miolane <ninamio78@gmail.com>
2019-08-28 11:31:13 +0200

    Working tPCA with sklearn API

---
+++ examples/tangent_pca_so3_sklearn.py
@@ -0,0 +1,59 @@
+"""
+Compute the mean of a data set of 3D rotations.
+Performs tangent PCA at the mean.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import geomstats.visualization as visualization
+
+from geomstats.learning.pca import TangentPCA
+
+from geomstats.special_orthogonal_group import SpecialOrthogonalGroup
+
+SO3_GROUP = SpecialOrthogonalGroup(n=3)
+METRIC = SO3_GROUP.bi_invariant_metric
+
+N_SAMPLES = 10
+N_COMPONENTS = 2
+
+
+def main():
+    fig = plt.figure(figsize=(15, 5))
+
+    data = SO3_GROUP.random_uniform(n_samples=N_SAMPLES)
+    mean = METRIC.mean(data)
+
+    tpca = TangentPCA(metric=METRIC, n_components=N_COMPONENTS)
+    tpca = tpca.fit(data, base_point=mean)
+    tangent_projected_data = tpca.transform(data)
+    print(
+        'Coordinates of the Log of the first 5 data points at the mean, '
+        'projected on the principal components:')
+    print(tangent_projected_data[:5])
+
+    ax_var = fig.add_subplot(121)
+    xticks = np.arange(1, N_COMPONENTS+1, 1)
+    ax_var.xaxis.set_ticks(xticks)
+    ax_var.set_title('Explained variance')
+    ax_var.set_xlabel('Number of Principal Components')
+    ax_var.set_ylim((0, 1))
+    ax_var.plot(xticks, tpca.explained_variance_ratio_)
+
+    ax = fig.add_subplot(122, projection="3d")
+    plt.setp(ax,
+             xlim=(-3, 3), ylim=(-3, 3), zlim=(-3, 3),
+             xlabel="X", ylabel="Y", zlabel="Z")
+
+    ax.set_title('Data in SO3 (black) and Frechet mean (color)')
+    visualization.plot(data, ax, space='SO3_GROUP', color='black')
+    visualization.plot(mean, ax, space='SO3_GROUP', linewidth=3)
+    ax.set_xlim((-2, 2))
+    ax.set_ylim((-2, 2))
+    ax.set_zlim((-2, 2))
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ geomstats/learning/pca.py
@@ -0,0 +1,239 @@
+""" Principal Component Analysis on Manifolds
+"""
+
+from math import log
+import numbers
+
+import numpy as np
+from scipy import linalg
+from scipy.special import gammaln
+
+from sklearn.utils.validation import check_array
+
+from sklearn.decomposition.base import _BasePCA
+from sklearn.utils.extmath import svd_flip
+from sklearn.utils.extmath import stable_cumsum
+
+
+def _assess_dimension_(spectrum, rank, n_samples, n_features):
+    """Compute the likelihood of a rank ``rank`` dataset
+
+    The dataset is assumed to be embedded in gaussian noise of shape(n,
+    dimf) having spectrum ``spectrum``.
+
+    Parameters
+    ----------
+    spectrum : array of shape (n)
+        Data spectrum.
+    rank : int
+        Tested rank value.
+    n_samples : int
+        Number of samples.
+    n_features : int
+        Number of features.
+
+    Returns
+    -------
+    ll : float,
+        The log-likelihood
+
+    Notes
+    -----
+    This implements the method of `Thomas P. Minka:
+    Automatic Choice of Dimensionality for PCA. NIPS 2000: 598-604`
+    """
+    if rank > len(spectrum):
+        raise ValueError("The tested rank cannot exceed the rank of the"
+                         " dataset")
+
+    pu = -rank * log(2.)
+    for i in range(rank):
+        pu += (gammaln((n_features - i) / 2.) -
+               log(np.pi) * (n_features - i) / 2.)
+
+    pl = np.sum(np.log(spectrum[:rank]))
+    pl = -pl * n_samples / 2.
+
+    if rank == n_features:
+        pv = 0
+        v = 1
+    else:
+        v = np.sum(spectrum[rank:]) / (n_features - rank)
+        pv = -np.log(v) * n_samples * (n_features - rank) / 2.
+
+    m = n_features * rank - rank * (rank + 1.) / 2.
+    pp = log(2. * np.pi) * (m + rank + 1.) / 2.
+
+    pa = 0.
+    spectrum_ = spectrum.copy()
+    spectrum_[rank:n_features] = v
+    for i in range(rank):
+        for j in range(i + 1, len(spectrum)):
+            pa += log((spectrum[i] - spectrum[j]) *
+                      (1. / spectrum_[j] - 1. / spectrum_[i])) + log(n_samples)
+
+    ll = pu + pl + pv + pp - pa / 2. - rank * log(n_samples) / 2.
+
+    return ll
+
+
+def _infer_dimension_(spectrum, n_samples, n_features):
+    """Infers the dimension of a dataset of shape (n_samples, n_features)
+
+    The dataset is described by its spectrum `spectrum`.
+    """
+    n_spectrum = len(spectrum)
+    ll = np.empty(n_spectrum)
+    for rank in range(n_spectrum):
+        ll[rank] = _assess_dimension_(spectrum, rank, n_samples, n_features)
+    return ll.argmax()
+
+
+class TangentPCA(_BasePCA):
+    """Tangent Principal component analysis (tPCA)
+
+    Linear dimensionality reduction using
+    Singular Value Decomposition of the
+    Riemannian Log of the data at the tangent space
+    of the mean.
+    """
+
+    def __init__(self, metric, n_components=None, copy=True,
+                 whiten=False, tol=0.0, iterated_power='auto',
+                 random_state=None):
+        self.metric = metric
+        self.n_components = n_components
+        self.copy = copy
+        self.whiten = whiten
+        self.tol = tol
+        self.iterated_power = iterated_power
+        self.random_state = random_state
+
+    def fit(self, X,
+            base_point=None, point_type='vector', y=None):
+        """Fit the model with X.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            Training data, where n_samples is the number of samples
+            and n_features is the number of features.
+
+        y : Ignored
+
+        Returns
+        -------
+        self : object
+            Returns the instance itself.
+        """
+        self._fit(X, base_point, point_type)
+        return self
+
+    def fit_transform(self, X,
+                      base_point=None, point_type='vector',
+                      y=None):
+        """Fit the model with X and apply the dimensionality reduction on X.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            Training data, where n_samples is the number of samples
+            and n_features is the number of features.
+
+        y : Ignored
+
+        Returns
+        -------
+        X_new : array-like, shape (n_samples, n_components)
+
+        """
+        U, S, V = self._fit(X, base_point, point_type)
+        U = U[:, :self.n_components_]
+
+        U *= S[:self.n_components_]
+
+        return U
+
+    def _fit(self, X, base_point=None, point_type='vector'):
+        """Fit the model by computing full SVD on X"""
+        print('hello')
+        if point_type == 'matrix':
+            raise NotImplementedError(
+                'This is currently only implemented for vectors.')
+        if base_point is None:
+            base_point = self.metric.mean(X)
+
+        tangent_vecs = self.metric.log(X, base_point=base_point)
+
+        # Convert to sklearn format
+        X = tangent_vecs
+
+        X = check_array(X, dtype=[np.float64, np.float32], ensure_2d=True,
+                        copy=self.copy)
+
+        # Handle n_components==None
+        if self.n_components is None:
+            n_components = min(X.shape)
+        else:
+            n_components = self.n_components
+        n_samples, n_features = X.shape
+
+        if n_components == 'mle':
+            if n_samples < n_features:
+                raise ValueError("n_components='mle' is only supported "
+                                 "if n_samples >= n_features")
+        elif not 0 <= n_components <= min(n_samples, n_features):
+            raise ValueError("n_components=%r must be between 0 and "
+                             "min(n_samples, n_features)=%r with "
+                             "svd_solver='full'"
+                             % (n_components, min(n_samples, n_features)))
+        elif n_components >= 1:
+            if not isinstance(n_components, (numbers.Integral, np.integer)):
+                raise ValueError("n_components=%r must be of type int "
+                                 "when greater than or equal to 1, "
+                                 "was of type=%r"
+                                 % (n_components, type(n_components)))
+
+        # Center data
+        self.mean_ = np.mean(X, axis=0)
+        X -= self.mean_
+
+        U, S, V = linalg.svd(X, full_matrices=False)
+        # flip eigenvectors' sign to enforce deterministic output
+        U, V = svd_flip(U, V)
+
+        components_ = V
+
+        # Get variance explained by singular values
+        explained_variance_ = (S ** 2) / (n_samples - 1)
+        total_var = explained_variance_.sum()
+        explained_variance_ratio_ = explained_variance_ / total_var
+        singular_values_ = S.copy()  # Store the singular values.
+
+        # Postprocess the number of components required
+        if n_components == 'mle':
+            n_components = \
+                _infer_dimension_(explained_variance_, n_samples, n_features)
+        elif 0 < n_components < 1.0:
+            # number of components for which the cumulated explained
+            # variance percentage is superior to the desired threshold
+            ratio_cumsum = stable_cumsum(explained_variance_ratio_)
+            n_components = np.searchsorted(ratio_cumsum, n_components) + 1
+
+        # Compute noise covariance using Probabilistic PCA model
+        # The sigma2 maximum likelihood (cf. eq. 12.46)
+        if n_components < min(n_features, n_samples):
+            self.noise_variance_ = explained_variance_[n_components:].mean()
+        else:
+            self.noise_variance_ = 0.
+
+        print('bonsoir')
+        self.n_samples_, self.n_features_ = n_samples, n_features
+        self.components_ = components_[:n_components]
+        self.n_components_ = n_components
+        self.explained_variance_ = explained_variance_[:n_components]
+        self.explained_variance_ratio_ = \
+            explained_variance_ratio_[:n_components]
+        self.singular_values_ = singular_values_[:n_components]
+
+        return U, S, V
