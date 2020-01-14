cd383fa3923c782a72a980e948c9077c3524e6a4
hadiz <hadizaatiti@gmail.com>
2020-01-14 19:03:08 +0100

    Add point_type for "Poincare" with metric and skeleton for exp and log.

--- geomstats/backend/pytorch/__init__.py
+++ geomstats/backend/pytorch/__init__.py
@@ -346,6 +346,8 @@ def clip(x, amin, amax):
         return torch.clamp(x, amin, amax)
     return _np.clip(x, amin, amax)
 
+def clamp(*args, **kwargs):
+    return torch.clamp(*args,**kwargs)
 
 def diag(*args, **kwargs):
     return torch.diag(*args, **kwargs)
@@ -447,7 +449,6 @@ def get_mask_i_float(i, n):
     mask_i = equal(range_n, i_float)
     mask_i_float = cast(mask_i, float32)
     return mask_i_float
-
 
 def copy(x):
     return x.clone()
