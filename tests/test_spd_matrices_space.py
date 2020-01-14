dc29e6ce3f749cf0ec4b4093271b18f16b75aa5e
ythanwer <yann.thanwerdas@gmail.com>
2020-01-14 12:35:13 +0100

    Test of the differential of the power function

--- tests/test_spd_matrices_space.py
+++ tests/test_spd_matrices_space.py
@@ -78,6 +78,22 @@ class TestSPDMatricesSpaceMethods(geomstats.tests.TestCase):
         self.assertTrue(gs.allclose(result, expected))
 
     @geomstats.tests.np_and_tf_only
+    def test_differential_power(self):
+        base_point = gs.array([[1., 0., 0.],
+                               [0., 2.5, 1.],
+                               [0., 1., 2.5]])
+        tangent_vec = gs.array([[2., 1., 1.],
+                               [1., .5, .5],
+                               [1., .5, .5]])
+        power = .5
+        result = self.space.differential_power(power=power, tangent_vec=tangent_vec, base_point=base_point)
+        expected = gs.array([[1., 1/3, 1/3],
+                             [1/3, .25, .25],
+                             [1/3, .25, .25]])
+
+        self.assertAllClose(result, expected)
+
+    @geomstats.tests.np_and_tf_only
     def test_log_and_exp(self):
         base_point = gs.array([[5., 0., 0.],
                                [0., 7., 2.],
