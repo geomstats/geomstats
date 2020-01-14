b2bed9cf71bb7a0e0fe00b0eabc3e3dbaccd426d
Yann Cabanes <yann.cabanes@u-bordeaux.fr>
2020-01-14 19:02:00 +0100

    poincqre polydisk

---
+++ geomstats/geometry/poincare_polydisk.py
@@ -0,0 +1,18 @@
+"""
+The Poincare polydisk
+"""
+
+from geomstats.geometry.hyperbolic_space import HyperbolicSpace
+from geomstats.geometry.product_manifold import ProductManifold
+
+
+class PoincarePolydisk(ProductManifold):
+    """
+    Class for the Poincare polydisk, which is a direct product
+    of n Poincare disks, i.e. hyperbolic spaces of dimension 2.
+    """
+    def __init__(self, n_disks):
+        disk = HyperbolicSpace(dimension=2)
+        list_disks = [disk,] * n_disks
+        super(PoincarePolydisk, self).__init__(
+            manifolds=list_disks)
---
+++ tests/test_poincare_polydisk.py
@@ -0,0 +1,23 @@
+"""
+Unit tests for the Poincare Polydisk.
+"""
+
+
+import geomstats.backend as gs
+import geomstats.tests
+
+from geomstats.geometry.poincare_polydisk import PoincarePolydisk
+
+
+class TestPoincarePolydiskMethods(geomstats.tests.TestCase):
+    _multiprocess_can_split_ = True
+
+    def setUp(self):
+        gs.random.seed(1234)
+        self.n_disks = 5
+        self.space = PoincarePolydisk(n_disks=self.n_disks)
+
+    def test_dimension(self):
+        expected = self.n_disks * 2
+        result = self.space.dimension
+        self.assertAllClose(result, expected)
