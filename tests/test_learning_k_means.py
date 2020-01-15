ef306b2ac946bd3fd787640e19653c4822715dd7
Thomas Gerald <thomas.l.p.gerald@gmail.com>
2020-01-15 11:49:09 +0100

    Change import remove a test

--- tests/test_learning_k_means.py
+++ tests/test_learning_k_means.py
@@ -6,25 +6,12 @@ import geomstats
 import geomstats.backend as gs
 import geomstats.tests
 
-from geomstats.geometry import hyperbolic_space
 from geomstats.geometry import hypersphere
-from geomstats.learning.k_means import KMeans
+from geomstats.learning.k_means import RiemannianKMeans
 
 
 class TestLearningKMeans(geomstats.tests.TestCase):
     _multiprocess_can_split_ = True
-
-    def setUp(self):
-        pass
-
-    def test_hyperbolic_space_k_means(self):
-        x = gs.rand(100, 2)
-        manifold = hyperbolic_space.HyperbolicSpace(2)
-        metric = hyperbolic_space.HyperbolicMetric(2)
-        x = manifold.intrinsic_to_extrinsic_coords(x)
-        k_means = KMeans(2, metric, tol=1e-4)
-        # test learning
-        k_means.fit(x, max_iter=100)
 
     def test_hypersphere_k_means_fit(self):
         gs.random.seed(55)
@@ -34,9 +21,9 @@ class TestLearningKMeans(geomstats.tests.TestCase):
 
         x = manifold.random_von_mises_fisher(kappa=100, n_samples=200)
 
-        clustering = KMeans(1, metric)
-        clustering.fit(x)
-        center = clustering.centroids
+        k_means = RiemannianKMeans(metric, 1, tol=1e-3)
+        k_means.fit(x)
+        center = k_means.centroids
         mean = metric.mean(x)
         result = metric.dist(center, mean)
         expected = 0.
@@ -50,14 +37,13 @@ class TestLearningKMeans(geomstats.tests.TestCase):
 
         x = manifold.random_von_mises_fisher(kappa=100, n_samples=200)
 
-        k_means = KMeans(5, metric, tol=1e-5)
+        k_means = RiemannianKMeans(metric, 5, tol=1e-5)
         k_means.fit(x, max_iter=100)
         result = k_means.predict(x)
 
         centroids = k_means.centroids
-        expected = gs.array([int(metric.closest_neighbor_index(x[i],
-                                 centroids))
-                             for i in range(len(x))])
+        expected = gs.array([int(metric.closest_neighbor_index(x_i, centroids))
+                             for x_i in x])
         self.assertAllClose(expected, result)
 
 
