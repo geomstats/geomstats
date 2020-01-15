96afea390fb0460b2f54cde6594eabc6080e554e
Niklas Koep <niklas.koep@gmail.com>
2020-01-15 10:56:44 +0100

    Don't use pytest in tests/test_pca.py
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- tests/test_pca.py
+++ tests/test_pca.py
@@ -1,43 +1,41 @@
-import pytest
+import unittest
 import numpy as np
 
-from sklearn.utils.testing import assert_allclose
-
+import geomstats.backend as gs
+assert_allclose = gs.testing.assert_allclose
 from geomstats.geometry.special_orthogonal_group import SpecialOrthogonalGroup
 from geomstats.learning.pca import TangentPCA
 
 
-SO3_GROUP = SpecialOrthogonalGroup(n=3)
-METRIC = SO3_GROUP.bi_invariant_metric
+SO3 = SpecialOrthogonalGroup(n=3)
+metric = SO3.bi_invariant_metric
 N_SAMPLES = 10
 N_COMPONENTS = 2
 
 
-@pytest.fixture
-def data():
-    data = SO3_GROUP.random_uniform(n_samples=N_SAMPLES)
-    return data
+class TestTangentPCA(unittest.TestCase):
+    _multiprocess_can_split_ = True
 
+    def setUp(self):
+        self.X = SO3.random_uniform(n_samples=N_SAMPLES)
 
-def test_tangent_pca_error(data):
-    X = data
-    trans = TangentPCA(n_components=N_COMPONENTS)
-    trans.fit(X)
-    with pytest.raises(ValueError, match="Shape of input is different"):
+    def test_tangent_pca_error(self):
+        X = self.X
+        trans = TangentPCA(metric, n_components=N_COMPONENTS)
+        trans.fit(X)
         X_diff_size = np.ones((10, X.shape[1] + 1))
-        trans.transform(X_diff_size)
+        self.assertRaises(trans.transform(X_diff_size), ValueError)
 
+    def test_tangent_pca(self):
+        X = self.X
+        trans = TangentPCA(metric, n_components=N_COMPONENTS)
+        self.assertEquals(trans.demo_param, 'demo')
 
-def test_tangent_pca(data):
-    X = data
-    trans = TangentPCA(n_components=N_COMPONENTS)
-    assert trans.demo_param == 'demo'
+        trans.fit(X)
+        self.assertEquals(trans.n_features_, X.shape[1])
 
-    trans.fit(X)
-    assert trans.n_features_ == X.shape[1]
+        X_trans = trans.transform(X)
+        assert_allclose(X_trans, np.sqrt(X))
 
-    X_trans = trans.transform(X)
-    assert_allclose(X_trans, np.sqrt(X))
-
-    X_trans = trans.fit_transform(X)
-    assert_allclose(X_trans, np.sqrt(X))
+        X_trans = trans.fit_transform(X)
+        assert_allclose(X_trans, np.sqrt(X))
