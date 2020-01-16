6d5f1e57054106430e6713784863d2cf37867d3e
Alice Le Brigant <alice.le-brigant@enac.fr>
2020-01-16 12:51:20 +0100

    Add online kmeans examples

---
+++ examples/plot_online_k_means_s1.py
@@ -0,0 +1,51 @@
+"""
+Plot the result of optimal quantization of the uniform distribution
+on the circle, using online k-means clustering of a sample.
+"""
+
+import os
+
+import matplotlib.pyplot as plt
+
+import geomstats.visualization as visualization
+from geomstats.geometry.hypersphere import Hypersphere
+from geomstats.learning.online_k_means import OnlineKmeans
+
+N_POINTS = 1000
+N_CENTERS = 5
+N_REPETITIONS = 20
+TOLERANCE = 1e-6
+
+
+def main():
+    circle = Hypersphere(dimension=1)
+
+    data = circle.random_uniform(n_samples=1000, bound=None)
+
+    n_clusters = 5
+    clustering = OnlineKmeans(metric=circle.metric, n_clusters=n_clusters)
+    clustering = clustering.fit(data)
+
+    plt.figure(0)
+    visualization.plot(points=clustering.cluster_centers_, space='S1',
+                       color='red')
+    plt.show()
+
+    plt.figure(1)
+    ax = plt.axes()
+    circle_plot = visualization.Circle()
+    circle_plot.draw(ax=ax)
+    for i in range(n_clusters):
+        cluster = data[clustering.labels_ == i, :]
+        circle_plot.draw_points(ax=ax, points=cluster)
+    plt.show()
+
+
+if __name__ == "__main__":
+    if os.environ['GEOMSTATS_BACKEND'] == 'tensorflow':
+        print('Examples with visualizations are only implemented '
+              'with numpy backend.\n'
+              'To change backend, write: '
+              'export GEOMSTATS_BACKEND = \'numpy\'.')
+    else:
+        main()
---
+++ examples/plot_online_k_means_s2.py
@@ -0,0 +1,47 @@
+"""
+Plot the result of optimal quantization of the von Mises Fisher distribution
+on the sphere using online k-means clustering of a sample.
+"""
+
+import os
+
+import matplotlib.pyplot as plt
+
+import geomstats.visualization as visualization
+from geomstats.geometry.hypersphere import Hypersphere
+from geomstats.learning.online_k_means import OnlineKmeans
+
+
+def main():
+    sphere = Hypersphere(dimension=2)
+
+    data = sphere.random_von_mises_fisher(kappa=10, n_samples=1000)
+
+    n_clusters = 4
+    clustering = OnlineKmeans(metric=sphere.metric, n_clusters=n_clusters)
+    clustering = clustering.fit(data)
+
+    plt.figure(0)
+    ax = plt.subplot(111, projection="3d")
+    visualization.plot(points=clustering.cluster_centers_, ax=ax,
+                       space='S2', c='r')
+    plt.show()
+
+    plt.figure(1)
+    ax = plt.subplot(111, projection="3d")
+    sphere_plot = visualization.Sphere()
+    sphere_plot.draw(ax=ax)
+    for i in range(n_clusters):
+        cluster = data[clustering.labels_ == i, :]
+        sphere_plot.draw_points(ax=ax, points=cluster)
+    plt.show()
+
+
+if __name__ == "__main__":
+    if os.environ['GEOMSTATS_BACKEND'] == 'tensorflow':
+        print('Examples with visualizations are only implemented '
+              'with numpy backend.\n'
+              'To change backend, write: '
+              'export GEOMSTATS_BACKEND = \'numpy\'.')
+    else:
+        main()
