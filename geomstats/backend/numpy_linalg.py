25b86a8928e103a6e59f0dc182b6ebbbd069eca7
ythanwer <yann.thanwerdas@gmail.com>
2020-01-16 12:27:38 +0100

    Symmetric exponential, symmetric logarithm, symmetric power Updated the existing tests Tests for powerm

--- geomstats/backend/numpy_linalg.py
+++ geomstats/backend/numpy_linalg.py
@@ -3,15 +3,83 @@
 import numpy as np
 import scipy.linalg
 
+from geomstats.backend.numpy import to_ndarray
+
+def exph(x):
+    eigvals, eigvecs = np.linalg.eigh(x)
+    eigvals = np.exp(eigvals)
+    eigvals = np.vectorize(np.diag, signature='(n)->(n,n)')(eigvals)
+    transp_eigvecs = np.transpose(eigvecs, axes=(0, 2, 1))
+    result = np.matmul(eigvecs, eigvals)
+    result = np.matmul(result, transp_eigvecs)
+    return result
+
 
 def expm(x):
-    return np.vectorize(
-        scipy.linalg.expm, signature='(n,m)->(n,m)')(x)
+    ndim = x.ndim
+    new_x = to_ndarray(x, to_ndim=3)
+    if (new_x - np.transpose(new_x, axes=(0, 2, 1)) == 0).all():
+        result = exph(new_x)
+    else:
+        result = np.vectorize(scipy.linalg.expm,
+                               signature='(n,m)->(n,m)')(new_x)
+
+    if ndim == 2:
+        return result[0]
+    else:
+        return result
 
 
 def logm(x):
-    return np.vectorize(
-        scipy.linalg.logm, signature='(n,m)->(n,m)')(x)
+    ndim = x.ndim
+    new_x = to_ndarray(x, to_ndim=3)
+    if (new_x - np.transpose(new_x, axes=(0, 2, 1)) == 0).all():
+        eigvals, eigvecs = np.linalg.eigh(new_x)
+        if (eigvals > 0).all():
+            eigvals = np.vectorize(np.diag, signature='(n)->(n,n)')(np.log(eigvals))
+            transp_eigvecs = np.transpose(eigvecs, axes=(0, 2, 1))
+            result = np.matmul(eigvecs, eigvals)
+            result = np.matmul(result, transp_eigvecs)
+        else:
+            result = np.vectorize(scipy.linalg.logm,
+                                  signature='(n,m)->(n,m)')(new_x)
+    else:
+        result = np.vectorize(scipy.linalg.logm,
+                              signature='(n,m)->(n,m)')(new_x)
+
+    if ndim == 2:
+        return result[0]
+    else:
+        return result
+
+
+def powerm(x, power):
+    ndim = x.ndim
+    new_x = to_ndarray(x, to_ndim=3)
+    if (new_x - np.transpose(new_x, axes=(0, 2, 1)) == 0).all():
+        eigvals, eigvecs = np.linalg.eigh(new_x)
+        if (eigvals > 0).all():
+            eigvals = np.vectorize(np.diag, signature='(n)->(n,n)')(eigvals**power)
+            transp_eigvecs = np.transpose(eigvecs, axes=(0, 2, 1))
+            result = np.matmul(eigvecs, eigvals)
+            result = np.matmul(result, transp_eigvecs)
+        else:
+            result = np.vectorize(scipy.linalg.logm,
+                                  signature='(n,m)->(n,m)')(new_x)
+            result = power * result
+            result = np.vectorize(scipy.linalg.expm,
+                                  signature='(n,m)->(n,m)')(result)
+    else:
+        result = np.vectorize(scipy.linalg.logm,
+                              signature='(n,m)->(n,m)')(new_x)
+        result = power * result
+        result = np.vectorize(scipy.linalg.expm,
+                              signature='(n,m)->(n,m)')(result)
+
+    if ndim == 2:
+        return result[0]
+    else:
+        return result
 
 
 def sqrtm(x):
