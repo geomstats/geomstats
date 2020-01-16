cfb62ce2a5025c521c9536e8a5a49d7775e2cdc7
Niklas Koep <niklas.koep@gmail.com>
2020-01-16 15:14:32 +0100

    numpy_random.py: forward-export numpy functions
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- geomstats/backend/numpy_random.py
+++ geomstats/backend/numpy_random.py
@@ -1,19 +1,4 @@
 """Numpy based random backend."""
 
 import numpy as np
-
-
-def rand(*args, **kwargs):
-    return np.random.rand(*args, **kwargs)
-
-
-def randint(*args, **kwargs):
-    return np.random.randint(*args, **kwargs)
-
-
-def seed(*args, **kwargs):
-    return np.random.seed(*args, **kwargs)
-
-
-def normal(*args, **kwargs):
-    return np.random.normal(*args, **kwargs)
+from numpy.random import normal, rand, randint, randn, seed
