f78586d01bb327cf8faea5e5e309a96ad4af389b
Niklas Koep <niklas.koep@gmail.com>
2020-01-16 08:05:27 +0100

    Announce backend after successful import
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- geomstats/backend/__init__.py
+++ geomstats/backend/__init__.py
@@ -9,14 +9,11 @@ if _BACKEND is None:
     os.environ['GEOMSTATS_BACKEND'] = _BACKEND = 'numpy'
 
 if _BACKEND == 'numpy':
-    print('Using numpy backend', file=sys.stderr)
     from geomstats.backend.numpy import *  # NOQA
 elif _BACKEND == 'pytorch':
-<<<<<<< HEAD
-    print('Using pytorch backend', file=sys.stderr)
     from geomstats.backend.pytorch import *  # NOQA
 elif _BACKEND == 'tensorflow':
-    print('Using tensorflow backend', file=sys.stderr)
     from geomstats.backend.tensorflow import *  # NOQA
 else:
     raise RuntimeError('Unknown backend \'{:s}\''.format(_BACKEND))
+print('Using {:s} backend'.format(_BACKEND), file=sys.stderr)
