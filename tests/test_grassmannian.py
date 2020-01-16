9ab3a2deab37d62cf1146a4012bce7d882d20170
Olivier Peltre <opeltre@gmail.com>
2020-01-16 11:50:56 +0100

    fix grassmann.exp test: np_only

--- tests/test_grassmannian.py
+++ tests/test_grassmannian.py
@@ -8,9 +8,19 @@ import geomstats.tests
 from geomstats.geometry.grassmannian import Grassmannian
 from geomstats.geometry.grassmannian import GrassmannianCanonicalMetric
 
-p_xy = gs.diag([1., 1., 0.])[0]
-p_yz = gs.diag([0., 1., 1.])[0]
-p_xz = gs.diag([1., 0., 1.])[0]
+p_xy = gs.array([
+    [1., 0., 0.],
+    [0., 1., 0.],
+    [0., 0., 0.]])
+p_yz = gs.array([
+    [0., 0., 0.],
+    [0., 1., 0.],
+    [0., 0., 1.]])
+p_xz = gs.array([
+    [1., 0., 0.],
+    [0., 0., 0.],
+    [0., 0., 1.]])
+
 r_y = gs.array([
     [0., 0., 1.],
     [0., 0., 0.],
@@ -29,12 +39,15 @@ class TestGrassmannianMethods(geomstats.tests.TestCase):
         self.k = 2
         self.space = Grassmannian(self.n, self.k)
         self.metric = GrassmannianCanonicalMetric(self.n, self.k)
-
-    def test_exp(self): 
-        result = self.metric.exp(pi_2 * r_y, [p_xy, p_yz])
+    
+    @geomstats.tests.np_only
+    def test_exp_np(self): 
+        result = self.metric.exp(
+                pi_2 * r_y, 
+                gs.array([p_xy, p_yz]))
         expected = gs.array([p_yz, p_xy])
         self.assertAllClose(result, expected)
-
+    
         result = self.metric.exp(
             pi_2 * gs.array([r_y, r_z]), 
             gs.array([p_xy, p_yz]))
