8f09c9320f67137ffe692ebec7519a5fb5b3bd20
xpennec <59821822+xpennec@users.noreply.github.com>
2020-01-17 11:03:21 +0100

    Added partially cooked example on the uncertainty of the empirical frechet mean

---
+++ examples/empirical_frechet_mean_uncertainty_s3.py
@@ -0,0 +1,74 @@
+"""
+Uncertainty of the empirical Fréchet mean on the sphere
+
+The variance of the Fréchet mean FM_n of a sample of n IID
+random variables of variance Var is decreasing more slowly
+in a sphere than in a Euclidean space.
+This example computes the  modulation factor
+     alpha = Var( FM_n) / ( n * Var)
+for isotropic distributions on hyperspheres or radius 0<r<Pi
+in the sphere s3.
+"""
+
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+import geomstats.backend as gs
+# import geomstats.visualization as visualization
+
+from geomstats.geometry.hypersphere import Hypersphere
+
+## n = number of samples for computing the mean
+## NN = number of trials for the stochastic integral of expectation
+## Ntheta = number of points on the curve sig_est(theta)
+
+
+def EmpiricalVar(n, theta, dim, NN=5000):
+    """
+    Compute the variance of the empirical Fréchet mean of n samples
+    drawn from an isotropic distributions on hyper sub-spheres of radius
+    0 < theta < Pi around the north pole of the sphere of dimension dim.
+    This is repeated NN times to make a stochastic approximation of the
+    expectation.
+    """
+    assert dim > 2, "We need dimension larger than 2 to draw a uniform sample on the subsphere"
+    var = []
+    sphere = Hypersphere(dimension=dim)
+    subsphere = Hypersphere(dimension=dim-1)
+
+    ## Beware that dim is the dimension of the sphere in sph.*
+    ## Define north pole
+    north_pole = np.zeros(dim+1)
+    north_pole[dim] = 1.0
+    for j in range(NN):
+        # Sample n points from the uniform distrib of a circle of radius r = cos(theta)
+        data = np.zeros((n, dim+1), dtype=float)
+        #phi =  np.random.random_sample(n)*2*math.pi
+        #Phi = sphere.RandomSample(dim-1, n)
+        ## For sampling on a subsphere, use RandomUniform(dim-1)
+        for i in range(n):
+            direction = subsphere.random_uniform(n)
+            for j in range(dim):
+                data[i,j] = math.sin(theta) * direction[j]
+            data[i,dim] = math.cos(theta)
+        ## Compute empirical Frechet mean of the n-sample
+        current_mean = sphere.  (data)
+        var.append( sph.Dist(en,xx)**2 )
+    return (np.mean(var), 2* np.std(var) / math.sqrt( NN ) )
+
+
+def main():
+
+
+
+    fig = plt.figure(figsize=(15, 5))
+
+    sphere = Hypersphere(dimension=2)
+
+    data = sphere.random_von_mises_fisher(kappa=15, n_samples=140)
+    mean = sphere.metric.mean(data)
+
+if __name__ == "__main__":
+    main()
