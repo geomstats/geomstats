28d474f9dc7aaf067d23653b461b65c89acfad80
ythanwer <yann.thanwerdas@gmail.com>
2020-01-17 18:09:10 +0100

    Skip test with pytorch backend

--- tests/test_spd_matrices.py
+++ tests/test_spd_matrices.py
@@ -283,6 +283,7 @@ class TestSPDMatricesMethods(geomstats.tests.TestCase):
         expected = point
         self.assertAllClose(result, expected)
 
+    @geomstats.tests.np_and_tf_only
     def test_log_and_exp_logeuclidean(self):
         base_point = gs.array([[5., 0., 0.],
                                [0., 7., 2.],
