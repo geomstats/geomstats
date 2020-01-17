e88b0b8402506f265203a61854d96879f76f0225
Nina Miolane <ninamio78@gmail.com>
2020-01-17 09:18:38 +0100

    Fix tests in tangent pca

--- tests/test_pca.py
+++ tests/test_pca.py
@@ -8,7 +8,7 @@ from geomstats.geometry.special_orthogonal_group import SpecialOrthogonalGroup
 from geomstats.learning.pca import TangentPCA
 
 
-class TestTangentPCA(geomstats.test.TestCase):
+class TestTangentPCA(geomstats.tests.TestCase):
     _multiprocess_can_split_ = True
 
     def setUp(self):
@@ -24,17 +24,11 @@ class TestTangentPCA(geomstats.test.TestCase):
         trans = TangentPCA(self.metric, n_components=self.n_components)
         trans.fit(X)
         X_diff_size = gs.ones((self.n_samples, gs.shape(X)[1] + 1))
-        self.assertRaises(trans.transform(X_diff_size), ValueError)
+        self.assertRaises(ValueError, trans.transform, X_diff_size)
 
     def test_tangent_pca(self):
         X = self.X
-        trans = TangentPCA(self.metric, n_components=self.n_components)
+        trans = TangentPCA(self.metric, n_components=gs.shape(X)[1])
 
         trans.fit(X)
         self.assertEquals(trans.n_features_, gs.shape(X)[1])
-
-        X_trans = trans.transform(X)
-        self.assertAllClose(X_trans, gs.sqrt(X))
-
-        X_trans = trans.fit_transform(X)
-        self.assertAllClose(X_trans, gs.sqrt(X))
