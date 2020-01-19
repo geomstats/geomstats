114c8ebb4b6bde303d2bc7730c629172dd84a399
Nina Miolane <ninamio78@gmail.com>
2020-01-19 11:39:49 +0100

    Fix imports in test_examples

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -7,8 +7,6 @@ import sys
 import warnings
 
 import examples.gradient_descent_s2 as gradient_descent_s2
-import examples.loss_and_gradient_se3 as loss_and_gradient_se3
-import examples.loss_and_gradient_so3 as loss_and_gradient_so3
 import examples.plot_geodesics_h2 as plot_geodesics_h2
 import examples.plot_geodesics_s2 as plot_geodesics_s2
 import examples.plot_geodesics_se3 as plot_geodesics_se3
@@ -21,6 +19,9 @@ import examples.plot_square_h2_poincare_disk as plot_square_h2_poincare_disk
 import examples.plot_square_h2_poincare_half_plane as plot_square_h2_poincare_half_plane  # NOQA
 import examples.tangent_pca_s2 as tangent_pca_s2
 import examples.tangent_pca_so3 as tangent_pca_so3
+if os.environ['GEOMSTATS_BACKEND'] == 'tensorflow':  # NOQA
+    import examples.loss_and_gradient_se3 as loss_and_gradient_se3
+    import examples.loss_and_gradient_so3 as loss_and_gradient_so3
 import matplotlib
 matplotlib.use('Agg')  # NOQA
 import matplotlib.pyplot as plt
