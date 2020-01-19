96002b87adaff6987a141ef4e14221d1464ef9a2
xpennec <59821822+xpennec@users.noreply.github.com>
2020-01-19 18:37:38 +0100

    Finished reimplementing the figures for the modulation factor in 2D and 3D spheres.
    * Saving figures does not work yet (to de debugged)
    
    * Profiling should be done as this implementation takes 10 to 15 times more than my original implementation...

--- examples/empirical_frechet_mean_uncertainty_s3.py
+++ examples/empirical_frechet_mean_uncertainty_s3.py
@@ -11,7 +11,6 @@ in the sphere s3.
 """
 
 
-import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -25,7 +24,7 @@ from geomstats.geometry.hypersphere import Hypersphere
 # Ntheta = number of points on the curve sig_est(theta)
 
 
-def EmpiricalVar(n, theta, dim, NN=5000):
+def empirical_var(n, theta, dim, NN=1000):
     """
     Compute the variance of the empirical Fréchet mean of n samples
     drawn from an isotropic distributions on hyper sub-spheres of radius
@@ -33,7 +32,7 @@ def EmpiricalVar(n, theta, dim, NN=5000):
     This is repeated NN times to make a stochastic approximation of the
     expectation.
     """
-    assert dim > 2, "Dim > 2 needed to draw a uniform sample on subsphere"
+    assert dim > 1, "Dim > 1 needed to draw a uniform sample on subsphere"
     var = []
     sphere = Hypersphere(dimension=dim)
     subsphere = Hypersphere(dimension=dim-1)
@@ -44,29 +43,124 @@ def EmpiricalVar(n, theta, dim, NN=5000):
     for j in range(NN):
         # Sample n points from the uniform distrib on a subsphere
         # of radius theta (i.e cos(theta) in ambiant space)
-        data = gz.zeros((n, dim+1), dtype=float)
+        data = gs.zeros((n, dim+1), dtype=gs.float64)
         # For sampling on a subsphere, use RandomUniform(dim-1)
+        directions = subsphere.random_uniform(n)
         for i in range(n):
-            directions = subsphere.random_uniform(n)
             for j in range(dim):
-                data[i,j] = gs.sin(theta) * directions[j]
+                data[i,j] = gs.sin(theta) * directions[i,j]
             data[i,dim] = gs.cos(theta)
         ## Compute empirical Frechet mean of the n-sample
-        current_mean = sphere.metric.adaptive_gradientdescent_mean(data,init_points=[north_pole])
-        var.append(sphere.metric.squared_dist(north_pole,current_mean)
-    return (np.mean(var), 2* np.std(var) / gs.sqrt( NN ) )
+        current_mean = sphere.metric.adaptive_gradientdescent_mean(data, n_max_iterations=64, init_points=[north_pole])
+        var.append(sphere.metric.squared_dist(north_pole, current_mean))
+    return (np.mean(var), 2* np.std(var) / np.sqrt( NN ) )
+
+
+def modulation_factor(n, theta, dim, NN=1000):
+    """
+    Compute the modulation factor as the ratio between the variance of
+    the empirical Fréchet mean of n samples drawn from an isotropic
+    distributions on hyper sub-spheres of radius 0 < theta < Pi around
+    the north pole of the sphere of dimension dim.
+    This is repeated NN times to make a stochastic approximation of the
+    expectation and stddev on the modulation factor is returned as second parameter
+    """
+    (var, stdvar) = empirical_var(n, theta, dim, NN=NN)
+    return var * n / theta ** 2, stdvar * n / theta ** 2
+
+
+
+def asymptotic_modulation(dim, theta):
+    gamma = 1.0 / dim + (1.0 - 1.0 / dim) * theta / np.tan(theta)
+    return (1.0 / gamma)**2
+
+
+def plot_modulation_factor(n, dim, NN=1000, Ntheta=20):
+    # modulation factor for n points on the sphere S_dim embedded in R^{dim+1}
+    theta = np.linspace(0.000001, np.pi / 2.0 - 0.000001, Ntheta)
+    measured_modulation_factor = []
+    error = []
+    small_var_modulation_factor = []
+    asymptotic_modulation_factor = []
+    for thetai in theta:
+        (var, stdvar) = modulation_factor(n, thetai, dim, NN=NN)
+        measured_modulation_factor.append(var)
+        error.append(stdvar)
+        print(n, thetai, var, stdvar, '\n')
+        small_var_modulation_factor.append(1.0 + 2.0 / 3.0 * thetai ** 2 * (1.0 - 1.0 / dim) * (1.0 - 1.0 / (n)))
+        asymptotic_modulation_factor.append(asymptotic_modulation(dim, thetai))
+    plt.figure()
+    plt.errorbar(theta, measured_modulation_factor, yerr=error, color='r', label='Measured')  # plotting t,a separately
+    plt.plot(theta, small_var_modulation_factor, 'g', label='Small variance prediction')  # plotting t,a separately
+    plt.plot(theta, asymptotic_modulation_factor, 'grey', label='Asymptotic prediction')  # plotting t,a separately
+    plt.xlabel(r'Standard deviation $\theta$')
+    plt.ylabel(r'Modulation factor $\alpha$')
+    plt.title("Convergence rate modulation factor, sphere dim={1}, n={0}".format(n, dim))
+    plt.legend(loc='best')
+    plt.show()
+    # TODO(Xavier): find why there is FileNotFoundError: [Errno 2] when saving
+    ## plt.savefig("Figures/SphVarModulation_N{0}_d{1}.svg".format(n, dim))
+    ## plt.savefig("./Figures/SphVarModulation_N{0}_d{1}.png".format(n, dim))
+    return plt
+
+
+
+def multiplot_modulation_factor(dim, NN=1000, Ntheta=20):
+    ## Implementation for the sphere S_dim in R^{dim+1}
+    theta = np.linspace(0.000001, np.pi / 2.0 - 0.000001, Ntheta)
+    small_var_modulation_factor = []
+    asymptotic_modulation_actor = []
+    plt.figure()
+    for thetai in theta:
+        small_var_modulation_factor.append(1.0 + 2.0 / 3.0 * thetai ** 2 * (1.0 - 1.0 / dim) * (1.0))
+        asymptotic_modulation_actor.append(asymptotic_modulation(dim, thetai))
+    plt.plot(theta, small_var_modulation_factor, 'g', label='Small variance prediction')
+    plt.plot(theta, asymptotic_modulation_actor, 'grey', label='Asymptotic prediction')
+    color = {10: 'red', 20: 'orange', 50: 'olive', 100: 'blue'}
+    for n in [10, 20, 50, 100]:
+        measured_modulation_factor = []
+        for thetai in theta:
+            (var, stdvar) = modulation_factor(n, thetai, dim, NN=NN)
+            measured_modulation_factor.append(var)
+            print(n, thetai, var, stdvar, '\n')
+        plt.plot(theta, measured_modulation_factor, color=color[n], label="N={0}".format(n))  # plotting t,a separately
+    plt.xlabel(r'Standard deviation $\theta$')
+    plt.ylabel(r'Modulation factor $\alpha$')
+    plt.legend(loc='best')
+    plt.title("Convergence rate modulation factor, sphere, dim={0}, N > 5".format(dim))
+    plt.show()
+    # TODO(Xavier): find why there is FileNotFoundError: [Errno 2] when saving
+    ## plt.savefig("Figures/SphVarModulation_N10p_d{0}.svg".format(dim))
+    ## plt.savefig("Figures/SphVarModulation_N10p_d{0}.png".format(dim))
+    return plt
 
 
 def main():
+    NN = 10;
+
+    print("Var of empirical mean for 1 sample, theta=0.1 in S2", EmpiricalVar(1, 0.1, 2), "\n")
+    print("Var of empirical mean for 1 sample, theta=0.1 in S3", EmpiricalVar(1, 0.1, 3), "\n")
+
+    print("Modulation factor for 1 sample theta=0.1 in S2 (should be close to 1):",
+          modulation_factor(1, 0.1, 2), "\n")
+
+    print("Modulation factor for 500 sample theta close to Pi/2 in S5 (should be around 26):",
+          modulation_factor(500, gs.pi / 2 - 0.001, 5), "\n")
+
+    plot_modulation_factor(2, 2)
+    # plot_modulation_factor(3, 2)
+    plot_modulation_factor(4, 2)
+    # plot_modulation_factor(5, 2)
+    plot_modulation_factor(10, 2)
+
+    # plot_modulation_factor(1)
+    plot_modulation_factor(2, 3)
+    # plot_modulation_factor(3, 3)
+    plot_modulation_factor(4, 3)
+    # plot_modulation_factor(5, 3)
 
 
-
-    fig = plt.figure(figsize=(15, 5))
-
-    sphere = Hypersphere(dimension=2)
-
-    data = sphere.random_von_mises_fisher(kappa=15, n_samples=140)
-    mean = sphere.metric.mean(data)
+    multiplot_modulation_factor(3)
 
 if __name__ == "__main__":
     main()
