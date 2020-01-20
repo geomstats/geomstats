aba47a4fcc380920cd203099cce0659b6ddc56b6
Nina Miolane <ninamio78@gmail.com>
2020-01-20 12:17:16 +0100

    Add code for vectorization and scalar type

--- geomstats/vectorization.py
+++ geomstats/vectorization.py
@@ -22,11 +22,44 @@ def squeeze_output_dim_0(initial_ndims, point_types):
     corresponding to their vectorized shape.
     """
     for ndim, point_type in zip(initial_ndims, point_types):
-        vectorized_ndim = POINT_TYPES_TO_NDIMS[point_type]
-        assert ndim <= vectorized_ndim
-        if ndim == vectorized_ndim:
+        vect_ndim = POINT_TYPES_TO_NDIMS[point_type]
+        assert ndim <= vect_ndim
+        if ndim == vect_ndim:
             return False
     return True
+
+
+def is_scalar(vect_array):
+    has_ndim_2 = vect_array.ndim == 2
+    has_singleton_dim_1 = vect_array.shape[1] == 1
+    return has_ndim_2 and has_singleton_dim_1
+
+
+def squeeze_output_dim_1(result, initial_shapes, point_types):
+    """Determine if the output needs to squeeze a singular dimension 1.
+
+    This happens if the user represents scalars as array of shapes:
+    [n_samples,] instead of [n_samples, 1]
+
+    The dimension 1 is squeezed iff:
+    - the return point type is a scalar,
+    - all input scalars have squeezed dimension 1.
+    """
+    if not is_scalar(result):
+        return False
+
+    for shape, point_type in zip(initial_shapes, point_types):
+        ndim = len(shape)
+        if point_type == 'scalar':
+            assert ndim <= 2
+            if ndim == 2:
+                return False
+            if ndim == 0:
+                return True
+            if ndim == 1:
+                if shape[0] > 1:
+                    return True
+    return False
 
 
 def decorator(param_names, point_types):
@@ -42,14 +75,19 @@ def decorator(param_names, point_types):
     def aux_decorator(function):
         def wrapper(*args, **kwargs):
             vect_args = []
+            initial_shapes = []
             initial_ndims = []
             for param, point_type in zip(args, point_types):
+                initial_shapes.append(param.shape)
                 initial_ndims.append(gs.ndim(param))
 
                 vect_param = gs.to_ndarray(
                     param, POINT_TYPES_TO_NDIMS[point_type])
                 vect_args.append(vect_param)
             result = function(*vect_args, **kwargs)
+
+            if squeeze_output_dim_1(result, initial_shapes, point_types):
+                result = gs.squeeze(result, axis=1)
 
             if squeeze_output_dim_0(initial_ndims, point_types):
                 result = gs.squeeze(result, axis=0)
