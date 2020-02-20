1c80400f1883de27ac700c870c3b3f8e86af731d
GUIGUI Nicolas <nicolas.guigui@inria.fr>
2020-02-20 15:30:30 +0100

    fix product_metric

--- geomstats/geometry/product_riemannian_metric.py
+++ geomstats/geometry/product_riemannian_metric.py
@@ -84,12 +84,32 @@ class ProductRiemannianMetric(RiemannianMetric):
         if base_point is None:
             base_point = [None, ] * self.n_metrics
 
-        inner_products = [metric.inner_product(tangent_vec_a[i],
-                                               tangent_vec_b[i], base_point[i])
-                          for i, metric in enumerate(self.metrics)]
-        inner_product = gs.sum(inner_products)
+        # FIXME: this assumes point_type is vector
+        tangent_vec_a = gs.to_ndarray(tangent_vec_a, to_ndim=2)
+        tangent_vec_b = gs.to_ndarray(tangent_vec_b, to_ndim=2)
 
-        return inner_product
+        # detect if intrinsic of extrinsic
+        if tangent_vec_a.shape[1] == self.dimension:
+            intrinsic = True
+        elif tangent_vec_a.shape[1] == sum([k + 1 for k in self.dimensions]):
+            intrinsic = False
+        else:
+            raise ValueError('invalid input dimension')
+
+        inner_prod = gs.zeros((len(tangent_vec_a), len(self.metrics)))
+        cum_dim = 0
+        for i, metric_i in enumerate(self.metrics):
+            cum_dim_next = cum_dim + self.dimensions[i]
+            if not intrinsic:
+                cum_dim_next += 1
+            tangent_vec_a_i = tangent_vec_a[:, cum_dim:cum_dim_next]
+            tangent_vec_b_i = tangent_vec_b[:, cum_dim:cum_dim_next]
+            base_point_i = base_point[:, cum_dim:cum_dim_next]
+            inner_prod[:, i] = metric_i.inner_product(
+                tangent_vec_a_i, tangent_vec_b_i, base_point_i).flatten()
+            cum_dim = cum_dim_next
+
+        return gs.sum(inner_prod, axis=1)
 
     def exp(self, tangent_vec, base_point=None):
         """Compute the Riemannian exponential of a tangent vector.
@@ -110,8 +130,28 @@ class ProductRiemannianMetric(RiemannianMetric):
         if base_point is None:
             base_point = [None, ] * self.n_metrics
 
-        exp = gs.asarray([metric.exp(tangent_vec[i], base_point[i])
-                          for i, metric in enumerate(self.metrics)])
+        # FIXME: this assumes point_type is vector
+        tangent_vec = gs.to_ndarray(tangent_vec, to_ndim=2)
+        # detect if intrinsic of extrinsic
+        if tangent_vec.shape[1] == self.dimension:
+            intrinsic = True
+        elif tangent_vec.shape[1] == sum([k + 1 for k in self.dimensions]):
+            intrinsic = False
+        else:
+            raise ValueError('invalid input dimension')
+
+        exp = gs.zeros_like(tangent_vec)
+        cum_dim = 0
+        for i, metric_i in enumerate(self.metrics):
+            cum_dim_next = cum_dim + self.dimensions[i]
+            if not intrinsic:
+                cum_dim_next += 1
+            tangent_vec_i = tangent_vec[:, cum_dim:cum_dim_next]
+            base_point_i = base_point[:, cum_dim:cum_dim_next]
+            exp[:, cum_dim:cum_dim_next] = metric_i.exp(
+                tangent_vec_i, base_point_i)
+            cum_dim = cum_dim_next
+
         return exp
 
     def log(self, point, base_point=None):
@@ -133,64 +173,27 @@ class ProductRiemannianMetric(RiemannianMetric):
         if base_point is None:
             base_point = [None, ] * self.n_metrics
 
-        log = gs.asarray([metric.log(point[i], base_point[i])
-                          for i, metric in enumerate(self.metrics)])
+        base_point = gs.to_ndarray(base_point, to_ndim=2)
+        # FIXME: this assumes point_type is vector
+        point = gs.to_ndarray(point, to_ndim=2)
+        # detect if intrinsic of extrinsic
+        if point.shape[1] == self.dimension:
+            intrinsic = True
+        elif point.shape[1] == sum([k + 1 for k in self.dimensions]):
+            intrinsic = False
+        else:
+            raise ValueError('invalid input dimension')
+
+        log = gs.zeros_like(point)
+        cum_dim = 0
+        for i, metric_i in enumerate(self.metrics):
+            cum_dim_next = cum_dim + self.dimensions[i]
+            if not intrinsic:
+                cum_dim_next += 1
+            point_i = point[:, cum_dim:cum_dim_next]
+            base_point_i = base_point[:, cum_dim:cum_dim_next]
+            log[:, cum_dim:cum_dim_next] = metric_i.log(
+                point_i, base_point_i)
+            cum_dim = cum_dim_next
+
         return log
-
-    def squared_dist(self, point_a, point_b):
-        """Compute the squared geodesic distance between two points.
-
-        Parameters
-        ----------
-        point_a : array-like, shape=[n_samples, dimension]
-            First point on the manifold.
-        point_b : array-like, shape=[n_samples, dimension]
-            Second point on the manifold.
-
-        Returns
-        -------
-        sq_dist : array-like, shape=[n_samples, 1]
-            Geodesic distance between the two points.
-        """
-        sq_distances = gs.asarray(
-            [metric.squared_dist(point_a[i], point_b[i])
-             for i, metric in enumerate(self.metrics)])
-
-        return sum(sq_distances)
-
-    def geodesic(self, initial_point,
-                 end_point=None, initial_tangent_vec=None,
-                 point_type=None):
-        """Compute the geodesic as a function of t.
-
-        This geodesic is seen as the product of the geodesic on each space.
-
-        Parameters
-        ----------
-        initial_point : array-like, shape=[n_samples, dim]
-            Initial point of the geodesic.
-        end_point : array-like, shape=[n_samples, dim], optional
-            End point of the geodesic.
-        initial_tangent_vec : array-like, shape=[n_samples, dim], optional
-            Initial tangent vector of the geodesic.
-        point_type : str, {'vector', 'matrix'}, optional
-            Representation of point.
-
-        Returns
-        -------
-        path : callable
-            The time-parameterized geodesic.
-        """
-        if point_type is None:
-            point_type = self.default_point_type
-        assert point_type in ['vector', 'matrix']
-
-        geodesics = [
-            metric.geodesic(initial_point[i], end_point=end_point[i],
-                            initial_tangent_vec=initial_tangent_vec[i],
-                            point_type=point_type[i])
-            for i, metric in enumerate(self.metrics)]
-
-        def path(t):
-            return [geodesic(t) for geodesic in geodesics]
-        return path
