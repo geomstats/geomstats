076789d444e18a26cfbee144cd13fa7599745f40
hadi.zaatiti <hadizaatiti@gmail.com>
2020-03-09 16:02:57 +0100

    Fix typo

---
+++ tests/test_load_graph.py
@@ -0,0 +1,23 @@
+"""Unit tests for loading Graph dataset."""
+
+import geomstats.tests
+from geomstats.learning.graph_data_preparation import Graph
+
+
+class TestLoadDefaultGraph(geomstats.tests.TestCase):
+    """Test for loading graph-structured data."""
+
+    def setUp(self):
+        """Declare the graph by default and the Karate club graph."""
+        self.G1 = Graph()
+        self.G2 = Graph(
+            Graph_Matrix_Path=r'examples\data_example\graph_karate\Karate.txt',
+            Labels_Path=r'examples\data_example'
+            r'\graph_karate\Karate_Labels.txt')
+
+    def test_graph_load(self):
+        """Test the correct number of edges and nodes for each graph."""
+        result = [len(self.G1.edges) + len(self.G1.labels),
+                  len(self.G2.edges) + len(self.G2.labels)]
+        expected = [20, 68]
+        self.assertAllClose(result, expected)
