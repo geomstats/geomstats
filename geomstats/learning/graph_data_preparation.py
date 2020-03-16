a818956328fb59003967abd596fbfd9091811f31
hadi.zaatiti <hadizaatiti@gmail.com>
2020-03-16 12:47:10 +0100

    Fix variable names

--- geomstats/learning/graph_data_preparation.py
+++ geomstats/learning/graph_data_preparation.py
@@ -1,8 +1,13 @@
-"""Prepare and Process Graph-structured data."""
+"""Prepare and process graph-structured data."""
 
 import random
 
 import geomstats.backend as gs
+
+DEFAULT_GRAPH_MATRIX_PATH = 'examples\\data\\' \
+                            'graph_random\\graph_random.txt'
+DEFAULT_GRAPH_LABELS_PATH = 'examples\\data\\' \
+                            'graph_random\\graph_random_labels.txt'
 
 
 class Graph:
@@ -14,19 +19,27 @@ class Graph:
     ----------
     graph_matrix_path : string
         Path to graph adjacency matrix.
-
     labels_path : string
         Path to labels of the nodes of the graph.
+
+    Attributes
+    ----------
+    edges : dict
+        Dictionary with node number as key
+        and edge connected node numbers as values.
+    n_nodes : int
+        Number of nodes in the graph.
+    labels : dict
+        Dictionary with node number as key and the true label number as values.
     """
 
     edges = None
+    n_nodes = None
     labels = None
 
     def __init__(self,
-                 graph_matrix_path='examples\\data'
-                                   '\\graph_random\\graph_random.txt',
-                 labels_path='examples\\data\\graph_random'
-                             '\\graph_random_labels.txt'):
+                 graph_matrix_path=DEFAULT_GRAPH_MATRIX_PATH,
+                 labels_path=DEFAULT_GRAPH_LABELS_PATH):
         self.edges = {}
         with open(graph_matrix_path, 'r') as edges_file:
             for i, line in enumerate(edges_file):
@@ -34,14 +47,16 @@ class Graph:
                 self.edges[i] = [k for k, value in
                                  enumerate(lsp) if (int(value) == 1)]
 
+        self.n_nodes = len(self.edges)
+
         if labels_path is not None:
             self.labels = {}
-            with open(labels_path, 'r') as label_file:
-                for i, line in enumerate(label_file):
+            with open(labels_path, 'r') as labels_file:
+                for i, line in enumerate(labels_file):
                     self.labels[i] = []
                     self.labels[i].append(int(line))
 
-    def random_walk(self, walk_length=5, number_walks_per_node=1):
+    def random_walk(self, walk_length=5, n_walks_per_node=1):
         """Compute a set of random walks on a graph.
 
         For each node of the graph, generates a a number of
@@ -52,20 +67,19 @@ class Graph:
         Parameters
         ----------
         walk_length : int
-            length of a random walk in terms of number of edges
-
-        number_walks_per_node : int
-            number of generated walks starting from each node of the graph
+            Length of a random walk in terms of number of edges.
+        n_walks_per_node : int
+            Number of generated walks starting from each node of the graph.
 
         Returns
         -------
         self : array-like,
-            shape=[number_walks_per_node*len(self.edges), walk_length]
-            array containing random walks
+            Shape=[n_walks_per_node*self.n_edges), walk_length]
+            array containing random walks.
         """
         paths = gs.empty((0, walk_length + 1), dtype=int)
-        for index in range(0, len(self.edges)):
-            for i in range(number_walks_per_node):
+        for index in range(len(self.edges)):
+            for i in range(n_walks_per_node):
                 paths = gs.vstack((paths, self._walk(index, walk_length)))
         return paths
 
