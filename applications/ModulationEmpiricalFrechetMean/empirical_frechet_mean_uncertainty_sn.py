799f8ae8f06f2a8c45d0db533ab3743cc28c87df
xpennec <59821822+xpennec@users.noreply.github.com>
2020-03-29 10:47:25 +0200

    Corrected typos in names of figures and naming of files.

--- applications/ModulationEmpiricalFrechetMean/empirical_frechet_mean_uncertainty_sn.py
+++ applications/ModulationEmpiricalFrechetMean/empirical_frechet_mean_uncertainty_sn.py
@@ -134,6 +134,8 @@ def plot_modulation_factor(n_samples, dim, n_expectation=1000, n_theta=20):
     error = []
     small_var_modulation_factor = []
     asymptotic_modulation_factor = []
+    print("Convergence rate modulation factor, "
+          "sphere dim={1}, n={0}".format(n_samples, dim))
     for theta_i in theta:
         (var, std_var) = modulation_factor(
             n_samples, theta_i, dim, n_expectation=n_expectation)
@@ -159,9 +161,9 @@ def plot_modulation_factor(n_samples, dim, n_expectation=1000, n_theta=20):
     plt.legend(loc='best')
     plt.draw()
     plt.pause(0.01)
-    plt.savefig("Figures/SphVarModulation_N{0}_d{1}_m{2}.png".format(
+    plt.savefig("Figures/SphVarModulation_n{0}_d{1}_m{2}.png".format(
         n_samples, dim, n_expectation))
-    plt.savefig("Figures/SphVarModulation_N{0}_d{1}_m{2}.pdf".format(
+    plt.savefig("Figures/SphVarModulation_n{0}_d{1}_m{2}.pdf".format(
         n_samples, dim, n_expectation))
     return plt
 
@@ -187,6 +189,8 @@ def multi_plot_modulation_factor(dim, n_expectation=1000, n_theta=20):
     theta = gs.linspace(0.000001, gs.pi / 2.0 - 0.000001, n_theta)
     small_var_modulation_factor = []
     asymptotic_modulation_actor = []
+    print("Convergence rate modulation factor, "
+          "sphere, dim={0}, n > 5".format(dim))
     plt.figure()
     for theta_i in theta:
         small_var_modulation_factor.append(
@@ -207,17 +211,17 @@ def multi_plot_modulation_factor(dim, n_expectation=1000, n_theta=20):
             print(
                 '{} {} {} {}\n'.format(n_samples, theta_i, var, std_var))
         plt.plot(theta, measured_modulation_factor,
-                 color=color[n_samples], label="N={0}".format(n_samples))
+                 color=color[n_samples], label="n={0}".format(n_samples))
     plt.xlabel(r'Standard deviation $\theta$')
     plt.ylabel(r'Modulation factor $\alpha$')
     plt.legend(loc='best')
     plt.title("Convergence rate modulation factor, "
-              "sphere, dim={0}, N > 5".format(dim))
+              "sphere, dim={0}, n > 5".format(dim))
     plt.draw()
     plt.pause(0.01)
-    plt.savefig("Figures/SphVarModulation_N10p_d{0}_m{1}.png".format(
+    plt.savefig("Figures/SphVarModulation_n10p_d{0}_m{1}.png".format(
         dim, n_expectation))
-    plt.savefig("Figures/SphVarModulation_N10p_d{0}_m{1}.pdf".format(
+    plt.savefig("Figures/SphVarModulation_n10p_d{0}_m{1}.pdf".format(
         dim, n_expectation))
     return plt
 
@@ -240,8 +244,9 @@ def main():
     print('Var of empirical mean for 1 sample, theta=0.1 in S3 {} \n'.format(
         empirical_frechet_var_bubble(1, 0.1, 3, n_expectation=n_expectation)))
 
-    print('Modulation factor for 1 sample theta=0.1 in S2 (should be close to 1): {} \n'.format(
-       modulation_factor(1, 0.1, 2, n_expectation=n_expectation)))
+    print('Modulation factor for 1 sample theta=0.1 in S2 (should be close to '
+          '1): {} \n'.format(modulation_factor(1, 0.1, 2,
+                                               n_expectation=n_expectation)))
 
     print('Modulation factor for 500 sample theta close to Pi/2 in S5 '
                  '(should be around 25): {} \n'.format(
@@ -251,13 +256,10 @@ def main():
 
     plot_modulation_factor(2, 2, n_expectation=n_expectation)
     plot_modulation_factor(4, 2, n_expectation=n_expectation)
-
-    multi_plot_modulation_factor(
-        2, n_expectation=n_expectation)
+    multi_plot_modulation_factor(2, n_expectation=n_expectation)
 
     plot_modulation_factor(2, 3, n_expectation=n_expectation)
     plot_modulation_factor(4, 3, n_expectation=n_expectation)
-
     multi_plot_modulation_factor(3, n_expectation=n_expectation)
 
 
