f02169b61368543c4936eb4e177589926b8f694a
Nina Miolane <ninamio78@gmail.com>
2020-04-01 14:02:59 -0700

    Override pytorch's einsum function

--- geomstats/backend/pytorch/__init__.py
+++ geomstats/backend/pytorch/__init__.py
@@ -285,7 +285,52 @@ def sum(x, axis=None, keepdims=None, **kwargs):
 
 
 def einsum(*args, **kwargs):
-    return torch.from_numpy(_np.einsum(*args, **kwargs)).float()
+    einsum_str = args[0]
+    input_tensors_list = args[1:]
+
+    einsum_list = einsum_str.split('->')
+    input_str = einsum_list[0]
+    output_str = einsum_list[1]
+
+    input_str_list = input_str.split(',')
+
+    is_ellipsis = [input_str[:3] == '...' for input_str in input_str_list]
+    at_least_one_ellipsis = bool(_np.sum(is_ellipsis))
+
+    if at_least_one_ellipsis:
+        if len(input_str_list) > 2:
+            raise NotImplementedError(
+                'Ellipsis support not implemented for >2 input tensors')
+        tensor_a = input_tensors_list[0]
+        tensor_b = input_tensors_list[1]
+        n_tensor_a = tensor_a.shape[0]
+        n_tensor_b = tensor_b.shape[0]
+
+        if n_tensor_a != n_tensor_b:
+            if n_tensor_a == 1:
+                tensor_a = squeeze(tensor_a, axis=0)
+                input_prefix_list = ['', 'n']
+                output_prefix = 'n'
+            elif n_tensor_b == 1:
+                tensor_b = squeeze(tensor_b, axis=0)
+                input_prefix_list = ['n', '']
+                output_prefix = 'n'
+            else:
+                raise ValueError('Shape mismatch for einsum.')
+        else:
+            input_prefix_list = ['n', 'n']
+            output_prefix = 'n'
+
+        input_str_list = [
+            input_str.replace('...', prefix) for input_str, prefix in zip(
+                input_str_list, input_prefix_list)]
+        output_str = output_str.replace('...', output_prefix)
+
+        input_str = input_str_list[0] + ',' + input_str_list[1]
+        einsum_str = input_str + '->' + output_str
+
+        return torch.einsum(einsum_str, tensor_a, tensor_b, **kwargs)
+    return torch.einsum(*args, **kwargs)
 
 
 def T(x):
