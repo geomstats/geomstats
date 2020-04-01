5dcf6a8194db9c9a22348053a4a7cffab4f923c9
pchauchat <62605255+pchauchat@users.noreply.github.com>
2020-04-01 23:46:35 +0200

    Back to tangent_vec (element of tangent space, not Lie algebra), with the associated exp and log functions.

--- geomstats/geometry/general_linear.py
+++ geomstats/geometry/general_linear.py
@@ -31,13 +31,25 @@ class GeneralLinear(Matrices):
         return gs.linalg.inv(point)
 
     @classmethod
-    def exp(cls, algebra_mat, base_point=None):
-        """
+    def exp(cls, tangent_vec, base_point=None):
+        r"""
         Exponentiate a left-invariant vector field from a base point.
+
+        The vector input is not an element of the Lie algebra, but of the
+        tangent space at base_point: if :math:`g` denotes `base_point`,
+        :math:`v` the tangent vector, and :math:'V = g^{-1} v' the associated
+        Lie algebra vector, then
+
+        .. math::
+
+            \exp(v, g) = mul(g, \exp(V))
+
+        Therefore, the Lie exponential is obtained when base_point is None, or
+        the identity.
 
         Parameters
         ----------
-        algebra_mat :   array-like, shape=[..., n, n]
+        tangent_vec :   array-like, shape=[..., n, n]
         base_point :    array-like, shape=[..., n, n]
             Defaults to identity.
 
@@ -49,14 +61,18 @@ class GeneralLinear(Matrices):
         """
         expm = gs.linalg.expm
         if base_point is None:
-            return expm(algebra_mat)
+            return expm(tangent_vec)
         else:
-            return cls.mul(base_point, expm(algebra_mat))
+            lie_algebra_vec = cls.mul(cls.inv(base_point), tangent_vec)
+            return cls.mul(base_point, cls.exp(lie_algebra_vec))
 
     @classmethod
     def log(cls, point, base_point=None):
-        """
+        r"""
         Calculate a left-invariant vector field bringing base_point to point.
+
+        The output is a vector of the tangent space at base_point, so not a Lie
+        algebra element if it is not the identity.
 
         Parameters
         ----------
@@ -66,14 +82,24 @@ class GeneralLinear(Matrices):
 
         Returns
         -------
-        algebra_mat :   array-like, shape=[..., n, n]
-            A matrix such that `exp(algebra_mat, base_point) = point`.
+        tangent_vec :   array-like, shape=[..., n, n]
+            A matrix such that `exp(tangent_vec, base_point) = point`.
+
+        Notes
+        -----
+        Denoting `point` by :math:`g` and `base_point` by :math:`h`,
+        the output satisfies:
+
+        .. math::
+
+            g = \exp(\log(g, h), h)
         """
         logm = gs.linalg.logm
         if base_point is None:
             return logm(point)
         else:
-            return logm(cls.mul(cls.inv(base_point), point))
+            lie_algebra_vec = logm(cls.mul(cls.inv(base_point), point))
+            return cls.mul(base_point, lie_algebra_vec)
 
     @classmethod
     def orbit(cls, point, base_point=None):
@@ -114,9 +140,9 @@ class GeneralLinear(Matrices):
 
         # TODO(nina): Will work when expm gets properly 4-D vectorized.
         """
-        algebra_mat = cls.log(point, base_point)
+        tangent_vec = cls.log(point, base_point)
 
         def path(time):
-            vecs = gs.einsum('t,...ij->...tij', time, algebra_mat)
+            vecs = gs.einsum('t,...ij->...tij', time, tangent_vec)
             return cls.exp(vecs, base_point)
         return path
