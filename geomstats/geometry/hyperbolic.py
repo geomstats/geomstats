8b6f5ac84ae211ca932fd3f2b8e155a756df77cc
Thomas Gerald <gerald@pop-os.localdomain>
2020-04-01 14:43:35 +0200

    Fixing error in retraction (dim error)

--- geomstats/geometry/hyperbolic.py
+++ geomstats/geometry/hyperbolic.py
@@ -905,7 +905,8 @@ class HyperbolicMetric(RiemannianMetric):
 
             retraction_factor = ((1 - (base_point**2).sum(axis=-1))**2) / 4
             retraction_factor =\
-                gs.repeat(gs.expand_dims(retraction_factor, -1), 2, axis=1)
+                gs.repeat(gs.expand_dims(retraction_factor, -1), base_point.shape[0],
+                          axis=1)
             return base_point - retraction_factor * tangent_vec
         else:
             raise NotImplementedError(
