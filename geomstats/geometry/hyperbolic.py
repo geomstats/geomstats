1a1b5711050c77a9d962f89ea6eef173e0a6d840
Thomas Gerald <gerald@pop-os.localdomain>
2020-04-03 11:33:43 +0200

    Change squared dist to call super in intrinsic extrinsic will be moved again after refactoring hyperbolic space

--- geomstats/geometry/hyperbolic.py
+++ geomstats/geometry/hyperbolic.py
@@ -882,7 +882,7 @@ class HyperbolicMetric(RiemannianMetric):
             raise NotImplementedError(
                 'dist is only implemented for ball and extrinsic')
 
-    def squared_distance(self, point_a, point_b):
+    def squared_dist(self, point_a, point_b):
         """Geodesic squared hyperbolic distance.
 
         Redefinition the squared distance implemented
@@ -900,6 +900,9 @@ class HyperbolicMetric(RiemannianMetric):
         dist : array-like, shape=[n_samples, 1]
             Geodesic squared distance between the two points.
         """
+        if self.coords_type in ["intrinsic", "extrinsic"]:
+            return super().squared_dist(point_a, point_b)
+
         return self.dist(point_a, point_b)**2
 
     def retraction(self, tangent_vec, base_point):
