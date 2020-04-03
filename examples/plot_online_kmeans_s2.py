c205199d35c839260553c6719405a915ec4a9f81
Yann Cabanes <yann.cabanes@u-bordeaux.fr>
2020-04-04 00:00:37 +0200

    Change double quotes to single quotes.

--- examples/plot_online_kmeans_s2.py
+++ examples/plot_online_kmeans_s2.py
@@ -23,13 +23,13 @@ def main():
     clustering = clustering.fit(data)
 
     plt.figure(0)
-    ax = plt.subplot(111, projection="3d")
+    ax = plt.subplot(111, projection='3d')
     visualization.plot(points=clustering.cluster_centers_, ax=ax,
                        space='S2', c='r')
     plt.show()
 
     plt.figure(1)
-    ax = plt.subplot(111, projection="3d")
+    ax = plt.subplot(111, projection='3d')
     sphere_plot = visualization.Sphere()
     sphere_plot.draw(ax=ax)
     for i in range(n_clusters):
@@ -38,7 +38,7 @@ def main():
     plt.show()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if os.environ['GEOMSTATS_BACKEND'] == 'tensorflow':
         logging.info('Examples with visualizations are only implemented '
                      'with numpy backend.\n'
