72fbf4e0e6c2f1bd497e385accb0850fb9e277f7
pchauchat <62605255+pchauchat@users.noreply.github.com>
2020-04-07 21:23:16 +0200

    Better cope with integer dimension for random distributions in torch.

--- geomstats/_backend/pytorch/random.py
+++ geomstats/_backend/pytorch/random.py
@@ -25,9 +25,7 @@ def normal(loc=0.0, scale=1.0, size=(1,)):
     return torch.normal(mean=loc, std=scale, size=size)
 
 
-def uniform(low=0.0, high=1.0, size=None):
-    if size is None:
-        size = (1,)
+def uniform(low=0.0, high=1.0, size=(1,)):
     if not hasattr(size, '__iter__'):
         size = (size,)
     assert low <= high
