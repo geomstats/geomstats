f1a7b639b68e4d1f00351990a79a2ce84235aff0
Niklas Koep <niklas.koep@gmail.com>
2020-04-09 22:08:05 +0200

    Add docstring to 'algebra_utils' module
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- geomstats/algebra_utils.py
+++ geomstats/algebra_utils.py
@@ -1,8 +1,22 @@
+"""Utility module of reusable algebra routines."""
+
 import geomstats.backend as gs
 
 
-def from_vector_to_diagonal_matrix(x):
-    n = gs.shape(x)[-1]
-    identity_n = gs.eye(n)
-    diagonals = gs.einsum('ki,ij->kij', x, identity_n)
+def from_vector_to_diagonal_matrix(vector):
+    """Create diagonal matrices from rows of a matrix.
+
+    Parameters
+    ----------
+    vector : array-like, shape=[m, n]
+
+    Returns
+    -------
+    diagonals : array-like, shape=[m, n, n]
+        3-dimensional array where the `i`-th n-by-n array `diagonals[i, :, :]`
+        is a diagonal matrix containing the `i`-th row of `vector`.
+    """
+    num_columns = gs.shape(vector)[-1]
+    identity = gs.eye(num_columns)
+    diagonals = gs.einsum('ki,ij->kij', vector, identity)
     return diagonals
