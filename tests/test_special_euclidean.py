9e5646533804bd8132636aa6743bce30198d0035
nguigs <nicolas.guigui@inria.fr>
2020-04-10 12:17:44 +0200

    fix deepsource

--- tests/test_special_euclidean.py
+++ tests/test_special_euclidean.py
@@ -333,7 +333,7 @@ class TestSpecialEuclideanMethods(geomstats.tests.TestCase):
         self.assertAllClose(result, expected)
 
         tangent_vec = gs.array([[.5, -2., 4., 12.],
-                                [2., 0.,  -4.5, 13.],
+                                [2., 0., -4.5, 13.],
                                 [-4., 4.5, 0., 14.],
                                 [0.5, 0., 0., 0.]])
         regularized = self.group.regularize_tangent_vec_at_identity(
