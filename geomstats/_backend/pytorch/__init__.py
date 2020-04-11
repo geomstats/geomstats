73608a45883859a64dd5b9745fd8583c2f6eb9db
nguigs <nicolas.guigui@inria.fr>
2020-04-11 23:59:11 +0200

    change amax and amin in pytorch backend to return tensors

--- geomstats/_backend/pytorch/__init__.py
+++ geomstats/_backend/pytorch/__init__.py
@@ -35,9 +35,7 @@ from torch import (  # NOQA
     log,
     lt as less,
     matmul,
-    max as amax,
     meshgrid,
-    min as amin,
     nonzero,
     ones,
     ones_like,
@@ -581,3 +579,11 @@ def array_from_sparse(indices, data, target_shape):
         torch.LongTensor(indices).t(),
         torch.FloatTensor(cast(data, float32)),
         torch.Size(target_shape)).to_dense()
+
+
+def amax(*args, **kwargs):
+    return torch.max(*args, **kwargs).values
+
+
+def amin(*args, **kwargs):
+    return torch.min(*args, **kwargs).values
