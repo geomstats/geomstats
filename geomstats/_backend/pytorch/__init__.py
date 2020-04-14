8f4ae9db37377b3e062634f7b5a05513cc0df135
Nina Miolane <ninamio78@gmail.com>
2020-04-14 09:30:27 -0700

    Modify pytorch's init

--- geomstats/_backend/pytorch/__init__.py
+++ geomstats/_backend/pytorch/__init__.py
@@ -1,5 +1,7 @@
 """Pytorch based computation backend."""
 
+from functools import wraps
+
 import numpy as _np
 import torch
 from torch import (  # NOQA
@@ -12,6 +14,7 @@ from torch import (  # NOQA
     atan2 as arctan2,
     ceil,
     clamp as clip,
+    cos,
     cosh,
     div as divide,
     empty_like,
@@ -41,6 +44,7 @@ from torch import (  # NOQA
     repeat_interleave as repeat,
     reshape,
     sign,
+    sin,
     sinh,
     stack,
     std,
@@ -64,16 +68,21 @@ searchsorted = _raise_not_implemented_error
 vectorize = _raise_not_implemented_error
 
 
-def sin(x):
-    if not torch.is_tensor(x):
-        x = torch.tensor(x)
-    return torch.sin(x)
+def _box_scalar(function):
+    @wraps(function)
+    def wrapper(x):
+        if not torch.is_tensor(x):
+            x = torch.tensor(x)
+        return function(x)
+    return wrapper
 
 
-def cos(x):
-    if not torch.is_tensor(x):
-        x = torch.tensor(x)
-    return torch.cos(x)
+cos = _box_scalar(cos)
+cosh = _box_scalar(cosh)
+exp = _box_scalar(exp)
+log = _box_scalar(log)
+sin = _box_scalar(sin)
+sinh = _box_scalar(sinh)
 
 
 def empty(shape, dtype=float64):
