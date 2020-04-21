a1d0ddf3afa3f53b75db3b1a48b0bea27f5352a1
Nina Miolane <ninamio78@gmail.com>
2020-04-20 17:23:15 -0700

    Remove sklearn warning on importing _BasePCA

--- geomstats/learning/pca.py
+++ geomstats/learning/pca.py
@@ -5,7 +5,7 @@ from math import log
 
 from scipy import linalg
 from scipy.special import gammaln
-from sklearn.decomposition.base import _BasePCA
+from sklearn.decomposition._base import _BasePCA
 from sklearn.utils.extmath import stable_cumsum
 from sklearn.utils.extmath import svd_flip
 from sklearn.utils.validation import check_array
