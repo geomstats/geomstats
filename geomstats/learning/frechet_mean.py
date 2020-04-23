d34e747afe5c2be3b4465f2c785b6b65d1577290
GUIGUI Nicolas <nicolas.guigui@inria.fr>
2020-04-23 11:31:17 +0200

    move transform from frechet_mean.py to a specific preprocessing module

--- geomstats/learning/frechet_mean.py
+++ geomstats/learning/frechet_mean.py
@@ -3,16 +3,14 @@
 import logging
 import math
 
-from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.base import BaseEstimator
 
 import geomstats.backend as gs
 import geomstats.error as error
 import geomstats.vectorization
 from geomstats.geometry.euclidean import EuclideanMetric
-from geomstats.geometry.matrices import Matrices, MatricesMetric
+from geomstats.geometry.matrices import MatricesMetric
 from geomstats.geometry.minkowski import MinkowskiMetric
-from geomstats.geometry.skew_symmetric_matrices import SkewSymmetricMatrices
-from geomstats.geometry.symmetric_matrices import SymmetricMatrices
 
 EPSILON = 1e-4
 
@@ -284,7 +282,7 @@ def _adaptive_gradient_descent(points,
     return current_mean
 
 
-class FrechetMean(BaseEstimator, TransformerMixin):
+class FrechetMean(BaseEstimator):
     """Empirical Frechet mean.
 
     Parameters
@@ -359,88 +357,3 @@ class FrechetMean(BaseEstimator, TransformerMixin):
         self.estimate_ = mean
 
         return self
-
-    def transform(self, X, y=None, base_point=None):
-        """Lift data to a tangent space.
-
-        Compute the logs of all data point and reshapes them to
-        1d vectors if necessary. By default the logs are taken at the mean
-        but any other base point can be passed. Any machine learning
-        algorithm can then be used with the output array.
-
-        Parameters
-        ----------
-        X : array-like, shape=[n_samples, {dim, [n, n]}]
-            Data to transform.
-        y : Ignored (Compliance with scikit-learn interface)
-        base_point : array-like, shape={dim, [n,n]}, optional (mean)
-            Point on the manifold, the returned samples will be tangent
-            vectors at the base point.
-
-        Returns
-        -------
-        X_new : array-like, shape=[n_samples, dim]
-        """
-        # TODO(nguis): put this in a dedicated class
-        if base_point is None:
-            base_point = self.estimate_
-
-            if self.estimate_ is None:
-                raise RuntimeError('fit needs to be called first or a '
-                                   'base_point passed.')
-
-        tangent_vecs = self.metric.log(X, base_point=base_point)
-
-        if self.point_type == 'vector':
-            return tangent_vecs
-
-        if gs.all(Matrices.is_symmetric(tangent_vecs)):
-            X = SymmetricMatrices.vector_from_symmetric_matrix(
-                tangent_vecs)
-        elif gs.all(Matrices.is_skew_symmetric(tangent_vecs)):
-            X = SkewSymmetricMatrices(
-                tangent_vecs.shape[-1]).basis_representation(tangent_vecs)
-        else:
-            X = gs.reshape(tangent_vecs, (len(X), - 1))
-
-        return X
-
-    def inverse_transform(self, X, base_point=None):
-        """Reconstruction of X.
-
-        The reconstruction will match X_original whose transform would be X.
-
-        Parameters
-        ----------
-        X : array-like, shape=[n_samples, dim]
-            New data, where dim is the dimension of the manifold data belong
-            to.
-        base_point : array-like, shape={dim, [n,n]}, optional (mean)
-            Point on the manifold, where the input samples are tangent
-            vectors.
-
-        Returns
-        -------
-        X_original : array-like, shape=[n_samples, {dim, [n, n]}
-            Data lying on the manifold.
-        """
-        if base_point is None:
-            base_point = self.estimate_
-
-            if self.estimate_ is None:
-                raise RuntimeError('fit needs to be called first or a '
-                                   'base_point passed.')
-
-        if self.point_type == 'matrix':
-            if gs.all(Matrices.is_symmetric(base_point)):
-                tangent_vecs = SymmetricMatrices(
-                    base_point.shape[-1]).symmetric_matrix_from_vector(X)
-            elif gs.all(Matrices.is_skew_symmetric(base_point)):
-                tangent_vecs = SkewSymmetricMatrices(
-                    base_point.shape[-1]).matrix_representation(X)
-            else:
-                dim = base_point.shape[-1]
-                tangent_vecs = gs.reshape(X, (len(X), dim, dim))
-        else:
-            tangent_vecs = X
-        return self.metric.exp(tangent_vecs, base_point)
