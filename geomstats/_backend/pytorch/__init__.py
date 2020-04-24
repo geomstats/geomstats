e305773cf024c7875c6723cf60f6e7f27f2e16b0
Nina Miolane <ninamio78@gmail.com>
2020-04-23 17:09:33 -0700

    Modify torch dot

--- geomstats/_backend/pytorch/__init__.py
+++ geomstats/_backend/pytorch/__init__.py
@@ -284,8 +284,9 @@ def shape(val):
 
 
 def dot(a, b):
-    np_dot = _np.dot(a, b)
-    return torch.from_numpy(_np.array(np_dot)).float()
+    return einsum('...i,...i->...', a, b)
+    #np_dot = _np.dot(a, b)
+    #return torch.from_numpy(_np.array(np_dot)).float()
 
 
 def maximum(a, b):
