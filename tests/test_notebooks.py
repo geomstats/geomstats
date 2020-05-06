ee6a1a9a592068631282fd9fb4f9404b621d3436
Nina Miolane <ninamio78@gmail.com>
2020-05-06 09:13:13 -0700

    Add docstring to notebooks tests

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,3 +1,5 @@
+"""Unit tests for the notebooks."""
+
 import subprocess
 import tempfile
 
@@ -5,10 +7,10 @@ import geomstats.tests
 
 
 def _exec_notebook(path):
-    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
-        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
-                "--ExecutePreprocessor.timeout=1000",
-                "--output", fout.name, path]
+    with tempfile.NamedTemporaryFile(suffix='.ipynb') as fout:
+        args = ['jupyter', 'nbconvert', '--to', 'notebook', '--execute',
+                '--ExecutePreprocessor.timeout=1000',
+                '--output', fout.name, path]
         subprocess.check_call(args)
 
 
@@ -16,5 +18,5 @@ class TestNotebooks(geomstats.tests.TestCase):
 
     @staticmethod
     @geomstats.tests.np_only
-    def test_notebook():
+    def test_tangent_pca_s2():
         _exec_notebook('notebooks/tangent_pca_s2.ipynb')
