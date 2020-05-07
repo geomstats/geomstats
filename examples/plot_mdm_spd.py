83890a5fc3717eb02602987224dd7661915971ef
Daniel Brooks <daniel.brooks@lip6.fr>
2020-05-07 20:12:20 +0200

    Resolve rebase merge issue

--- examples/plot_mdm_spd.py
+++ examples/plot_mdm_spd.py
@@ -1,52 +1,60 @@
+"""Illustration for the MDM classification of SPD matrices in  dim 2."""
+
+
+import geomstats.datasets.sample_sdp_2d
+import geomstats.visualization as visualization
 from geomstats.geometry.spd_matrices import SPDMatrices
 from geomstats.geometry.spd_matrices import SPDMetricAffine
 from geomstats.learning.mdm import RiemannianMinimumDistanceToMeanClassifier
 
-import matplotlib.pyplot as plt
-import sys
-sys.path.append('.')
-import plot_mdm_spd_helper
 
 def main():
-
-    n_samples=100
-    n_features=2
-    n_classes=3
+    """Execute illustration of MDM supervised classifier."""
+    n_samples = 100
+    n_features = 2
+    n_classes = 3
 
     # generate toy dataset of 2D SPD matrices
-    dataset_generator=plot_mdm_spd_helper.DatasetSPD_2D(n_samples,n_features,n_classes)
-    data,labels=dataset_generator.generate_sample_dataset()
+    dataset_generator = geomstats.datasets.sample_sdp_2d.DatasetSPD2D(
+        n_samples, n_features, n_classes)
+    data, labels = dataset_generator.generate_sample_dataset()
 
     # plot dataset as ellipses
-    plot_helper=plot_mdm_spd_helper.PlotHelper()
+    ellipsis = visualization.Ellipsis2D()
     for i in range(n_samples):
-        x=data[i]
-        y=dataset_generator.data_helper.get_label_at_index(i,labels)
-        plot_helper.plot_ellipse(x,color=plot_helper.colors[y],alpha=.1)
+        x = data[i]
+        y = dataset_generator.data_helper.get_label_at_index(i, labels)
+        ellipsis.draw(x, color=ellipsis.colors[y], alpha=.1)
 
     # define and fit MDM classifier to data
-    metric=SPDMetricAffine(n=n_features)
-    MDMEstimator=RiemannianMinimumDistanceToMeanClassifier(metric,point_type='matrix')
-    MDMEstimator.fit(data,labels)
+    metric = SPDMetricAffine(n=n_features)
+    MDMEstimator = RiemannianMinimumDistanceToMeanClassifier(
+        metric, n_classes, point_type='matrix')
+    MDMEstimator.fit(data, labels)
 
     # plot Frechet means computed in the MDM
-    # for i in range(n_classes):
-    #     plot_helper.plot_ellipse(MDMEstimator.G[i],color=plot_helper.colors_alt[i],linewidth=5)
-
-    # data_vec=numpy.array([sample_data.mat2vec(data[i]) for i in range(n_samples)])
-    # data_vec=numpy.reshape(data,(-1,n*n))
+    for i in range(n_classes):
+        ellipsis.draw(
+            MDMEstimator.mean_estimate[i],
+            color=ellipsis.colors_alt[i],
+            linewidth=5,
+            label='Barycenter of class ' + str(i))
 
     # generate random test samples, and predict with MDM classifier
-    data_test=SPDMatrices(n=n_features).random_uniform(n_samples=3)
-    predictions=MDMEstimator.predict(data_test)
+    data_test = SPDMatrices(n=n_features).random_uniform(n_samples=3)
+    predictions = MDMEstimator.predict(data_test)
 
     for i in range(data_test.shape[0]):
-        plot_helper.plot_ellipse(data_test[i],color=plot_helper.colors[i],linewidth=5)
+        c = list(predictions[i] == 1).index(True)
+        x_from, y_from = ellipsis.draw(
+            data_test[i], color=ellipsis.colors[c], linewidth=5)
+        _, _, x_to, y_to = ellipsis.compute_coordinates(
+            MDMEstimator.mean_estimate[c])
+        arrow = visualization.DataArrow(ellipsis.fig)
+        arrow.draw(x_from, y_from, x_to, y_to)
 
-    plt.show()
+    ellipsis.plot()
 
-    # clustering = RiemannianKMeans(riemannian_metric=SPDMetricAffine(n=n),n_clusters=n_classes,point_type='matrix')
-    # clustering = clustering.fit(data,max_iter=10)
 
-if(__name__=='__main__'):
+if __name__ == '__main__':
     main()
