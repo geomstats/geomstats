ba83b095e40c4dba0edbc1f7d759eb7cc43708d1
Nina Miolane <ninamio78@gmail.com>
2020-05-14 14:12:42 -0700

    Remove double source_suffix

--- docs/conf.py
+++ docs/conf.py
@@ -32,9 +32,6 @@ napoleon_include_init_with_doc = False
 nbsphinx_execute = 'never'
 nbsphinx_allow_errors = True
 
-# Add type of source files
-source_suffix = ['.rst', '.md', '.ipynb']
-
 templates_path = ['_templates']
 
 source_suffix = ['.rst', '.ipynb']
