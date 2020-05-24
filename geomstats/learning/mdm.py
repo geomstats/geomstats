f457963e8a438f53dd1dc7d3f5f55e2078c35b1e
Daniel Brooks <daniel.brooks@lip6.fr>
2020-05-24 10:21:26 +0200

    Docstrings updated in mdm.py and spd_matrices.py, minor fixes

--- geomstats/learning/mdm.py
+++ geomstats/learning/mdm.py
@@ -13,7 +13,7 @@ class RiemannianMinimumDistanceToMeanClassifier:
     riemannian_metric : string or callable.
         The distance metric to use.
     n_clusters: int, number of clusters.
-    mean_estimate_: array-like, shape=[n_classes, n_features] if
+    mean_estimates_: array-like, shape=[n_classes, n_features] if
     point_type='vector'
                    shape=[n_classes, n_features, n_features]
                     if point_type='matrix'
@@ -28,19 +28,22 @@ class RiemannianMinimumDistanceToMeanClassifier:
         self.riemannian_metric = riemannian_metric
         self.n_clusters = n_clusters
         self.point_type = point_type
-        self.mean_estimate_ = None
+        self.mean_estimates_ = None
 
     def fit(self, X, y):
         """
         Compute Frechet mean of each class.
 
-        :param X: array-like, shape=[n_samples, n_features]
+        Stores the mean in the class attributes mean_estimates_.
+        Parameters
+        ----------
+        X : array-like, shape=[n_samples, n_features]
                               if point_type='vector'
                               shape=[n_samples, n_features, n_features]
                               if point_type='matrix'
                   Training data, where n_samples is the number of samples
                   and n_features is the number of features.
-        :param y: array-like, shape=[n_samples, n_classes]
+        y : array-like, shape=[n_samples, n_classes]
                   Training labels, where n_classes is the number of classes.
         """
         mean_estimator = FrechetMean(
@@ -51,26 +54,31 @@ class RiemannianMinimumDistanceToMeanClassifier:
             data_class = self.split_data_in_classes(X, y, c)
             # frechet_means.append(mean_estimator.fit(data_class).estimate_[0])
             frechet_means.append(mean_estimator.fit(data_class).estimate_)
-        self.mean_estimate_ = gs.array(frechet_means)
+        self.mean_estimates_ = gs.array(frechet_means)
 
     def predict(self, X):
         """
         Compute closest neighbor according to riemannian_metric.
 
-        :param X: array-like, shape=[n_samples, n_features]
+        Parameters
+        ----------
+        X : array-like, shape=[n_samples, n_features]
                               if point_type='vector'
                               shape=[n_samples, n_features, n_features]
                               if point_type='matrix'
                   Test data, where n_samples is the number of samples
                   and n_features is the number of features.
-        :return: y: array-like, shape=[n_samples, n_classes]
+
+        Returns
+        -------
+        y : array-like, shape=[n_samples, n_classes]
                     Predicted labels, where n_classes is the number of classes.
         """
         n_samples = X.shape[0]
         y = gs.zeros((n_samples, self.n_clusters))
         for i in range(n_samples):
             label = self.riemannian_metric.closest_neighbor_index(
-                X[i], self.mean_estimate_)
+                X[i], self.mean_estimates_)
             y[i, label] = 1
         return y
 
@@ -79,21 +87,27 @@ class RiemannianMinimumDistanceToMeanClassifier:
         """
         Split a labelled dataset in sub-datasets of each label.
 
-        :param X: array-like, shape=[n_samples, n_features]
+        Parameters
+        ----------
+        X : array-like, shape=[n_samples, n_features]
                               if point_type='vector'
                               shape=[n_samples, n_features, n_features]
                               if point_type='matrix'
                   Labelled dataset, where n_samples is the number of samples
                   and n_features is the number of features.
-        :param y: array-like, shape=[n_samples, n_classes]
+        y : array-like, shape=[n_samples, n_classes]
                   Labels, where n_classes is the number of classes.
-        :param c: int
+        c : int
                   Class index
-        :return: array-like, shape=[n_samples_in_class, n_features]
+
+        Returns
+        -------
+        X_split : array-like, shape=[n_samples_in_class, n_features]
                              if point_type='vector'
                              shape=[n_samples_in_class, n_features, n_features]
                              if point_type='matrix'
                   Labelled dataset,
                   where n_samples_in_class is the number of samples in class c
         """
-        return X[gs.where(gs.where(y)[1] == c)]
+        X_split = X[gs.where(gs.where(y)[1] == c)]
+        return X_split
