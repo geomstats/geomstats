37bc9495d410d4d4d033ce79f42fe1852e7beb65
GUIGUI Nicolas <nicolas.guigui@inria.fr>
2020-05-27 19:11:33 +0200

    add docstrings

--- geomstats/_backend/tensorflow/autograd.py
+++ geomstats/_backend/tensorflow/autograd.py
@@ -3,6 +3,21 @@ import tensorflow as tf
 
 
 def value_and_grad(objective):
+    """'Returns a function that returns both value and gradient.
+
+    Suitable for use in scipy.optimize
+
+    Parameters
+    ----------
+    objective : callable
+        Function to compute the gradient. It must be real-valued.
+
+    Returns
+    -------
+    objective_with_grad : callable
+        Function that takes the argument of the objective function as input
+        and returns both value and grad at the input.
+    '"""
     def objective_with_grad(velocity):
         if isinstance(velocity, np.ndarray):
             velocity = tf.Variable(velocity)
