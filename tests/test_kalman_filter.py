88a2032bb494ffb4582e650e54bec8ac59ed53ab
pchauchat <62605255+pchauchat@users.noreply.github.com>
2020-06-14 20:22:52 +0200

    Separate the tests of Localization and LocalizationLinear

--- tests/test_kalman_filter.py
+++ tests/test_kalman_filter.py
@@ -21,96 +21,126 @@ class TestKalmanFilter(geomstats.tests.TestCase):
         self.process_cov = gs.eye(1)
         self.obs_cov = 2. * gs.eye(1)
 
-    def test_LocalizationLinear(self):
+    def test_LocalizationLinear_propagate(self):
         initial_state = gs.array([0.5, 1.])
         time_step = 0.5
         acc = 2.
         increment = gs.array([time_step, acc])
-        measurement = gs.array([0.7])
 
-        # Test propagation
         expected = gs.array([1., 2.])
         result = self.linear_model.propagate(initial_state, increment)
         self.assertAllClose(expected, result)
 
-        # Test propagation Jacobian
+    def test_LocalizationLinear_propagation_jacobian(self):
+        time_step = 0.5
+        acc = 2.
+        increment = gs.array([time_step, acc])
         expected = gs.array([[1., 0.5],
                              [0., 1.]])
         result = self.linear_model.propagation_jacobian(None, increment)
         self.assertAllClose(expected, result)
 
-        # Test observation model
+    def test_LocalizationLinear_observation_model(self):
+        initial_state = gs.array([0.5, 1.])
         expected = gs.array([0.5])
         result = self.linear_model.observation_model(initial_state)
         self.assertAllClose(expected, result)
 
-        # Test observation Jacobian
+    def test_LocalizationLinear_observation_jacobian(self):
         expected = gs.array([[1., 0.]])
         result = self.linear_model.observation_jacobian(None, None)
         self.assertAllClose(expected, result)
 
-        # Test innovation
+    def test_LocalizationLinear_innovation(self):
+        initial_state = gs.array([0.5, 1.])
+        measurement = gs.array([0.7])
         expected = gs.array([0.2])
         result = self.linear_model.innovation(initial_state, measurement)
         self.assertAllClose(expected, result)
 
-    def test_Localization(self):
-        initial_state = gs.array([0.5, 1., 2.])
+    def test_Localization_preprocess_input(self):
         time_step = gs.array([0.5])
         linear_vel = gs.array([1., 0.5])
         angular_vel = gs.array([0.])
         increment = gs.concatenate((
             time_step, linear_vel, angular_vel), axis=0)
-        measurement = gs.array([0.7, 2.1])
 
-        angle = initial_state[0]
-        rotation = gs.array([[gs.cos(angle), -gs.sin(angle)],
-                             [gs.sin(angle), gs.cos(angle)]])
-
-        # Test preprocess_input
         expected = time_step[0], linear_vel, angular_vel
         result = self.nonlinear_model.preprocess_input(increment)
         for i in range(3):
             self.assertAllClose(expected[i], result[i])
 
-        # Test rotation_matrix
+    def test_Localization_rotation_matrix(self):
+        initial_state = gs.array([0.5, 1., 2.])
+
+        angle = initial_state[0]
+        rotation = gs.array([[gs.cos(angle), -gs.sin(angle)],
+                             [gs.sin(angle), gs.cos(angle)]])
         expected = rotation
         result = self.nonlinear_model.rotation_matrix(angle)
         self.assertAllClose(expected, result)
 
-        # Test adjoint
+    def test_Localization_adjoint_map(self):
+        initial_state = gs.array([0.5, 1., 2.])
+
+        angle = initial_state[0]
+        rotation = gs.array([[gs.cos(angle), -gs.sin(angle)],
+                             [gs.sin(angle), gs.cos(angle)]])
         first_line = gs.eye(1, 3)
         last_lines = gs.hstack((gs.array([[2.], [-1.]]), rotation))
         expected = gs.vstack((first_line, last_lines))
         result = self.nonlinear_model.adjoint_map(initial_state)
         self.assertAllClose(expected, result)
 
-        # Test propagation
+    def test_Localization_propagate(self):
+        initial_state = gs.array([0.5, 1., 2.])
+        time_step = gs.array([0.5])
+        linear_vel = gs.array([1., 0.5])
+        angular_vel = gs.array([0.])
+        increment = gs.concatenate((
+            time_step, linear_vel, angular_vel), axis=0)
+
+        angle = initial_state[0]
+        rotation = gs.array([[gs.cos(angle), -gs.sin(angle)],
+                             [gs.sin(angle), gs.cos(angle)]])
         next_position = initial_state[1:] + time_step * gs.matmul(
             rotation, linear_vel)
         expected = gs.concatenate((gs.array([angle]), next_position), axis=0)
         result = self.nonlinear_model.propagate(initial_state, increment)
         self.assertAllClose(expected, result)
 
-        # Test propagation Jacobian
+    def test_Localization_propagation_jacobian(self):
+        time_step = gs.array([0.5])
+        linear_vel = gs.array([1., 0.5])
+        angular_vel = gs.array([0.])
+        increment = gs.concatenate((
+            time_step, linear_vel, angular_vel), axis=0)
+
         first_line = gs.eye(1, 3)
         last_lines = gs.hstack((gs.array([[-0.25], [0.5]]), gs.eye(2)))
         expected = gs.vstack((first_line, last_lines))
         result = self.nonlinear_model.propagation_jacobian(None, increment)
         self.assertAllClose(expected, result)
 
-        # Test observation model
+    def test_Localization_observation_model(self):
+        initial_state = gs.array([0.5, 1., 2.])
         expected = gs.array([1., 2.])
         result = self.nonlinear_model.observation_model(initial_state)
         self.assertAllClose(expected, result)
 
-        # Test observation Jacobian
+    def test_Localization_observation_jacobian(self):
         expected = gs.array([[0., 1., 0.],
                              [0., 0., 1.]])
         result = self.nonlinear_model.observation_jacobian(None, None)
         self.assertAllClose(expected, result)
 
-        # Test innovation
+    def test_Localization_innovation(self):
+        initial_state = gs.array([0.5, 1., 2.])
+        measurement = gs.array([0.7, 2.1])
+
+        angle = initial_state[0]
+        rotation = gs.array([[gs.cos(angle), -gs.sin(angle)],
+                             [gs.sin(angle), gs.cos(angle)]])
         expected = gs.matmul(gs.transpose(rotation), gs.array([-0.3, 0.1]))
         result = self.nonlinear_model.innovation(initial_state, measurement)
         self.assertAllClose(expected, result)
