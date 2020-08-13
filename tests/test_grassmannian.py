c05deb85236c5bef7f91c6caf58a07860c825b96
GUIGUI Nicolas <nicolas.guigui@inria.fr>
2020-08-13 11:02:16 +0200

    modify exp/log to take and return tangent vectors

--- tests/test_grassmannian.py
+++ tests/test_grassmannian.py
@@ -4,6 +4,7 @@ import geomstats.backend as gs
 import geomstats.tests
 from geomstats.geometry.grassmannian import Grassmannian
 from geomstats.geometry.grassmannian import GrassmannianCanonicalMetric
+from geomstats.geometry.matrices import Matrices
 
 p_xy = gs.array([
     [1., 0., 0.],
@@ -40,24 +41,24 @@ class TestGrassmannian(geomstats.tests.TestCase):
         self.metric = GrassmannianCanonicalMetric(self.n, self.k)
 
     def test_exp_np(self):
+        vec = Matrices.bracket(pi_2 * r_y, gs.array([p_xy, p_yz]))
         result = self.metric.exp(
-            pi_2 * r_y,
-            gs.array([p_xy, p_yz]))
+            vec, gs.array([p_xy, p_yz]))
         expected = gs.array([p_yz, p_xy])
         self.assertAllClose(result, expected)
 
+        vec = Matrices.bracket(
+            pi_2 * gs.array([r_y, r_z]), gs.array([p_xy, p_yz]))
         result = self.metric.exp(
-            pi_2 * gs.array([r_y, r_z]),
-            gs.array([p_xy, p_yz]))
+            vec, gs.array([p_xy, p_yz]))
         expected = gs.array([p_yz, p_xz])
         self.assertAllClose(result, expected)
 
-    @geomstats.tests.np_only
     def test_log(self):
+        expected = Matrices.bracket(pi_4 * r_y, p_xy)
         result = self.metric.log(
-            self.metric.exp(pi_4 * r_y, p_xy),
-            p_xy)
-        expected = pi_4 * r_y
+            self.metric.exp(expected, p_xy), p_xy)
+        self.assertTrue(self.space.is_tangent(result, p_xy))
         self.assertAllClose(result, expected)
 
     @geomstats.tests.np_only
