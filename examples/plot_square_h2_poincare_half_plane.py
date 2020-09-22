41289495f66e660eeaf042b1fe54e51d92a673cc
Alice Le Brigant <alice.le-brigant@enac.fr>
2020-09-22 13:21:27 +0200

    remove commented lines

--- examples/plot_square_h2_poincare_half_plane.py
+++ examples/plot_square_h2_poincare_half_plane.py
@@ -28,9 +28,6 @@ def main():
         dst_id = (i + 1) % len(corners_ext)
         dst = corners_ext[dst_id]
         geodesic = METRIC.geodesic(initial_point=src, end_point=dst)
-        # tangent_vec = METRIC.log(point=dst, base_point=src)
-        # geodesic = METRIC.geodesic(
-        #     initial_point=src, initial_tangent_vec=tangent_vec)
         t = gs.linspace(0., 1., n_steps)
         edge_points.append(geodesic(t))
 
