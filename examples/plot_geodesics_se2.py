2f66905c9902ab2a63e749f07a245bc5d9634e59
GUIGUI Nicolas <nicolas.guigui@inria.fr>
2020-11-16 15:43:44 +0100

    compare with right invariant metric from vector representation

--- examples/plot_geodesics_se2.py
+++ examples/plot_geodesics_se2.py
@@ -14,6 +14,7 @@ from geomstats.geometry.special_euclidean import SpecialEuclidean
 SE2_GROUP = SpecialEuclidean(n=2, point_type='matrix')
 N_STEPS = 40
 METRIC = SE2_GROUP.left_canonical_metric
+SE2_VEC = SpecialEuclidean(n=2, point_type='vector')
 
 
 def main():
@@ -28,12 +29,20 @@ def main():
     group_geo_points = SE2_GROUP.exp(tangent_vec)
     left_geo_points = METRIC.exp(tangent_vec)
 
+    initial_right_vec = gs.array([-theta, 2, 2])
+    right_vec = gs.einsum('t, i-> ti', t, initial_right_vec)
+    right_geo_points = SE2_VEC.right_canonical_metric.exp(right_vec)
+    right_points = SE2_VEC.matrix_from_vector(right_geo_points)
+
     ax = visualization.plot(
         group_geo_points, space='SE2_GROUP', color='black',
         label='Group Geodesics')
     ax = visualization.plot(
         left_geo_points, ax=ax, space='SE2_GROUP', color='green',
         label='Left Geodesics')
+    ax = visualization.plot(
+        right_points, ax=ax, space='SE2_GROUP', color='yellow',
+        label='Right Geodesics')
     ax.set_aspect('equal')
     plt.legend(loc='lower right')
     plt.show()
