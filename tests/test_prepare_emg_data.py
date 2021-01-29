b0a4e46eba5f197d873ff39649031a085f5e9a6b
Marius <marius.guerard@gmail.com>
2021-01-28 19:03:02 -0800

    Changed input data format in prepare_emg_data.TimeSeriesCovariance from DataFrame to dictionary

--- tests/test_prepare_emg_data.py
+++ tests/test_prepare_emg_data.py
@@ -1,7 +1,6 @@
 """Unit tests for TimeSeriesCovariance class."""
 
-import numpy as np
-
+import geomstats.backend as gs
 import geomstats.tests
 from geomstats.datasets.prepare_emg_data import TimeSeriesCovariance
 from geomstats.datasets.utils import load_emg
@@ -16,7 +15,13 @@ class TestPrepareEmgData(geomstats.tests.TestCase):
         self.n_elec = 8
         self.label_map = {'rock': 0, 'scissors': 1, 'paper': 2, 'ok': 3}
         self.margin = 1000
-        self.emg_data = load_emg()
+        data = load_emg()
+        data = data[data.label != 'rest']
+        self.emg_data = {'time_vec': gs.array(data.time),
+                         'raw_data': gs.array(data[['c{}'.format(i)
+                                                    for i in range(8)]]),
+                         'label': gs.array(data.label),
+                         'exp': gs.array(data.exp)}
 
         self.cov_transformer = TimeSeriesCovariance(self.emg_data,
                                                     self.n_steps,
@@ -36,11 +41,11 @@ class TestPrepareEmgData(geomstats.tests.TestCase):
         """Test the shape of the vectorized covariance."""
         dim_vec = int(self.n_elec * (self.n_elec + 1) / 2)
         result_shape = (len(self.cov_transformer.batches), dim_vec)
-        shape = np.stack(self.cov_transformer.df.covec.values).shape
+        shape = self.cov_transformer.covecs.shape
         self.assertTrue(shape == result_shape)
 
     def test_diag_shape(self):
         """Test the shape of the diagonal."""
         result_shape = (len(self.cov_transformer.batches), self.n_elec)
-        shape = np.stack(self.cov_transformer.df.diag.values).shape
+        shape = self.cov_transformer.diags.shape
         self.assertTrue(shape == result_shape)
