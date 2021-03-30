c795813b0f74e63977f7379cc44c6b856fef25ce
Nina Miolane <nmiolane@gmail.com>
2021-03-29 19:18:08 -0600

    Convert comments into docstrings

--- tests/test__my_manifold.py
+++ tests/test__my_manifold.py
@@ -29,34 +29,49 @@ from geomstats.geometry._my_manifold import MyManifold
 
 
 class TestMyManifold(geomstats.tests.TestCase):
-    # Use the setUp method to define variables that stay constant
-    # during all tests. For example, here we test the
-    # 4-dimensional manifold of the class MyManifold.
+    """Class testing the methods of MyManifold.
+
+    In the class TestMyManifold, each test method:
+    - needs to start with `test_`
+    - represents a unit-test, i.e. tests one and only one method
+    or attribute of the class MyManifold,
+    - ends with the line: `self.assertallclose(result, expected)`, see below.
+    """
     def setUp(self):
+        """setUp method.
+
+        Use the setUp method to define variables that stay constant
+        during all tests. For example, here we test the
+        4-dimensional manifold of the class MyManifold.
+        """
         self.dimension = 4
         self.another_parameter = 3
         self.manifold = MyManifold(
             dim=self.dimension, another_parameter=3)
 
-    # Each method:
-    # - needs to start with `test_`
-    # - represents a unit-test, i.e. tests one and only one method
-    #  or attribute of the class MyManifold.
-
-    # The method test_dimension tests the `dim` attribute.
     def test_dimension(self):
+        """Test dimension.
+
+        The method test_dimension tests the `dim` attribute.
+        """
+
         result = self.manifold.dim
         expected = self.dimension
         # Each test ends with the following syntax, comparing
         # the result with the expected result, using self.assertAllClose
-        self.assertAllClose(result, expected)
+        self.assertallclose(result, expected)
 
-    # The method test_belongs tests the `belongs` method.
     def test_belongs(self):
-        # Arrays are defined using geomstats backend through the prefix `gs.`.
-        # This allows the code to be tested simultaneously in numpy,
-        # pytorch and tensorflow. `gs.` is the equivalent of numpy's `np.` and
-        # most of numpy's functions are available with `gs.`.
+        """Test belongs.
+
+        The method test_belongs tests the `belongs` method.
+
+        Note that arrays are defined using geomstats backend
+        through the prefix `gs.`.
+        This allows the code to be tested simultaneously in numpy,
+        pytorch and tensorflow. `gs.` is the equivalent of numpy's `np.` and
+        most of numpy's functions are available with `gs.`.
+        """
         point = gs.array([1., 2., 3.])
         result = self.manifold.belongs(point)
         expected = False
