010c73b12fa7929efc08887caf1a3b2ceb8af5e3
Saiteja Utpala <saitejautpala@gmail.com>
2021-05-18 23:26:13 +0530

    vectorize code heavily

--- geomstats/sampling/lognormal.py
+++ geomstats/sampling/lognormal.py
@@ -6,6 +6,7 @@ class LogNormal:
     Parameters:
     ----------
     manifold: Name of Manifold
+        
 
     """
     def __init__(self,manifold,mean,cov=None):
@@ -21,14 +22,22 @@ class LogNormal:
             cov_SPDmanifold  = spd.SPDMatrices(self.cov_n)
 
             if (not mean_SPDmanifold.belongs(self.mean)):
-                 raise ValueError("Invalid Value in mean" , mean)
-
-
-
+                 raise ValueError("Invalid Value in mean, Not Symmetric Positive Definite" , mean)
 
     
     def _sample_spd(self,samples):
-        pass
+        SPDmanifold = spd.SPDMatrices(self.n)
+        i,  = gs.diag_indices(self.n)
+        j,k = gs.triu_indices(self.n,k=1)
+        sym_matrix = SPDmanifold.logm(self.mean)
+
+        mean_euclidean = gs.hstack((sym_matrix[i,i],gs.sqrt(2)*sym_matrix[j,k]))
+        samples_spd = gs.zeros((samples,n,n))
+        samples_euclidean = gs.random.multivariate_normal(mean_euclidean, cov, samples)
+        samples_spd[:,i,i] = samples_euclidean[:,:n]
+        samples_spd[:,j,k] = samples_euclidean[:,n:]/gs.sqrt(2)
+        samples_spd = SPDmanifold.expm(samples_spd)
+        return samples_spd
 
     def _sample_euclidean(self,samples):
         pass    
