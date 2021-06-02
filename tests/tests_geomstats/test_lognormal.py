b5582714d1811d1e0e3f754643b223c2bc45bf3c
Saiteja Utpala <73220310+SaitejaUtpala@users.noreply.github.com>
2021-06-02 18:45:41 +0530

    Update test_lognormal.py

--- tests/tests_geomstats/test_lognormal.py
+++ tests/tests_geomstats/test_lognormal.py
@@ -26,12 +26,12 @@ class TestLogNormal(geomstats.tests.TestCase):
         LogNormalSampler = LogNormal(self.Euclidean, mean, cov)
         data = LogNormalSampler.sample(self.samples)
 
-        result = self.Euclidean.belongs(data).all()
+        result = gs.all(self.Euclidean.belongs(data))
         expected = True
         self.assertAllClose(result, expected)
 
     def test_spd_belongs(self):
-        """Test if the samples to SPD Manifold"""
+        """Test if the samples belong to SPD Manifold"""
         mean = 2 * gs.eye(self.n)
         cov = gs.eye(self.spd_cov_n)
         LogNormalSampler = LogNormal(self.SPDManifold, mean, cov)
@@ -44,26 +44,26 @@ class TestLogNormal(geomstats.tests.TestCase):
     def test_euclidean_frechet_mean(self):
         """Test if the frechet mean of the samples is close to mean"""
         mean = gs.zeros(self.n)
-        cov = gs.eye(self.n) / self.n
-        data = LogNormal(self.Euclidean, mean, cov).sample(1000)
+        cov = gs.eye(self.n) 
+        data = LogNormal(self.Euclidean, mean, cov).sample(5000)
         log_data = gs.log(data)
         fm = gs.mean(log_data, mean(axis=0))
 
         expected = mean
         result = fm
-        self.assertAllClose(result, expected)
+        self.assertAllClose(result, expected, atol=1e-2)
 
     def test_spd_frechet_mean(self):
         """Test if the frechet mean of the samples is close to mean"""
         mean = gs.eye(self.n)
-        cov = gs.eye(self.spd_cov_n) / self.spd_cov_n
-        data = LogNormal(self.SPDManifold, mean, cov).sample(1000)
+        cov = gs.eye(self.spd_cov_n) 
+        data = LogNormal(self.SPDManifold, mean, cov).sample(5000)
         _fm = gs.mean(self.SPDManifold.logm(data), axis=0)
         fm = self.SPDManifold.expm(_fm)
 
         expected = mean
         result = fm
-        self.assertAllClose(result, expected)
+        self.assertAllClose(result, expected, atol=1e-2)
 
     def test_error_handling(self):
         """Test if the erros are raised for invalid parameters"""
