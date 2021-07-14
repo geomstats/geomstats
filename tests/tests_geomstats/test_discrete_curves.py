cc7f6b845d9f17d24250aa0e3de3df542fcae575
Florent-Michel <66306609+florent-michel@users.noreply.github.com>
2021-07-14 17:26:58 +0200

    solve conflict for test_discrete_curves.py

--- tests/tests_geomstats/test_discrete_curves.py
+++ tests/tests_geomstats/test_discrete_curves.py
@@ -12,12 +12,8 @@ from geomstats.geometry.hypersphere import Hypersphere
 class TestDiscreteCurves(geomstats.tests.TestCase):
     def setUp(self):
         s2 = Hypersphere(dim=2)
-<<<<<<< HEAD
-        r3 = s2.embedding_space
-=======
         r2 = Euclidean(dim=2)
-        r3 = s2.embedding_manifold
->>>>>>> Clean code and add unit tests
+        r3 = s2.embedding_space
 
         initial_point = [0., 0., 1.]
         initial_tangent_vec_a = [1., 0., 0.]
@@ -40,12 +36,12 @@ class TestDiscreteCurves(geomstats.tests.TestCase):
         discretized_curve_b = curve_b(sampling_times)
         discretized_curve_c = curve_c(sampling_times)
 
-        self.n_discretized_curves = 5
-        self.times = gs.linspace(0., 1., self.n_discretized_curves)
-        gs.random.seed(1234)
         self.space_closed_curves_in_euclidean_2d = ClosedDiscreteCurves(
             ambient_manifold=r2)
 
+        self.n_discretized_curves = 5
+        self.times = gs.linspace(0., 1., self.n_discretized_curves)
+        gs.random.seed(1234)
         self.space_curves_in_euclidean_3d = DiscreteCurves(
             ambient_manifold=r3)
         self.space_curves_in_sphere_2d = DiscreteCurves(
@@ -269,7 +265,6 @@ class TestDiscreteCurves(geomstats.tests.TestCase):
         expected = self.srv_metric_r3.dist(self.curve_a, self.curve_b)[0]
         self.assertAllClose(result, expected)
 
-<<<<<<< HEAD
     def test_random_and_belongs(self):
         random = self.space_curves_in_sphere_2d.random_point()
         result = self.space_curves_in_sphere_2d.belongs(random)
@@ -296,7 +291,7 @@ class TestDiscreteCurves(geomstats.tests.TestCase):
         result = self.space_curves_in_sphere_2d.is_tangent(
             tangent_vec, point)
         self.assertTrue(gs.all(result))
-=======
+
     def test_projection_closed_curves(self):
         """Test that projecting the projection returns the projection
 
@@ -315,4 +310,3 @@ class TestDiscreteCurves(geomstats.tests.TestCase):
             result = proj[-1, :]
             expected = proj[0, :]
             self.assertAllClose(result, expected)
->>>>>>> Clean code and add unit tests
