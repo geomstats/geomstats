235aa1497af8af24710b8f5d3ebcf451c4bb2863
Nina Miolane <nmiolane@gmail.com>
2021-08-17 08:46:19 -0700

    Add a doc-requirements with jupyter

--- setup.py
+++ setup.py
@@ -56,7 +56,6 @@ if __name__ == '__main__':
             'Topic :: Scientific/Engineering :: Mathematics',
             'License :: OSI Approved :: BSD License',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8'
