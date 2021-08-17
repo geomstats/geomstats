f61fcaf134bc791cb907b83401e195fa92278711
Nina Miolane <nmiolane@gmail.com>
2021-08-17 10:15:03 -0700

    Add np_only decorator to stiefel frechet mean test

--- tests/tests_geomstats/test_frechet_mean.py
+++ tests/tests_geomstats/test_frechet_mean.py
@@ -458,6 +458,7 @@ class TestFrechetMean(geomstats.tests.TestCase):
         expected = gs.stack([expected_1, expected_2])
         self.assertAllClose(expected, result)
 
+    @geomstats.tests.np_only
     def test_stiefel_two_samples(self):
         space = Stiefel(3, 2)
         metric = space.metric
@@ -469,6 +470,7 @@ class TestFrechetMean(geomstats.tests.TestCase):
             metric.log(point[0], point[1]) / 2, point[1])
         self.assertAllClose(expected, result)
 
+    @geomstats.tests.np_only
     def test_stiefel_n_samples(self):
         space = Stiefel(3, 2)
         metric = space.metric
