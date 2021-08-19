fb84daf9615f305d9c870013a83fe1b983d27a3a
Nina Miolane <nmiolane@gmail.com>
2021-08-19 15:36:22 -0700

    Mv to autodiff in numpy backend

--- geomstats/_backend/numpy/autodiff.py
+++ geomstats/_backend/numpy/autodiff.py
@@ -2,6 +2,24 @@ from autograd import elementwise_grad, jacobian, value_and_grad # NOQA
 from autograd.extend import defjvp, defvjp, primitive
 
 
+def value_and_grad(objective):
+    """Return an error when using automatic differentiation with numpy."""
+    raise RuntimeError(
+        "Automatic differentiation is not supported with numpy backend. "
+        "Use autograd, pytorch or tensorflow backend instead.\n"
+        "Change backend via the command "
+        "export GEOMSTATS_BACKEND=autograd in a terminal.")
+
+
+def jacobian(f):
+    """Return an error when using automatic differentiation with numpy."""
+    raise RuntimeError(
+        "Automatic differentiation is not supported with numpy backend. "
+        "Use autograd, pytorch or tensorflow backend instead.\n"
+        "Change backend via the command "
+        "export GEOMSTATS_BACKEND=autograd in a terminal.")
+
+
 def custom_gradient(*grad_func):
     """Decorate a function to define its custom gradient
 
