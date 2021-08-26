62e675cbad6740b4d14a60ef5705b2ba785d3dba
Nina Miolane <nmiolane@gmail.com>
2021-08-26 10:31:53 -0700

    Chain rule of one variable is passing

--- tests/tests_geomstats/test_backends.py
+++ tests/tests_geomstats/test_backends.py
@@ -1113,10 +1113,10 @@ class TestBackends(geomstats.tests.TestCase):
         self.assertAllClose(result_value, expected_value)
         self.assertAllClose(result_grad, expected_grad)
 
-    def test_custom_grad_chain_rule(self):
+    def test_custom_grad_chain_rule_one_var(self):
 
         def fun1_grad(x):
-            return 3
+            return 1.
 
         @gs.autodiff.custom_gradient(fun1_grad)
         def fun1(x):
@@ -1129,11 +1129,39 @@ class TestBackends(geomstats.tests.TestCase):
         def fun2_grad(x):
             return 2 * x
 
-        arg = gs.array([10., 2.])
+        arg = gs.array(10.)
+
+        result_value, result_grad = gs.autodiff.value_and_grad(fun2)(arg)
+        expected_value = fun2(arg)
+        expected_grad = 20.
+
+        expected_grad2 = fun2_grad(fun1(arg)) * fun1_grad(arg)
+
+        self.assertAllClose(result_value, expected_value)
+        self.assertAllClose(result_grad, expected_grad)
+        self.assertAllClose(result_grad, expected_grad2)
+
+    def test_custom_grad_chain_rule_bis(self):
+
+        def fun1_grad(x):
+            return 1.
+
+        @gs.autodiff.custom_gradient(fun1_grad)
+        def fun1(x):
+            return x
+
+        def fun2(x):
+            out = fun1(x) ** 2
+            return out
+
+        def fun2_grad(x):
+            return 2 * x
+
+        arg = gs.array(10.)
 
         result = gs.autodiff.value_and_grad(fun2)(arg)
         result = tuple(gs.array(k) for k in result)
-        expected = (fun2(arg), fun2_grad(fun1(arg)) * fun1_grad(arg))
+        expected = (fun2(arg), )
 
         self.assertAllClose(result[0], expected[0])
         self.assertAllClose(result[1], expected[1])
