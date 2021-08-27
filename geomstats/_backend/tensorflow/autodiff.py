6602dcf193ac006dcd71bd0dc952ff79cf095456
Nina Miolane <nmiolane@gmail.com>
2021-08-27 07:36:27 -0700

    All tests pass in tf

--- geomstats/_backend/tensorflow/autodiff.py
+++ geomstats/_backend/tensorflow/autodiff.py
@@ -76,11 +76,12 @@ def value_and_grad(func, to_numpy=False):
             print(f"---- SEVERAL args here! {len(arg_x)}")
             if isinstance(arg_x[0], np.ndarray):
                 arg_x = (tf.Variable(one_arg_x) for one_arg_x in arg_x)
-            with tf.GradientTape() as t:
-                for one_arg_x in arg_x:
-                    t.watch(one_arg_x)
-                loss = func(*arg_x)
-            return loss, t.gradient(loss, *arg_x)
+            return tfm.value_and_gradient(func, *arg_x)
+            # with tf.GradientTape() as t:
+            #     for one_arg_x in arg_x:
+            #         t.watch(one_arg_x)
+            #     loss = func(*arg_x)
+            # return loss, t.gradient(loss, *arg_x)
 
     return func_with_grad
 
