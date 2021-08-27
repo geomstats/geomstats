75f6b330db1a3f13c9ce0f1ae74f6bcbd67ad6d2
Nina Miolane <nmiolane@gmail.com>
2021-08-27 10:31:41 -0700

    use einsum in custom_grad

--- geomstats/_backend/tensorflow/autodiff.py
+++ geomstats/_backend/tensorflow/autodiff.py
@@ -18,10 +18,22 @@ def custom_gradient(*grad_funcs):
     def wrapper(func):
         def func_with_grad(*args, **kwargs):
             def grad(upstream):
-                grad_vals = (
-                    upstream * grad_fun(*args, **kwargs)
-                    for grad_fun in grad_funcs
-                )
+                print(type(upstream))
+                print(type(grad_funcs[0](*args, **kwargs)))
+                # upstream = tf.convert_to_tensor(upstream)
+                if upstream.ndim < 1:
+                    upstream = tf.expand_dims(upstream, axis=0)
+                if upstream.ndim < 2:
+                    upstream = tf.expand_dims(upstream, axis=1)
+                grad_vals = []
+                for grad_fun in grad_funcs:
+                    grad_func_val = tf.convert_to_tensor(grad_fun(*args, **kwargs))
+                    #     if upstream.ndim < 1:
+                    # upstream = tf.expand_dims(upstream, axis=0)
+                    grad_vals.append(
+                    tf.squeeze(tf.einsum("...,...->...", upstream, grad_func_val))
+                    #upstream * grad_fun(*args, **kwargs)
+                    )
                 return tuple(grad_vals)
 
             return func(*args, **kwargs), grad
