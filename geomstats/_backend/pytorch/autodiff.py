4be5593d493392267501ad7325c2e205dad70c05
Nina Miolane <nmiolane@gmail.com>
2021-08-30 19:15:45 -0700

    Clean autodiff in pytorch test_backends pass

--- geomstats/_backend/pytorch/autodiff.py
+++ geomstats/_backend/pytorch/autodiff.py
@@ -21,50 +21,26 @@ def custom_gradient(*args):
         class function_with_grad(torch.autograd.Function):
             @staticmethod
             def forward(ctx, *args):
-                print("in forward")
-                print("ctx")
-                print(ctx)
-                print("args")
-                print(args)
-                print(f"there are {len(args)} args in forward")
+
                 ctx.save_for_backward(*args)
-                print("ctx")
-                print(ctx)
-                print("\n")
                 return function(*args)
 
             @staticmethod
             def backward(ctx, grad_output):
-                print("ctx")
-                print(ctx)
-                print("grad_output")
-                print(grad_output)
-                #grad_output = (torch.tensor(1.), torch.tensor(1.))
 
                 inputs = ctx.saved_tensors
-                print("inputs in backward")
-                print(inputs)
 
                 grads = []
-                print("args?")
-                print(args)
 
                 for custom_grad in args:
                     grads.append(grad_output * custom_grad(*inputs))
                 grads = tuple(grads)
 
-
-                print("\ngrads")
-                print(type(grads))
-                print(grads)
                 if len(grads) == 1:
                     return grads[0]
                 return grads
 
         def wrapper(*args):
-            print("using apply...")
-            print("args")
-            print(args)
             out = function_with_grad.apply(*args)
             return out
 
@@ -104,11 +80,7 @@ def value_and_grad(func, to_numpy=False):
             new_args.append(one_arg)
         args = tuple(new_args)
 
-        #x, y = args
         value = func(*args)
-        # print()
-        # print(type(value))
-        # print(value)
         if value.ndim > 0:
             value.backward(gradient=torch.ones_like(one_arg), retain_graph=True)
         else:
@@ -117,8 +89,6 @@ def value_and_grad(func, to_numpy=False):
         all_grads = []
         for one_arg in args:
             all_grads.append(torch.autograd.grad(value, one_arg, retain_graph=True)[0])
-        #all_grads.append(torch.autograd.grad(value, y)[0])
-        #all_grads.append(torch.autograd.grad(value, y, allow_unused=True)[0])
         if len(args) == 1:
             return value, all_grads[0]
         return value, tuple(all_grads)
