e11c20049918f0cc6b2f75dd1613f08827a37a48
Saiteja Utpala <saitejautpala@gmail.com>
2021-09-16 20:05:59 +0530

    better  tests

--- tests/tests_geomstats/test_geometric_median.py
+++ tests/tests_geomstats/test_geometric_median.py
@@ -5,23 +5,30 @@ import geomstats.tests
 from geomstats.geometry.spd_matrices import SPDMetricAffine
 from geomstats.learning.geometric_median import WeiszfeldAlgorithm
 
-
-
-
+ROOT2 = gs.sqrt(2)
 
 class TestGeometricMedianEstimation(geomstats.tests.TestCase):
     """Test of Geometric Meidan Estimators"""
 
-    def test_fit(self):
-        """Test the fit method."""
-        n_clusters = 2
+    def test_fit_SPD_manifold(self):
+        """Test the fit method on SPD manifold"""
+        
         wa_estimator = WeiszfeldAlgorithm(SPDMetricAffine(n=2))
-
-        X = gs.array([[1, 0], [0, 1]],
-                     [[1, 0], [0, 1]]])
+        X = gs.array([[[1., 0.], [0., 1.]],
+                      [[1., 0.], [0., 1]]])
         
         wa_estimator.fit(X)
-        gm_expected = gs.array([[1, 0], [0, 1]])
+        gm_expected = gs.array([[1., 0.], [0., 1.]])
         gm_result = wa_estimator.estimate_
 
         self.assertAllClose(gm_expected, gm_result )
+
+
+        X = gs.array([[[1., 0.], [0., 1.]],
+                      [[2., 0.], [0., 2]]])
+
+        wa_estimator.fit(X)
+        gm_expected = gs.array([[ROOT2, 0.], [0., ROOT2]])
+        gm_result = wa_estimator.estimate_
+
+    
