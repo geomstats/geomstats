cbefbfd1744f27576cb08cb2048c962fe82aa630
Saiteja Utpala <73220310+SaitejaUtpala@users.noreply.github.com>
2021-09-17 11:11:16 +0530

    fix linting

--- geomstats/learning/intrinsic_dimension_estimation.py
+++ geomstats/learning/intrinsic_dimension_estimation.py
@@ -2,24 +2,24 @@
 
 import geomstats.backend as gs
 
+
 class LevinaBickelEstimator:
-    
     """Levina Bickel Estimator for intrinsic dimension estimation
-    
-    Levian Bickel Estimator calculates intrinsic dimension of  
-    manifolds embedded in euclidean space through maximum likelihood 
-    estimation. 
-    
+
+    Levian Bickel Estimator calculates intrinsic dimension of
+    manifolds embedded in euclidean space through maximum likelihood
+    estimation.
+
     .. math::
-        T_j(x_i) = \text{j th Nearest Neighbor to }  x_i \text{ in the data}
+        T_j(x_i)=\text{j th Nearest Neighbor to }  x_i \text{ in the data}
 
-        m_k(x_i)  =  \Bigg[\frac{1}{k-1} \sum_{j=1}^{k} \frac{\log T_k(x)}{\log T_{j}(x)}\Bigg]^{-1} 
+        m_k(x_i)=[\frac{1}{k-1} \sum_{j=1}^{k}
+        \frac{\log T_k(x)}{\log T_{j}(x)}]^{-1}
 
-        m_k = \frac{1}{N} \sum_{i=1}^{N} m_{k}(x_i)  
+        m_k=\frac{1}{N} \sum_{i=1}^{N} m_{k}(x_i)
 
-        m = \frac{1}{k_2 - k_1 + 1} \sum_{k=k_1}^{k_2} m_k
-       
-       
+        m=\frac{1}{k_2 - k_1 + 1} \sum_{k=k_1}^{k_2} m_k
+
     Parameters
     ----------
     min_neighbors : int
@@ -28,22 +28,21 @@ class LevinaBickelEstimator:
     max_neighbors : int
         Maximum Neighbors used for computing estimator
         Optional, default: 20.
-    
-    
+
     Example
     -------
     >>> from sklearn import  datasets
     >>> import geomstats.backend as gs
-    >>> from geomstats.learning.intrinsic_dimension_estimation import LevinaBickelEstimator
+    >>> import geomstats.learning.intrinsic_dimension_estimation as ide
+    >>> from ede import LevinaBickelEstimator
     >>> X, _ = datasets.make_swiss_roll(n_samples=1500)
     >>> X = gs.array(X)
     >>> lbe = LevinaBickelEstimator()
     >>> predicted_intrinsic_dim =lbe.fit(X).predict()
-    
-    
+
     Reference
     ----------
-    .. [2004] . "Maximum Likelihood Estimation of Intrinsic Dimension" 
+    .. [2004] . "Maximum Likelihood Estimation of Intrinsic Dimension"
     Advances in Neural Information Processing Systems 17 (NIPS 2004)
     https://arxiv.org/abs/1908.09326
     """
@@ -54,7 +53,7 @@ class LevinaBickelEstimator:
 
     def fit(self, X):
         """Calculate distance matrix
-        
+
         Parameters
         ----------
         X : array-like
@@ -65,23 +64,22 @@ class LevinaBickelEstimator:
         self : object
             Returns self.
         """
-        pairwise_sub = gs.expand_dims(X, axis = 0) - gs.expand_dims(X, axis=1)
-        pairwise_dist = gs.linalg.norm(pairwise_sub, axis = -1)
-        sorted_dist = gs.sort(pairwise_dist , axis = -1)[:,1:]
+        pairwise_sub = gs.expand_dims(X, axis 0) - gs.expand_dims(X, axis=1)
+        pairwise_dist = gs.linalg.norm(pairwise_sub, axis=-1)
+        sorted_dist = gs.sort(pairwise_dist, axis=-1)[:, 1:]
         self.log_sorted_dist = gs.log(sorted_dist)
         return self
 
+    def T(self, j, x):
+        """Return jth nearest neighbor to x in data X
 
-    def T(self, j,x):
-        """Return jth nearest neighbor to x in data X 
-        
         Parameters
         ---------
         j : int
             j th nearest neighbor
         x : int
-            index of the point x in the data X 
-            
+            index of the point x in the data X
+
         Returns
         -------
         X : array like, shape=[N, dim]
@@ -92,12 +90,11 @@ class LevinaBickelEstimator:
         """Predict intrinsic dimension"""
         num = self.log_sorted_dist[:, self.min_neighbors-1: self.max_neighbors]
         int_dim_for_each_k = []
-        for k in range(self.min_neighbors , self.max_neighbors+1):
+        for k in range(self.min_neighbors, self.max_neighbors+1):
             t1 = self.log_sorted_dist[:, k-1]
-            t2 = gs.mean(self.log_sorted_dist[:,:k-1], axis=1)
+            t2 = gs.mean(self.log_sorted_dist[:, :k-1], axis=1)
             int_dim_k = gs.mean(1.0/(t1-t2))
             int_dim_for_each_k.append(int_dim_k)
 
-        intrinsic_dim = sum(int_dim_for_each_k)/len(int_dim_for_each_k)  
+        intrinsic_dim = sum(int_dim_for_each_k)/len(int_dim_for_each_k)
         return intrinsic_dim
-
