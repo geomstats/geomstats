ecb2cafaa89ffa4c5fc9f19cca230cc9297d049e
Saiteja Utpala <73220310+SaitejaUtpala@users.noreply.github.com>
2021-09-17 11:35:50 +0530

    fix filenames

--- tests/tests_geomstats/test_intrinsic_dimension_estimation.py
+++ tests/tests_geomstats/test_intrinsic_dimension_estimation.py
@@ -1,7 +1,6 @@
 """Methods for testing the Intrinsic Dimension Estimators"""
 
 from sklearn import datasets
-
 import geomstats.backend as gs
 import geomstats.tests
 from geomstats.learning.intrinsic_dimension_estimation import LevinaBickelEstimator
@@ -9,8 +8,10 @@ from geomstats.learning.intrinsic_dimension_estimation import LevinaBickelEstima
 
 class TestIntrinsicDimensionEstimator(geomstats.tests.TestCase):
     """Test of IntrinsicDimensionEstimators."""
+
     def test_fit(self):
         """Test the fit method"""
+
         lbe = LevinaBickelEstimator()
         X = gs.random.rand(500, 3)
         dist_mat = lbe.fit(X).log_sorted_dist
@@ -20,6 +21,7 @@ class TestIntrinsicDimensionEstimator(geomstats.tests.TestCase):
 
     def test_predict(self):
         """Test the predict method."""
+
         X, _ = datasets.make_swiss_roll(n_samples=2000)
         lbe = LevinaBickelEstimator(min_neighbors=10, max_neighbors=200)
         int_dim = lbe.fit(X).predict()
