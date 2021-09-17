0b209f093d439a9c585e3294c0e2812b5f72d629
Saiteja Utpala <73220310+SaitejaUtpala@users.noreply.github.com>
2021-09-17 08:18:32 +0530

    Update intrinsic_dimension_estimation.py

--- geomstats/learning/intrinsic_dimension_estimation.py
+++ geomstats/learning/intrinsic_dimension_estimation.py
@@ -23,7 +23,7 @@ class LevinaBickelEstimator:
 
         pairwise_sub = gs.expand_dims(X, axis = 0) - gs.expand_dims(X, axis=1)
         pairwise_dist = gs.linalg.norm(pairwise_sub, axis = -1)
-        sorted_dist = gs.sort(pairwise_dist , axis = -1)
+        sorted_dist = gs.sort(pairwise_dist , axis = -1)[:,1:]
         self.log_sorted_dist = gs.log(sorted_dist)
 
 
@@ -31,7 +31,7 @@ class LevinaBickelEstimator:
         return gs.exp(self.log_sorted_dist[x-1, j-1])
 
     def predict(self):
-        num = self.sorted_dist[:, self.min_neighbors-1: self.max_neighbors]
+        num = self.log_sorted_dist[:, self.min_neighbors-1: self.max_neighbors]
         int_dim_for_each_k = []
         for k in range(self.min_neighbors , self.max_neighbors+1):
             t1 = self.log_sorted_dist[:, k-1]
