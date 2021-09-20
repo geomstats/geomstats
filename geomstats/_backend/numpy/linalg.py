bdc656d5ff272af63c5e3f97955e51e651a9c1b4
Anna Calissano <acalissa@ad.inria.fr>
2021-09-20 15:25:47 +0200

    added function to compute the rank in gs, added the test file for full rank matrices

--- geomstats/_backend/numpy/linalg.py
+++ geomstats/_backend/numpy/linalg.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import scipy.linalg
-from numpy.linalg import (
+from numpy.linalg import (  # NOQA
     cholesky,
     det,
     eig,
@@ -11,13 +11,7 @@ from numpy.linalg import (
     inv,
     matrix_rank,
     norm,
-<<<<<<< HEAD
-    solve,
-    svd,
-)  # NOQA
-=======
     svd)
->>>>>>> c600b8e4 (added function to compute the rank in gs, added the test file for full)
 
 from .common import to_ndarray
 
@@ -28,7 +22,8 @@ def _is_symmetric(x, tol=1e-12):
 
 
 def expm(x):
-    return np.vectorize(scipy.linalg.expm, signature="(n,m)->(n,m)")(x)
+    return np.vectorize(
+        scipy.linalg.expm, signature='(n,m)->(n,m)')(x)
 
 
 def logm(x):
@@ -38,14 +33,16 @@ def logm(x):
         eigvals, eigvecs = np.linalg.eigh(new_x)
         if (eigvals > 0).all():
             eigvals = np.log(eigvals)
-            eigvals = np.vectorize(np.diag, signature="(n)->(n,n)")(eigvals)
+            eigvals = np.vectorize(np.diag, signature='(n)->(n,n)')(eigvals)
             transp_eigvecs = np.transpose(eigvecs, axes=(0, 2, 1))
             result = np.matmul(eigvecs, eigvals)
             result = np.matmul(result, transp_eigvecs)
         else:
-            result = np.vectorize(scipy.linalg.logm, signature="(n,m)->(n,m)")(new_x)
+            result = np.vectorize(scipy.linalg.logm,
+                                  signature='(n,m)->(n,m)')(new_x)
     else:
-        result = np.vectorize(scipy.linalg.logm, signature="(n,m)->(n,m)")(new_x)
+        result = np.vectorize(scipy.linalg.logm,
+                              signature='(n,m)->(n,m)')(new_x)
 
     if ndim == 2:
         return result[0]
@@ -55,23 +52,26 @@ def logm(x):
 def solve_sylvester(a, b, q):
     if a.shape == b.shape:
         axes = (0, 2, 1) if a.ndim == 3 else (1, 0)
-        if np.all(a == b) and np.all(np.abs(a - np.transpose(a, axes)) < 1e-12):
+        if np.all(a == b) and np.all(
+                np.abs(a - np.transpose(a, axes)) < 1e-12):
             eigvals, eigvecs = eigh(a)
             if np.all(eigvals >= 1e-12):
                 tilde_q = np.transpose(eigvecs, axes) @ q @ eigvecs
-                tilde_x = tilde_q / (eigvals[..., :, None] + eigvals[..., None, :])
+                tilde_x = tilde_q / (
+                    eigvals[..., :, None] + eigvals[..., None, :])
                 return eigvecs @ tilde_x @ np.transpose(eigvecs, axes)
 
     return np.vectorize(
-        scipy.linalg.solve_sylvester, signature="(m,m),(n,n),(m,n)->(m,n)"
-    )(a, b, q)
+        scipy.linalg.solve_sylvester,
+        signature='(m,m),(n,n),(m,n)->(m,n)')(a, b, q)
 
 
 def sqrtm(x):
-    return np.vectorize(scipy.linalg.sqrtm, signature="(n,m)->(n,m)")(x)
+    return np.vectorize(
+        scipy.linalg.sqrtm, signature='(n,m)->(n,m)')(x)
 
 
 def qr(*args, **kwargs):
-    return np.vectorize(
-        np.linalg.qr, signature="(n,m)->(n,k),(k,m)", excluded=["mode"]
-    )(*args, **kwargs)
+    return np.vectorize(np.linalg.qr,
+                        signature='(n,m)->(n,k),(k,m)',
+                        excluded=['mode'])(*args, **kwargs)
