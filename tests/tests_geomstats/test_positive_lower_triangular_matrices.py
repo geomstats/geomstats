b2517a1e23f8f7e0c49f47fa6d7e40bf7d298551
Saiteja Utpala <saitejautpala@gmail.com>
2021-09-26 23:57:32 +0530

    tests for metric on cholesky manifold

--- tests/tests_geomstats/test_positive_lower_triangular_matrices.py
+++ tests/tests_geomstats/test_positive_lower_triangular_matrices.py
@@ -1,5 +1,7 @@
 """Unit tests for the manifold of lower triangular matrices with positive diagonal elmeents"""
 
+from geomstats.geometry.symmetric_matrices import SymmetricMatrices
+from geomstats.geometry.matrices import Matrices
 import math
 import warnings
 
@@ -11,6 +13,8 @@ from geomstats.geometry.positive_lower_triangular_matrices import (
     PositiveLowerTriangularMatrices,
     CholeskyMetric,
 )
+from geomstats.geometry.spd_matrices import SPDMatrices
+from geomstats.geometry.symmetric_matrices import SymmetricMatrices
 
 
 class TestPositiveLowerTriangularMatrices(geomstats.tests.TestCase):
@@ -25,7 +29,7 @@ class TestPositiveLowerTriangularMatrices(geomstats.tests.TestCase):
         self.n = 3
         self.space = PositiveLowerTriangularMatrices(n=self.n)
         self.metric_cholesky = CholeskyMetric(n=self.n)
-        self.n_samples = 4
+        self.n_samples = 5
 
     def test_belongs(self):
         """Test of belongs method."""
@@ -90,3 +94,91 @@ class TestPositiveLowerTriangularMatrices(geomstats.tests.TestCase):
         result = self.space.belongs(points)
         expected = gs.array([True] * 4)
         self.assertAllClose(result, expected)
+
+    def test_gram(self):
+        """Test gram method for single point"""
+        point = self.space.random_point()
+        gram = self.space.gram(point)
+
+        gram_result = gram
+        gram_expected = gs.matmul(point, Matrices.transpose(point))
+        self.assertAllClose(gram_expected, gram_result)
+
+        belongs_result = SPDMatrices(self.n).belongs(gram)
+        belongs_expected = True
+        self.assertAllClose(belongs_expected, belongs_result)
+
+    def test_gram_vectorization(self):
+        """Test gram method for batch of points"""
+        point = self.space.random_point(n_samples=5)
+        gram = self.space.gram(point)
+
+        gram_result = gram
+        gram_expected = gs.matmul(point, Matrices.transpose(point))
+        self.assertAllClose(gram_expected, gram_result)
+
+        belongs_result = gs.all(SPDMatrices(self.n).belongs(gram))
+        belongs_expected = True
+        self.assertAllClose(belongs_expected, belongs_result)
+
+    def test_differential_gram(self):
+        """Test differential of gram"""
+        base_point = self.space.random_point()
+        tangent_vec = self.space.ambient_space.random_point()
+
+        diff1 = gs.matmul(base_point, Matrices.transpose(tangent_vec))
+        diff2 = gs.matmul(tangent_vec, Matrices.transpose(base_point))
+        diff = diff1 + diff2
+
+        belongs_result = SymmetricMatrices(self.n).belongs(diff)
+        belongs_expected = True
+        self.assertAllClose(belongs_expected, belongs_result)
+
+        diff_expected = diff
+        diff_result = self.space.differential_gram(tangent_vec, base_point)
+        self.assertAllClose(diff_expected, diff_result)
+
+    def test_differential_gram_vectorization(self):
+        """Test differential of gram"""
+        base_point = self.space.random_point(self.n_samples)
+        tangent_vec = self.space.ambient_space.random_point(self.n_samples)
+
+        diff1 = gs.matmul(base_point, Matrices.transpose(tangent_vec))
+        diff2 = gs.matmul(tangent_vec, Matrices.transpose(base_point))
+        diff = diff1 + diff2
+
+        belongs_result = gs.all(SymmetricMatrices(self.n).belongs(diff))
+        belongs_expected = True
+        self.assertAllClose(belongs_expected, belongs_result)
+
+        diff_expected = diff
+        diff_result = self.space.differential_gram(tangent_vec, base_point)
+        self.assertAllClose(diff_expected, diff_result)
+
+    def test_inv_differential_gram(self):
+        """Test inverse differential of gram"""
+        pass
+
+    
+    def test_diag_inner_product(self):
+        """Test inner product on diag part"""
+        pass
+
+    def test_strictly_lower_inner_product(self):
+        """Test inner product on diag part"""
+        pass
+
+    def test_inner_product(self):
+        """Test inner product"""
+        pass
+
+    def test_exp_metric(self):
+        """Test exp map"""
+        pass
+    def test_log_metric(self):
+        """Test log map"""
+        pass
+
+    def test_squared_dist(self):
+        """Test squared dist function"""
+        pass
