ebdd7d0ab12c2b1f2189ec967d4613476894c8fe
qbarthelemy <q.barthelemy@gmail.com>
2021-11-05 19:58:43 +0100

    improve mdm

--- geomstats/learning/mdm.py
+++ geomstats/learning/mdm.py
@@ -1,11 +1,17 @@
 """The MDM classifier on manifolds."""
 
+from sklearn.metrics import accuracy_score
 import geomstats.backend as gs
 from geomstats.learning.frechet_mean import FrechetMean
 
 
 class RiemannianMinimumDistanceToMeanClassifier:
-    r"""Classifier implementing the MDM scheme on manifolds.
+    r"""Minimum Distance to Mean (MDM) classifier on manifolds.
+
+    Classification by nearest centroid. For each of the given classes, a
+    centroid is estimated according to the chosen metric. Then, for each new
+    point, the class is affected according to the nearest centroid (see
+    [BBCJ2012]_).
 
     Parameters
     ----------
@@ -16,6 +22,19 @@ class RiemannianMinimumDistanceToMeanClassifier:
     point_type : str, {\'vector\', \'matrix\'}
         Point type.
         Optional, default: \'matrix\'.
+
+    Attributes
+    ----------
+    mean_estimates_ : list
+        Centroids.
+    classes_ : list
+        Classes.
+
+    References
+    ----------
+    .. [BBCJ2012] A. Barachant, S. Bonnet, M. Congedo and C. Jutten, Multiclass
+        Brain-Computer Interface Classification by Riemannian Geometry. IEEE
+        Trans. Biomed. Eng., vol. 59, pp. 920-928, 2012.
     """
 
     def __init__(self, riemannian_metric, n_classes, point_type="matrix"):
@@ -35,15 +54,16 @@ class RiemannianMinimumDistanceToMeanClassifier:
                               if point_type='matrix'
             Training data, where n_samples is the number of samples
             and n_features is the number of features.
-        y : array-like, shape=[n_samples, n_classes]
-            Training labels, where n_classes is the number of classes.
+        y : array-like, shape=[n_samples,]
+            Training labels.
         """
+        self.classes_ = gs.unique(y)
         mean_estimator = FrechetMean(
             metric=self.riemannian_metric, point_type=self.point_type
         )
         frechet_means = []
-        for c in range(self.n_classes):
-            data_class = self.split_data_in_classes(X, y, c)
+        for c in self.classes_:
+            data_class = X[y == c]
             frechet_means.append(mean_estimator.fit(data_class).estimate_)
         self.mean_estimates_ = gs.array(frechet_means)
 
@@ -62,43 +82,39 @@ class RiemannianMinimumDistanceToMeanClassifier:
 
         Returns
         -------
-        y : array-like, shape=[n_samples, n_classes]
-            Predicted labels, where n_classes is the number of classes.
+        y : array-like, shape=[n_samples,]
+            Predicted labels.
         """
-        n_samples = X.shape[0]
-        _labels = []
-        for i in range(n_samples):
-            label = self.riemannian_metric.closest_neighbor_index(
+        y = []
+        for i in range(X.shape[0]):
+            index = self.riemannian_metric.closest_neighbor_index(
                 X[i], self.mean_estimates_
             )
-            _labels.append(label)
-        y = gs.one_hot(_labels, self.n_classes)
+            y.append(self.classes_[index])
         return y
 
-    @staticmethod
-    def split_data_in_classes(X, y, c):
-        """Split a labelled dataset in sub-datasets of each label.
+    def score(self, X, y, weights=None):
+        """Compute score on the given test data and labels.
+
+        Compute the score defined as accuracy.
 
         Parameters
         ----------
         X : array-like, shape=[n_samples, dim]
                               if point_type='vector'
-                              shape=[n_samples, n, n]
+                              shape=[n_samples, n , n]
                               if point_type='matrix'
-            Labelled dataset, where n_samples is the number of samples
+            Test data, where n_samples is the number of samples
             and n_features is the number of features.
-        y : array-like, shape=[n_samples, n_classes]
-            Labels, where n_classes is the number of classes.
-        c : int
-            Class index
+        y : array-like, shape=[n_samples,]
+            True labels for `X`.
+        weights : array-like, shape=[n_samples,]
+            Weights associated to the samples.
+            Optional, default: None.
 
         Returns
         -------
-        X_split : array-like, shape=[n_samples_in_class, dim]
-                             if point_type='vector'
-                             shape=[n_samples_in_class, n, n]
-                             if point_type='matrix'
-            Labelled dataset,
-            Where n_samples_in_class is the number of samples in class c
+        score : float
+            Mean accuracy of ``self.predict(X)`` wrt. `y`.
         """
-        return X[gs.argmax(y, axis=1) == c]
+        return accuracy_score(y, self.predict(X), sample_weight=weights)
