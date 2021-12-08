1ad4415ec37e48a1bb48b43a9cd343ef4fd6d3bd
L. F. Pereira <luisfgpereira95@gmail.com>
2021-12-08 23:56:21 +0100

    Add special linear tests

--- tests/tests_geomstats/test_special_linear.py
+++ tests/tests_geomstats/test_special_linear.py
@@ -19,28 +19,59 @@ class TestSpecialLinear(geomstats.tests.TestCase):
         warnings.simplefilter("ignore", category=ImportWarning)
 
     def test_belongs(self):
-        pass
+        matrix = gs.eye(self.n)
+        result = self.group.belongs(matrix)
+        self.assertTrue(result)
+
+        matrix = 2 * gs.eye(self.n)
+        result = self.group.belongs(matrix)
+        self.assertFalse(result)
+
+        matrix = gs.eye(self.n - 1)
+        result = self.group.belongs(matrix)
+        self.assertFalse(result)
 
     def test_belongs_vectorization(self):
-        pass
+        mats = gs.array([gs.eye(3), 2 * gs.eye(3)])
+        result = self.group.belongs(mats)
+        expected = gs.array([True, False])
+        self.assertAllClose(result, expected)
 
     def test_random_and_belongs(self):
-        pass
+        matrices = self.group.random_point(n_samples=4)
+        result = self.group.belongs(matrices)
+        self.assertTrue(gs.all(result))
 
     def test_projection_and_belongs(self):
         shape = (self.n_samples, self.n, self.n)
         result = helper.test_projection_and_belongs(self.group, shape)
-        for res in result:
-            self.assertTrue(res)
+        self.assertTrue(gs.all(result))
 
     def test_belongs_algebra(self):
-        pass
+        matrix = gs.array([[2., 0., 0.], [0., -1., 0.], [0., 0., -1]])
+        result = self.algebra.belongs(matrix)
+        self.assertTrue(result)
+
+        matrix = gs.array([[2., 0., 0.], [0., 2., 0.], [0., 0., -1]])
+        result = self.algebra.belongs(matrix)
+        self.assertFalse(result)
+
+        matrix = gs.array([[2., 0.], [0., -2.]])
+        result = self.algebra.belongs(matrix)
+        self.assertFalse(result)
 
     def test_random_and_belongs_algebra(self):
-        pass
+        matrices = self.algebra.random_point(n_samples=4)
+        result = self.algebra.belongs(matrices)
+        self.assertTrue(gs.all(result))
 
     def test_projection_and_belongs_algebra(self):
         shape = (self.n_samples, self.n, self.n)
         result = helper.test_projection_and_belongs(self.algebra, shape)
-        for res in result:
-            self.assertTrue(res)
+        self.assertTrue(gs.all(result))
+
+    def test_matrix_and_basis_representation_algebra(self):
+        matrix = self.algebra.random_point()
+        basis_repr = self.algebra.basis_representation(matrix)
+        matrix_repr = self.algebra.matrix_representation(basis_repr)
+        self.assertAllClose(matrix, matrix_repr)
