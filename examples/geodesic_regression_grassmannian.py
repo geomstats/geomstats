fe6c37ec1302e91ff072963b11d453278dc6982e
nguigs <nicolas.guigui@inria.fr>
2021-12-13 17:42:31 +0100

    refactor to match gs examples style

--- examples/geodesic_regression_grassmannian.py
+++ examples/geodesic_regression_grassmannian.py
@@ -1,58 +1,77 @@
-import os
+r"""Compute a geodesic regression on Grassmann manifold (2, 3).
 
-os.environ["GEOMSTATS_BACKEND"] = "tensorflow"
+The generative model of the data is:
+:math:`Z = Exp_{\beta_0}(\beta_1.X)` and :math:`Y = Exp_Z(\epsilon)`
+where:
+- :math:`Exp` denotes the Riemannian exponential,
+- :math:`\beta_0` is called the intercept,
+- :math:`\beta_1` is called the coefficient,
+- :math:`\epsilon \sim N(0, 1)` is a standard Gaussian noise,
+- :math:`X` is called the input, :math:`Y` is called the y.
+"""
 
 import geomstats.backend as gs
 from geomstats.geometry.grassmannian import GeneralLinear, Grassmannian
-from geomstats.learning.frechet_mean import FrechetMean, variance
 from geomstats.learning.geodesic_regression import GeodesicRegression
 
-space = Grassmannian(3, 2)
-metric = space.metric
+SPACE = Grassmannian(3, 2)
+METRIC = SPACE.metric
 gs.random.seed(0)
-p_xy = gs.array([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 0.0]])
 
-# Generate data
-n_samples = 10
-data = gs.random.rand(n_samples)
-data -= gs.mean(data)
 
-intercept = space.random_uniform()
-beta = space.to_tangent(GeneralLinear(3).random_point(), intercept)
-target = metric.exp(
-    tangent_vec=gs.einsum("...,jk->...jk", data, beta), base_point=intercept
-)
+def main():
+    r"""Compute a geodesic regression on Grassmann manifold (2, 3).
 
-# True variance
-estimator = FrechetMean(metric)
-estimator.fit(target)
-variance_ = variance(target, estimator.estimate_, metric=metric)
+    The generative model of the data is:
+    :math:`Z = Exp_{\beta_0}(\beta_1.X)` and :math:`Y = Exp_Z(\epsilon)`
+    where:
+    - :math:`Exp` denotes the Riemannian exponential,
+    - :math:`\beta_0` is called the intercept,
+    - :math:`\beta_1` is called the coefficient,
+    - :math:`\epsilon \sim N(0, 1)` is a standard Gaussian noise,
+    - :math:`X` is called the input, :math:`Y` is called the y.
+    """
+    # Generate data
+    n_samples = 10
+    data = gs.random.rand(n_samples)
+    data -= gs.mean(data)
 
-gr = GeodesicRegression(
-    space,
-    metric=metric,
-    center_X=False,
-    method="riemannian",
-    verbose=True,
-    max_iter=50,
-    learning_rate=0.1,
-)
-gr.fit(data, target, compute_training_score=True)
-intercept_hat, beta_hat = gr.intercept_, gr.coef_
-
-# Measure Mean Squared Error
-mse_intercept = metric.squared_dist(intercept_hat, intercept)
-mse_beta = metric.squared_norm(
-    metric.parallel_transport(
-        beta_hat, metric.log(intercept_hat, intercept), intercept_hat
+    intercept = SPACE.random_uniform()
+    beta = SPACE.to_tangent(GeneralLinear(3).random_point(), intercept)
+    target = METRIC.exp(
+        tangent_vec=gs.einsum("...,jk->...jk", data, beta), base_point=intercept
     )
-    - beta,
-    intercept,
-)
 
-# Measure goodness of fit
-r2_hat = gr.training_score_
+    gr = GeodesicRegression(
+        SPACE,
+        metric=METRIC,
+        center_X=False,
+        method="riemannian",
+        verbose=True,
+        max_iter=50,
+        learning_rate=0.1,
+    )
 
-print(f"MSE on the intercept: {mse_intercept:.2e}")
-print(f"MSE on the initial velocity beta: {mse_beta:.2e}")
-print(f"Determination coefficient: R^2={r2_hat:.2f}")
+    gr.fit(data, target, compute_training_score=True)
+    intercept_hat, beta_hat = gr.intercept_, gr.coef_
+
+    # Measure Mean Squared Error
+    mse_intercept = METRIC.squared_dist(intercept_hat, intercept)
+    mse_beta = METRIC.squared_norm(
+        METRIC.parallel_transport(
+            beta_hat, METRIC.log(intercept_hat, intercept), intercept_hat
+        )
+        - beta,
+        intercept,
+    )
+
+    # Measure goodness of fit
+    r2_hat = gr.training_score_
+
+    print(f"MSE on the intercept: {mse_intercept:.2e}")
+    print(f"MSE on the initial velocity beta: {mse_beta:.2e}")
+    print(f"Determination coefficient: R^2={r2_hat:.2f}")
+
+
+if __name__ == "__main__":
+    main()
