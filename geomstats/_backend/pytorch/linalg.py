66e168bf3842a11540235abcb1fbfc0e490241cb
Elodie Maignant <elodie.maignant@orange.fr>
2021-12-14 17:58:50 +0100

    add new subcase in solve_sylvester when q is a skew matrix and a is symmetric positive of rank m-1

--- geomstats/_backend/pytorch/linalg.py
+++ geomstats/_backend/pytorch/linalg.py
@@ -89,12 +89,12 @@ def solve_sylvester(a, b, q):
                 tilde_q = eigvecs.transpose(-2, -1) @ q @ eigvecs
                 tilde_x = tilde_q / (eigvals[..., :, None] + eigvals[..., None, :])
                 return eigvecs @ tilde_x @ eigvecs.transpose(-2, -1)
-            if eigvals.shape[-1] >= 2. and torch.all(eigvals[..., 0] > - 1e-6) \
+            if torch.all(eigvals.shape[..., -1]) >= 2. and torch.all(eigvals[..., 0] > - 1e-6) \
                     and torch.all(eigvals[..., 1] >= 1e-6) \
                     and torch.all(torch.abs(q + q.transpose(-2, -1)) < 1e-6):
                 tilde_q = eigvecs.transpose(-2, -1) @ q @ eigvecs
                 tilde_x = tilde_q / (eigvals[..., :, None] + eigvals[..., None, :]
-                                     + torch.eye(len(eigvals)))
+                                     + torch.eye(eigvals.shape[..., -1]))
                 return eigvecs @ tilde_x @ eigvecs.transpose(-2, -1)
 
     solution = np.vectorize(
