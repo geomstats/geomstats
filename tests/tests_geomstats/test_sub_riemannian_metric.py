d33b3b4e3a24c5281c6ab6a9df21eddd20f4dc97
mortenapedersen <morten.akhoej@gmail.com>
2021-12-16 17:06:32 +0100

    add setup_method

--- tests/tests_geomstats/test_sub_riemannian_metric.py
+++ tests/tests_geomstats/test_sub_riemannian_metric.py
@@ -8,10 +8,8 @@ from geomstats.geometry.sub_riemannian_metric import SubRiemannianMetric
 
 
 class TestSubRiemannianMetric(geomstats.tests.TestCase):
-    def setUp(self):
+    def set_method(self):
         warnings.simplefilter("ignore", category=UserWarning)
-        self.dim = 3
-        self.dist_dim = 2
 
         new_sub_riemannian_metric = SubRiemannianMetric(dim=self.dim,
                                                         dist_dim=self.dist_dim)
