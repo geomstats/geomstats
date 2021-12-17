56d74be10b8be34e0b2bc9fd562f9a375e1577d5
Nina Miolane <nmiolane@gmail.com>
2021-12-17 19:00:27 +0100

    Ignore numpy

--- tests/tests_geomstats/test_sub_riemannian_metric.py
+++ tests/tests_geomstats/test_sub_riemannian_metric.py
@@ -14,9 +14,7 @@ class ExampleMetric(SubRiemannianMetric):
         )
 
     def cometric_matrix(self, base_point=None):
-        return gs.array([[1., 0., 0.],
-                         [0., 1.,  0.],
-                         [0., 0., 1.]])
+        return gs.array([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
 
 
 class TestSubRiemannianMetric(geomstats.tests.TestCase):
@@ -26,65 +24,78 @@ class TestSubRiemannianMetric(geomstats.tests.TestCase):
         self.example_metric = ExampleMetric(dim=3, dist_dim=2)
 
     def test_inner_coproduct(self):
-        cotangent_vec_a = gs.array([1., 1., 1.])
-        cotangent_vec_b = gs.array([1., 10., 1.])
-        base_point = gs.array([2., 1., 10.])
+        cotangent_vec_a = gs.array([1.0, 1.0, 1.0])
+        cotangent_vec_b = gs.array([1.0, 10.0, 1.0])
+        base_point = gs.array([2.0, 1.0, 10.0])
 
         result = self.example_metric.inner_coproduct(
-            cotangent_vec_a, cotangent_vec_b, base_point)
-        expected = gs.array(12.)
+            cotangent_vec_a, cotangent_vec_b, base_point
+        )
+        expected = gs.array(12.0)
         self.assertAllClose(result, expected)
 
+    @geomstats.tests.autograd_tf_and_torch_only
     def test_hamiltonian(self):
-        cotangent_vec = gs.array([1., 1., 1.])
-        base_point = gs.array([2., 1., 10.])
+        cotangent_vec = gs.array([1.0, 1.0, 1.0])
+        base_point = gs.array([2.0, 1.0, 10.0])
         state = gs.array([base_point, cotangent_vec])
 
         result = self.example_metric.hamiltonian(state)
         expected = gs.array([1.5])
         self.assertAllClose(result, expected)
 
+    @geomstats.tests.autograd_tf_and_torch_only
     def test_symp_grad(self):
-        test_state = gs.array([[1., 1., 1.], [2., 3., 4.]])
-        result = self.example_metric.symp_grad(self.example_metric.hamiltonian)(test_state)
-        expected = gs.array([[2.,  3.,  4.], [-0., -0., -0.]])
+        test_state = gs.array([[1.0, 1.0, 1.0], [2.0, 3.0, 4.0]])
+        result = self.example_metric.symp_grad(self.example_metric.hamiltonian)(
+            test_state
+        )
+        expected = gs.array([[2.0, 3.0, 4.0], [-0.0, -0.0, -0.0]])
         self.assertAllClose(result, expected)
 
+    @geomstats.tests.autograd_tf_and_torch_only
     def test_symp_euler(self):
-        test_state = gs.array([[1., 1., 1.], [2., 3., 4.]])
+        test_state = gs.array([[1.0, 1.0, 1.0], [2.0, 3.0, 4.0]])
         step_size = 0.01
-        result = self.example_metric.symp_euler(self.example_metric.hamiltonian, step_size)(test_state)
-        expected = gs.array([[1.02, 1.03, 1.04], [2., 3., 4.]])
+        result = self.example_metric.symp_euler(
+            self.example_metric.hamiltonian, step_size
+        )(test_state)
+        expected = gs.array([[1.02, 1.03, 1.04], [2.0, 3.0, 4.0]])
         self.assertAllClose(result, expected)
 
     def test_iterate(self):
-        test_state = gs.array([[1., 1., 1.], [2., 3., 4.]])
+        test_state = gs.array([[1.0, 1.0, 1.0], [2.0, 3.0, 4.0]])
         n_steps = 20
         step_size = 0.01
 
         step = self.example_metric.symp_euler
 
-        result = self.example_metric.iterate(step(self.example_metric.hamiltonian, step_size), n_steps)(test_state)[-10]
-        expected = gs.array([[ 1.22,  1.33, 1.44], [ 2.,  3.,  4.]])
+        result = self.example_metric.iterate(
+            step(self.example_metric.hamiltonian, step_size), n_steps
+        )(test_state)[-10]
+        expected = gs.array([[1.22, 1.33, 1.44], [2.0, 3.0, 4.0]])
         self.assertAllClose(result, expected)
 
+    @geomstats.tests.autograd_tf_and_torch_only
     def test_symp_flow(self):
-        test_state = gs.array([[1., 1., 1.], [2., 3., 4.]])
+        test_state = gs.array([[1.0, 1.0, 1.0], [2.0, 3.0, 4.0]])
         end_time = 1.0
         n_steps = 20
 
         step = self.example_metric.symp_euler
 
-        result = self.example_metric.symp_flow(self.example_metric.hamiltonian, end_time, n_steps)(test_state)[-10]
-        expected = gs.array([[2.1, 2.65, 3.2], [2., 3., 4.]])
+        result = self.example_metric.symp_flow(
+            self.example_metric.hamiltonian, end_time, n_steps
+        )(test_state)[-10]
+        expected = gs.array([[2.1, 2.65, 3.2], [2.0, 3.0, 4.0]])
         self.assertAllClose(result, expected)
 
+    @geomstats.tests.autograd_tf_and_torch_only
     def test_exp(self):
-        cotangent_vec = gs.array([1., 1., 1.])
-        base_point = gs.array([2., 1., 10.])
+        cotangent_vec = gs.array([1.0, 1.0, 1.0])
+        base_point = gs.array([2.0, 1.0, 10.0])
         N_STEPS = 20
 
-        result = self.example_metric.exp(
-            cotangent_vec, base_point, n_steps=N_STEPS)
+        result = self.example_metric.exp(cotangent_vec, base_point, n_steps=N_STEPS)
         expected = base_point + cotangent_vec
         self.assertAllClose(result, expected)
