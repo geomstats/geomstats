7d771cdeb0cdebd859ce26bdb8a00c5113fd7cee
Nina Miolane <nmiolane@gmail.com>
2021-12-17 16:04:23 +0100

    Add first imports

--- geomstats/geometry/__init__.py
+++ geomstats/geometry/__init__.py
@@ -1,1 +1,11 @@
 """The Geometry Package."""
+from .base import LevelSet, OpenSet, VectorSpace
+from .connection import Connection
+from .discrete_curves import (
+    ClosedDiscreteCurves,
+    ClosedSRVMetric,
+    DiscreteCurves,
+    QuotientSRVMetric,
+    SRVMetric,
+)
+from .matrices import Matrices
