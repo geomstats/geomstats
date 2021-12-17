17e5427b2eed984a98db2ce1a0e6c179836849bc
mortenapedersen <morten.akhoej@gmail.com>
2021-12-17 16:58:05 +0100

    remove uncommented regions and various

--- geomstats/geometry/sub_riemannian_metric.py
+++ geomstats/geometry/sub_riemannian_metric.py
@@ -28,68 +28,70 @@ class SubRiemannianMetric(abc.ABC):
         self.dist_dim = dist_dim
         self.default_point_type = default_point_type
 
-    # def metric_matrix(self, base_point):
-    #     """Metric matrix at the tangent space at a base point.
+    def metric_matrix(self, base_point):
+        """Metric matrix at the tangent space at a base point.
 
-    #     This is a sub-Riemannian metric, so it is assumed to satisfy the conditions
-    #     of an inner product only on each distribution subspace.
+        This is a sub-Riemannian metric, so it is assumed to satisfy the conditions
+        of an inner product only on each distribution subspace.
 
-    #     Parameters
-    #     ----------
-    #     base_point : array-like, shape=[..., dim]
-    #         Base point.
-    #         Optional, default: None.
+        Parameters
+        ----------
+        base_point : array-like, shape=[..., dim]
+            Base point.
+            Optional, default: None.
 
-    #     Returns
-    #     -------
-    #     _ : array-like, shape=[..., dim, dim]
-    #         Inner-product matrix.
-    #     """
-    #     raise NotImplementedError(
-    #         "The computation of the metric matrix" " is not implemented."
-    #     )
+        Returns
+        -------
+        _ : array-like, shape=[..., dim, dim]
+            Inner-product matrix.
+        """
+        raise NotImplementedError(
+            "The computation of the metric matrix" " is not implemented."
+        )
 
-    # @abc.abstractmethod
-    # def frame(self, point):
-    #     """Frame field for the distribution.
+    def frame(self, point):
+        """Frame field for the distribution.
 
-    #     The frame field spans the distribution at 'point'.The frame field is
-    #     represented as a matrix, whose columns are the frame field vectors.
+        The frame field spans the distribution at 'point'.The frame field is
+        represented as a matrix, whose columns are the frame field vectors.
 
-    #     Parameters
-    #     ----------
-    #     point : array-like, shape=[..., dim]
-    #         point.
-    #         Optional, default: None.
+        Parameters
+        ----------
+        point : array-like, shape=[..., dim]
+            point.
+            Optional, default: None.
 
-    #     Returns
-    #     -------
-    #     _ : array-like, shape=[..., dim, dist_dim]
-    #         Frame field matrix.
-    #     """
+        Returns
+        -------
+        _ : array-like, shape=[..., dim, dist_dim]
+            Frame field matrix.
+        """
+        raise NotImplementedError(
+            "The frame field" " is not implemented."
+        )
 
-    # def cometric_sub_matrix(self, basepoint):
-    #     """Cometric  sub matrix of dimension dist_dim x dist_dim.
+    def cometric_sub_matrix(self, basepoint):
+        """Cometric  sub matrix of dimension dist_dim x dist_dim.
 
-    #     Let {X_i}, i = 1, .., dist_dim, be an arbitrary frame for the distribution
-    #     and let g be the sub-Riemannian metric. Then cometric_sub_matrix is the
-    #     matrix given by the inverse of the matrix g_ij = g(X_i, X_j),
-    #     where i,j = 1, .., dist_dim.
+        Let {X_i}, i = 1, .., dist_dim, be an arbitrary frame for the distribution
+        and let g be the sub-Riemannian metric. Then cometric_sub_matrix is the
+        matrix given by the inverse of the matrix g_ij = g(X_i, X_j),
+        where i,j = 1, .., dist_dim.
 
-    #     Parameters
-    #     ----------
-    #     base_point : array-like, shape=[..., dim]
-    #         Base point.
-    #         Optional, default: None.
+        Parameters
+        ----------
+        base_point : array-like, shape=[..., dim]
+            Base point.
+            Optional, default: None.
 
-    #     Returns
-    #     -------
-    #     _ : array-like, shape=[..., dist_dim, dist_dim]
-    #         Cometric submatrix.
-    #     """
-    #     raise NotImplementedError(
-    #         "The computation of the cometric submatrix" " is not implemented."
-    #     )
+        Returns
+        -------
+        _ : array-like, shape=[..., dist_dim, dist_dim]
+            Cometric submatrix.
+        """
+        raise NotImplementedError(
+            "The computation of the cometric submatrix" " is not implemented."
+        )
 
     @abc.abstractmethod
     def cometric_matrix(self, base_point=None):
