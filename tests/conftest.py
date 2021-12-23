350dee998aa27b3032a16d68eb0d0a0ee84016be
Saiteja Utpala <saitejautpala@gmail.com>
2021-12-23 09:28:33 +0530

    change func signature

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ smoke = pytest.mark.smoke
 random = pytest.mark.rt
 
 
-def generate_tests(smoke_test_data, random_test_data=None):
+def generate_tests(smoke_test_data, random_test_data=[]):
     smoke_tests = [pytest.param(*data, marks=smoke) for data in smoke_test_data]
     random_tests = [pytest.param(*data, marks=random) for data in random_test_data]
     return smoke_tests + random_tests
