fddef3f82f3859c2f2156149017c11a73b25c8e3
Saiteja Utpala <saitejautpala@gmail.com>
2021-12-28 08:28:06 -0800

    move testing data to inner class

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,16 +17,14 @@ def generate_tests(smoke_test_data, random_test_data=[]):
 
 class Parametrizer(type):
     def __new__(cls, name, bases, attrs):
-        print(locals())
         for attr_name, attr_value in attrs.items():
             if isinstance(attr_value, types.FunctionType):
-                print("attr_name", attr_name)
-                print("attr_value", attr_value)
                 if attr_name.startswith("test_"):
                     args_str = ", ".join(inspect.getfullargspec(attr_value)[0][1:])
                     data_fn_str = attr_name[5:] + "_data"
                     attrs[attr_name] = pytest.mark.parametrize(
-                        args_str, locals()[data_fn_str]()
+                        args_str,
+                        getattr(locals()["attrs"]["testing_data"], data_fn_str)(),
                     )(attr_value)
 
         return super(Parametrizer, cls).__new__(cls, name, bases, attrs)
