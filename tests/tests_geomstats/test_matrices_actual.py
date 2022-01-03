3561e5efebdbb73c171ccc3c025f9b422743cd46
Saiteja Utpala <saitejautpala@gmail.com>
2022-01-02 20:22:47 -0800

    add

--- tests/tests_geomstats/test_matrices_actual.py
+++ tests/tests_geomstats/test_matrices_actual.py
@@ -7,6 +7,9 @@ from tests.conftest import Parametrizer, TestCase, TestData
 EYE_2 = [[1.0, 0], [0.0, 1.0]]
 EYE_3 = [[1.0, 0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
 MAT_2_3 = [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]
+MAT1_33 = [[1.0, 2.0, 3.0], [2.0, 4.0, 5.0], [3.0, 5.0, 6.0]]
+MAT2_33 = [[1.0, 2.0, 3.0], [2.0, 4.0, 7.0], [3.0, 5.0, 6.0]]
+MAT3_33 = [[0.0, 1.0, -2.0], [-1.0, 0.0, -3.0], [2.0, 3.0, 0.0]]
 
 
 class TestMatrices(TestCase, metaclass=Parametrizer):
@@ -45,19 +48,57 @@ class TestMatrices(TestCase, metaclass=Parametrizer):
                 [[-1.0, 2.0], [-3.0, 4.0]],
                 [[1.0, -2.0], [3.0, -4.0]],
             )
-
             mats_2 = [[[2.0], [4.0]], [[1.0], [3.0]], [[1.0], [3.0]]]
             mat_1_X_mat_2 = [[[10.0], [22.0]], [[5.0], [9.0]], [[-5.0], [-9.0]]]
             smoke_data = [
                 dict(mat=mats_1, expected=[[23.0, -26.0], [51.0, -58.0]]),
                 dict(mat=(list(mats_1), mats_2), expected=mat_1_X_mat_2),
             ]
-
             return self.generate_tests(smoke_data)
 
         def commutator_data(self):
             smoke_data = []
             return self.generate_tests(smoke_data)
+
+        def is_symmetric_data(self):
+            smoke_data = [
+                dict(mat=EYE_2, expected=True),
+                dict(mat=[EYE_2, EYE_2 + 1], expected=[True, True]),
+                dict(mat=[MAT1_33, MAT2_33, MAT3_33], expected=[True, False, True]),
+            ]
+            self.generate_tests(smoke_data)
+
+        def is_skew_symmetric_data(self):
+            smoke_data = [
+                dict(mat=EYE_2, expected=False),
+                dict(mat=[EYE_2, -1 * EYE_2], expected=[False, False]),
+                dict(mat=[MAT2_33, MAT3_33], expected=[False, True]),
+            ]
+            self.generate_tests(smoke_data)
+
+        def is_pd(self):
+            smoke_data = []
+            self.generate_tests(smoke_data)
+
+        def is_spd(self):
+            smoke_data = []
+            self.generate_tests(smoke_data)
+
+        def is_upper_triangualr(self):
+            smoke_data = []
+            self.generate_tests(smoke_data)
+
+        def is_lower_triangular(self):
+            smoke_data = []
+            self.generate_tests(smoke_data)
+
+        def is_strictly_lower_triangular(self):
+            smoke_data = []
+            self.generate_tests(smoke_data)
+
+        def is_strictly_upper_triangular(self):
+            smoke_data = []
+            self.generate_tests(smoke_data)
 
     def test_belongs(self, m, n, mat, expected):
         self.assertAllClose(Matrices(m, n).belongs(gs.array(mat)), gs.array(expected))
@@ -72,3 +113,37 @@ class TestMatrices(TestCase, metaclass=Parametrizer):
 
     def test_commutator(self, mat_a, mat_b, expected):
         self.assertAllClose(Matrices.commutator(mat_a, mat_b), expected)
+
+    def is_symmetric_data(self):
+        pass
+
+    def is_skew_symmetric_data(self):
+        smoke_data = []
+        self.generate_tests(smoke_data)
+
+    def is_pd(self):
+        smoke_data = []
+        self.generate_tests(smoke_data)
+
+    def is_spd(self):
+        smoke_data = []
+        self.generate_tests(smoke_data)
+
+    def is_upper_triangualr(self):
+        smoke_data = []
+        self.generate_tests(smoke_data)
+
+    def is_lower_triangular(self):
+        smoke_data = []
+        self.generate_tests(smoke_data)
+
+    def is_strictly_lower_triangular(self):
+        smoke_data = []
+        self.generate_tests(smoke_data)
+
+    def is_strictly_upper_triangular(self):
+        smoke_data = []
+        self.generate_tests(smoke_data)
+
+    def test_to_matrixtype_belongs(self, matrix_type, points):
+        self.assertAllClose(Matrices.matrix_type(points))
