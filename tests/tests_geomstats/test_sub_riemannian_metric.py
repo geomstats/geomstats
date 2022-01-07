5567cdb4e2dd82a7e4c82b755847bb2671985f1f
Saiteja Utpala <saitejautpala@gmail.com>
2022-01-07 12:41:50 -0800

    fix flake 8 errors

--- tests/tests_geomstats/test_sub_riemannian_metric.py
+++ tests/tests_geomstats/test_sub_riemannian_metric.py
@@ -2,7 +2,6 @@
 
 import warnings
 
-import geomstats.backend as gs
 import geomstats.tests
 from geomstats.geometry.sub_riemannian_metric import SubRiemannianMetric
 
@@ -10,6 +9,3 @@ from geomstats.geometry.sub_riemannian_metric import SubRiemannianMetric
 class TestSubRiemannianMetric(geomstats.tests.TestCase):
     def setup_method(self):
         warnings.simplefilter("ignore", category=UserWarning)
-
-        new_sub_riemannian_metric = SubRiemannianMetric(dim=self.dim,
-                                                        dist_dim=self.dist_dim)
