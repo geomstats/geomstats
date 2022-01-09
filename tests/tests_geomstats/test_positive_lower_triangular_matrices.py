92d169ad33311f5cd0724288980d8c7bed7f4913
Saiteja Utpala <saitejautpala@gmail.com>
2022-01-08 23:53:02 -0800

    add PLT

--- tests/tests_geomstats/test_positive_lower_triangular_matrices.py
+++ tests/tests_geomstats/test_positive_lower_triangular_matrices.py
@@ -2,11 +2,8 @@
 
 
 import math
-import warnings
 
 import geomstats.backend as gs
-import geomstats.tests
-import tests.helper as helper
 from geomstats.geometry.matrices import Matrices
 from geomstats.geometry.positive_lower_triangular_matrices import (
     CholeskyMetric,
@@ -14,98 +11,107 @@ from geomstats.geometry.positive_lower_triangular_matrices import (
 )
 from geomstats.geometry.spd_matrices import SPDMatrices
 from geomstats.geometry.symmetric_matrices import SymmetricMatrices
+from tests.conftest import Parametrizer, TestCase, TestData
 
 EULER = gs.exp(1.0)
 SQRT_2 = math.sqrt(2)
 
 
-class TestPositiveLowerTriangularMatrices(geomstats.tests.TestCase):
+class TestPositiveLowerTriangularMatrices(TestCase, metaclass=Parametrizer):
     """Test of Cholesky methods."""
 
-    def setup_method(self):
-        """Set up the test."""
-        warnings.simplefilter("ignore", category=ImportWarning)
+    space = PositiveLowerTriangularMatrices
 
-        gs.random.seed(1234)
-
-        self.n = 2
-        self.space = PositiveLowerTriangularMatrices(n=self.n)
-        self.metric_cholesky = CholeskyMetric(n=self.n)
-        self.n_samples = 5
-
-    def test_belongs(self):
-        """Test of belongs method."""
-        mats = gs.array([[1.0, 0.0], [-1.0, 3.0]])
-        result = self.space.belongs(mats)
-        expected = True
-        self.assertAllClose(result, expected)
-
-        mats = gs.array([[1.0, -1.0], [-1.0, 3.0]])
-        result = self.space.belongs(mats)
-        expected = False
-        self.assertAllClose(result, expected)
-
-        mats = gs.array([[-1.0, 0.0], [-1.0, 3.0]])
-        result = self.space.belongs(mats)
-        expected = False
-        self.assertAllClose(result, expected)
-
-        mats = gs.eye(3)
-        result = self.space.belongs(mats)
-        expected = False
-        self.assertAllClose(result, expected)
-
-    def test_belongs_vectorization(self):
-        """Test of belongs method."""
-        mats_2dim = gs.array(
-            [
-                [[1.0, 0], [0, 1.0]],
-                [[1.0, 2.0], [2.0, 1.0]],
-                [[-1.0, 0.0], [1.0, 1.0]],
-                [[0.0, 0.0], [1.0, 1.0]],
+    class TestDataPositiveLowerTriangularMatrices(TestData):
+        def belongs_data(self):
+            smoke_data = [
+                dict(n=2, mat=[[1.0, 0.0], [-1.0, 3.0]], expected=True),
+                dict(n=2, mat=[[1.0, -1.0], [-1.0, 3.0]], expected=False),
+                dict(n=2, mat=[[-1.0, 0.0], [-1.0, 3.0]], expected=False),
+                dict(n=3, mat=[[1.0, 0], [0, 1.0]], expected=False),
+                dict(
+                    n=2,
+                    mat=[
+                        [[1.0, 0], [0, 1.0]],
+                        [[1.0, 2.0], [2.0, 1.0]],
+                        [[-1.0, 0.0], [1.0, 1.0]],
+                        [[0.0, 0.0], [1.0, 1.0]],
+                    ],
+                    expected=[True, False, False, False],
+                ),
+                dict(
+                    n=3,
+                    mat=[
+                        [[1.0, 0.0, 1.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]],
+                        [[0.0, 0.0, 0.0], [2.0, 1.0, 0.0], [0.0, 0.0, 1.0]],
+                        [[1.0, 0.0, 0.0], [2.0, 1.0, 0.0], [0.0, 0.0, 1.0]],
+                        [[-1.0, 0.0, 0.0], [2.0, 1.0, 0.0], [0.0, 0.0, 1.0]],
+                    ],
+                    expected=[False, False, True, False],
+                ),
             ]
+            return self.belongs_data(smoke_data)
+
+        def random_point_and_belongs_data(self):
+            smoke_data = [
+                dict(n=1, n_points=1),
+                dict(n=2, n_points=2),
+                dict(n=10, n_points=100),
+                dict(n=100, n_points=10),
+            ]
+            return self.generate_tests(smoke_data)
+
+        def gram_data(self):
+            smoke_data = [
+                dict(
+                    n=2,
+                    point=[[1.0, 0.0], [2.0, 1.0]],
+                    expected=[[1.0, 2.0], [2.0, 5.0]],
+                )
+            ]
+            return self.generate_tests(smoke_data)
+
+        def differential_gram_data(self):
+            smoke_data = [
+                dict(
+                    n=2,
+                    tangent_vec=[[-1.0, 0.0], [2.0, -1.0]],
+                    base_point=[[1.0, 0.0], [2.0, 1.0]],
+                )
+            ]
+            return self.generate_tests(smoke_data)
+
+        def inverse_differential_gram_data(self):
+            smoke_data = [
+                dict(
+                    n=2,
+                    tangent_vec=[[1.0, 2.0], [2.0, 5.0]],
+                    base_point=[[1.0, 0.0], [2.0, 2.0]],
+                    expected=[[0.5, 0.0], [1.0, 0.25]],
+                )
+            ]
+            return self.generate_tests(smoke_data)
+
+    def test_belongs(self, n, mat, expected):
+        self.assertAllClose(self.space(n).belongs(gs.array(mat)), gs.array(expected))
+
+    def test_random_point_and_belongs(self, n, n_points):
+        self.assertAllClose(gs.all(self.space(n).random_point(n_points)), True)
+
+    def test_gram(self, n, point, expected):
+        self.assertAllClose(self.space(n).gram(gs.array(point), gs.array(expected)))
+
+    def test_differential_gram(self, n, tangent_vec, base_point):
+        self.assertAllClose(
+            self.space(n).differential_gram(gs.array(tangent_vec), gs.array(base_point))
         )
 
-        mats_3dim = gs.array(
-            [
-                [[1.0, 0.0, 1.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]],
-                [[0.0, 0.0, 0.0], [2.0, 1.0, 0.0], [0.0, 0.0, 1.0]],
-                [[1.0, 0.0, 0.0], [2.0, 1.0, 0.0], [0.0, 0.0, 1.0]],
-                [[-1.0, 0.0, 0.0], [2.0, 1.0, 0.0], [0.0, 0.0, 1.0]],
-            ]
+    def test_inverse_differential_gram(self, n, tangent_vec, base_point):
+        self.assertAllClose(
+            self.space(n).inverse_differential_gram(tangent_vec, base_point)
         )
-
-        result = self.space.belongs(mats_2dim)
-        expected = gs.array([True, False, False, False])
-        self.assertAllClose(result, expected)
-
-        result = PositiveLowerTriangularMatrices(3).belongs(mats_3dim)
-        expected = gs.array([False, False, True, False])
-        self.assertAllClose(result, expected)
-
-    def test_random_point_and_belongs(self):
-        """Test of random_point and belongs methods."""
-        point = self.space.random_point()
-        result = self.space.belongs(point)
-        expected = True
-        self.assertAllClose(result, expected)
-
-    def test_random_point_and_belongs_vectorization(self):
-        """Test of random_point and belongs methods."""
-        points = self.space.random_point(4)
-        result = self.space.belongs(points)
-        expected = gs.array([True] * 4)
-        self.assertAllClose(result, expected)
-
-    def test_gram(self):
-        """Test gram method for single point"""
-        L = gs.array([[1.0, 0.0], [2.0, 1.0]])
-        gram_expected = gs.array([[1.0, 2.0], [2.0, 5.0]])
-        gram_result = self.space.gram(L)
-        self.assertAllClose(gram_expected, gram_result)
 
     def test_gram_vectorization(self):
-        """Test gram method for batch of points"""
         point = self.space.random_point(n_samples=5)
         gram = self.space.gram(point)
 
@@ -117,16 +123,7 @@ class TestPositiveLowerTriangularMatrices(geomstats.tests.TestCase):
         belongs_expected = True
         self.assertAllClose(belongs_expected, belongs_result)
 
-    def test_differential_gram(self):
-        """Test differential of gram for single point"""
-        ell = gs.array([[1.0, 0.0], [2.0, 1.0]])
-        ex = gs.array([[-1.0, 0.0], [2.0, -1.0]])
-        diff_gram_result = self.space.differential_gram(ex, ell)
-        diff_gram_expected = gs.array([[-2.0, 0.0], [0.0, 6.0]])
-        self.assertAllClose(diff_gram_expected, diff_gram_result)
-
     def test_differential_gram_vectorization(self):
-        """Test differential of gram"""
         base_point = self.space.random_point(self.n_samples)
         tangent_vec = self.space.ambient_space.random_point(self.n_samples)
 
@@ -142,16 +139,7 @@ class TestPositiveLowerTriangularMatrices(geomstats.tests.TestCase):
         diff_result = self.space.differential_gram(tangent_vec, base_point)
         self.assertAllClose(diff_expected, diff_result)
 
-    def test_inv_differential_gram(self):
-        """Test inverse differential of gram"""
-        L = gs.array([[1.0, 0.0], [2.0, 2.0]])
-        W = gs.array([[1.0, 2.0], [2.0, 5.0]])
-        inv_diff_gram_expected = gs.array([[0.5, 0.0], [1.0, 0.25]])
-        inv_diff_gram_result = self.space.inverse_differential_gram(W, L)
-        self.assertAllClose(inv_diff_gram_expected, inv_diff_gram_result)
-
     def test_inv_differential_gram_belongs(self):
-        """Test inverse differential of gram belongs"""
         L = self.space.random_point(5)
         W = SymmetricMatrices(2).random_point(5)
         inv_diff_gram_result = self.space.inverse_differential_gram(W, L)
@@ -159,64 +147,138 @@ class TestPositiveLowerTriangularMatrices(geomstats.tests.TestCase):
         belongs_expected = True
         self.assertAllClose(belongs_result, belongs_expected)
 
-    def test_diag_inner_product(self):
-        """Test inner product on diag part"""
-        X = gs.array([[1.0, 0.0], [-2.0, -1.0]])
-        Y = gs.array([[2.0, 0.0], [-3.0, -1.0]])
-        L = gs.array([[SQRT_2, 0.0], [-3.0, 1.0]])
-        dip_result = self.metric_cholesky.diag_inner_product(X, Y, L)
-        dip_expected = 2
-        self.assertAllClose(dip_expected, dip_result)
 
-    def test_strictly_lower_inner_product(self):
-        """Test inner product on diag part"""
-        X = gs.array([[1.0, 0.0], [-2.0, -1.0]])
-        Y = gs.array([[2.0, 0.0], [-3.0, -1.0]])
-        dip_result = self.metric_cholesky.strictly_lower_inner_product(X, Y)
-        dip_expected = 6
-        self.assertAllClose(dip_expected, dip_result)
+class TestCholeskyMetric(TestCase):
+    cls = CholeskyMetric
 
-    def test_inner_product(self):
-        """Test inner product"""
-        X = gs.array([[1.0, 0.0], [-2.0, -1.0]])
-        Y = gs.array([[2.0, 0.0], [-3.0, -1.0]])
-        L = gs.array([[SQRT_2, 0.0], [-3.0, 1.0]])
-        ip_result = self.metric_cholesky.inner_product(X, Y, L)
-        ip_expected = 2 + 6
-        self.assertAllClose(ip_expected, ip_result)
+    class TestDataCholeskyMetric(TestData):
+        def diag_inner_product_data(self):
+            smoke_data = [
+                dict(
+                    n=2,
+                    tangent_vec_a=[[1.0, 0.0], [-2.0, -1.0]],
+                    tangent_vec_b=[[2.0, 0.0], [-3.0, -1.0]],
+                    base_point=[[SQRT_2, 0.0], [-3.0, 1.0]],
+                    expected=2.0,
+                )
+            ]
+            return self.generate_tests(smoke_data)
 
-    def test_inner_product_vectorization(self):
-        """Test inner product"""
-        n_samples = 5
-        X = self.space.ambient_space.random_point(n_samples)
-        Y = self.space.ambient_space.random_point(n_samples)
-        L = self.space.random_point(n_samples)
-        inv_D_L = gs.linalg.inv(Matrices.to_diagonal(L))
-        D_X = Matrices.to_diagonal(X)
-        D_Y = Matrices.to_diagonal(Y)
-        ip_result = self.metric_cholesky.inner_product(X, Y, L)
-        ip_expected = (
-            Matrices.frobenius_product(
-                Matrices.to_strictly_lower_triangular(X),
-                Matrices.to_strictly_lower_triangular(Y),
-            )
-            + Matrices.frobenius_product(
-                gs.matmul(inv_D_L, D_X), gs.matmul(inv_D_L, D_Y)
+        def strictly_lower_inner_product_data(self):
+            smoke_data = [
+                dict(
+                    n=2,
+                    tangent_vec_a=[[1.0, 0.0], [-2.0, -1.0]],
+                    tangent_vec_b=[[2.0, 0.0], [-3.0, -1.0]],
+                    expected=6.0,
+                )
+            ]
+            return self.generate_tests(smoke_data)
+
+        def inner_product_data(self):
+            smoke_data = [
+                dict(
+                    n=2,
+                    tangent_vec_a=[[1.0, 0.0], [-2.0, -1.0]],
+                    tangent_vec_b=[[2.0, 0.0], [-3.0, -1.0]],
+                    base_point=[[SQRT_2, 0.0], [-3.0, 1.0]],
+                    expected=8.0,
+                )
+            ]
+            return self.generate_tests(smoke_data)
+
+        def exp_data(self):
+            smoke_data = [
+                dict(
+                    n=2,
+                    tangent_vec=[[-1.0, 0.0], [2.0, 3.0]],
+                    base_point=[[1.0, 0.0], [2.0, 2.0]],
+                    expected=[[1 / EULER, 0.0], [4.0, 2 * gs.exp(1.5)]],
+                )
+            ]
+            return self.generate_tests(smoke_data)
+
+        def log_data(self):
+            smoke_data = [
+                dict(
+                    n=2,
+                    point=[[EULER, 0.0], [2.0, EULER ** 3]],
+                    base_point=[[EULER ** 3, 0.0], [4.0, EULER ** 4]],
+                    expected=[[-2.0 * EULER ** 3, 0.0], [-2.0, -1 * EULER ** 4]],
+                )
+            ]
+
+        def squared_dist_data(self):
+            smoke_data = [
+                dict(
+                    n=2,
+                    point_a=[[EULER, 0.0], [2.0, EULER ** 3]],
+                    point_b=[[EULER ** 3, 0.0], [4.0, EULER ** 4]],
+                    expected=9,
+                ),
+                dict(
+                    n=2,
+                    point_a=[
+                        [[EULER, 0.0], [2.0, EULER ** 3]],
+                        [[EULER, 0.0], [4.0, EULER ** 3]],
+                    ],
+                    point_b=[
+                        [[EULER ** 3, 0.0], [4.0, EULER ** 4]],
+                        [[EULER ** 3, 0.0], [7.0, EULER ** 4]],
+                    ],
+                    expected=[9, 14],
+                ),
+            ]
+
+    testing_data = TestDataCholeskyMetric()
+
+    def test_diag_inner_product(
+        self, n, tangent_vec_a, tangent_vec_b, base_point, expected
+    ):
+        self.assertAllClose(
+            self.cls(n).diag_inner_product(
+                gs.array(tangent_vec_a), gs.array(tangent_vec_b), gs.array(base_point)
+            ),
+            gs.array(expected),
+        )
+
+    def test_strictly_lower_inner_product(
+        self, n, tangent_vec_a, tangent_vec_b, expected
+    ):
+        self.assertAllClose(
+            self.cls(n).strictly_lower_inner_product(
+                gs.array(tangent_vec_a), gs.array(tangent_vec_b)
+            ),
+            gs.array(expected),
+        )
+
+    def test_diag_inner_product(
+        self, n, tangent_vec_a, tangent_vec_b, base_point, expected
+    ):
+        self.assertAllClose(
+            self.cls(n).inner_product(
+                gs.array(tangent_vec_a), gs.array(tangent_vec_b), gs.array(base_point)
+            ),
+            gs.array(expected),
+        )
+
+    def test_exp(self, n, tangent_vec, base_point, expected):
+        self.assertAllClose(
+            self.cls(n).exp(
+                gs.array(tangent_vec), gs.array(base_point), gs.array(expected)
             )
         )
-        ip_shape_result = ip_result.shape[0]
-        ip_shape_expected = n_samples
 
-        self.assertAllClose(ip_expected, ip_result)
-        self.assertAllClose(ip_shape_expected, ip_shape_result)
+    def test_log(self, n, point, base_point, expected):
+        self.assertAllClose(
+            self.cls(n).log(gs.array(point), gs.array(base_point)), gs.array(expected)
+        )
 
-    def test_exp(self):
-        """Test exp map"""
-        L = gs.array([[1.0, 0.0], [2.0, 2.0]])
-        X = gs.array([[-1.0, 0.0], [2.0, 3.0]])
-        exp_expected = gs.array([[1 / EULER, 0.0], [4.0, 2 * gs.exp(1.5)]])
-        exp_result = self.metric_cholesky.exp(X, L)
-        self.assertAllClose(exp_expected, exp_result)
+    def test_squared_dist(self, n, point_a, point_b, expected):
+        self.assertAllClose(
+            self.cls(n).squared_dist(gs.array(point_a), gs.array(point_b)),
+            gs.array(expected),
+        )
 
     def test_exp_vectorization(self):
         """Test exp map vectorization"""
@@ -237,16 +299,6 @@ class TestPositiveLowerTriangularMatrices(geomstats.tests.TestCase):
         self.assertAllClose(exp_expected, exp_result)
         self.assertAllClose(belongs_expected, belongs_result)
 
-    def test_log(self):
-        """Test log map"""
-        K = gs.array([[EULER, 0.0], [2.0, EULER ** 3]])
-        L = gs.array([[EULER ** 3, 0.0], [4.0, EULER ** 4]])
-        log_result = self.metric_cholesky.log(K, L)
-        log_expected = gs.array([[0.0, 0.0], [-2.0, 0.0]]) + gs.array(
-            [[-2.0 * EULER ** 3, 0.0], [0.0, -1 * EULER ** 4]]
-        )
-        self.assertAllClose(log_expected, log_result)
-
     def test_log_vectorization(self):
         """Test log map"""
         K = self.space.random_point(5)
@@ -264,25 +316,3 @@ class TestPositiveLowerTriangularMatrices(geomstats.tests.TestCase):
         belongs_expected = True
         self.assertAllClose(log_expected, log_result)
         self.assertAllClose(belongs_expected, belongs_result)
-
-    def test_squared_dist(self):
-        """Test squared dist function"""
-        K = gs.array([[EULER, 0.0], [2.0, EULER ** 3]])
-        L = gs.array([[EULER ** 3, 0.0], [4.0, EULER ** 4]])
-
-        squared_dist_result = self.metric_cholesky.squared_dist(K, L)
-        squared_dist_expected = 4 + 5
-
-        batch_K = gs.array(
-            [[[EULER, 0.0], [2.0, EULER ** 3]], [[EULER, 0.0], [4.0, EULER ** 3]]]
-        )
-        batch_L = gs.array(
-            [
-                [[EULER ** 3, 0.0], [4.0, EULER ** 4]],
-                [[EULER ** 3, 0.0], [7.0, EULER ** 4]],
-            ]
-        )
-        batch_squared_dist_result = self.metric_cholesky.squared_dist(batch_K, batch_L)
-        batch_squared_dist_expected = gs.array([4 + 5, 9 + 5])
-        self.assertAllClose(squared_dist_expected, squared_dist_result)
-        self.assertAllClose(batch_squared_dist_expected, batch_squared_dist_result)
