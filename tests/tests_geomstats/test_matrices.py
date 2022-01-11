218668e210c5fd40b7a27abdaae42018b0fa8057
Saiteja Utpala <saitejautpala@gmail.com>
2022-01-10 17:18:56 -0800

    add MatsV1

--- tests/tests_geomstats/test_matrices.py
+++ tests/tests_geomstats/test_matrices.py
@@ -1,328 +1,165 @@
-"""Unit tests for the manifold of matrices."""
-
 import geomstats.backend as gs
 import geomstats.tests
 from geomstats.geometry.matrices import Matrices
 from geomstats.geometry.spd_matrices import SPDMatrices
+from tests.conftest import Parametrizer, TestCase, TestData
+
+EYE_2 = [[1.0, 0], [0.0, 1.0]]
+EYE_3 = [[1.0, 0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
+MAT_2_3 = [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]
+MAT1_33 = [[1.0, 2.0, 3.0], [2.0, 4.0, 5.0], [3.0, 5.0, 6.0]]
+MAT2_33 = [[1.0, 2.0, 3.0], [2.0, 4.0, 7.0], [3.0, 5.0, 6.0]]
+MAT3_33 = [[0.0, 1.0, -2.0], [-1.0, 0.0, -3.0], [2.0, 3.0, 0.0]]
+MAT4_33 = [[2.0, -1.0, 0.0], [-1.0, 2.0, 1.0], [0.0, -1.0, 2.0]]
 
 
-class TestMatrices(geomstats.tests.TestCase):
-    def setup_method(self):
-        gs.random.seed(1234)
-
-        self.m = 2
-        self.n = 3
-        self.space = Matrices(m=self.n, n=self.n)
-        self.space_nonsquare = Matrices(m=self.m, n=self.n)
-        self.metric = self.space.metric
-        self.n_samples = 2
-
-    @geomstats.tests.np_and_autograd_only
-    def test_mul(self):
-        a = gs.eye(3, 3, 1)
-        b = gs.eye(3, 3, -1)
-        c = gs.array([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 0.0]])
-        d = gs.array([[0.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
-        result = self.space.mul([a, b], [b, a])
-        expected = gs.array([c, d])
-        self.assertAllClose(result, expected)
-
-        result = self.space.mul(a, [a, b])
-        expected = gs.array([gs.eye(3, 3, 2), c])
-        self.assertAllClose(result, expected)
-
-    @geomstats.tests.np_and_autograd_only
-    def test_bracket(self):
-        x = gs.array([[0.0, 0.0, 0.0], [0.0, 0.0, -1.0], [0.0, 1.0, 0.0]])
-        y = gs.array([[0.0, 0.0, 1.0], [0.0, 0.0, 0.0], [-1.0, 0.0, 0.0]])
-        z = gs.array([[0.0, -1.0, 0.0], [1.0, 0.0, 0.0], [0.0, 0.0, 0.0]])
-        result = self.space.bracket([x, y], [y, z])
-        expected = gs.array([z, x])
-        self.assertAllClose(result, expected)
-
-        result = self.space.bracket(x, [x, y, z])
-        expected = gs.array([gs.zeros((3, 3)), z, -y])
-        self.assertAllClose(result, expected)
-
-    @geomstats.tests.np_and_autograd_only
-    def test_transpose(self):
-        tr = self.space.transpose
-        ar = gs.array
-        a = gs.eye(3, 3, 1)
-        b = gs.eye(3, 3, -1)
-        self.assertAllClose(tr(a), b)
-        self.assertAllClose(tr(ar([a, b])), ar([b, a]))
-
-    def test_is_symmetric(self):
-        not_squared = gs.array([[1.0, 2.0], [2.0, 1.0], [3.0, 1.0]])
-        result = self.space.is_symmetric(not_squared)
-        expected = False
-        self.assertAllClose(result, expected)
-
-        sym_mat = gs.array([[1.0, 2.0], [2.0, 1.0]])
-        result = self.space.is_symmetric(sym_mat)
-        expected = gs.array(True)
-        self.assertAllClose(result, expected)
-
-        not_a_sym_mat = gs.array([[1.0, 0.6, -3.0], [6.0, -7.0, 0.0], [0.0, 7.0, 8.0]])
-        result = self.space.is_symmetric(not_a_sym_mat)
-        expected = gs.array(False)
-        self.assertAllClose(result, expected)
-
-    @geomstats.tests.np_and_autograd_only
-    def test_is_skew_symmetric(self):
-        skew_mat = gs.array([[0, -2.0], [2.0, 0]])
-        result = self.space.is_skew_symmetric(skew_mat)
-        expected = gs.array(True)
-        self.assertAllClose(result, expected)
-
-        not_a_sym_mat = gs.array([[1.0, 0.6, -3.0], [6.0, -7.0, 0.0], [0.0, 7.0, 8.0]])
-        result = self.space.is_skew_symmetric(not_a_sym_mat)
-        expected = gs.array(False)
-        self.assertAllClose(result, expected)
-
-    @geomstats.tests.np_autograd_and_tf_only
-    def test_is_symmetric_vectorization(self):
-        points = gs.array(
-            [
-                [[1.0, 2.0], [2.0, 1.0]],
-                [[3.0, 4.0], [4.0, 5.0]],
-                [[1.0, 2.0], [3.0, 4.0]],
+class TestMatrices(TestCase, metaclass=Parametrizer):
+    class TestDataMatrices(TestData):
+        def belongs_data(self):
+            sq_mat = EYE_2
+            non_sq_mat = MAT_2_3
+            smoke_data = [
+                dict(m=2, n=2, mat=sq_mat, expected=True),
+                dict(m=2, n=1, mat=sq_mat, expected=False),
+                dict(m=2, n=3, mat=non_sq_mat, expected=True),
+                dict(m=2, n=1, mat=non_sq_mat, expected=False),
+                dict(m=2, n=3, mat=[non_sq_mat, non_sq_mat], expected=[True, True]),
+                dict(m=2, n=3, mat=[non_sq_mat, sq_mat], expected=[True, False]),
             ]
+            return self.generate_tests(smoke_data)
+
+        def equal_data(self):
+
+            smoke_data = [
+                dict(m=2, n=2, mat_1=EYE_2, mat_2=EYE_2, expected=True),
+                dict(m=2, n=2, mat_1=EYE_2, mat_2=2 * EYE_2, expected=False),
+                dict(
+                    m=2,
+                    n=3,
+                    mat_1=[MAT_2_3, 2 * MAT_2_3],
+                    mat_2=[MAT_2_3, 3 * MAT_2_3],
+                    expected=[True, False],
+                ),
+            ]
+            return self.generate_tests(smoke_data)
+
+        def mul(self):
+            mats_1 = (
+                [[1.0, 2.0], [3.0, 4.0]],
+                [[-1.0, 2.0], [-3.0, 4.0]],
+                [[1.0, -2.0], [3.0, -4.0]],
+            )
+            mats_2 = [[[2.0], [4.0]], [[1.0], [3.0]], [[1.0], [3.0]]]
+            mat_1_X_mat_2 = [[[10.0], [22.0]], [[5.0], [9.0]], [[-5.0], [-9.0]]]
+            smoke_data = [
+                dict(mat=mats_1, expected=[[23.0, -26.0], [51.0, -58.0]]),
+                dict(mat=(list(mats_1), mats_2), expected=mat_1_X_mat_2),
+            ]
+            return self.generate_tests(smoke_data)
+
+        def commutator_data(self):
+            smoke_data = []
+            return self.generate_tests(smoke_data)
+
+        def is_symmetric_data(self):
+            smoke_data = [
+                dict(mat=EYE_2, expected=True),
+                dict(mat=[EYE_2, EYE_2 + 1], expected=[True, True]),
+                dict(mat=[MAT1_33, MAT2_33, MAT3_33], expected=[True, False, True]),
+            ]
+            self.generate_tests(smoke_data)
+
+        def is_skew_symmetric_data(self):
+            smoke_data = [
+                dict(n=2, mat=EYE_2, expected=False),
+                dict(n=2, mat=[EYE_2, -1 * EYE_2], expected=[False, False]),
+                dict(n=3, mat=[MAT2_33, MAT3_33], expected=[False, True]),
+            ]
+            self.generate_tests(smoke_data)
+
+        def is_pd(self):
+            smoke_data = [
+                dict(n=2, mat=EYE_2, expected=True),
+                dict(n=2, mat=[EYE_2, -1 * EYE_2], expected=[True, False]),
+                dict(n=3, mat=[MAT2_33, MAT3_33], expected=[False, False]),
+                dict(n=3, mat=[MAT2_33, MAT3_33], expected=[False, False]),
+            ]
+            self.generate_tests(smoke_data)
+
+        def is_spd(self):
+            smoke_data = [
+                dict(n=2, mat=EYE_2, expected=True),
+                dict(n=3, mat=MAT4_33, expected=True),
+                dict(
+                    n=2,
+                    mat=[EYE_2, -1 * EYE_2, -2 * MAT4_33],
+                    expected=[True, False, False],
+                ),
+                dict(n=3, mat=[MAT2_33, MAT3_33], expected=[False, False]),
+                dict(n=3, mat=[MAT2_33, MAT3_33], expected=[False, False]),
+            ]
+            self.generate_tests(smoke_data)
+
+        def is_upper_triangualr(self):
+            smoke_data = [dict(mat=[])]
+            self.generate_tests(smoke_data)
+
+        def is_lower_triangular(self):
+            smoke_data = []
+            self.generate_tests(smoke_data)
+
+        def is_strictly_lower_triangular(self):
+            smoke_data = []
+            self.generate_tests(smoke_data)
+
+        def is_strictly_upper_triangular(self):
+            smoke_data = []
+            self.generate_tests(smoke_data)
+
+    def test_belongs(self, m, n, mat, expected):
+        self.assertAllClose(Matrices(m, n).belongs(gs.array(mat)), gs.array(expected))
+
+    def test_equal(self, m, n, mat1, mat2, expected):
+        self.assertAllClose(
+            Matrices(m, n).equal(gs.array(mat1), gs.array(mat2)), gs.array(expected)
         )
-        result = self.space.is_symmetric(points)
-        expected = [True, True, False]
-        self.assertAllClose(result, expected)
 
-    @geomstats.tests.np_autograd_and_torch_only
-    def test_make_symmetric(self):
-        sym_mat = gs.array([[1.0, 2.0], [2.0, 1.0]])
-        result = self.space.to_symmetric(sym_mat)
-        expected = sym_mat
-        self.assertAllClose(result, expected)
+    def test_mul(self, mat, expected):
+        self.assertAllClose(Matrices.mul(mat), expected)
 
-        mat = gs.array([[1.0, 2.0, 3.0], [0.0, 0.0, 0.0], [3.0, 1.0, 1.0]])
-        result = self.space.to_symmetric(mat)
-        expected = gs.array([[1.0, 1.0, 3.0], [1.0, 0.0, 0.5], [3.0, 0.5, 1.0]])
-        self.assertAllClose(result, expected)
+    def test_commutator(self, mat_a, mat_b, expected):
+        self.assertAllClose(Matrices.commutator(mat_a, mat_b), expected)
 
-        mat = gs.array(
-            [[1e100, 1e-100, 1e100], [1e100, 1e-100, 1e100], [1e-100, 1e-100, 1e100]]
-        )
-        result = self.space.to_symmetric(mat)
+    def is_symmetric_data(self):
+        pass
 
-        res = 0.5 * (1e100 + 1e-100)
+    def is_skew_symmetric_data(self):
+        smoke_data = []
+        self.generate_tests(smoke_data)
 
-        expected = gs.array([[1e100, res, res], [res, 1e-100, res], [res, res, 1e100]])
-        self.assertAllClose(result, expected)
+    def is_pd(self):
+        smoke_data = []
+        self.generate_tests(smoke_data)
 
-    @geomstats.tests.np_autograd_and_tf_only
-    def test_make_symmetric_and_is_symmetric_vectorization(self):
-        points = gs.array([[[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [4.0, 9.0]]])
+    def is_spd(self):
+        smoke_data = []
+        self.generate_tests(smoke_data)
 
-        sym_points = self.space.to_symmetric(points)
-        result = gs.all(self.space.is_symmetric(sym_points))
-        expected = True
-        self.assertAllClose(result, expected)
+    def is_upper_triangualr(self):
+        smoke_data = []
+        self.generate_tests(smoke_data)
 
-    def test_inner_product(self):
-        base_point = gs.array([[1.0, 2.0, 3.0], [0.0, 0.0, 0.0], [3.0, 1.0, 1.0]])
+    def is_lower_triangular(self):
+        smoke_data = []
+        self.generate_tests(smoke_data)
 
-        tangent_vector_1 = gs.array(
-            [[1.0, 2.0, 3.0], [0.0, -10.0, 0.0], [30.0, 1.0, 1.0]]
-        )
+    def is_strictly_lower_triangular(self):
+        smoke_data = []
+        self.generate_tests(smoke_data)
 
-        tangent_vector_2 = gs.array([[1.0, 4.0, 3.0], [5.0, 0.0, 0.0], [3.0, 1.0, 1.0]])
+    def is_strictly_upper_triangular(self):
+        smoke_data = []
+        self.generate_tests(smoke_data)
 
-        result = self.metric.inner_product(
-            tangent_vector_1, tangent_vector_2, base_point=base_point
-        )
-
-        expected = gs.trace(gs.matmul(gs.transpose(tangent_vector_1), tangent_vector_2))
-
-        self.assertAllClose(result, expected)
-
-    def test_cong(self):
-        base_point = gs.array([[1.0, 2.0, 3.0], [0.0, 0.0, 0.0], [3.0, 1.0, 1.0]])
-
-        tangent_vector = gs.array(
-            [[1.0, 2.0, 3.0], [0.0, -10.0, 0.0], [30.0, 1.0, 1.0]]
-        )
-
-        result = self.space.congruent(tangent_vector, base_point)
-        expected = gs.matmul(tangent_vector, gs.transpose(base_point))
-        expected = gs.matmul(base_point, expected)
-
-        self.assertAllClose(result, expected)
-
-    def test_belongs(self):
-        base_point_square = gs.zeros((self.n, self.n))
-        base_point_nonsquare = gs.zeros((self.m, self.n))
-
-        result = self.space.belongs(base_point_square)
-        expected = True
-        self.assertAllClose(result, expected)
-        result = self.space_nonsquare.belongs(base_point_square)
-        expected = False
-        self.assertAllClose(result, expected)
-
-        result = self.space.belongs(base_point_nonsquare)
-        expected = False
-        self.assertAllClose(result, expected)
-        result = self.space_nonsquare.belongs(base_point_nonsquare)
-        expected = True
-        self.assertAllClose(result, expected)
-
-        result = self.space.belongs(gs.zeros((2, 2, 3)))
-        self.assertFalse(gs.all(result))
-
-        result = self.space.belongs(gs.zeros((2, 3, 3)))
-        self.assertTrue(gs.all(result))
-
-    def test_is_diagonal(self):
-        base_point = gs.array([[1.0, 2.0, 3.0], [0.0, 0.0, 0.0], [3.0, 1.0, 1.0]])
-        result = self.space.is_diagonal(base_point)
-        expected = False
-        self.assertAllClose(result, expected)
-
-        diagonal = gs.eye(3)
-        result = self.space.is_diagonal(diagonal)
-        self.assertTrue(result)
-
-        base_point = gs.stack([base_point, diagonal])
-        result = self.space.is_diagonal(base_point)
-        expected = gs.array([False, True])
-        self.assertAllClose(result, expected)
-
-        base_point = gs.stack([diagonal] * 2)
-        result = self.space.is_diagonal(base_point)
-        self.assertTrue(gs.all(result))
-
-        base_point = gs.reshape(gs.arange(6), (2, 3))
-        result = self.space.is_diagonal(base_point)
-        self.assertTrue(~result)
-
-    def test_is_pd(self):
-        symmetric_but_not_pd = gs.array([[1.0, 1.0], [1.0, 1.0]])
-        result = self.space.is_pd(symmetric_but_not_pd)
-        expected = False
-        self.assertAllClose(result, expected)
-
-        pd = gs.array([[2.0, 6.0], [6.0, 20]])
-        result = self.space.is_pd(pd)
-        expected = True
-        self.assertAllClose(result, expected)
-
-    def test_is_spd(self):
-
-        symmetric_but_not_pd = gs.array([[1.0, 1.0], [1.0, 1.0]])
-        result = self.space.is_spd(symmetric_but_not_pd)
-        expected = False
-        self.assertAllClose(result, expected)
-
-        n = 4
-        n_samples = 10
-        spdManifold = SPDMatrices(n)
-        spd = spdManifold.random_point(n_samples)
-        not_spd = -1 * spd
-        mixed = gs.vstack((spd, not_spd))
-
-        spd_expected = [True] * n_samples
-        not_spd_expected = [False] * n_samples
-        mixed_expected = spd_expected + not_spd_expected
-
-        spd_result = Matrices.is_spd(spd)
-        not_spd_result = Matrices.is_spd(not_spd)
-        mixed_result = Matrices.is_spd(mixed)
-
-        self.assertAllClose(spd_expected, spd_result)
-        self.assertAllClose(not_spd_expected, not_spd_result)
-        self.assertAllClose(mixed_expected, mixed_result)
-
-    def test_norm(self):
-        for n_samples in [1, 2]:
-            mat = self.space.random_point(n_samples)
-            result = self.metric.norm(mat)
-            expected = self.space.frobenius_product(mat, mat) ** 0.5
-            self.assertAllClose(result, expected)
-
-    def test_flatten_reshape(self):
-        matrix_list = self.space_nonsquare.random_point(n_samples=1)
-        result = self.space_nonsquare.reshape(self.space_nonsquare.flatten(matrix_list))
-        self.assertAllClose(result, matrix_list)
-
-        matrix_list = self.space_nonsquare.random_point(n_samples=2)
-        result = self.space_nonsquare.reshape(self.space_nonsquare.flatten(matrix_list))
-        self.assertAllClose(result, matrix_list)
-
-    def test_diagonal(self):
-        mat = gs.eye(3)
-        result = Matrices.diagonal(mat)
-        expected = gs.ones(3)
-        self.assertAllClose(result, expected)
-
-        mat = gs.stack([mat] * 2)
-        result = Matrices.diagonal(mat)
-        expected = gs.ones((2, 3))
-        self.assertAllClose(result, expected)
-
-    def test_to_diagonal(self):
-        mat = gs.array([[1.0, 2.0], [3.0, 4.0]])
-        result = Matrices.to_diagonal(mat)
-        expected = gs.array([[1.0, 0.0], [0.0, 4.0]])
-
-        batch_mat = gs.array([[[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]]])
-        result = Matrices.to_diagonal(batch_mat)
-        expected = gs.array([[[1.0, 0.0], [0.0, 4.0]], [[5.0, 0.0], [0.0, 8.0]]])
-        self.assertAllClose(result, expected)
-
-    def test_to_lower_triangular(self):
-        mat = gs.array([[1.0, 2.0], [3.0, 4.0]])
-        result = Matrices.to_lower_triangular(mat)
-        expected = gs.array([[1.0, 0.0], [3.0, 4.0]])
-
-        batch_mat = gs.array([[[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]]])
-        result = Matrices.to_lower_triangular(batch_mat)
-        expected = gs.array([[[1.0, 0.0], [3.0, 4.0]], [[5.0, 0.0], [7.0, 8.0]]])
-        self.assertAllClose(result, expected)
-
-    def test_to_strictly_lower_triangular(self):
-        mat = gs.array([[1.0, 2.0], [3.0, 4.0]])
-        result = Matrices.to_strictly_lower_triangular(mat)
-        expected = gs.array([[0.0, 0.0], [3.0, 0.0]])
-
-        batch_mat = gs.array([[[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]]])
-        result = Matrices.to_strictly_lower_triangular(batch_mat)
-        expected = gs.array([[[0.0, 0.0], [3.0, 0.0]], [[0.0, 0.0], [7.0, 0.0]]])
-        self.assertAllClose(result, expected)
-
-    def test_to_upper_triangular(self):
-        mat = gs.array([[1.0, 2.0], [3.0, 4.0]])
-        result = Matrices.to_upper_triangular(mat)
-        expected = gs.array([[1.0, 2.0], [0.0, 4.0]])
-
-        batch_mat = gs.array([[[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]]])
-        result = Matrices.to_upper_triangular(batch_mat)
-        expected = gs.array([[[1.0, 2.0], [0.0, 4.0]], [[5.0, 6.0], [0.0, 8.0]]])
-        self.assertAllClose(result, expected)
-
-    def test_to_strictly_upper_triangular(self):
-        mat = gs.array([[1.0, 2.0], [3.0, 4.0]])
-        result = Matrices.to_strictly_upper_triangular(mat)
-        expected = gs.array([[1.0, 2.0], [0.0, 4.0]])
-
-        batch_mat = gs.array([[[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]]])
-        result = Matrices.to_strictly_upper_triangular(batch_mat)
-        expected = gs.array([[[0.0, 2.0], [0.0, 0.0]], [[0.0, 6.0], [0.0, 0.0]]])
-        self.assertAllClose(result, expected)
-
-    def test_to_lower_triagular_diagonal_scaled(self):
-        mat = gs.array([[2.0, 2.0], [3.0, 4.0]])
-        result = Matrices.to_lower_triangular_diagonal_scaled(mat)
-        expected = gs.array([[1.0, 0.0], [3.0, 2.0]])
-        self.assertAllClose(result, expected)
-
-        batch_mat = gs.array([[[2.0, 2.0], [3.0, 4.0]], [[6.0, 6.0], [7.0, 8.0]]])
-        result = Matrices.to_lower_triangular_diagonal_scaled(batch_mat)
-        expected = gs.array([[[1.0, 0], [3.0, 2.0]], [[3.0, 0.0], [7.0, 4.0]]])
-        self.assertAllClose(result, expected)
+    def test_to_matrixtype_belongs(self, matrix_type, points):
+        self.assertAllClose(Matrices.matrix_type(points))
