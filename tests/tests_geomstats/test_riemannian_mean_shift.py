b38768cf79c3c48851a895bd7233913d7cd64759
Shubham Talbar <91352665+shubhamtalbar96@users.noreply.github.com>
2022-01-20 14:42:50 -0800

    fixing doc string issues in RMS class and methods

--- tests/tests_geomstats/test_riemannian_mean_shift.py
+++ tests/tests_geomstats/test_riemannian_mean_shift.py
@@ -1,5 +1,7 @@
 """Unit tests for Riemannian Mean Shift method."""
 
+import os
+
 import geomstats.backend as gs
 import geomstats.tests
 from geomstats.geometry.hypersphere import Hypersphere, HypersphereMetric
@@ -8,6 +10,8 @@ from geomstats.learning.frechet_mean import FrechetMean
 from geomstats.learning.riemannian_mean_shift import (
     RiemannianMeanShift as riemannian_mean_shift,
 )
+
+os.environ["GEOMSTATS_BACKEND"] = "pytorch"
 
 
 class TestRiemannianMeanShift(geomstats.tests.TestCase):
