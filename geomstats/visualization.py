24ad3977cf3d2fcedf78b9b4ebcee4e73a3d9a89
Nina Miolane <nmiolane@gmail.com>
2022-01-20 21:14:19 -0800

    Clean D400 flake8errors

--- geomstats/visualization.py
+++ geomstats/visualization.py
@@ -234,8 +234,11 @@ class Sphere:
                     )
 
     def fibonnaci_points(self, n_points=16000):
-        """Spherical Fibonacci point sets yield nearly uniform point
-        distributions on the unit sphere."""
+        """Plot spherical Fibonacci point set.
+
+        Spherical Fibonacci point set yields nearly uniform point
+        distributions on the unit sphere.
+        """
         x_vals = []
         y_vals = []
         z_vals = []
