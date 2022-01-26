3044ed968b1cd6333094ae8b3cb79c38d75bc86b
Jonas Lueg <jonas.lueg@uni-goettingen.de>
2022-01-26 12:42:31 +0100

    refined the example for waldspace

--- examples/plot_geodesics_waldspace3.py
+++ examples/plot_geodesics_waldspace3.py
@@ -1,27 +1,56 @@
-import matplotlib.pyplot as plt
+"""
+Plot three geodesics in Wald space with three labels.
+
+The visualization is done via plotting the three off-diagonal entries of the embedded
+points of Wald space that are strictly positive definite symmetric matrices.
+
+The wireframes are two-dimensional boundaries of the embedded Wald space.
+The black lines in the origin are the one-dimensional boundaries of Wald space.
+
+Note that this is not an isometric embedding, e.g. the non-marked ends of the wireframes
+are infinitely far away from points in the interior.
+
+Lead author: Jonas Lueg
+"""
+
+import numpy as np
 
 import geomstats.backend as gs
 import geomstats.visualization as visualization
+
 from geomstats.geometry.waldspace import WaldSpace
 from geomstats.geometry.trees import Split, Structure, Wald
+
+# Wald space with three labels.
 WS3 = WaldSpace(n=3)
 
 
 def main():
     """Plot an approximated geodesic from p to q in Wald space. """
+    # Construct the tree with three leaves and three edges with one interior vertex.
     sp0 = Split(n=3, part1=(0,), part2=(1, 2))
     sp1 = Split(n=3, part1=(1,), part2=(0, 2))
     sp2 = Split(n=3, part1=(2,), part2=(0, 1))
     st = Structure(n=3, partition=((0, 1, 2),), split_sets=((sp0, sp1, sp2),))
 
-    p = Wald(n=3, st=st, x=gs.array([0.1, 0.9, 0.07]))
-    q = Wald(n=3, st=st, x=gs.array([0.08, 0.1, 0.9]))
+    # Construct the three points in Wald space.
+    p1 = Wald(n=3, st=st, x=gs.array([0.1, 0.9, 0.07]))
+    p2 = Wald(n=3, st=st, x=gs.array([0.08, 0.1, 0.9]))
+    p3 = Wald(n=3, st=st, x=gs.array([0.3, 0.001, 0.01]))
 
+    # Compute the approximations of geodesics.
     proj_args = {'method': 'local', 'btol': 10**-8, 'gtol': 10**-5}
-    curve = WS3.geodesic(p=p, q=q, n_points=20, **proj_args)
+    curve12 = WS3.geodesic(p=p1, q=p2, n_points=20, **proj_args)
+    curve23 = WS3.geodesic(p=p2, q=p3, n_points=20, **proj_args)
+    curve13 = WS3.geodesic(p=p1, q=p3, n_points=30, **proj_args)
 
+    # Plot those paths and points in Wald space embedded into SPD(3).
     ws3_plot = visualization.WaldSpace3()
-    ws3_plot.pass_curve(curve=curve, label="geodesic between p and q")
+    for curve in [curve12, curve23, curve13]:
+        ws3_plot.pass_curve(curve=curve,
+                            label=f"length = {np.round(WS3.length(curve), 5)}")
+    ws3_plot.pass_points(points=[p1, p2, p3], marker='.',
+                         text=[r'$p_1$', r'$p_2$', r'$p_3$'], color='black')
     ws3_plot.show()
 
 
