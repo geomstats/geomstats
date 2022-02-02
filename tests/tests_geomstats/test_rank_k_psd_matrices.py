627d6dd16119761ac31cfe042c76c19fac8465db
Saiteja Utpala <saitejautpala@gmail.com>
2022-02-01 22:54:08 -0800

    rank_k_psd

--- tests/tests_geomstats/test_rank_k_psd_matrices.py
+++ tests/tests_geomstats/test_rank_k_psd_matrices.py
@@ -1,77 +1,73 @@
 r"""Unit tests for the space of PSD matrices of rank k."""
 
-import warnings
+import random
 
 import geomstats.backend as gs
-import geomstats.tests
+import tests.helper as helper
 from geomstats.geometry.rank_k_psd_matrices import PSDMatrices
-from geomstats.geometry.symmetric_matrices import SymmetricMatrices
+from tests.conftest import Parametrizer, TestCase, TestData
 
 
-class TestPSDMatricesRankK(geomstats.tests.TestCase):
-    r"""Test of PSD Matrices Rank k methods."""
+class TestPSDMatrices(TestCase, metaclass=Parametrizer):
+    cls = PSDMatrices
 
-    def setup_method(self):
-        r"""Set up the test."""
-        warnings.simplefilter("ignore", category=ImportWarning)
-
-        gs.random.seed(1234)
-
-        self.n = 3
-        self.k = 2
-        self.space = PSDMatrices(self.n, self.k)
-        self.sym = SymmetricMatrices(self.n)
-
-    def test_belongs(self):
-        r"""Test of belongs method."""
-        psd_n_k = self.space
-        mat_not_psd_n_k = gs.array(
-            [
-                [0.8369314, -0.7342977, 1.0402943],
-                [0.04035992, -0.7218659, 1.0794858],
-                [0.9032698, -0.73601735, -0.36105633],
+    class TestDataPSDMatrices(TestData):
+        def belongs_data(self):
+            smoke_data = [
+                dict(
+                    n=3,
+                    k=2,
+                    mat=[
+                        [0.8369314, -0.7342977, 1.0402943],
+                        [0.04035992, -0.7218659, 1.0794858],
+                        [0.9032698, -0.73601735, -0.36105633],
+                    ],
+                    expected=False,
+                ),
+                dict(
+                    n=3,
+                    k=2,
+                    mat=[[1.0, 1.0, 0], [1.0, 4.0, 0], [0, 0, 0]],
+                    expected=True,
+                ),
             ]
-        )
-        mat_psd_n_k = gs.array([[1.0, 1.0, 0], [1.0, 4.0, 0], [0, 0, 0]])
-        result = psd_n_k.belongs(mat_not_psd_n_k)
-        self.assertFalse(result)
+            return self.generate_tests(smoke_data)
 
-        result = psd_n_k.belongs(mat_psd_n_k)
-        self.assertTrue(result)
+        def projection_and_belongs_data(self):
+            smoke_data = [
+                dict(n=2, k=2, n_samples=10),
+                dict(n=2, k=1, n_samples=1),
+                dict(n=3, k=3, n_samples=5),
+            ]
+            random_data = []
+            n_list = random.sample(range(2, 50), 5)
+            for n in n_list:
+                k_list = random.sample(range(1, n), 5)
+                for k in k_list:
+                    n_samples = random.sample(range(2, 50))
+                    random_data += [dict(n=n, k=k, n_samples=n_samples)]
 
-    def test_projection_and_belongs(self):
-        r"""Test the projection and the belongs methods."""
-        points = self.sym.random_point(3)
-        proj_points = self.space.projection(points)
-        result = self.space.belongs(proj_points)
-        self.assertTrue(gs.all(result))
+            return self.generate_tests(smoke_data, random_data)
 
-    def test_random_and_belongs(self):
-        r"""Test the random and the belongs methods."""
-        mat = self.space.random_point(4)
-        result = self.space.belongs(mat)
-        self.assertTrue(gs.all(result))
+        def to_tangent_is_tangent_data(self):
+            random_data = []
+            n_list = random.sample(range(2, 50), 5)
+            for n in n_list:
+                k_list = random.sample(range(1, n), 5)
+                for k in k_list:
+                    n_samples = random.sample(range(1, 50))
+                    base_point = PSDMatrices(n, k).random_point(n_samples)
+                    mat = gs.random.normal(size=(n_samples, n, n))
+                    random_data += [dict(n=n, k=k, mat=mat, base_point=base_point)]
 
-    def test_is_tangent_and_to_tangent(self):
-        r"""Test the tangent functions."""
-        base_point = self.space.random_point(3)
-        vectors = self.sym.random_point(3)
-        vectors_t = self.space.to_tangent(base_point=base_point, vector=vectors)
-        vectors_t_bp0 = self.space.to_tangent(base_point=base_point[0], vector=vectors)
+            return self.generate_tests([], random_data)
 
-        result = self.space.is_tangent(base_point=base_point, vector=vectors)
-        self.assertFalse(gs.all(result))
-        result = self.space.is_tangent(base_point=base_point, vector=vectors_t)
-        self.assertTrue(gs.all(result))
+    def test_belongs(self, n, k, mat, expected):
+        space = self.cls(n, k)
+        self.assertAllClose(space.belongs(gs.array(mat)), gs.array(expected))
 
-        result = self.space.is_tangent(base_point=base_point[0], vector=vectors)
-        self.assertFalse(gs.all(result))
-        result = self.space.is_tangent(base_point=base_point[0], vector=vectors_t_bp0)
-        self.assertTrue(gs.all(result))
-
-        result = self.space.is_tangent(base_point=base_point[0], vector=vectors[0])
-        self.assertFalse(gs.all(result))
-        result = self.space.is_tangent(
-            base_point=base_point[0], vector=vectors_t_bp0[0]
-        )
-        self.assertTrue(gs.all(result))
+    def test_projection_and_belongs(self, n, k, n_samples):
+        group = self.cls(n, k)
+        shape = (n_samples, n, n)
+        result = helper.test_projection_and_belongs(group, shape)
+        self.assertAllClose(gs.all(result), gs.array(True))
