4d969cfac507755d4594be06cbe8c8be7dd96caf
Saiteja Utpala <saitejautpala@gmail.com>
2022-02-04 12:50:07 -0800

    tests/properties.py

--- tests/properties.py
+++ tests/properties.py
@@ -13,3 +13,10 @@ class ManifoldTestProperties:
         tangent = space.to_tangent(gs.array(data))
         self.assertAllClose(gs.all(space.is_tangent(tangent)), gs.array(True))
         self.assertAllclose(gs.shape(tangent), expected)
+
+
+class OpenSetProperites:
+    def to_tangent_belongs_ambient_space(self, space_args, data, atol):
+        space = self.space(*space_args)
+        result = gs.all(space.ambient_space.belongs(gs.array(data), atol))
+        self.asertAllClose(result, gs.array(True))
