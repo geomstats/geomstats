c5f6a454452bf0e418b6a9aba4ecc2bbf5078a1e
Saiteja Utpala <saitejautpala@gmail.com>
2022-02-14 10:10:31 -0800

    skip to_tan and is_tan

--- tests/tests_geomstats/test_stiefel.py
+++ tests/tests_geomstats/test_stiefel.py
@@ -32,7 +32,7 @@ class TestStiefel(TestCase, metaclass=LevelSetParametrizer):
     space = Stiefel
     skip_test_extrinsic_intrinsic_composition = True
     skip_test_intrinsic_extrinsic_composition = True
-    skip_test_to_tangent_is_tangent_ = True
+    skip_test_to_tangent_is_tangent = True
 
     class TestDataStiefel(LevelSetTestData):
         def random_point_belongs_data(self):
