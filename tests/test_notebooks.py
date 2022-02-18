d307326a8ece398fdd7e38aa64332589d0ca2eed
Adele Myers <adele@ucsb.edu>
2022-02-17 17:37:07 -0800

    adding a01_manifold_class notebook to the test notebook file

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -97,3 +97,8 @@ class TestNotebooks(geomstats.tests.TestCase):
             "notebooks/"
             "usecase_optic_nerve_heads_analysis_in_kendall_shape_space.ipynb"
         )
+
+    @staticmethod
+    @geomstats.tests.np_and_autograd_only
+    def test_a01_manifold_class():
+        _exec_notebook("notebooks/" "a01_manifold_class.ipynb")
