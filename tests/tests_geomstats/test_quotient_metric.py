de3d8a90bbe8bf97bfb61a16d54fc526acf5c367
Saiteja Utpala <saitejautpala@gmail.com>
2022-02-27 19:21:13 -0800

    complete tests

--- tests/tests_geomstats/test_quotient_metric.py
+++ tests/tests_geomstats/test_quotient_metric.py
@@ -11,8 +11,8 @@ from geomstats.geometry.quotient_metric import QuotientMetric
 from geomstats.geometry.spd_matrices import SPDMatrices, SPDMetricBuresWasserstein
 from geomstats.geometry.special_orthogonal import SpecialOrthogonal
 from tests.conftest import TestCase
-from tests.data_generation import RiemannianMetricTestData, TestData
-from tests.parametrizers import Parametrizer, RiemannianMetricParametrizer
+from tests.data_generation import TestData
+from tests.parametrizers import Parametrizer
 
 
 class BuresWassersteinBundle(GeneralLinear, FiberBundle):
