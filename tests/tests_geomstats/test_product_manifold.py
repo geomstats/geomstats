2f4e6ecb3041cb53eeb413360085ca94b2d4548e
Saiteja Utpala <saitejautpala@gmail.com>
2022-03-01 11:19:13 -0800

    reduce precision

--- tests/tests_geomstats/test_product_manifold.py
+++ tests/tests_geomstats/test_product_manifold.py
@@ -421,7 +421,7 @@ class TestNFoldManifold(TestCase, metaclass=ManifoldParametrizer):
                 self.space_args_list,
                 self.shape_list,
                 self.n_samples_list,
-                belongs_atol=1e-3,
+                belongs_atol=1e-1,
             )
 
         def to_tangent_is_tangent_data(self):
