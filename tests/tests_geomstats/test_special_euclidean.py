f96123e4174ae47a154b2366ec0f8c4038688891
Saiteja Utpala <saitejautpala@gmail.com>
2022-03-15 11:43:24 -0700

    add

--- tests/tests_geomstats/test_special_euclidean.py
+++ tests/tests_geomstats/test_special_euclidean.py
@@ -74,8 +74,8 @@ ATOL = 1e-5
 class TestSpecialEuclidean(TestCase, metaclass=LieGroupParametrizer):
 
     space = group = SpecialEuclidean
-    skip_exp_log_composition_test_data = tf_backend()
-    skip_log_exp_composition_test_data = tf_backend()
+    skip_test_exp_log_composition_test = tf_backend()
+    skip_test_log_exp_composition_test = tf_backend()
 
     class SpecialEuclideanTestData(_LieGroupTestData):
         n_list = random.sample(range(2, 4), 2)
