42a7905bb8409be16dc00b047dc92ccc0b672901
Tom Szwagier <tom.szwagier@mines-paristech.fr>
2022-04-06 16:15:13 +0200

    implement is_tangent, without handling several points for now

--- geomstats/geometry/flag.py
+++ geomstats/geometry/flag.py
@@ -3,10 +3,19 @@ import geomstats.backend as gs
 from geomstats.geometry.manifold import Manifold
 from geomstats.geometry.matrices import Matrices
 
+"""
+Implementation of the Flag manifold based on the Grassmannian one.
+TODO:
+- generalize to several points (returns array instead of single element)
+- use gs.all() instead of ().all()
+"""
 
 class Flag(Manifold):
-    def __init__(self, index, n,
-                 ambient_manifold=None):  # set the problem of the structure. List of matrices is not a manifold I guess.
+    def __init__(self, index, n, ambient_manifold=None):
+        # set the problem of the structure. List of matrices is not a manifold I guess.
+        # using block diagonal matrices like in the paper would be cool because of the SPD structure,
+        # but too memory expensive
+
         d = len(index)
         extended_index = gs.concatenate(([0], index), dtype="int")
         dim = int(gs.sum(np.diff(extended_index) * (n - gs.array(index))))  # cf [Ye2021] p 17
@@ -18,11 +27,7 @@ class Flag(Manifold):
         self.n = n
         self.ambient_manifold = ambient_manifold
 
-    def belongs(self, point, atol=gs.atol):
-        # later, must handle several points
-        # maybe having a list point of length d+1 like extended_index with the first point being zero would be cleaner?
-        # we could do R_i = point[i]. Just, we would have a first point that isn't useful, so memory for nothing
-        # and in terms of representation of flags, it wouldn't be rigorous
+    def belongs(self, point, atol=gs.atol):  # characterization from [Ye2021] Proposition 21
         belongs = True
         for i in range(1, self.d + 1):
             R_i = point[i - 1]  # the length of point is d while the length of extended indexes is d+1
@@ -50,8 +55,34 @@ class Flag(Manifold):
 
         return belongs
 
-    def is_tangent(self, vector, base_point, atol=gs.atol):
-        pass
+    def is_tangent(self, vector, base_point, atol=gs.atol):  # characterization from [Ye2021] Proposition 22
+        is_tangent = True
+        for i in range(1, self.d + 1):
+            R_i = base_point[i - 1]  # the length of point is d while the length of extended indexes is d+1
+            Z_i = vector[i - 1]
+            is_tangent = gs.logical_and(is_tangent, gs.isclose(Matrices.mul(R_i, Z_i) + Matrices.mul(Z_i, R_i), Z_i, atol=atol).all())
+            if not gs.any(is_tangent):
+                return is_tangent
+            is_tangent = gs.logical_and(is_tangent, gs.isclose(Z_i, Matrices.transpose(Z_i), atol=atol).all())
+            if not gs.any(is_tangent):
+                return is_tangent
+            is_tangent = gs.logical_and(is_tangent, gs.isclose(Matrices.mul(R_i, Z_i) + Matrices.mul(Z_i, R_i), Matrices.transpose(Z_i), atol=atol).all())
+            if not gs.any(is_tangent):
+                return is_tangent
+            is_tangent = gs.logical_and(is_tangent, gs.isclose(gs.trace(Z_i), gs.zeros((self.n, self.n)),
+                                                               atol=atol).all())
+            if not gs.any(is_tangent):
+                return is_tangent
+
+            for j in range(1, i):
+                R_j = base_point[j - 1]
+                Z_j = vector[j - 1]
+                is_tangent = gs.logical_and(is_tangent, gs.isclose(Matrices.mul(Z_i, R_j) + Matrices.mul(R_i, Z_j), gs.zeros((self.n, self.n)),
+                                                             atol=atol).all())
+                if not gs.any(is_tangent):
+                    return is_tangent
+
+        return is_tangent
 
     def to_tangent(self, vector, base_point):
         pass
@@ -66,3 +97,5 @@ if __name__ == "__main__":
     point2 = [gs.array(np.diag([1, 0, 0, 0, 0])), gs.array(np.diag([0, 1, 1, 0, 0])), gs.array(np.diag([0, 0, 0, 0, 1]))]
     print(flag.belongs(point1))  # False
     print(flag.belongs(point2))  # True
+    print(flag.is_tangent(point2, base_point=point1))  # False
+    print(flag.is_tangent(point1, base_point=point2))  # False
