b1b93f312954e15f21de25499019e5a27d4f9efe
Tom Szwagier <tom.szwagier@mines-paristech.fr>
2022-04-06 12:03:51 +0200

    implement __init__

--- geomstats/geometry/flag.py
+++ geomstats/geometry/flag.py
@@ -1,6 +1,17 @@
+import numpy as np
 from geomstats.geometry.manifold import Manifold
+import geomstats.backend as gs
+
 
 class Flag(Manifold):
+    def __init__(self, index, n, ambient_manifold):
+        d = len(index)
+        dim = gs.sum(np.diff(gs.concatenate(([0], index))) * (n-gs.array(index)))  # cf [Ye2021] p 17
+        super(Flag, self).__init__(dim=dim, shape=(n*d, n*d), default_point_type="matrix")
+        self.n = n
+        self.d = d
+        self.ambient_manifold = ambient_manifold
+
     def belongs(self, point, atol=gs.atol):
         pass
 
