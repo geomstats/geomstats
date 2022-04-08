fe385706357d950c0e9b0c8f447f1018c5b1b074
yann_dm <yann.montmarin@gmail.com>
2022-04-08 11:40:45 +0200

    Add pullback new structure and simple fix and generalization

--- geomstats/geometry/pullback_metric.py
+++ geomstats/geometry/pullback_metric.py
@@ -2,7 +2,7 @@
 
 Lead author: Nina Miolane.
 """
-
+import abc
 import itertools
 
 import joblib
@@ -42,23 +42,28 @@ class PullbackMetric(RiemannianMetric):
     def __init__(
         self,
         dim,
-        embedding_dim,
+        embedding_space,
         immersion,
         jacobian_immersion=None,
         tangent_immersion=None,
     ):
         super(PullbackMetric, self).__init__(dim=dim)
-        self.embedding_metric = EuclideanMetric(embedding_dim)
+        self.embedding_space = embedding_space
+        self.embedding_metric = embedding_space.metric
+
         self.immersion = immersion
+
         if jacobian_immersion is None:
             jacobian_immersion = gs.autodiff.jacobian(immersion)
-        self.jacobian_immersion = jacobian_immersion
+
         if tangent_immersion is None:
 
             def _tangent_immersion(v, x):
-                return gs.matmul(jacobian_immersion(x), v)
+                # TODO: More general for shape
+                return gs.einsum('...ij,...j->...j', jacobian_immersion(x), v)
+            tangent_immersion = _tangent_immersion
 
-        self.tangent_immersion = _tangent_immersion
+        self.tangent_immersion = tangent_immersion
 
     def metric_matrix(self, base_point=None, n_jobs=1, **joblib_kwargs):
         r"""Metric matrix at the tangent space at a base point.
@@ -106,3 +111,53 @@ class PullbackMetric(RiemannianMetric):
 
         metric_mat = gs.reshape(gs.array(out), (-1, self.dim, self.dim))
         return metric_mat[0] if base_point.ndim == 1 else metric_mat
+
+
+class PullbackDiffeoMetric(RiemannianMetric, abc.ABC):
+
+    def __init__(self, dim):
+        super(PullbackMetric, self).__init__(dim=dim)
+        self.embedding_space = self._create_embedding_space()
+        self.embedding_metric = self.embedding_space.metric
+        self._jacobian = None
+        self._inverse_jacobian = None
+
+    @abc.abstractmethod
+    def _create_embedding_space(self):
+        pass
+
+    @abc.abstractmethod
+    def diffeomorphism(self, base_point):
+        pass
+
+    @abc.abstractmethod
+    def inverse_diffeomorphism(self, im_point):
+        pass
+
+    @property
+    def jacobian(self):
+        # Can be overwritten or never use if tangent method are
+        # How it works when batched with autodiff ??
+        if self._jacobian is None:
+            self._jacobian = gs.autodiff.jacobian(self.diffeomorphism)
+        return self._jacobian
+
+    @property
+    def inverse_jacobian(self):
+        # Can be overwritten or never use if tangent method are
+        # How it works when batched with autodiff ??
+        if self._inverse_jacobian is None:
+            self._inverse_jacobian = gs.autodiff.jacobian(self.inverse_diffeomorphism)
+        return self._inverse_jacobian
+
+    def tangent_diffeomorphism(self, tangent_vec, base_point):
+        # Can be overwritten when close form
+        # TODO: More general for shape
+        return gs.matmul(self.jacobian(base_point), tangent_vec)
+
+    def inverse_tangent_diffeomorphism(self, tangent_vec, im_point):
+        # Can be overwritten when close form
+        # TODO: More general for shape
+        return gs.matmul(self.inverse_jacobian(im_point), tangent_vec)
+
+    # HERE ALL THE METHOD IN THE CASE OF DIFFEO
