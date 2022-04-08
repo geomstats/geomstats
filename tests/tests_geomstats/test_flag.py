ad8ac5fdbb262b33ada1bbbee2a3a91ffce524aa
Tom Szwagier <tom.szwagier@mines-paristech.fr>
2022-04-08 17:58:44 +0200

    update `belongs_test_data` to make it actually tested

--- tests/tests_geomstats/test_flag.py
+++ tests/tests_geomstats/test_flag.py
@@ -15,32 +15,31 @@ class TestFlag(ManifoldTestCase, metaclass=Parametrizer):
     skip_test_random_tangent_vec_is_tangent = True
 
     class TestDataFlag(_ManifoldTestData):
-        n = 5
-        index = [1, 3, 4]
-        p1 = gs.array(
-            [gs.array(np.diag([1, 0, 0, 0, 0])), gs.array(np.diag([0, 1, 1, 0, 0])),
-             gs.array(np.diag([0, 0, 0, 1, 0]))])
-        p2 = gs.array(
-            [gs.array(np.diag([0, 1, 0, 0, 0])), gs.array(np.diag([1, 0, 0, 1, 0])),
-             gs.array(np.diag([0, 0, 1, 0, 0]))])
-        p3 = gs.array(
-            [gs.array(np.diag([1, 0, 0, 0, 0])), gs.array(np.diag([1, 0, 1, 0, 0])),
-             gs.array(np.diag([0, 0, 0, 1, 0]))])
-        p4 = gs.array(
-            [gs.array(np.diag([1, 0, 0, 0, 0])), gs.array(np.diag([0, 1, 1, 0, 0])),
-             gs.array(np.diag([0, 0, 0, 1, 1]))])
-        p5 = gs.zeros((10, len(index), n, n))
 
         def belongs_test_data(self):
-            smoke_data = [
-                dict(n=5, index=[1, 3, 4], point=p1, expected=True),
-                dict(n=5, index=[1, 3, 4], point=p2, expected=True),
-                dict(n=5, index=[1, 3, 4], point=gs.array([p1, p2]), expected=[
-                    True, True]),
-                dict(n=5, index=[1, 3, 4], point=p3, expected=False),
-                dict(n=5, index=[1, 3, 4], point=p4, expected=False),
-                dict(n=5, index=[1, 3, 4], point=p5, expected=10 * [False]),
+            n = 5
+            index = [1, 3, 4]
+            p1 = gs.array(
+                [gs.array(np.diag([1, 0, 0, 0, 0])), gs.array(np.diag([0, 1, 1, 0, 0])),
+                 gs.array(np.diag([0, 0, 0, 1, 0]))])
+            p2 = gs.array(
+                [gs.array(np.diag([0, 1, 0, 0, 0])), gs.array(np.diag([1, 0, 0, 1, 0])),
+                 gs.array(np.diag([0, 0, 1, 0, 0]))])
+            p3 = gs.array(
+                [gs.array(np.diag([1, 0, 0, 0, 0])), gs.array(np.diag([1, 0, 1, 0, 0])),
+                 gs.array(np.diag([0, 0, 0, 1, 0]))])
+            p4 = gs.array(
+                [gs.array(np.diag([1, 0, 0, 0, 0])), gs.array(np.diag([0, 1, 1, 0, 0])),
+                 gs.array(np.diag([0, 0, 0, 1, 1]))])
+            p5 = gs.zeros((10, len(index), n, n))
 
+            smoke_data = [
+                dict(n=n, index=index, point=p1, expected=True),
+                dict(n=n, index=index, point=p2, expected=True),
+                dict(n=n, index=index, point=gs.array([p1, p2]), expected=2*[True]),
+                dict(n=n, index=index, point=p3, expected=False),
+                dict(n=n, index=index, point=p4, expected=False),
+                dict(n=n, index=index, point=p5, expected=10*[False]),
             ]
             return self.generate_tests(smoke_data)
 
@@ -51,9 +50,12 @@ class TestFlag(ManifoldTestCase, metaclass=Parametrizer):
             pass
 
         def to_tangent_is_tangent_test_data(self):
-            pass
+            return []
 
         def random_tangent_vec_is_tangent_test_data(self):
             pass
 
     testing_data = TestDataFlag()
+
+    def test_belongs(self, n, k, point, expected):
+        self.assertAllClose(self.space(n, k).belongs(point), gs.array(expected))
