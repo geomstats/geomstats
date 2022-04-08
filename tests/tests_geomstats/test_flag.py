47e45614dfce635b220002397ab63b70b444da3e
Tom Szwagier <tom.szwagier@mines-paristech.fr>
2022-04-08 18:12:54 +0200

    implement `is_tangent_test_data`

--- tests/tests_geomstats/test_flag.py
+++ tests/tests_geomstats/test_flag.py
@@ -37,9 +37,28 @@ class TestFlag(ManifoldTestCase, metaclass=Parametrizer):
                 dict(n=n, index=index, point=p1, expected=True),
                 dict(n=n, index=index, point=p2, expected=True),
                 dict(n=n, index=index, point=gs.array([p1, p2]), expected=2*[True]),
-                dict(n=n, index=index, point=p3, expected=False),
-                dict(n=n, index=index, point=p4, expected=False),
-                dict(n=n, index=index, point=p5, expected=10*[False]),
+                dict(n=n, index=index, point=p3, expected=False),  # same subspace
+                dict(n=n, index=index, point=p4, expected=False),  # wrong trace
+                dict(n=n, index=index, point=p5, expected=10*[False])  # wrong trace
+            ]
+            return self.generate_tests(smoke_data)
+
+        def is_tangent_test_data(self):
+            n = 5
+            index = [1, 3, 4]
+            p1 = gs.array(
+                [gs.array(np.diag([1, 0, 0, 0, 0])), gs.array(np.diag([0, 1, 1, 0, 0])),
+                 gs.array(np.diag([0, 0, 0, 1, 0]))])
+            v1 = gs.array(
+                [gs.array(np.diag([1, 0, 0, 0, 0])), gs.array(np.diag([0, 1, 1, 0, 0])),
+                 gs.array(np.diag([0, 0, 0, 1, 0]))])
+            v2 = gs.zeros((len(index), n, n))
+
+            smoke_data = [
+                dict(n=n, index=index, vector=v1, base_point=p1, expected=False),
+                dict(n=n, index=index, vector=v2, base_point=p1, expected=True),
+                dict(n=n, index=index, vector=gs.array([v1, v2]), base_point=gs.array(
+                    [p1, p1]), expected=[False, True])
             ]
             return self.generate_tests(smoke_data)
 
@@ -57,5 +76,9 @@ class TestFlag(ManifoldTestCase, metaclass=Parametrizer):
 
     testing_data = TestDataFlag()
 
-    def test_belongs(self, n, k, point, expected):
-        self.assertAllClose(self.space(n, k).belongs(point), gs.array(expected))
+    def test_belongs(self, n, index, point, expected):
+        self.assertAllClose(self.space(n, index).belongs(point), gs.array(expected))
+
+    def test_is_tangent(self, n, index, vector, base_point, expected):
+        self.assertAllClose(self.space(n, index).is_tangent(vector, base_point),
+                            gs.array(expected))
