5be9407cbb67ddbdc167015d6ac7d1a571e79f23
ythanwer <yann.thanwerdas@gmail.com>
2022-04-08 16:17:42 +0200

    Exp, log, dist, curvature, parallel transport for the diffeomorphic pullback metric.

--- geomstats/geometry/pullback_metric.py
+++ geomstats/geometry/pullback_metric.py
@@ -159,3 +159,135 @@ class PullbackDiffeoMetric(RiemannianMetric, abc.ABC):
         return gs.matmul(self.inverse_jacobian(im_point), tangent_vec)
 
     # HERE ALL THE METHOD IN THE CASE OF DIFFEO
+    def exp(self, tangent_vec, base_point, **kwargs):
+        """
+        Compute the exponential map via diffeomorphic pullback.
+        Parameters
+        ----------
+        tangent_vec : array-like
+            Tangent vector.
+        base_point : array-like
+            Base point.
+        Returns
+        -------
+        exp : array-like
+            Riemannian exponential of tangent_vec at base_point.
+        """
+        new_base_point = self.diffeomorphism(base_point)
+        new_tangent_vec = self.tangent_diffeomorphism(tangent_vec, base_point)
+        new_exp = self.embedding_metric.exp(new_tangent_vec,
+                                            new_base_point, **kwargs)
+        exp = self.inverse_diffeomorphism(new_exp)
+        return exp
+
+    def log(self, point, base_point, **kwargs):
+        """
+        Compute the logarithm map via diffeomorphic pullback.
+        Parameters
+        ----------
+        point : array-like
+            Point.
+        base_point : array-like
+            Point.
+        Returns
+        -------
+        log : array-like
+            Logarithm of point from base_point.
+        """
+        new_base_point = self.diffeomorphism(base_point)
+        new_point = self.diffeomorphism(point)
+        new_log = self.embedding_metric.log(new_point, new_base_point,
+                                            **kwargs)
+        log = self.inverse_tangent_diffeomorphism(new_log, new_base_point)
+        return log
+
+    def dist(self, point_a, point_b, **kwargs):
+        """
+        Compute astutely the distance when the immersion is a diffeomorphism.
+        Parameters
+        ----------
+        point_a : array-like
+            Point a.
+        point_b : array-like
+            Point b.
+        Returns
+        -------
+        dist : array-like
+            Distance between point_a and point_b.
+        """
+        new_point_a = self.diffeomorphism(point_a)
+        new_point_b = self.diffeomorphism(point_b)
+        new_distance = self.embedding_metric.dist(new_point_a,
+                                                  new_point_b, **kwargs)
+        return new_distance
+
+    def curvature(
+            self, tangent_vec_a, tangent_vec_b, tangent_vec_c, base_point):
+        """
+        Compute astutely the curvature when the immersion is a diffeomorphism.
+        Parameters
+        ----------
+        tangent_vec_a : array-like
+            Tangent vector a.
+        tangent_vec_b : array-like
+            Tangent vector b.
+        tangent_vec_c : array-like
+            Tangent vector c.
+        base_point : array-like
+            Base point.
+        Returns
+        -------
+        curvature : array-like
+            Curvature in directions tangent_vec a, b, c at base_point.
+        """
+        new_base_point = self.diffeomorphism(base_point)
+        new_tangent_vec_a = self.tangent_diffeomorphism(tangent_vec_a,
+                                                        base_point)
+        new_tangent_vec_b = self.tangent_diffeomorphism(tangent_vec_b,
+                                                        base_point)
+        new_tangent_vec_c = self.tangent_diffeomorphism(tangent_vec_c,
+                                                        base_point)
+        new_curvature = self.embedding_metric.curvature(
+            new_tangent_vec_a, new_tangent_vec_b, new_tangent_vec_c,
+            new_base_point)
+        curvature = self.inverse_tangent_diffeomorphism(new_curvature,
+                                                        new_base_point)
+        return curvature
+
+    def parallel_transport(
+            self, tangent_vec, base_point, direction=None, end_point=None
+    ):
+        """
+        Compute astutely the parallel transport when the immersion is a diffeo.
+        Parameters
+        ----------
+        tangent_vec : array-like
+            Tangent vector.
+        base_point : array-like
+            Base point.
+        direction : array-like
+            Direction.
+        end_point : array-like
+            End point.
+        Returns
+        -------
+        parallel_transport : array-like
+            Parallel transport.
+        """
+        new_base_point = self.diffeomorphism(base_point)
+        new_tangent_vec = self.tangent_diffeomorphism(tangent_vec)
+        if direction is None:
+            new_direction = None
+        else:
+            new_direction = self.tangent_diffeomorphism(direction)
+        if end_point is None:
+            new_end_point = None
+        else:
+            new_end_point = self.diffeomorphism(end_point)
+        new_parallel_transport = self.embedding_metric.parallel_transport(
+            new_tangent_vec, new_base_point, direction=new_direction,
+            end_point=new_end_point)
+        parallel_transport = self.inverse_tangent_diffeomorphism(
+            new_parallel_transport, new_base_point
+        )
+        return parallel_transport
