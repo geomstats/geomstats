0edf1d44d15c6cc8a2641bac3c034a854efed84f
Tom Szwagier <tom.szwagier@mines-paristech.fr>
2022-04-08 11:32:00 +0200

    clean PEP8

--- geomstats/geometry/flag.py
+++ geomstats/geometry/flag.py
@@ -12,7 +12,7 @@ from geomstats.geometry.matrices import Matrices
 
 
 class Flag(Manifold):
-    r""" Class for flag manifolds.
+    r"""Class for flag manifolds.
 
     Representation, notations and formulas inspired from [Ye2021].
 
@@ -195,7 +195,39 @@ class Flag(Manifold):
         return each_is_tangent(vector, base_point)
 
     def to_tangent(self, vector, base_point):
+        """Project a vector to a tangent space of the manifold.
+
+        Parameters
+        ----------
+        vector : array-like, shape=[..., dim]
+            Vector.
+        base_point : array-like, shape=[..., dim]
+            Point on the manifold.
+
+        Returns
+        -------
+        tangent_vec : array-like, shape=[..., dim]
+            Tangent vector at base point.
+        """
         pass
 
     def random_point(self, n_samples=1, bound=1.0):
+        """Sample random points on the manifold.
+
+        If the manifold is compact, a uniform distribution is used.
+
+        Parameters
+        ----------
+        n_samples : int
+            Number of samples.
+            Optional, default: 1.
+        bound : float
+            Bound of the interval in which to sample for non compact manifolds.
+            Optional, default: 1.
+
+        Returns
+        -------
+        samples : array-like, shape=[..., {dim, [n, n]}]
+            Points sampled on the manifold.
+        """
         pass
