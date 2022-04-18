a4efb80d477cefa8c2e90bb4a5482390c0d55691
Saiteja Utpala <saitejautpala@gmail.com>
2022-04-18 16:48:34 -0700

    increase tol

--- tests/data/hyperbolic_data.py
+++ tests/data/hyperbolic_data.py
@@ -121,7 +121,10 @@ class HyperbolicTestData(_LevelSetTestData):
 
     def random_tangent_vec_is_tangent_test_data(self):
         return self._random_tangent_vec_is_tangent_test_data(
-            Hyperbolic, self.space_args_list, self.n_vecs_list
+            Hyperbolic,
+            self.space_args_list,
+            self.n_vecs_list,
+            is_tangent_atol=gs.atol * 1000,
         )
 
 
