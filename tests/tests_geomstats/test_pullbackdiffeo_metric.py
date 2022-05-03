a9e5da4eca23b9b3725712950f759b45a5c75959
yann_dm <yann.montmarin@gmail.com>
2022-05-03 21:50:21 +0200

    Better skipping

--- tests/tests_geomstats/test_pullbackdiffeo_metric.py
+++ tests/tests_geomstats/test_pullbackdiffeo_metric.py
@@ -1,6 +1,6 @@
 """Unit tests for the pull-back diffeo metrics."""
 
-import geomstats.tests
+from geomstats.tests import autograd_backend, pytorch_backend
 from tests.conftest import Parametrizer
 from tests.data.pullback_diffeo_metric_data import (
     CircleAsSO2Metric,
@@ -11,9 +11,9 @@ from tests.geometry_test_cases import PullbackDiffeoMetricTestCase
 from tests.tests_geomstats.test_hypersphere import HypersphereMetricTestCase
 
 
-@geomstats.tests.autograd_and_torch_only
 class TestHypersphereBisMetric(HypersphereMetricTestCase, metaclass=Parametrizer):
     metric = connection = CircleAsSO2Metric
+
     skip_test_exp_geodesic_ivp = True
     skip_test_dist_point_to_itself_is_zero = True
     skip_test_christoffels_shape = True
@@ -21,15 +21,17 @@ class TestHypersphereBisMetric(HypersphereMetricTestCase, metaclass=Parametrizer
     skip_test_triangle_inequality_of_dist = True
     skip_test_parallel_transport_bvp_is_isometry = True
     skip_test_parallel_transport_ivp_is_isometry = True
+    skip_all = not autograd_backend() or not pytorch_backend()
 
     testing_data = CircleAsSO2MetricTestData()
 
 
-@geomstats.tests.autograd_and_torch_only
 class TestCircleAsSO2PullbackDiffeoMetric(
     PullbackDiffeoMetricTestCase, metaclass=Parametrizer
 ):
 
     metric = CircleAsSO2Metric
+
+    skip_all = not autograd_backend() or not pytorch_backend()
 
     testing_data = CircleAsSO2PullbackDiffeoMetricTestData()
