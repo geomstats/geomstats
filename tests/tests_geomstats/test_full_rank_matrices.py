12b9d478eb1d4c354aa175cb0d19f10496ab49cb
L. F. Pereira <luisfgpereira95@gmail.com>
2022-05-12 10:28:24 +0200

    Simplify FullRankMatricesTestData

--- tests/tests_geomstats/test_full_rank_matrices.py
+++ tests/tests_geomstats/test_full_rank_matrices.py
@@ -1,7 +1,6 @@
 """Unit tests for full rank matrices."""
 
 import geomstats.backend as gs
-from geomstats.geometry.full_rank_matrices import FullRankMatrices
 from tests.conftest import Parametrizer
 from tests.data.full_rank_matrices_data import FullRankMatricesTestData
 from tests.geometry_test_cases import OpenSetTestCase
@@ -9,8 +8,8 @@ from tests.geometry_test_cases import OpenSetTestCase
 
 class TestFullRankMatrices(OpenSetTestCase, metaclass=Parametrizer):
 
-    space = FullRankMatrices
     testing_data = FullRankMatricesTestData()
+    space = testing_data.space
 
     def test_belongs(self, m, n, mat, expected):
         self.assertAllClose(self.space(m, n).belongs(gs.array(mat)), gs.array(expected))
