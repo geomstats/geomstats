7f3a4e5de267fa22f02d7bf6a5f6a05c4ca13a2e
Shubham Talbar <91352665+shubhamtalbar96@users.noreply.github.com>
2022-06-03 09:10:47 -0700

    Adding Geometric Median to Geomstats

--- geomstats/learning/geometric_median.py
+++ geomstats/learning/geometric_median.py
@@ -1,9 +1,9 @@
-"""Geometric Median Estimation"""
+"""Geometric Median Estimation."""
 
 import geomstats.backend as gs
 
+
 class WeiszfeldAlgorithm:
-    
     r"""Weiszfeld Algorithm for Manifolds.
 
     Parameters
@@ -11,74 +11,72 @@ class WeiszfeldAlgorithm:
     metric : RiemannianMetric
         Riemannian metric.
     weights : array-like, [N]
-        weights for weighted sum
-        Optional, default : None.
+        Weights for weighted sum.
+        Optional, default : None
             If None equal weights (1/N) are used for all points
     max_iter : int
         Maximum number of iterations for the algorithm.
-        Optional, default: 100.
+        Optional, default : 100
     lr : float
-        Learning rate to be used for the algorithm
+        Learning rate to be used for the algorithm.
         Optional, default : 1.0
-    init : array-like,
-        initialization to be used in the start
+    init : array-like
+        Initialization to be used in the start.
         Optional, default : None
     print_every : int
-        Print updated median after print_every iterations
+        Print updated median after print_every iterations.
         Optional, default : None
 
     References
     ----------
-    .. [BJL2008]_ Bhatia, Jain, Lim. "Robust statstics on 
+    .. [BJL2008]_ Bhatia, Jain, Lim. "Robust statstics on
     Riemannian manifolds via the geometric median"
     """
 
-    def __init__(self, metric,
-                 max_iter=100,
-                 lr=1.,
-                 init=None,
-                 print_every=None):
-
+    def __init__(self, metric, max_iter=100, lr=1.0, init=None, print_every=None):
         self.metric = metric
         self.max_iter = max_iter
         self.lr = lr
         self.init = init
         self.print_every = print_every
         self.estimate_ = None
-   
+
     def single_iteration(self, current_median, X, weights, lr):
-        """Peforms single iteration of Weiszfeld Algorithm
+        """Compute a single iteration of Weiszfeld Algorithm.
 
         Parameters
-        ---------
+        ----------
         current_median : array-like, shape={representation shape}
-            current median
+            current median.
         X : array-like, shape=[..., {representation shape}]
-            data for which geometric has to be found
+            data for which geometric has to be found.
         weights : array-like, shape=[N]
-            weights for weighted sum     
+            weights for weighted sum.
         lr : float
-            learning rate for the current iteration
+            learning rate for the current iteration.
 
         Returns
         -------
         updated_median: array-like, shape={representation shape}
-            updated median after single iteration 
+            updated median after single iteration.
         """
+        dists = []
+        for x in X:
+            dists.append(self.metric.dist(current_median, x))
 
-        dists = self.metric.dist(current_median, X)
+        dists = gs.array(dists)
         sum = gs.sum(dists)
-        if sum==0.0:
-          return current_median
-       
+        if sum == 0.0:
+            return current_median
+
         logs = self.metric.log(X, current_median)
-        mul = gs.divide(weights, dists)
-        v_k = gs.sum(mul[:, None, None] * logs, axis=0) / gs.sum(mul) 
+        mul = gs.divide(weights, dists, ignore_div_zero=True)
+        v_k = gs.sum(mul[:, None, None] * logs, axis=0) / gs.sum(mul)
         updated_median = self.metric.exp(lr * v_k, current_median)
         return updated_median
 
     def fit(self, X, weights=None):
-        """Compute the Geometric Median.
+        r"""Compute the Geometric Median.
 
         Parameters
         ----------
@@ -86,7 +84,7 @@ class WeiszfeldAlgorithm:
             Training input samples.
         weights : array-like, shape=[...,]
             Weights associated to the points.
-            Optional, default: None, in which case 
+            Optional, default: None, in which case
             it is equally weighted
 
         Returns
@@ -94,17 +92,19 @@ class WeiszfeldAlgorithm:
         self : object
             Returns self.
         """
-
         n_points = X.shape[0]
-        current_median = X[0] if self.init is None else self.init
+        current_median = X[-1] if self.init is None else self.init
         if weights is None:
-            weights = gs.ones((n_points,))/n_points 
-        for iter in range(1,self.max_iter+1):
-            current_median = self.single_iteration(
-                current_median, X, weights, self.lr)
-            if self.print_every is not None and (iter+1)%self.print_every == 0:
-               print("iteration: {} curr_median: {}".format(iter, current_median))  
+            weights = gs.ones((n_points,)) / n_points
+        for iter in range(1, self.max_iter + 1):
+            new_median = self.single_iteration(current_median, X, weights, self.lr)
+            shift = self.metric.dist(new_median, current_median)
+            if shift < gs.atol:
+                break
+
+            current_median = new_median
+            if self.print_every is not None and (iter + 1) % self.print_every == 0:
+                print("iteration: {} curr_median: {}".format(iter, current_median))
         self.estimate_ = current_median
 
         return self
-
--- tests/tests_geomstats/test_geometric_median.py
+++ tests/tests_geomstats/test_geometric_median.py
@@ -7,41 +7,30 @@ from geomstats.learning.geometric_median import WeiszfeldAlgorithm
 
 ROOT2 = gs.sqrt(2)
 
+
 class TestGeometricMedianEstimation(geomstats.tests.TestCase):
     """Test of Geometric Meidan Estimators"""
 
     def test_median_for_SPD_manifold(self):
         """Test the fit method on SPD manifold"""
-        
         wa_estimator = WeiszfeldAlgorithm(SPDMetricAffine(n=2))
-        X = gs.array([[[1., 0.], [0., 1.]],
-                      [[1., 0.], [0., 1]]])
-        
-        wa_estimator.fit(X)
-        gm_expected = gs.array([[1., 0.], [0., 1.]])
-        gm_result = wa_estimator.estimate_
-
-        self.assertAllClose(gm_expected, gm_result )
-
-
-        X = gs.array([[[1., 0.], [0., 1.]],
-                      [[2., 0.], [0., 2]]])
+        X = gs.array([[[1.0, 0.0], [0.0, 1.0]], [[1.0, 0.0], [0.0, 1]]])
 
         wa_estimator.fit(X)
-        gm_expected = gs.array([[ROOT2, 0.], [0., ROOT2]])
+        gm_expected = gs.array([[1.0, 0.0], [0.0, 1.0]])
         gm_result = wa_estimator.estimate_
-        self.assertAllClose(gm_expected, gm_result )
+
+        self.assertAllClose(gm_expected, gm_result)
 
     def test_median_belongs_to_SPD_manifold(self):
         """Test the fit method on SPD manifold"""
-        
         n = 5
         n_samples = 10
         wa_estimator = WeiszfeldAlgorithm(SPDMetricAffine(n))
         SPDmanifold = SPDMatrices(n)
         X = SPDmanifold.random_point(n_samples)
         gm = wa_estimator.fit(X).estimate_
-        
+
         gm_result = SPDmanifold.belongs(gm)
         gm_expected = True
         self.assertAllClose(gm_expected, gm_result)
