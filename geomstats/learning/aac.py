0fc48a771184e15137a4765fb2f00e4c5e5b8626
L. F. Pereira <luisfgpereira95@gmail.com>
2022-07-19 18:45:11 +0200

    Add AAC for GPCA

--- geomstats/learning/aac.py
+++ geomstats/learning/aac.py
@@ -1,7 +1,13 @@
 import logging
 import random
 
+import scipy
+from sklearn.decomposition import PCA
+
+import geomstats.backend as gs
 from geomstats.learning.frechet_mean import FrechetMean
+
+# TODO: create AAC and control flow with __new__
 
 
 class AACFrechet:
@@ -30,12 +36,13 @@ class AACFrechet:
         previous_estimate = (
             random.choice(X) if self.init_point is None else self.init_point
         )
+        aligned_X = X
         error = self.epsilon + 1
         iteration = 0
         while error > self.epsilon and iteration < self.max_iter:
             iteration += 1
 
-            aligned_X = self.metric.align_point_to_point(previous_estimate, X)
+            aligned_X = self.metric.align_point_to_point(previous_estimate, aligned_X)
             new_estimate = self.mean_estimator.fit(aligned_X).estimate_
             error = self.metric.total_space_metric.dist(previous_estimate, new_estimate)
 
@@ -51,3 +58,201 @@ class AACFrechet:
         self.n_iter_ = iteration
 
         return self
+
+
+class _WrappedPCA(PCA):
+    # TODO: wrap by manipulating __new__?
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._init_shape = None
+
+    def __repr__(self):
+        # to use *args and **kwargs
+        return object.__repr__(self)
+
+    @property
+    def reshaped_components_(self):
+        if self.components_ is None:
+            return None
+        return gs.reshape(self.components_, (self.n_components, *self._init_shape[1:]))
+
+    @property
+    def reshaped_mean_(self):
+        # TODO: suggest changes in sklearn to do same as components_
+        if self.mean_ is None:
+            return None
+
+        return gs.reshape(self.mean_, self._init_shape[1:])
+
+    def _reshape_X(self, X):
+        self._init_shape = X.shape
+        return gs.reshape(X, (X.shape[0], -1))
+
+    def fit(self, X, y=None):
+        return super().fit(self._reshape_X(X))
+
+    def fit_transform(self, X, y=None):
+        return super().fit_transform(self._reshape_X(X))
+
+    def score_samples(self, X, y=None):
+        return super().score_samples(self._reshape_X(X))
+
+    def score(self, X, y=None):
+        return super().score(self._reshape_X(X))
+
+
+class AACGPC:
+    def __init__(
+        self, metric, n_components=2, epsilon=1e-6, max_iter=20, init_point=None
+    ):
+        self.metric = metric
+        self.epsilon = epsilon
+        self.max_iter = max_iter
+        self.init_point = init_point
+
+        self.pca_solver = _WrappedPCA(n_components=n_components)
+        self.aligner = None
+
+        self.components_ = None
+        self.explained_variance_ = None
+        self.explained_variance_ratio_ = None
+        self.singular_values_ = None
+        self.mean_ = None
+
+    def set_default_aligner(self, s_min, s_max, n_sample_points=10):
+        self.set_aligner("default", s_min, s_max, n_sample_points=n_sample_points)
+
+    def set_aligner(self, aligner, *args, **kwargs):
+        if aligner == "default":
+            self.aligner = PointToGeodesicAligner(self.metric, *args, **kwargs)
+        else:
+            self.aligner = aligner
+
+    def fit(self, X, y=None):
+        x = random.choice(X) if self.init_point is None else self.init_point
+        aligned_X = self.metric.align_point_to_point(x, X)
+
+        self.pca_solver.fit(aligned_X)
+        previous_expl = self.pca_solver.explained_variance_ratio_[0]
+
+        error = self.epsilon + 1
+        iteration = 0
+        while error > self.epsilon and iteration < self.max_iter:
+            mean = self.pca_solver.reshaped_mean_
+            direc = self.pca_solver.reshaped_components_[0]
+
+            geodesic = self.metric.total_space_metric.geodesic(
+                initial_point=mean, initial_tangent_vec=direc
+            )
+
+            aligned_X = self.aligner.align(geodesic, aligned_X)
+            self.pca_solver.fit(aligned_X)
+            expl_ = self.pca_solver.explained_variance_ratio_[0]
+
+            error = expl_ - previous_expl
+            previous_expl = expl_
+
+        if iteration == self.max_iter:
+            logging.warning(
+                f"Maximum number of iterations {self.max_iter} reached. "
+                "The estimate may be inaccurate"
+            )
+
+        self.components_ = self.pca_solver.reshaped_components_
+        self.explained_variance_ = self.pca_solver.explained_variance_
+        self.explained_variance_ratio_ = self.pca_solver.explained_variance_ratio_
+        self.singular_values_ = self.pca_solver.singular_values_
+        self.mean_ = self.pca_solver.reshaped_mean_
+
+        return self
+
+
+class PointToGeodesicAligner:
+    # TODO: move to metric
+    # TODO: create base class
+
+    def __init__(self, metric, s_min, s_max, n_sample_points=10):
+        self.metric = metric
+        self.s_min = s_min
+        self.s_max = s_max
+        self.n_sample_points = n_sample_points
+
+        self.min_dists_ = None
+
+    @property
+    def _s(self):
+        return gs.linspace(self.s_min, self.s_max, num=self.n_sample_points)
+
+    def _get_gamma_s(self, geodesic):
+        return geodesic(self._s)
+
+    def align(self, geodesic, x):
+        gamma_s = self._get_gamma_s(geodesic)
+
+        n_points = 1 if gs.ndim(x) == 2 else gs.shape(x)[0]
+        if n_points > 1:
+            gamma_s = gs.repeat(gamma_s, n_points, axis=0)
+            rep_x = gs.concatenate([x for _ in range(self.n_sample_points)])
+        else:
+            rep_x = x
+
+        dists = gs.reshape(
+            self.metric.dist(gamma_s, rep_x), (self.n_sample_points, n_points)
+        )
+
+        min_dists_idx = gs.argmin(dists, axis=0)
+        perm_indices = min_dists_idx * n_points + gs.arange(n_points)
+        if n_points == 1:
+            perm_indices = perm_indices[0]
+
+        perms = gs.take(self.metric.perm_, perm_indices, axis=0)
+
+        # TODO: delete?
+        self.min_dists_ = gs.take(
+            gs.transpose(dists),
+            min_dists_idx + gs.arange(n_points) * self.n_sample_points,
+        )
+
+        return self.metric.space.permute(x, perms)
+
+
+class GeodesicToPointAligner:
+    def __init__(self, metric, method="BFGS"):
+        self.metric = metric
+
+        self.method = method
+
+        self.opt_results_ = None
+
+    def _objective(self, s, x, geodesic):
+        point = geodesic(s)
+        dist = self.metric.dist(point, x)
+
+        return dist
+
+    def align(self, geodesic, x):
+        n_points = 1 if gs.ndim(x) == 2 else gs.shape(x)[0]
+
+        if n_points == 1:
+            x = gs.expand_dims(x, axis=0)
+
+        perms = []
+        min_dists = []
+        opt_results = []
+        for xx in x:
+            s0 = 0.0
+            res = scipy.optimize.minimize(
+                self._objective, x0=s0, args=(xx, geodesic), method=self.method
+            )
+            perms.append(self.metric.perm_[0])
+            min_dists.append(res.fun)
+
+            opt_results.append(res)
+
+        self.min_dists_ = gs.array(min_dists)
+        self.opt_results_ = opt_results
+
+        new_x = self.metric.space.permute(x, gs.array(perms))
+        return new_x[0] if n_points == 1 else new_x
