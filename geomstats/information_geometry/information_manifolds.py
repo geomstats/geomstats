8a51b1807a5da953faa2a96722d32f5d5535e95c
Jules <jules.deschamps@student-cs.fr>
2022-07-29 11:15:38 -0700

    draft of the information manifold along with fisherraometric

--- geomstats/information_geometry/information_manifolds.py
+++ geomstats/information_geometry/information_manifolds.py
@@ -1,7 +1,10 @@
 """Mixin for manifolds of probability distributions."""
 
+from scipy.integrate import quad
+
 import geomstats.backend as gs
 import geomstats.errors
+from geomstats.geometry.riemannian_metric import RiemannianMetric
 
 
 class InformationManifold:
@@ -16,7 +19,7 @@ class InformationManifold:
         Parameters
         ----------
         point : array-like, shape=[..., dim]
-            Point representing a Gamma distribution.
+            Point representing a probability distribution.
         n_samples : int
             Number of points to sample for each set of parameters in point.
             Optional, default: 1.
@@ -27,6 +30,18 @@ class InformationManifold:
             Sample from the probability distributions.
         """
         raise NotImplementedError("The sample method is not yet implemented.")
+
+    def _hard_pdf(self, point, x):
+        """Hard-coded probability density function.
+
+        Parameters
+        ----------
+        point : array-like, shape=[..., dim]
+            Point representing a probability distribution.
+        x : array-like, shape=[...,]
+            Point where to evaluate the pdf
+        """
+        raise NotImplementedError("The pdf has not been hardcoded yet.")
 
     def point_to_pdf(self, point):
         """Compute pdf associated to point.
@@ -105,3 +120,51 @@ class InformationManifold:
             raise NotImplementedError("The cdf method has not yet been implemented.")
 
         return cdf
+
+
+class FisherRaoMetric(RiemannianMetric):
+    """Class to derive the information metric from the pdf in InformationManifold."""
+
+    def __init__(self, pdf, support, dim):
+        self.pdf = pdf
+        self.support = support
+        self.dim = dim
+
+    def metric_matrix(self, base_point):
+        """Compute the inner-product matrix.
+
+        Compute the inner-product matrix of the Fisher information metric
+        at the tangent space at base point.
+
+        Parameters
+        ----------
+        base_point : array-like, shape=[..., 2]
+            Base point.
+
+        Returns
+        -------
+        mat : array-like, shape=[..., 2, 2]
+            Inner-product matrix.
+        """
+
+        def log_pdf_at_x(x):
+            return lambda point: gs.log(self.pdf(point, x))
+
+        def sqrtscore(x):
+            return gs.autodiff.jacobian(log_pdf_at_x(x))(base_point)
+
+        def score(x):
+            return gs.einsum("...i, ...j -> ...ij", sqrtscore(x), sqrtscore(x))
+
+        return gs.array(
+            [
+                [
+                    quad(
+                        lambda x: score(x)[i, j] * self.pdf(base_point, x),
+                        *self.support
+                    )[0]
+                    for i in range(self.dim)
+                ]
+                for j in range(self.dim)
+            ]
+        )
