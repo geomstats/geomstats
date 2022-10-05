63c40a10577b126973c1636b172a09baba42cf41
Nina Miolane <nmiolane@gmail.com>
2022-10-05 17:32:28 +0200

    Add test to test fisher rao metric against normal distributions: fail

--- tests/data/fisher_rao_metric_data.py
+++ tests/data/fisher_rao_metric_data.py
@@ -2,7 +2,7 @@
 
 import geomstats.backend as gs
 from geomstats.information_geometry.fisher_rao_metric import FisherRaoMetric
-from geomstats.information_geometry.normal import NormalDistributions
+from geomstats.information_geometry.normal import NormalDistributions, NormalMetric
 from tests.data_generation import _RiemannianMetricTestData
 
 
@@ -50,5 +50,18 @@ class FisherRaoMetricTestData(_RiemannianMetricTestData):
                 support=self.support,
                 base_point=gs.array([-1.0, 3.0]),
             ),
+        ]
+        return self.generate_tests(smoke_data)
+
+    def inner_product_and_closed_form_inner_product_test_data(self):
+        smoke_data = [
+            dict(
+                information_manifold=NormalDistributions(),
+                support=(-10, 10),
+                closed_form_metric=NormalMetric(),
+                tangent_vec_a=gs.array([1.0, 2.0]),
+                tangent_vec_b=gs.array([1.0, 2.0]),
+                base_point=gs.array([1.0, 2.0]),
+            )
         ]
         return self.generate_tests(smoke_data)
--- tests/tests_geomstats/test_fisher_rao_metric.py
+++ tests/tests_geomstats/test_fisher_rao_metric.py
@@ -3,27 +3,14 @@
 
 import geomstats.backend as gs
 import tests.conftest
-from tests.conftest import Parametrizer, autograd_backend, np_backend
+from tests.conftest import Parametrizer
 from tests.data.fisher_rao_metric_data import FisherRaoMetricTestData
 from tests.geometry_test_cases import TestCase
 
 
-@tests.conftest.autograd_tf_and_torch_only
+@tests.conftest.autograd_and_torch_only
+# Note: it also works in tensorflow but it is insanely slow.
 class TestFisherRaoMetric(TestCase, metaclass=Parametrizer):
-    skip_test_parallel_transport_ivp_is_isometry = True
-    skip_test_parallel_transport_bvp_is_isometry = True
-    skip_test_exp_geodesic_ivp = True
-    skip_test_exp_shape = np_backend()
-    skip_test_geodesic_ivp_belongs = True
-    skip_test_exp_ladder_parallel_transport = np_backend()
-    skip_test_log_is_tangent = np_backend()
-    skip_test_log_shape = np_backend()
-    skip_test_geodesic_bvp_belongs = np_backend()
-    skip_test_exp_after_log = np_backend() or autograd_backend()
-    skip_test_geodesic_bvp_belongs = True
-    skip_test_log_after_exp = True
-    skip_test_dist_point_to_itself_is_zero = np_backend()
-    skip_test_triangle_inequality_of_dist = True
     testing_data = FisherRaoMetricTestData()
 
     Metric = testing_data.Metric
@@ -41,9 +28,29 @@ class TestFisherRaoMetric(TestCase, metaclass=Parametrizer):
     ):
         metric = self.Metric(information_manifold=information_manifold, support=support)
         inner_prod_mat = metric.metric_matrix(base_point=base_point)
-        print("MAT")
-        print(inner_prod_mat)
         inv_inner_prod_mat = gs.linalg.inv(inner_prod_mat)
         result = gs.matmul(inv_inner_prod_mat, inner_prod_mat)
         expected = gs.eye(information_manifold.dim)
         self.assertAllClose(result, expected)
+
+    def test_inner_product_and_closed_form_inner_product(
+        self,
+        information_manifold,
+        support,
+        closed_form_metric,
+        tangent_vec_a,
+        tangent_vec_b,
+        base_point,
+    ):
+        metric = self.Metric(information_manifold=information_manifold, support=support)
+        inner_prod_mat = metric.inner_product(
+            tangent_vec_a=tangent_vec_a,
+            tangent_vec_b=tangent_vec_b,
+            base_point=base_point,
+        )
+        normal_metric_mat = closed_form_metric.inner_product(
+            tangent_vec_a=tangent_vec_a,
+            tangent_vec_b=tangent_vec_b,
+            base_point=base_point,
+        )
+        self.assertAllClose(inner_prod_mat, normal_metric_mat)
