d90b0ec78ef9d9342626d44c6097150b955418a6
yann_dm <yann.montmarin@gmail.com>
2022-10-18 15:15:14 +0200

    Local basis on manifold level

--- geomstats/geometry/manifold.py
+++ geomstats/geometry/manifold.py
@@ -191,3 +191,196 @@ class Manifold(abc.ABC):
                 gs.random.normal(size=(n_samples,) + self.shape), base_point
             )
         )
+
+    def local_basis(self, base_point):
+        """Local basis of the tangent space of the manifold at the point.
+
+        It correspond to patial_i vector field for an implicit
+        minimal system of coordinate x_i
+
+        Parameters
+        ----------
+        base_point : array-like, shape=[..., *shape]
+            Point.
+
+        Returns
+        -------
+        local_basis : array-like, shape=[..., dim, *shape]
+            Regularized point.
+        """
+        if not self.default_coords_type == "intrinsic":
+            raise NotImplementedError(
+                "Default local basis is only for intrinsic vector "
+                "point case with shape (dim,)"
+            )
+        if base_point.shape == self.shape:
+            return gs.eye(self.dim)
+        return gs.broadcast_to(
+            gs.expand_dims(gs.eye(self.dim), 0),
+            (base_point.shape[0], self.dim, self.dim),
+        )
+
+    def local_gram(self, base_point, inverse=False, _local_basis=None):
+        """Inverse gram matrix of the local basis.
+
+        It is the gram in the sense of the canonical inner product of the shape
+        representation choice as if it was embedded in Eucliden.
+        It is just a matter of representation and does not impact any choices
+        other than the local basis
+
+        Parameters
+        ----------
+        base_point : array-like, shape=[..., *shape]
+            Point.
+
+        Returns
+        -------
+        local_gram : array-like, shape=[..., dim, dim]
+            Regularized point.
+        """
+        if self.default_coords_type == "intrinsic":
+            if base_point.shape == self.shape:
+                return gs.eye(self.dim)
+            return gs.broadcast_to(
+                gs.expand_dims(gs.eye(self.dim), 0),
+                (base_point.shape[0], self.dim, self.dim),
+            )
+
+        local_basis = (
+            self.local_basis(base_point) if _local_basis is None else _local_basis
+        )
+
+        lb_flat = gs.reshape(local_basis, (-1, self.dim, self.shape_dim))
+        gram = gs.einsum("...ik, ...jk -> ...ij", lb_flat, lb_flat)
+
+        if inverse:
+            gram = gs.linalg.inv(gram)
+
+        if base_point.shape == self.shape:
+            return gram[0]
+        return gram
+
+    def local_basis_representation(self, vector, base_point, _local_basis=None):
+        """Express the vector in the local basis of the manifold.
+
+        v=sum v^i e_i so <v,e_j> = sum v^i<e_i,e_j>
+        [v^i] = G^{-1}Pv where P = [e_i] and G = [<e_i,e_j>] = PP^T
+
+        Parameters
+        ----------
+        vector : array-like, shape=[..., *shape]
+            Vector.
+        base_point : array-like, shape=[..., *shape]
+            Point on the manifold.
+
+        Returns
+        -------
+        vector_intra : array-like, shape=[..., dim]
+            Coordinate of the local vector in the locale basis
+        """
+        if self.default_coords_type == "intrinsic":
+            return vector
+
+        local_basis = (
+            self.local_basis(base_point) if _local_basis is None else _local_basis
+        )
+
+        scal_repr = gs.einsum(
+            "...k, ...jk -> ...j",
+            gs.reshape(vector, (-1, self.shape_dim)),
+            gs.reshape(local_basis, (-1, self.dim, self.shape_dim)),
+        )
+        inv_gram = self.local_gram(base_point, local_basis, inverse=True)
+
+        lb_repr = gs.einsum(
+            "...ij, ...j -> ...i",
+            gs.reshape(inv_gram, (-1, self.dim, self.dim)),
+            gs.reshape(scal_repr, (-1, self.dim)),
+        )
+
+        if base_point.shape == self.shape:
+            return lb_repr[0]
+        return lb_repr
+
+    def local_shape_representation(self, vector_intra, base_point, _local_basis=None):
+        """Given a vector express in local base, return.
+
+        The vector in standard shape form
+        v=sum v^i e_i = P[v^i] where P = [e_i]
+
+        Parameters
+        ----------
+        vector_intra : array-like, shape=[..., dim]
+            Vector in local basis representation.
+        base_point : array-like, shape=[..., *shape]
+            Point on the manifold.
+
+        Returns
+        -------
+        vector : array-like, shape=[..., *shape]
+            Coordinate of the local vector in the locale basis
+        """
+        if self.default_coords_type == "intrinsic":
+            return vector_intra
+
+        local_basis = (
+            self.local_basis(base_point) if _local_basis is None else _local_basis
+        )
+
+        flat_repr = gs.einsum(
+            "...i,il -> ...l",
+            gs.reshape(vector_intra, (-1, self.dim)),
+            gs.reshape(local_basis, (self.dim, self.shape_dim)),
+        )
+        shape_rept = gs.reshape(flat_repr, (-1,) + self.shape)
+
+        if base_point.shape == self.shape:
+            return shape_rept[0]
+        return shape_rept
+
+    def _local_basis_representation_vectorize(
+        self, vector, base_point, _local_basis=None
+    ):
+        """Express the vector in the local basis of the manifold.
+
+        WIP: Exemple of total factorization.
+        v=sum v^i e_i so <v,e_j> = sum v^i<e_i,e_j>
+        [v^i] = G^{-1}Pv where P = [e_i] and G = [<e_i,e_j>] = PP^T
+
+        Parameters
+        ----------
+        vector : array-like, shape=[..., *shape]
+            Vector.
+        base_point : array-like, shape=[..., *shape]
+            Point on the manifold.
+
+        Returns
+        -------
+        vector_intra : array-like, shape=[..., dim]
+            Coordinate of the local vector in the locale basis
+        """
+        if self.default_coords_type == "intrinsic":
+            return vector
+
+        rad = vector.shape[: -len(self.shape)]
+
+        local_basis = (
+            self.local_basis(base_point) if _local_basis is None else _local_basis
+        )
+        bc_local_basis = gs.broadcast_to(local_basis, rad + (self.dim,) + self.shape)
+
+        scal_repr = gs.einsum(
+            "...k, ...jk -> ...j",
+            gs.reshape(vector, (-1, self.shape_dim)),
+            gs.reshape(bc_local_basis, (-1, self.dim, self.shape_dim)),
+        )
+        inv_gram = self.local_gram(base_point, _local_basis, inverse=True)
+        bc_inv_gram = gs.broadcast_to(inv_gram, rad + (self.dim, self.dim))
+
+        lb_repr = gs.einsum(
+            "...ij, ...j -> ...i",
+            gs.reshape(bc_inv_gram, (-1, self.dim, self.dim)),
+            gs.reshape(scal_repr, (-1, self.dim)),
+        )
+
+        return lb_repr.reshape(rad + (self.dim,))
