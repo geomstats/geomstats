c50b6dec9cff969ad21176d096ab43f1ad5cef57
johnharveymath <john.harvey.math@gmail.com>
2022-10-19 13:17:09 +0200

    Correcting vectorization issues with metric_matrix

--- geomstats/geometry/euclidean.py
+++ geomstats/geometry/euclidean.py
@@ -97,8 +97,8 @@ class EuclideanMetric(RiemannianMetric):
         """
         mat = gs.eye(self.dim)
         if base_point is not None:
-            if base_point.ndim == 2:
-                mat = gs.tile(mat, (base_point.shape[0], 1, 1))
+            if base_point.ndim > 1:
+                mat = gs.broadcast_to(mat, base_point.shape + (self.dim,))
         return mat
 
     def inner_product(self, tangent_vec_a, tangent_vec_b, base_point=None):
