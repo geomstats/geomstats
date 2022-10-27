adc533487dfbfcf787ab5cece7324ed4fff0044e
Yann Cabanes <yann.cabanes@gmail.com>
2022-10-26 23:54:20 -0400

    Write [Cabanes2022]_ instead of [Cabanes2022] in introduction for proper rendering

--- geomstats/geometry/siegel.py
+++ geomstats/geometry/siegel.py
@@ -6,7 +6,7 @@ It is defined as the set of complex matrices M such that:
 I - M @ M.conj().T is a positive definite matrix.
 Warning: another more restrictive definition of the Siegel disk also exists
 which add a symmetry condition on the matrices.
-It has been proven in [Cabanes2022] that the sub-manifold of symmetric Siegel
+It has been proven in [Cabanes2022]_ that the sub-manifold of symmetric Siegel
 matrices is a totally geodesic sub-manifold of the Siegel space.
 The sub-manifold of real Siegel matrices is also a totally geodesic sub-manifold
 of the Siegel space.
