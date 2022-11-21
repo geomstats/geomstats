1860c28a96f206f6c88f454a1d324fb4d535d484
L. F. Pereira <luisfgpereira95@gmail.com>
2022-11-21 13:07:38 +0100

    Fix linting

--- geomstats/_backend/tensorflow/__init__.py
+++ geomstats/_backend/tensorflow/__init__.py
@@ -343,7 +343,7 @@ def _assignment_single_value(x, value, indices, mode="replace", axis=0):
     if use_vectorization:
         full_shape = shape(x)
         n_samples = full_shape[axis]
-        tile_shape = list(full_shape[:axis]) + list(full_shape[axis + 1:])
+        tile_shape = list(full_shape[:axis]) + list(full_shape[axis + 1 :])
         mask = _vectorized_mask_from_indices(
             n_samples, indices, tile_shape, axis, x.dtype
         )
