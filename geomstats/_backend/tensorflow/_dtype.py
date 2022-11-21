1dd57d8defb5f53a8727a823867939b9f3aac8e4
L. F. Pereira <luisfgpereira95@gmail.com>
2022-11-21 15:19:37 +0100

    Update distributions to accept complex type

--- geomstats/_backend/tensorflow/_dtype.py
+++ geomstats/_backend/tensorflow/_dtype.py
@@ -7,12 +7,18 @@ from geomstats._backend import _backend_config as _config
 from geomstats._backend._dtype_utils import (
     _dyn_update_dtype,
     _modify_func_default_dtype,
+    _pre_allow_complex_dtype,
     _pre_cast_out_from_dtype,
     _pre_cast_out_to_input_dtype,
     _pre_set_default_dtype,
     _update_default_dtypes,
     get_default_cdtype,
     get_default_dtype,
+)
+
+_COMPLEX_DTYPES = (
+    _tf.complex64,
+    _tf.complex128,
 )
 
 
@@ -39,6 +45,39 @@ _cast_out_to_input_dtype = _pre_cast_out_to_input_dtype(
 )
 
 _cast_out_from_dtype = _pre_cast_out_from_dtype(_tf.cast, is_floating, is_complex)
+
+
+def _allow_complex_dtype(target=None):
+    """Allow complex type by calling the function twice.
+
+    Assumes function do not support dtype.
+
+    How it works?
+    -------------
+    Function is called twice if dtype is complex.
+    Output is casted if not corresponding to expected dtype.
+    """
+
+    def _decorator(func):
+        @functools.wraps(func)
+        def _wrapped(*args, **kwargs):
+            dtype = kwargs.get("dtype", None)
+
+            if dtype not in _COMPLEX_DTYPES:
+                return func(*args, **kwargs)
+
+            del kwargs["dtype"]
+            real = _tf.cast(func(*args, **kwargs), dtype)
+            imag = 1j * _tf.cast(func(*args, **kwargs), dtype)
+
+            return real + imag
+
+        return _wrapped
+
+    if target is None:
+        return _decorator
+
+    return _decorator(target)
 
 
 def _box_unary_scalar(target=None):
