6d50001b3f8dd9aaa889bf07007de4bfddb662bc
L. F. Pereira <luisfgpereira95@gmail.com>
2022-11-29 15:05:45 +0100

    Add MatrixLieAlgebra test cases

--- tests2/data/base_data.py
+++ tests2/data/base_data.py
@@ -85,3 +85,39 @@ class MatrixVectorSpaceMixinsTestData(TestData):
     def to_vector_after_from_vector_test_data(self):
         data = [dict(n_points=n_points) for n_points in self.N_RANDOM_POINTS]
         return self.generate_tests(data)
+
+
+class MatrixLieAlgebraTestData(VectorSpaceTestData):
+    def baker_campbell_hausdorff_vec_test_data(self):
+        order = [2] + random.sample(range(3, 10), 1)
+        data = []
+        for order_ in order:
+            data.extend(
+                [dict(n_reps=n_reps, order=order_) for n_reps in self.N_VEC_REPS]
+            )
+
+        return self.generate_tests(data)
+
+    def basis_representation_vec_test_data(self):
+        data = [dict(n_reps=n_reps) for n_reps in self.N_VEC_REPS]
+        return self.generate_tests(data)
+
+    def basis_representation_and_basis_test_data(self):
+        data = [dict(n_points=n_points) for n_points in self.N_RANDOM_POINTS]
+        return self.generate_tests(data)
+
+    def matrix_representation_vec_test_data(self):
+        data = [dict(n_reps=n_reps) for n_reps in self.N_VEC_REPS]
+        return self.generate_tests(data)
+
+    def matrix_representation_belongs_test_data(self):
+        data = [dict(n_points=n_points) for n_points in self.N_RANDOM_POINTS]
+        return self.generate_tests(data)
+
+    def matrix_representation_after_basis_representation_test_data(self):
+        data = [dict(n_points=n_points) for n_points in self.N_RANDOM_POINTS]
+        return self.generate_tests(data)
+
+    def basis_representation_after_matrix_representation_test_data(self):
+        data = [dict(n_points=n_points) for n_points in self.N_RANDOM_POINTS]
+        return self.generate_tests(data)
