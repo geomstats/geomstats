b83e53b7c33342d8f06a39af4ba66fdbe327b444
L. F. Pereira <luisfgpereira95@gmail.com>
2022-12-08 14:01:22 +0100

    Simplify FisherRaoMetric metric_matrix computation

--- geomstats/information_geometry/fisher_rao_metric.py
+++ geomstats/information_geometry/fisher_rao_metric.py
@@ -76,46 +76,31 @@ class FisherRaoMetric(RiemannianMetric):
             Differential Geometric Methods in Statistics, Berlin, Springer – Verlag.
         """
 
-        def log_pdf_at_x(x):
-            """Compute the log function of the pdf for a fixed value on the support.
+        def pdf(x):
+            """Compute pdf at a fixed point on the support.
 
             Parameters
             ----------
             x : float, shape (,)
                 Point on the support of the distribution
             """
-            return lambda point: gs.log(
-                self.information_manifold.point_to_pdf(point)(x)
+            return lambda point: self.information_manifold.point_to_pdf(point)(x)
+
+        def _function_to_integrate(x):
+            pdf_x = pdf(x)
+            pdf_x_at_base_point = pdf_x(base_point)
+            pdf_x_derivative = gs.autodiff.jacobian(pdf_x)
+            pdf_x_derivative_at_base_point = pdf_x_derivative(base_point)
+            return (
+                gs.einsum(
+                    "...i,...j->...ij",
+                    pdf_x_derivative_at_base_point,
+                    pdf_x_derivative_at_base_point,
+                )
+                / pdf_x_at_base_point
             )
 
-        def log_pdf_derivative(x):
-            """Compute the derivative of the log-pdf with respect to the parameters.
-
-            Parameters
-            ----------
-            x : float, shape (,)
-                Point on the support of the distribution
-            """
-            return gs.autodiff.jacobian(log_pdf_at_x(x))(base_point)
-
-        def log_pdf_derivative_squared(x):
-            """Compute the square (in matrix terms) of dlog.
-
-            This is the variable whose expectance is the Fisher-Rao information.
-
-            Parameters
-            ----------
-            x : float, shape (,)
-                Point on the support of the distribution
-            """
-            dlog = log_pdf_derivative(x)
-            return gs.einsum("...i, ...j -> ...ij", dlog, dlog)
-
-        metric_mat = quad_vec(
-            lambda x: log_pdf_derivative_squared(x)
-            * self.information_manifold.point_to_pdf(base_point)(x),
-            *self.support
-        )[0]
+        metric_mat = quad_vec(_function_to_integrate, *self.support)[0]
 
         if metric_mat.ndim == 3 and metric_mat.shape[0] == 1:
             return metric_mat[0]
