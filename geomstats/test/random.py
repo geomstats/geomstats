f93778e3fcd9bb3415a81b0d0587d7e3f1712123
L. F. Pereira <luisfgper@gmail.com>
2023-01-19 11:00:46 +0100

    Update random tangent vec generation

--- geomstats/test/random.py
+++ geomstats/test/random.py
@@ -1,19 +1,18 @@
 import geomstats.backend as gs
 from geomstats.geometry.base import LevelSet, OpenSet, VectorSpace
-from geomstats.geometry.lie_group import LieGroup
 from geomstats.test.vectorization import get_n_points
+
+
+def _get_random_tangent_vec(space, base_point):
+    n_points = get_n_points(space, base_point)
+    batch_shape = (n_points,) if n_points > 1 else ()
+    vec = gs.random.uniform(size=batch_shape + space.shape)
+    return space.to_tangent(vec, base_point)
 
 
 def _get_random_tangent_vec_vector_space(space, base_point):
     n_points = get_n_points(space, base_point)
     vec = space.random_point(n_points)
-    return space.to_tangent(vec, base_point)
-
-
-def _get_random_tangent_vec_lie_group(space, base_point):
-    n_points = get_n_points(space, base_point)
-    batch_shape = (n_points,) if n_points > 1 else ()
-    vec = gs.random.uniform(size=batch_shape + space.shape)
     return space.to_tangent(vec, base_point)
 
 
@@ -27,11 +26,10 @@ def get_random_tangent_vec(space, base_point):
     if isinstance(space, (LevelSet, OpenSet)):
         return _get_random_tangent_vec_from_embedding_space(space, base_point)
 
-    if isinstance(space, LieGroup):
-        return _get_random_tangent_vec_lie_group(space, base_point)
-
     if isinstance(space, VectorSpace):
         return _get_random_tangent_vec_vector_space(space, base_point)
+
+    return _get_random_tangent_vec(space, base_point)
 
 
 def get_random_quaternion(n_points=1):
