fdc13416bde00faaf6081a198930e400385a3536
Christine8388811 <lwan@umail.ucsb.edu>
2023-01-28 13:30:44 -0800

    fixed the import error in notebook

--- tests/tests_geomstats/test_visualization_grassmannian_manifold.py
+++ tests/tests_geomstats/test_visualization_grassmannian_manifold.py
@@ -18,24 +18,25 @@ matplotlib.use("Agg")  # NOQA
 
 class TestVisualizationGrassmanian(tests.conftest.TestCase):
     def setup_method(self):
+        """setup for testing Grassmannian manifold
+        """
         self.n_samples = 10
         self.grassmannian21 = Grassmannian(2, 1)
         self.grassmannian31 = Grassmannian(3, 1)
         plt.figure()
 
     def test_plot_2d(self):
-        """
-        Test the plotting function of Grassmannian for 2 dimensions
+        """Test the plotting function of Grassmannian for 2 dimensions
         """
         self.grassmannian21.plot(True)
 
     def test_plot_2d_render(self):
-        """
-        Test the plotting function of Grassmannian for 2 dimensions
+        """Test the plotting function of Grassmannian for 2 dimensions
         when it's regularly sampled
         """
         self.grassmannian21.plot_rendering(True)
 
     @staticmethod
     def teardown_method():
+        
         plt.close()
