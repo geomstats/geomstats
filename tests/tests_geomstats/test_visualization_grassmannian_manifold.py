6f365fb4741c511ca4fe4e4ffb139a7745c56bb4
Joshua Kim <skim945@umail.ucsb.edu>
2023-01-28 20:59:20 -0800

    fixing test cases

--- tests/tests_geomstats/test_visualization_grassmannian_manifold.py
+++ tests/tests_geomstats/test_visualization_grassmannian_manifold.py
@@ -2,20 +2,22 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import tests.conftest
-from grassmannian_manifold.grassmannian import Grassmannian
+from geomstats.visualization.grassmannian import Grassmannian
 
 matplotlib.use("Agg")  # NOQA
 
 
 class TestVisualizationGrassmannian(tests.conftest.TestCase):
+    
     """
     Test class specifically to test Grassmannian
     manifold by plotting 2D and 3D figure
     """
+
     def setup_method(self):
         """
-        Prepares a Grassmannian object for 2D and 3D plotting
-         that uses 10 samples in the figure.
+        Test a Grassmannian object for 2D and 3D plotting
+        that uses 10 samples in the figure.
         """
         self.n_samples = 10
         self.grassmannian21 = Grassmannian(2, 1)
@@ -23,7 +25,8 @@ class TestVisualizationGrassmannian(tests.conftest.TestCase):
         plt.figure()
 
     def test_plot_3d(self):
-        """Test to see if the plot function return a 3d plot
+        """
+        Test to see if the plot function return a 3d plot
         with grid existing or not based on the boolean value
         passed onto the function.
         """
@@ -35,8 +38,10 @@ class TestVisualizationGrassmannian(tests.conftest.TestCase):
     #     points = self.grassmannian31.random_uniform(self.n_samples)
     #     self.grassmannian31.plot(False)
     def test_plot_rendering_3d(self):
-        """ "Test to see if the plot function returns a 3d plot
-        with a manifold that has a regularly sampled data."""
+        """
+        Test to see if the plot function returns a 3d plot
+        with a manifold that has a regularly sampled data.
+        """
         # self.tick = True
         # points = self.grassmannian31.random_uniform(self.n_samples)
         self.grassmannian31.plot(True)
