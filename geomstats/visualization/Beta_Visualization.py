9b382504854686e04b4712dae994482f0deb60dd
mariannearriola <marianne.arriola@gmail.com>
2023-01-30 21:57:41 -0800

    Added updated scripts for visualizing manifold of beta distributions

---
+++ geomstats/visualization/Beta_Visualization.py
@@ -0,0 +1,459 @@
+"""
+This module contains a class Beta capable of plotting Beta manifolds.
+
+By Yiliang Chen & Sunpeng Duan & Marianne Arriola & Ryan Stofer & Allen Wang
+"""
+import geomstats.backend as gs
+import matplotlib.pyplot as plt
+import numpy as np
+from geomstats.geometry.special_euclidean import SpecialEuclidean
+from geomstats.information_geometry.beta import BetaDistributions
+
+SE2_GROUP = SpecialEuclidean(n=2, point_type="matrix")
+
+beta = BetaDistributions()
+
+
+class Beta:
+    """Class for the visualization of beta manifold.
+
+    Parameters
+    ----------
+    points : array-like, shape=[..., 2]
+            Point representing a beta distribution.
+    Returns
+    -------
+    Plots
+    """
+
+    def __init__(self):
+        """No attributes in Beta class."""
+        pass
+
+    def process_points(self, points, **kwargs):
+        """Process all manifold points.
+
+        Confirms that points passed into function lie on manifold.
+        Prepares points for plotting.
+
+        by Marianne Arriola
+
+        Parameters
+        ----------
+        points : array-like, shape=[..., 2]
+            Beta manifold points to be plotted.
+        """
+        points = np.array(points)
+        if len(points.shape) == 1:
+            points = np.expand_dims(points, axis=0)
+
+        if not len(points) > 0:
+            raise ValueError("No points given")
+        if not np.all(points > 0):
+            raise ValueError(
+                "Points must be in the upper-right quadrant of Euclidean space"
+            )
+        if not ((points.shape[-1] == 2 and len(points.shape) == 2)):
+            raise ValueError("Points must lie in 2D space")
+        limit = np.max(points)
+        limit += limit / 10
+        return points, limit
+
+    def plot(self, points, size=None, **kwargs):
+        """Draws the beta manifold.
+
+        by Yiliang Chen & Marianne Arriola & Ryan Stofer
+
+        Parameters
+        ----------
+        points : array-like, shape=[..., 2]
+            Beta manifold points to be plotted.
+        size : array-like, shape=[..., 2]
+            Defines the range of the manifold to be shown.
+            Optional, default: None
+        """
+        points, limit = self.process_points(points)
+        fig = plt.figure(figsize=(5, 5))
+        ax = fig.add_subplot(111)
+        if not size:
+            ax.set(xlim=(0, limit), ylim=(0, limit))
+        else:
+            ax.set(xlim=(0, size[0]), ylim=(0, size[1]))
+        ax.scatter(points[:, 0], points[:, 1], **kwargs)
+        plt.title("Points on 2D manifold of beta distributions")
+        plt.xlabel(r"$\alpha$")
+        plt.ylabel(r"$\beta$")
+
+    def plot_rendering(
+        self,
+        initial_point=[0, 0],
+        size=[10, 10],
+        sampling_period=1
+    ):
+        """Draws the beta manifold.
+
+        by Yiliang Chen & Allen Wang
+
+        Parameters
+        ----------
+        Initial_point : array-like, shape=[1, 2]
+            Defines initial point for plot rendering
+            Optional, default: [0,0]
+        size : array-like, shape=[..., 2]
+            Defines the range of the samples to be shown
+            Optional, default: [10,10]
+        sampling_period : float, >0
+            Defines the sampling period of the sampled data
+            Optional, default: 1
+        """
+        for value in initial_point:
+            if value < 0:
+                raise ValueError(
+                    "Initial Point {} is not in the first quadrant".format(
+                        initial_point
+                    )
+                )
+
+        sz = gs.array(size)
+        if sz.size != 2:
+            raise ValueError("size should be a 1*2 array")
+        x = gs.linspace(
+            initial_point[0],
+            (initial_point[0] + size[0] - 1) * sampling_period,
+            size[0],
+        )
+        y = gs.linspace(
+            initial_point[1],
+            (initial_point[1] + size[1] - 1) * sampling_period,
+            size[1],
+        )
+        points = [[i, j] for i in x for j in y]
+        points_x = [i[0] for i in points]
+        points_y = [i[1] for i in points]
+        fig = plt.figure(figsize=(5, 5))
+        ax = fig.add_subplot(111)
+
+        ax.scatter(points_x, points_y)
+        plt.xlabel(r"$\alpha$")
+        plt.ylabel(r"$\beta$")
+
+    def plot_grid(
+        self,
+        size,
+        initial_point=[0, 0],
+        n_steps=100,
+        n_points=10,
+        step=1,
+        **kwargs
+    ):
+        """Draws the grids of beta manifold.
+
+        by Yiliang Chen
+
+        Parameters
+        ----------
+        size : array-like, shape=[..., 2]
+            Defines the range of the grids to be shown.
+        initial_point : array-like, shape=[1,2]
+            Defines the initial point for plotting the beta manifold grid.
+            Optional, default: [0,0]
+        n_steps : int, >0
+            Defines the number of steps for integration.
+            Optional, default: 100
+        n_points : int, >0
+            Defines the number of points for interpolation.
+            Optional, default: 10
+        step : float, >0
+            Defines the length of a step for the grid
+            Optional, default: 1
+        """
+        for value in initial_point:
+            if value < 0:
+                raise ValueError(
+                    "Initial Point {} is not in the first quadrant".format(
+                        initial_point
+                    )
+                )
+
+        sz = gs.array(size)
+        if sz.size != 2:
+            raise ValueError("size should be a 1*2 array")
+        b = [(initial_point[0] + i * step) for i in range(size[0])]
+        gF = [(initial_point[1] + i * step) for i in range(size[1])]
+
+        t = gs.linspace(0, 1, n_points)
+        fig = plt.figure(figsize=(5, 5))
+        ax = fig.add_subplot(111)
+
+        for i in b:
+            for j in gF:
+                start = [i, j]
+                end_h = [i + step, j]
+                end_v = [i, j + step]
+                if i < (size[0] + initial_point[0] - 1):
+                    grid_h = beta.metric.geodesic(
+                        initial_point=start, end_point=end_h, n_steps=n_steps
+                    )
+                    ax.plot(*gs.transpose(gs.array([grid_h(k) for k in t])))
+                if j < (size[1] + initial_point[1] - 1):
+                    grid_v = beta.metric.geodesic(
+                        initial_point=start, end_point=end_v, n_steps=n_steps
+                    )
+                    ax.plot(*gs.transpose(gs.array([grid_v(k) for k in t])))
+        plt.xlabel(r"$\alpha$")
+        plt.ylabel(r"$\beta$")
+        plt.title("Grids in the beta manifold")
+
+    def scatter(self, points, **kwargs):
+        """Scatter plot of beta manifold.
+
+        by Sunpeng Duan & Marianne Arriola
+
+        Parameters
+        ----------
+        points : array-like, shape=[..., 2]
+            Manifold point representing a beta distribution.
+        """
+        points, limit = self.process_points(points)
+
+        fig = plt.figure(figsize=(5, 5))
+        ax = fig.add_subplot(111)
+        ax.set(xlim=(0, limit), ylim=(0, limit))
+        ax.scatter(points[:, 0], points[:, 1], **kwargs)
+        plt.xlabel(r"$\alpha$")
+        plt.ylabel(r"$\beta$")
+        ax.set_title("Scatter plot of beta manifolds")
+
+    def plot_geodesic(
+        self,
+        initial_point,
+        end_point=None,
+        initial_tangent_vec=None,
+        n_steps=100,
+        n_points=10,
+        **kwargs,
+    ):
+        """Geodesic plot of beta manifold.
+
+        Plots geodesic of beta manifold.
+
+        by Sunpeng Duan & Allen Wang
+
+        Parameters
+        ----------
+        initial_point : array-like, shape=[1, 2]
+            Starting point representing a beta distribution.
+        end_point : array-like, shape=[1, 2]
+            Ending point representing a beta distribution.
+            Optional, default: None.
+        initial_tangent_vec : array-like, shape=[1, 2]
+            Initial tangent vector for the starting point.
+            Optional, default: None.
+        n_steps : int, >0
+            Number of steps for integration.
+            Optional, default: 100.
+        n_points : int, >0
+            Number of points for interpolation.
+            Optional, default: 10.
+        """
+        if end_point is None and initial_tangent_vec is None:
+            raise ValueError(
+                "Specify an end point or an initial tangent "
+                "vector to define the geodesic."
+            )
+
+        t = gs.linspace(0, 1, n_points)
+
+        if end_point is not None:
+
+            for point in [initial_point, end_point]:
+                x, y = point
+                if x < 0 or y < 0:
+                    raise ValueError(
+                        "Point {} is not in the first quadrant"
+                        .format(point)
+                    )
+
+            u_lim = np.max(list(zip(initial_point, end_point))) + 1
+            l_lim = np.min(list(zip(initial_point, end_point))) - 1
+            geod = beta.metric.geodesic(
+                initial_point=initial_point,
+                end_point=end_point,
+                n_steps=n_steps
+            )(t)
+
+            fig = plt.figure(figsize=(5, 5))
+            ax = fig.add_subplot(111)
+            ax.set(xlim=(l_lim, u_lim), ylim=(l_lim, u_lim))
+            ax.scatter(geod[:, 0], geod[:, 1], **kwargs)
+            ax.set_title(
+                "Geodesic between two beta distributions for Fisher-Rao metric"
+            )
+            plt.xlabel(r"$\alpha$")
+            plt.ylabel(r"$\beta$")
+
+        if initial_tangent_vec is not None:
+            if (initial_point < 0).any():
+                raise ValueError("Initial point is not in the first quadrant")
+            geod = beta.metric.geodesic(
+                initial_point=initial_point,
+                initial_tangent_vec=initial_tangent_vec,
+                n_steps=n_steps,
+            )(t)
+            u_lim = np.max(geod) + 1
+            l_lim = np.min(geod) - 1
+            fig = plt.figure(figsize=(5, 5))
+            ax = fig.add_subplot(111)
+            ax.set(xlim=(l_lim, u_lim), ylim=(l_lim, u_lim))
+            ax.scatter(geod[:, 0], geod[:, 1], **kwargs)
+            ax.set_title(
+                "Geodesic between two beta distributions for Fisher-Rao metric"
+            )
+            plt.xlabel(r"$\alpha$")
+            plt.ylabel(r"$\beta$")
+
+    def get_vector_field(
+        self,
+        initial_point,
+        tangent_vecs,
+        ray_length=10,
+        n_points=50,
+        n_steps=10,
+        **kwargs,
+    ):
+        """Geodesic vector field of beta manifold.
+
+        Calculates vector field given initial point and tangent vectors.
+
+        by Sunpeng Duan & Allen Wang & Marianne Arriola
+
+        Parameters
+        ----------
+        points : array-like, shape=[..., 2]
+            Point representing a beta distribution.
+        """
+        if not np.all(initial_point > 0):
+            raise ValueError(
+                "Points must be in the upper-right quadrant of Euclidean space"
+            )
+        if not (initial_point.shape[-1] == 2 and
+                len(initial_point.shape) == 2):
+            raise ValueError("Points must lie in 2D space")
+        if (len(tangent_vecs.shape) != 2 or
+                tangent_vecs.shape[1] != 2):
+            raise ValueError("Tangent vector needs to be of shape N x 2")
+        scaled_tangent_vecs = ray_length * tangent_vecs
+
+        t = gs.linspace(0, 1, n_points)
+        for j in range(len(scaled_tangent_vecs)):
+            geod = beta.metric.geodesic(
+                initial_point=initial_point,
+                initial_tangent_vec=scaled_tangent_vecs[j, :],
+                n_steps=n_steps,
+            )
+            geod = gs.transpose(gs.array([geod(k) for k in t]))
+            geod = np.expand_dims(geod, 0)
+            if j == 0:
+                geods = geod
+            else:
+                geods = np.vstack((geods, geod))
+        x_lowerLimit = np.min(geods[:, 0, :])
+        x_lowerLimit -= x_lowerLimit / 10
+        x_upperLimit = np.max(geods[:, 0, :])
+        x_upperLimit += x_upperLimit / 10
+        y_lowerLimit = np.min(geods[:, 1, :])
+        y_lowerLimit -= y_lowerLimit / 10
+        y_upperLimit = np.max(geods[:, 1, :])
+        y_upperLimit += y_upperLimit / 10
+        xlims = [x_lowerLimit, x_upperLimit]
+        ylims = [y_lowerLimit, y_upperLimit]
+
+        return geods, xlims, ylims
+
+    def plot_vector_field(
+        self,
+        initial_point,
+        tangent_vecs,
+        ray_length=0.25,
+        n_points=50,
+        n_steps=10,
+        **kwargs,
+    ):
+        """Get vector field of beta manifold.
+
+        Plots vector field of the beta manifold.
+
+        by Sunpeng Duan & Allen Wang & Marianne Arriola
+
+        Parameters
+        ----------
+        points : array-like, shape=[..., 2]
+            Point representing a beta distribution.
+        """
+        center, _ = self.process_points(initial_point)
+
+        fig = plt.figure(figsize=(5, 5))
+        ax = fig.add_subplot(111)
+
+        geods, xlims, ylims = self.get_vector_field(
+            center, tangent_vecs, ray_length, n_points, n_steps
+        )
+
+        for geod in geods:
+            ax.plot(*geod)
+        ax.scatter(center[:, 0], center[:, 1])
+        ax.set(xlim=(xlims[0], xlims[1]), ylim=(ylims[0], ylims[1]))
+        ax.set_title("Vector field in the manifold of beta distributions")
+        plt.xlabel(r"$\alpha$")
+        plt.ylabel(r"$\beta$")
+
+    def plot_geodestic_ball(
+        self,
+        initial_point,
+        n_rays,
+        ray_length,
+        n_steps=100,
+        n_points=10,
+        **kwargs,
+    ):
+        """Geodesic ball plot of beta manifold.
+
+        Plots the geodesic ball of beta manifold.
+
+        by Sunpeng Duan & Allen Wang & Marianne Arriola
+
+        Parameters
+        ----------
+        inital_point : array-like, shape=[1, 2]
+            Point representing a beta distribution.
+        tangent_vecs : array-like, shape=[..., 2]
+            Set of tangent vectors for geodesic ball.
+        n_steps : int, >0
+            Number of steps for integration.
+            Optional, default: 100.
+        n_points : int, >0
+            Number of points for interpolation.
+            Optional, default: 10.
+        """
+        center, _ = self.process_points(initial_point)
+        theta = gs.linspace(-gs.pi, gs.pi, n_rays)
+        directions = gs.transpose(gs.stack((gs.cos(theta), gs.sin(theta))))
+        direction_norms = beta.metric.squared_norm(directions, center)
+        direction_norms = direction_norms ** (1 / 2)
+        unit_vectors = directions / gs.expand_dims(direction_norms, 1)
+        tangent_vecs = ray_length * unit_vectors
+
+        fig = plt.figure(figsize=(5, 5))
+        ax = fig.add_subplot(111)
+
+        geods, xlims, ylims = self.get_vector_field(
+            center, tangent_vecs, ray_length, n_points, n_steps
+        )
+        for geod in geods:
+            ax.plot(*geod)
+        ax.scatter(center[:, 0], center[:, 1])
+
+        ax.set(xlim=(xlims[0], xlims[1]), ylim=(ylims[0], ylims[1]))
+        ax.set_title("Geodesic ball in the manifold of beta distributions")
+        plt.xlabel(r"$\alpha$")
+        plt.ylabel(r"$\beta$")
---
+++ tests/tests_geomstats/test_visualization_beta_distributions.py
@@ -0,0 +1,87 @@
+"""Unit tests for visualization."""
+import random
+import sys; sys.path.append("../../geomstats/visualization")
+
+import geomstats.backend as gs
+import matplotlib
+import matplotlib.pyplot as plt
+import tests.conftest
+from Beta_Visualization import Beta
+from geomstats.information_geometry.beta import BetaDistributions
+
+matplotlib.use("Agg")  # NOQA
+
+
+class TestVisualizationBeta(tests.conftest.TestCase):
+    def setup_method(self):
+        self.n_samples = 10
+        self.Beta = BetaDistributions()
+        self.beta_viz = Beta()
+
+    def test_plot_beta(self):
+        points = gs.random.rand(2, 2)
+        self.beta_viz.plot(points)
+
+    def test_scatter_beta(self):
+        num_points = random.randint(2, 50)
+        points = gs.random.rand(num_points, 2)
+        self.beta_viz.plot(points)
+
+    def test_plot_geodesic_ball(self):
+        center = gs.random.rand(1, 2)
+        n_rays = random.randint(2, 100)
+        ray_length = 1 - random.uniform(0.1, 1)
+        self.beta_viz.plot_geodestic_ball(center, n_rays, ray_length)
+
+    def test_plot_vector_field(self):
+        center = gs.random.rand(1, 2)
+        num_vec = gs.random.randint(1, 20)
+        tan_vec = gs.array(
+            [[random.uniform(-1, 1) for i in range(2)] for j in range(num_vec)]
+        )
+        ray_length = 1 - random.uniform(0.1, 1)
+        self.beta_viz.plot_vector_field(center, tan_vec, ray_length)
+
+    def test_plot_grid(self):
+        size = gs.array([random.randint(1, 6) for i in range(2)])
+        initial_point = gs.array([random.uniform(0, 1) for i in range(2)])
+        n_steps = 100
+        n_points = gs.random.randint(1, 15)
+        step = random.uniform(0, 2)
+        self.beta_viz.plot_grid(size, initial_point, n_steps, n_points, step)
+
+    def test_plot_rendering(self):
+        initial_point = gs.array([random.uniform(0, 1) for i in range(2)])
+        size = gs.array([random.randint(1, 8) for i in range(2)])
+        sampling_period = random.uniform(0.1, 15)
+
+        self.beta_viz.plot_rendering(initial_point, size, sampling_period)
+
+    def test_plot_geodesic(self):
+        n_steps = 100
+        n_points = random.randint(20, 50)
+        cc = gs.zeros((n_points, 3))
+        cc[:, 2] = gs.linspace(0, 1, n_points)
+        point_a = gs.array([random.uniform(0, 10) for i in range(2)])
+        point_b = gs.array([random.uniform(0, 10) for i in range(2)])
+
+        self.beta_viz.plot_geodesic(
+            initial_point=point_a,
+            end_point=point_b,
+            n_points=n_points,
+            color=cc,
+            n_steps=n_steps,
+        )
+
+        tangent_vector = gs.array([random.uniform(-1, 1), random.uniform(-1, 1)])
+        self.beta_viz.plot_geodesic(
+            initial_point=point_a,
+            initial_tangent_vec=tangent_vector,
+            n_points=n_points,
+            color=cc,
+            n_steps=n_steps,
+        )
+
+    @staticmethod
+    def teardown_method():
+        plt.close()
