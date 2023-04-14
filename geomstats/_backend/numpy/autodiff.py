5bf7173a28caff88027cbe046f06a1376f8548b4
L. F. Pereira <luisfgper@gmail.com>
2023-04-14 14:22:10 +0200

    Add AutodiffNotImplementedError

--- geomstats/_backend/numpy/autodiff.py
+++ geomstats/_backend/numpy/autodiff.py
@@ -4,6 +4,8 @@ NumPy backend does not offer automatic differentiation.
 The following functions return error messages.
 """
 
+from geomstats.exceptions import AutodiffNotImplementedError
+
 _USE_OTHER_BACKEND_MSG = (
     "Automatic differentiation is not supported with numpy backend. "
     "Use autograd or pytorch backend instead.\n"
@@ -14,32 +16,32 @@ _USE_OTHER_BACKEND_MSG = (
 
 def value_and_grad(*args, **kwargs):
     """Return an error when using automatic differentiation with numpy."""
-    raise RuntimeError(_USE_OTHER_BACKEND_MSG)
+    raise AutodiffNotImplementedError(_USE_OTHER_BACKEND_MSG)
 
 
 def jacobian(*args, **kwargs):
     """Return an error when using automatic differentiation with numpy."""
-    raise RuntimeError(_USE_OTHER_BACKEND_MSG)
+    raise AutodiffNotImplementedError(_USE_OTHER_BACKEND_MSG)
 
 
 def jacobian_vec(*args, **kwargs):
     """Return an error when using automatic differentiation with numpy."""
-    raise RuntimeError(_USE_OTHER_BACKEND_MSG)
+    raise AutodiffNotImplementedError(_USE_OTHER_BACKEND_MSG)
 
 
 def hessian(*args, **kwargs):
     """Return an error when using automatic differentiation with numpy."""
-    raise RuntimeError(_USE_OTHER_BACKEND_MSG)
+    raise AutodiffNotImplementedError(_USE_OTHER_BACKEND_MSG)
 
 
 def hessian_vec(*args, **kwargs):
     """Return an error when using automatic differentiation with numpy."""
-    raise RuntimeError(_USE_OTHER_BACKEND_MSG)
+    raise AutodiffNotImplementedError(_USE_OTHER_BACKEND_MSG)
 
 
 def jacobian_and_hessian(*args, **kwargs):
     """Return an error when using automatic differentiation with numpy."""
-    raise RuntimeError(_USE_OTHER_BACKEND_MSG)
+    raise AutodiffNotImplementedError(_USE_OTHER_BACKEND_MSG)
 
 
 def custom_gradient(*grad_funcs):
@@ -55,4 +57,4 @@ def custom_gradient(*grad_funcs):
 
 
 def value_jacobian_and_hessian(*args, **kwargs):
-    raise RuntimeError(_USE_OTHER_BACKEND_MSG)
+    raise AutodiffNotImplementedError(_USE_OTHER_BACKEND_MSG)
---
+++ geomstats/exceptions.py
@@ -0,0 +1,2 @@
+class AutodiffNotImplementedError(RuntimeError):
+    """Raised when autodiff is not implemented."""
