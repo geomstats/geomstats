b30b43dd8c39723864d99630f28ea33a1307e04b
L. F. Pereira <luisfgper@gmail.com>
2023-04-17 11:58:36 +0200

    Remove try-except from linspace in pytorch

--- geomstats/_backend/pytorch/__init__.py
+++ geomstats/_backend/pytorch/__init__.py
@@ -349,27 +349,30 @@ def trace(x):
 
 
 def linspace(start, stop, num=50, dtype=None):
-    try:
-        return _torch.linspace(start=start, end=stop, steps=num, dtype=dtype)
-    except TypeError:
-        if not _torch.is_tensor(start):
-            start = _torch.tensor(start)
-        if not _torch.is_tensor(stop):
-            stop = _torch.tensor(stop)
-        start, stop = _torch.broadcast_tensors(start, stop)
-        result_shape = (num, *start.shape)
-        start = _torch.flatten(start)
-        stop = _torch.flatten(stop)
+    start_is_array = _torch.is_tensor(start)
+    stop_is_array = _torch.is_tensor(stop)
 
-        return _torch.reshape(
-            _torch.vstack(
-                [
-                    _torch.linspace(start=start[i], end=stop[i], steps=num, dtype=dtype)
-                    for i in range(start.shape[0])
-                ]
-            ).T,
-            result_shape,
-        )
+    if not (start_is_array or stop_is_array):
+        return _torch.linspace(start=start, end=stop, steps=num, dtype=dtype)
+
+    if not start_is_array:
+        start = _torch.tensor(start)
+    if not stop_is_array:
+        stop = _torch.tensor(stop)
+    start, stop = _torch.broadcast_tensors(start, stop)
+    result_shape = (num, *start.shape)
+    start = _torch.flatten(start)
+    stop = _torch.flatten(stop)
+
+    return _torch.reshape(
+        _torch.vstack(
+            [
+                _torch.linspace(start=start[i], end=stop[i], steps=num, dtype=dtype)
+                for i in range(start.shape[0])
+            ]
+        ).T,
+        result_shape,
+    )
 
 
 def equal(a, b, **kwargs):
