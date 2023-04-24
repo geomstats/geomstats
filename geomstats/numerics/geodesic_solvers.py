5b8a3f5da88a0a6188657d159cf5ebfbce69052c
L. F. Pereira <luisfgper@gmail.com>
2023-04-24 18:35:46 +0200

    Refactor LogPolynomialApproxSolver

--- geomstats/numerics/geodesic_solvers.py
+++ geomstats/numerics/geodesic_solvers.py
@@ -5,8 +5,6 @@ from geomstats.numerics.bvp_solvers import ScipySolveBVP
 from geomstats.numerics.ivp_solvers import GSIntegrator
 from geomstats.numerics.optimizers import ScipyMinimize
 
-# TODO: check uses of space.dim
-
 
 class ExpSolver(ABC):
     @abstractmethod
@@ -183,8 +181,6 @@ class LogShootingSolver:
 
 
 class _LogShootingSolverFlatten(_GeodesicBVPFromExpMixins, LogSolver):
-    # TODO: add a (linear) initialization here?
-
     def __init__(self, optimizer=None, initialization=None):
         if optimizer is None:
             optimizer = ScipyMinimize(jac="autodiff")
@@ -341,8 +337,32 @@ class LogBVPSolver(_LogBatchMixins, LogSolver):
         return gs.transpose(result[: space.dim, :])
 
 
-class LogPolynomialSolver(_LogBatchMixins, LogSolver):
-    def __init__(self, n_nodes=201, degree=5, optimizer=None, initialization=None):
+class _Polynomial:
+    # TODO: add tests against numpy
+
+    def __init__(self, coeffs):
+        self.coeffs = coeffs
+
+    @property
+    def degree(self):
+        return self.coeffs.shape[-1] - 1
+
+    def __call__(self, x):
+        x_t = gs.stack(
+            [gs.power(x, power) for power in range(self.degree + 1)], axis=-1
+        )
+        return gs.einsum("...j,nj->n...", self.coeffs, x_t)
+
+    def first_derivative(self, x):
+        dx_t = gs.stack(
+            [power * gs.power(x, power - 1) for power in range(1, self.degree + 1)],
+            axis=-1,
+        )
+        return gs.einsum("...j,nj->n...", self.coeffs[..., 1:], dx_t)
+
+
+class LogPolynomialApproxSolver(_LogBatchMixins, LogSolver):
+    def __init__(self, n_nodes=10, degree=5, optimizer=None, initialization=None):
         if optimizer is None:
             optimizer = ScipyMinimize()
 
@@ -354,118 +374,108 @@ class LogPolynomialSolver(_LogBatchMixins, LogSolver):
         self.initialization = initialization
         self.optimizer = optimizer
 
-    def _default_initialization(self, space, point, base_point, param, t=None):
-        last_coef = point - base_point - gs.sum(param, axis=0)
-        coef = gs.vstack((base_point, param, last_coef))
+        self._poly = _Polynomial(coeffs=None)
 
-        t = gs.linspace(0.0, 1.0, self.n_nodes) if t is None else t
+    def _default_initialization(self, space, point, base_point):
+        return gs.zeros(space.dim * (self.degree - 1))
 
-        t_x = gs.stack([t**i for i in range(self.degree + 1)])
-        x = gs.einsum("ij,ik->kj", coef, t_x)
+    def _create_grid(self):
+        return gs.linspace(0.0, 1.0, num=self.n_nodes)
 
-        t_y = gs.stack([i * t ** (i - 1) for i in range(1, self.degree + 1)])
-        y = gs.einsum("ij,ik->kj", coef[1:], t_y)
-
-        return x, y
-
-    def _cost_fun(self, param, space, point, base_point):
-        """Compute the energy of the polynomial curve defined by param."""
-
-        x, y = self.initialization(space, point, base_point, param)
-
-        velocity_sqnorm = space.metric.squared_norm(vector=y, base_point=x)
-        energy = gs.sum(velocity_sqnorm) / self.n_nodes
-        return energy, x, y
-
-    def _cost_jacobian(self, param, space, point, base_point):
-        """Compute the jacobian of the cost function at polynomial curve."""
-
-        t = gs.linspace(0.0, 1.0, self.n_nodes)
-        x, y = self.initialization(space, point, base_point, param, t=t)
-
-        fac1 = gs.stack(
-            [
-                k * t ** (k - 1) - self.degree * t ** (self.degree - 1)
-                for k in range(1, self.degree)
-            ]
+    def _get_coeffs(self, coeffs_mid, space, point, base_point):
+        coeffs_0 = gs.transpose(gs.expand_dims(base_point, axis=0))
+        coeffs_last = gs.transpose(
+            gs.expand_dims(point - base_point - gs.sum(coeffs_mid, axis=-1), axis=0)
         )
-        fac2 = gs.stack([t**k - t**self.degree for k in range(1, self.degree)])
-        fac3 = (y * gs.polygamma(1, x)).T - gs.sum(y, 1) * gs.polygamma(1, gs.sum(x, 1))
-        fac4 = (y**2 * gs.polygamma(2, x)).T - gs.sum(y, 1) ** 2 * gs.polygamma(
-            2, gs.sum(x, 1)
+        return gs.hstack([coeffs_0, coeffs_mid, coeffs_last])
+
+    def _update_poly_coeffs(self, coeffs_mid, space, point, base_point):
+        coeffs_mid = gs.reshape(coeffs_mid, (space.dim, self.degree - 1))
+        coeffs = self._get_coeffs(coeffs_mid, space, point, base_point)
+        self._poly.coeffs = coeffs
+        return self._poly
+
+    def _cost_fun(self, poly, t, space):
+        """Compute the energy of the polynomial curve defined by param.
+
+        Parameters
+        ----------
+        poly : _Polynomial
+            Polynomial object with coefficients.
+        t : array-like
+
+        Returns
+        -------
+        energy : float
+        """
+        point = poly(t)
+        tangent_vec = poly.first_derivative(t)
+
+        velocity_sqnorm = space.metric.squared_norm(
+            vector=tangent_vec, base_point=point
         )
+        return gs.sum(velocity_sqnorm) / t.shape[-1]
 
-        cost_jac = (
-            2 * gs.einsum("ij,kj->ik", fac1, fac3) + gs.einsum("ij,kj->ik", fac2, fac4)
-        ) / self.n_nodes
-        return gs.reshape(gs.transpose(cost_jac), (space.dim * (self.degree - 1),))
-
-    def _f2minimize(self, x, space, point, base_point):
+    def _objective(self, coeffs_mid, grid, space, point, base_point):
         """Compute function to minimize."""
-        param = gs.transpose(gs.reshape(x, (space.dim, self.degree - 1)))
-        return self._cost_fun(param, space, point, base_point)[0]
+        poly = self._update_poly_coeffs(coeffs_mid, space, point, base_point)
+        return self._cost_fun(poly, grid, space)
 
-    def _jacobian(self, x, space, point, base_point):
-        """Compute jacobian of the function to minimize."""
-        param = gs.transpose(gs.reshape(gs.from_numpy(x), (space.dim, self.degree - 1)))
-        return self._cost_jacobian(param, space, point, base_point)
+    def _solve(self, space, point, base_point):
+        x0 = self.initialization(space, point, base_point)
+        grid = self._create_grid()
 
-    def _solve(
-        self,
-        space,
-        point,
-        base_point,
-        jac_on=True,
-    ):
-
-        x0 = gs.ones(space.dim * (self.degree - 1))
-        jac = self._jacobian if jac_on else None
-        sol = self.optimizer.optimize(
-            self._f2minimize, x0, jac=jac, args=(space, point, base_point)
+        objective = lambda x: self._objective(
+            x, grid=grid, space=space, point=point, base_point=base_point
         )
-        opt_param = gs.transpose(gs.reshape(sol.x, (space.dim, self.degree - 1)))
-        _, x, y = self._cost_fun(opt_param, space, point, base_point)
 
-        return x, y
+        return self.optimizer.minimize(objective, x0)
 
     def _log_single(self, space, point, base_point):
-        _, y = self._solve(space, point, base_point)
-        return y[0]
+        res = self._solve(space, point, base_point)
+        return self._simplify_log_result(res, space, point, base_point)
 
-    def _simplify_result_t(self, x, t):
-        """Get point at t using interpolation"""
-        n_segments = x.shape[0] - 1
-        points_at_t = []
-        for t_ in t:
-            i1 = int(t_ * (n_segments))
-            t1 = i1 / n_segments
-            if i1 == n_segments:
-                points_at_t.append(x[i1])
-                continue
-            i2 = i1 + 1
-            t2 = i2 / n_segments
-            points_at_t.append(x[i1] + (t_ - t1) * (x[i2] - x[i1]) / (t2 - t1))
-        return gs.stack(points_at_t)
+    def _simplify_log_result(self, result, space, point, base_point):
+        poly = self._update_poly_coeffs(result.x, space, point, base_point)
+        log = poly.first_derivative(gs.array([1.0]))
+
+        if point.ndim == 1:
+            return log[0]
+
+        return log
 
     def geodesic_bvp(self, space, point, base_point):
-        # TODO: add to docstrings: 0 <= t <= 1
-
         if point.ndim != base_point.ndim:
             point, base_point = gs.broadcast_arrays(point, base_point)
 
         is_batch = point.ndim > space.point_ndim
         if not is_batch:
-            result = self._solve(space, point, base_point)[0]
+            result = self._solve(space, point, base_point)
+            coeffs = self._update_poly_coeffs(result.x, space, point, base_point).coeffs
+
         else:
             results = [
-                self._solve(space, point_, base_point_)[0]
+                self._solve(space, point_, base_point_)
                 for point_, base_point_ in zip(point, base_point)
             ]
+            coeffs = gs.stack(
+                [
+                    self._update_poly_coeffs(
+                        result.x, space, point_, base_point_
+                    ).coeffs
+                    for result, point_, base_point_ in zip(results, point, base_point)
+                ]
+            )
+
+        poly = _Polynomial(coeffs)
 
         def path(t):
-            if not is_batch:
-                return self._simplify_result_t(result, t)
+            if not gs.is_array(t):
+                t = gs.array([t])
 
-            return gs.array([self._simplify_result_t(result, t) for result in results])
+            if gs.ndim(t) == 0:
+                t = gs.expand_dims(t, axis=0)
+
+            return poly(t)
 
         return path
