3ec6a1e7df25f89d47ab808e1b717e8559452d0a
L. F. Pereira <luisfgper@gmail.com>
2023-05-24 16:55:49 +0200

    Remove code duplication between numpy and autograd backends by adding _shared_numpy folder and dispatch at import time

---
+++ geomstats/_backend/_shared_numpy/_dispatch.py
@@ -0,0 +1,13 @@
+from geomstats._backend import BACKEND_NAME
+
+if BACKEND_NAME == "numpy":
+    import numpy
+    import scipy
+
+    from ..numpy import _common
+elif BACKEND_NAME == "autograd":
+    from autograd import numpy, scipy
+
+    from ..autograd import _common
+else:
+    raise ValueError("Unknown numpy.")
