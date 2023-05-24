98eb71fa8b5e2bcf0bd7b978de271d0d635845fe
L. F. Pereira <luisfgper@gmail.com>
2023-05-24 17:32:14 +0200

    Fix pytorch backend import error

--- geomstats/_backend/_shared_numpy/_dispatch.py
+++ geomstats/_backend/_shared_numpy/_dispatch.py
@@ -1,13 +1,12 @@
 from geomstats._backend import BACKEND_NAME
 
-if BACKEND_NAME == "numpy":
+if BACKEND_NAME == "autograd":
+    from autograd import numpy, scipy
+
+    from ..autograd import _common
+
+else:
     import numpy
     import scipy
 
     from ..numpy import _common
-elif BACKEND_NAME == "autograd":
-    from autograd import numpy, scipy
-
-    from ..autograd import _common
-else:
-    raise ValueError("Unknown numpy.")
