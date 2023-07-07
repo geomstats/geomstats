b4f1d7b64be58a9fc123a36ea1ba956c638c673c
L. F. Pereira <luisfgper@gmail.com>
2023-07-07 16:43:55 +0200

    Refactor WrappedGaussianProcesses

--- geomstats/learning/wrapped_gaussian_process.py
+++ geomstats/learning/wrapped_gaussian_process.py
@@ -28,106 +28,44 @@ class WrappedGaussianProcess(MultiOutputMixin, RegressorMixin, BaseEstimator):
     Parameters
     ----------
     space : Manifold
-        Manifold.
-    metric : RiemannianMetric
-        Riemannian metric.
-    prior : function
+        Equipped manifold.
+    prior : callable
         Associate to each input a manifold valued point.
-     kernel : kernel instance, default=None
-        The kernel specifying the covariance function of the GP. If None is
-        passed, the kernel ``ConstantKernel(1.0, constant_value_bounds="fixed")
-        * RBF(1.0, length_scale_bounds="fixed")`` is used as default. Note that
-        the kernel hyperparameters are optimized during fitting unless the
-        bounds are marked as "fixed".
-    alpha : float or ndarray of shape (n_samples,), default=1e-10
-        Value added to the diagonal of the kernel matrix during fitting.
-        This can prevent a potential numerical issue during fitting, by
-        ensuring that the calculated values form a positive definite matrix.
-        It can also be interpreted as the variance of additional Gaussian
-        measurement noise on the training observations. Note that this is
-        different from using a `WhiteKernel`. If an array is passed, it must
-        have the same number of entries as the data used for fitting and is
-        used as datapoint-dependent noise level. Allowing to specify the
-        noise level directly as a parameter is mainly for convenience and
-        for consistency with :class:`~sklearn.linear_model.Ridge`.
-    optimizer : "fmin_l_bfgs_b" or callable, default="fmin_l_bfgs_b"
-        Can either be one of the internally supported optimizers for optimizing
-        the kernel's parameters, specified by a string, or an externally
-        defined optimizer passed as a callable. If a callable is passed, it
-        must have the signature::
-            def optimizer(obj_func, initial_theta, bounds):
-                # * 'obj_func': the objective function to be minimized, which
-                #   takes the hyperparameters theta as a parameter and an
-                #   optional flag eval_gradient, which determines if the
-                #   gradient is returned additionally to the function value
-                # * 'initial_theta': the initial value for theta, which can be
-                #   used by local optimizers
-                # * 'bounds': the bounds on the values of theta
-                ....
-                # Returned are the best found hyperparameters theta and
-                # the corresponding value of the target function.
-                return theta_opt, func_min
-        Per default, the L-BFGS-B algorithm from `scipy.optimize.minimize`
-        is used. If None is passed, the kernel's parameters are kept fixed.
-        Available internal optimizers are: `{'fmin_l_bfgs_b'}`.
-    n_restarts_optimizer : int, default=0
-        The number of restarts of the optimizer for finding the kernel's
-        parameters which maximize the log-marginal likelihood. The first run
-        of the optimizer is performed from the kernel's initial parameters,
-        the remaining ones (if any) from thetas sampled log-uniform randomly
-        from the space of allowed theta-values. If greater than 0, all bounds
-        must be finite. Note that `n_restarts_optimizer == 0` implies that one
-        run is performed.
-    copy_X_train : bool, default=True
-        If True, a persistent copy of the training data is stored in the
-        object. Otherwise, just a reference to the training data is stored,
-        which might cause predictions to change if the data is modified
-        externally.
-    random_state : int, RandomState instance or None, default=None
-        Determines random number generation used to initialize the centers.
-        Pass an int for reproducible results across multiple function calls.
 
+    References
+    ----------
     [1] Mallasto, A. and Feragen, A. Wrapped gaussian process
     regression on riemannian manifolds. In 2018 IEEE/CVF
     Conference on Computer Vision and Pattern Recognition
     """
 
-    def __init__(
-        self,
-        space,
-        metric,
-        prior,
-        kernel=None,
-        *,
-        alpha=1e-10,
-        optimizer="fmin_l_bfgs_b",
-        n_restarts_optimizer=0,
-        copy_X_train=True,
-        random_state=None,
-    ):
-        if metric is None:
-            metric = space.metric
-        self.metric = metric
+    def __init__(self, space, prior):
         self.space = space
         self.prior = prior
-        self.copy_X_train = copy_X_train
 
-        self.y_train_ = None
-        self.tangent_y_train_ = None
-        self.y_train_shape_ = None
-
-        self._euclidean_gpr = GaussianProcessRegressor(
-            kernel=kernel,
-            alpha=alpha,
-            optimizer=optimizer,
-            n_restarts_optimizer=n_restarts_optimizer,
+        self.euclidean_gpr = GaussianProcessRegressor(
+            kernel=None,
+            alpha=1e-10,
+            optimizer="fmin_l_bfgs_b",
+            n_restarts_optimizer=0,
             normalize_y=False,
-            copy_X_train=copy_X_train,
-            random_state=random_state,
+            copy_X_train=True,
+            random_state=None,
         )
 
-        self.__dict__.update(self._euclidean_gpr.__dict__)
-        self.log_marginal_likelihood = self._euclidean_gpr.log_marginal_likelihood
+        self.tangent_y_train_ = None
+
+    def set(self, **kwargs):
+        """Set euclidean_gpr parameters.
+
+        Especially useful for one line instantiations.
+        """
+        for param_name, value in kwargs.items():
+            if not hasattr(self.euclidean_gpr, param_name):
+                raise ValueError(f"Unknown parameter {param_name}.")
+
+            setattr(self.euclidean_gpr, param_name, value)
+        return self
 
     def _get_tangent_targets(self, X, y):
         """Compute the tangent targets, using the provided prior.
@@ -148,7 +86,7 @@ class WrappedGaussianProcess(MultiOutputMixin, RegressorMixin, BaseEstimator):
                 Target projected on the associated (by the prior) tangent space.
         """
         base_points = self.prior(X)
-        return self.metric.log(y, base_point=base_points)
+        return self.space.metric.log(y, base_point=base_points)
 
     def fit(self, X, y):
         """Fit Wrapped Gaussian process regression model.
@@ -161,34 +99,20 @@ class WrappedGaussianProcess(MultiOutputMixin, RegressorMixin, BaseEstimator):
 
         Parameters
         ----------
-        X : array-like of shape (n_samples, n_features) or list of object
-            Feature vectors or other representations of training data.
-        y : array-like of shape (n_samples,) or (n_samples, n_targets)
-        or (n_samples, n1_targets, n2_targets)
-            Target values. The target must belongs to the manifold space
+        X : array-like, shape=[n_samples,]
+            Training input samples.
+        y : array-like, shape[n_samples, {dim, [n,n]}]
+            Training target values.
 
         Returns
         -------
         self : object
-            WrappedGaussianProcessRegressor class instance.
+            Returns self.
         """
-        if not gs.all(self.space.belongs(y)):
-            raise AttributeError("The target values must belongs to the given space")
+        self.tangent_y_train_ = tangent_y = self._get_tangent_targets(X, y)
+        tangent_y = gs.reshape(tangent_y, (y.shape[0], -1))
 
-        # compute the tangent dataset using the prior
-        tangent_y = self._get_tangent_targets(X, y)
-        self.y_train_shape_ = y.shape[
-            1:
-        ]  # this is really useful when the samples are matrices, or tensor of dim>1
-        tangent_y = gs.reshape(tangent_y, (y.shape[0], -1))  # flatten the samples.
-        # fit a gpr on the tangent dataset
-
-        self._euclidean_gpr.fit(X, tangent_y)
-        # update the attributes of the wgpr using the new attributes of the gpr
-
-        self.__dict__.update(self._euclidean_gpr.__dict__)
-        self.y_train_ = y
-        self.tangent_y_train_ = tangent_y  # = self._euclidean_gpr.y_train_
+        self.euclidean_gpr.fit(X, tangent_y)
 
         return self
 
@@ -236,7 +160,7 @@ class WrappedGaussianProcess(MultiOutputMixin, RegressorMixin, BaseEstimator):
             In the case where the target is matrix valued,
             return the covariance of the vectorized prediction.
         """
-        euc_result = self._euclidean_gpr.predict(
+        euc_result = self.euclidean_gpr.predict(
             X, return_cov=return_tangent_cov, return_std=return_tangent_std
         )
 
@@ -245,13 +169,13 @@ class WrappedGaussianProcess(MultiOutputMixin, RegressorMixin, BaseEstimator):
 
         base_points = self.prior(X)
         tangent_means = gs.reshape(
-            gs.cast(tangent_means, dtype=X.dtype),
-            (X.shape[0], *self.y_train_shape_),
+            gs.from_numpy(tangent_means),
+            (X.shape[0], *self.space.shape),
         )
-        y_mean = self.metric.exp(tangent_means, base_point=base_points)
+        y_mean = self.space.metric.exp(tangent_means, base_point=base_points)
 
         if return_multiple:
-            tangent_std_cov = gs.cast(euc_result[1], dtype=X.dtype)
+            tangent_std_cov = gs.from_numpy(euc_result[1])
             return (y_mean, tangent_std_cov)
 
         return y_mean
@@ -289,20 +213,23 @@ class WrappedGaussianProcess(MultiOutputMixin, RegressorMixin, BaseEstimator):
             Values of n_samples samples drawn from wrapped Gaussian process and
             evaluated at query points.
         """
-        tangent_samples = self._euclidean_gpr.sample_y(X, n_samples, random_state)
-        tangent_samples = gs.cast(tangent_samples, dtype=X.dtype)
+        tangent_samples = gs.from_numpy(
+            self.euclidean_gpr.sample_y(X, n_samples, random_state)
+        )
 
         if gs.ndim(tangent_samples) > 2:
             tangent_samples = gs.moveaxis(tangent_samples, -2, -1)
 
-        flat_tangent_samples = gs.reshape(tangent_samples, (-1, *self.y_train_shape_))
+        flat_tangent_samples = gs.reshape(tangent_samples, (-1, *self.space.shape))
 
         base_points = gs.repeat(self.prior(X), n_samples, axis=0)
 
-        flat_y_samples = self.metric.exp(flat_tangent_samples, base_point=base_points)
+        flat_y_samples = self.space.metric.exp(
+            flat_tangent_samples, base_point=base_points
+        )
 
         y_samples = gs.reshape(
-            flat_y_samples, (X.shape[0], n_samples, *self.y_train_shape_)
+            flat_y_samples, (X.shape[0], n_samples, *self.space.shape)
         )
 
         if gs.ndim(tangent_samples) > 2:
