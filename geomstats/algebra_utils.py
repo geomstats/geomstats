71baa127cf0fd5be3f03d78b291d6c8e1154bd91
Yann Cabanes <yann.cabanes@gmail.com>
2023-12-21 11:22:00 -0500

    Fix error in algebra_utils.py during creation of identity matrix using PyTorch GPU

--- geomstats/algebra_utils.py
+++ geomstats/algebra_utils.py
@@ -114,8 +114,10 @@ def from_vector_to_diagonal_matrix(vector, num_diag=0):
         is a diagonal matrix containing the `i`-th row of `vector`.
     """
     num_columns = gs.shape(vector)[-1]
-    identity = gs.eye(num_columns)
-    identity = gs.cast(identity, vector.dtype)
+    identity = gs.repeat(
+        gs.zeros_like(gs.reshape(vector, (-1, num_columns))[:1, :]),
+        repeats=num_columns, axis=0)
+    identity[range(num_columns), range(num_columns)] = 1
     diagonals = gs.einsum("...i,ij->...ij", vector, identity)
     diagonals = gs.to_ndarray(diagonals, to_ndim=3)
     num_lines = diagonals.shape[0]
