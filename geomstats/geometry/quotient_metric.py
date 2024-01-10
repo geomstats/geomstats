7a640e3fea0ba44e1a9b20d23d1d593ed8f4e4d4
Alice Le-Brigant <alice.le-brigant@univ-paris1.fr>
2024-01-10 13:18:44 +0100

    Modify geodesic in quotient metric

--- geomstats/geometry/quotient_metric.py
+++ geomstats/geometry/quotient_metric.py
@@ -451,3 +451,57 @@ class QuotientMetric(RiemannianMetric):
             - a_x_ver_nabla_curvature_top
             + 3.0 * (nabla_x_a_y_a_x_y - a_x_a_y_a_x_y)
         )
+
+    def geodesic(self, initial_point, end_point=None, initial_tangent_vec=None):
+        """Generate parameterized function for the geodesic curve.
+
+        Geodesic curve defined by either:
+
+        - an initial point and an initial tangent vector,
+        - an initial point and an end point.
+
+        Parameters
+        ----------
+        initial_point : array-like, shape=[..., dim]
+            Point on the manifold, initial point of the geodesic.
+        end_point : array-like, shape=[..., dim], optional
+            Point on the manifold, end point of the geodesic. If None,
+            an initial tangent vector must be given.
+        initial_tangent_vec : array-like, shape=[..., dim],
+            Tangent vector at base point, the initial speed of the geodesics.
+            Optional, default: None.
+            If None, an end point must be given and a logarithm is computed.
+
+        Returns
+        -------
+        path : callable
+            Time parameterized geodesic curve. If a batch of initial
+            conditions is passed, the output array's first dimension
+            represents the different initial conditions, and the second
+            corresponds to time.
+        """
+        if end_point is None and initial_tangent_vec is None:
+            raise ValueError(
+                "Specify an end point or an initial tangent "
+                "vector to define the geodesic."
+            )
+        if end_point is not None:
+            if initial_tangent_vec is not None:
+                raise ValueError(
+                    "Cannot specify both an end point and an initial tangent vector."
+                )
+            initial_fiber_point = self.fiber_bundle.lift(initial_point)
+            end_fiber_point = self.fiber_bundle.lift(end_point)
+            aligned_end_point = self.fiber_bundle.align(
+                end_fiber_point, initial_fiber_point
+            )
+            geodesic = self.fiber_bundle.total_space.metric.geodesic(
+                initial_point=initial_point, end_point=aligned_end_point
+            )
+
+            def projected_geodesic(t):
+                return self.fiber_bundle.riemannian_submersion(geodesic(t))
+
+            return projected_geodesic
+
+        return self._geodesic_ivp(initial_point, initial_tangent_vec)
