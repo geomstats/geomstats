32e19cbf19a5d9fef0fb02242c85722df6c2fa58
Luís F. Pereira <luisfgper@gmail.com>
2024-01-19 15:01:10 +0100

    Fix instantiation of FrechetMean in a more generic way

--- geomstats/learning/kmeans.py
+++ geomstats/learning/kmeans.py
@@ -10,9 +10,8 @@ from scipy.stats import rv_discrete
 from sklearn.base import BaseEstimator, ClusterMixin
 
 import geomstats.backend as gs
-from geomstats.geometry.euclidean import Euclidean
 from geomstats.learning._template import TransformerMixin
-from geomstats.learning.frechet_mean import FrechetMean, LinearMean
+from geomstats.learning.frechet_mean import FrechetMean
 
 
 class RiemannianKMeans(TransformerMixin, ClusterMixin, BaseEstimator):
@@ -77,13 +76,9 @@ class RiemannianKMeans(TransformerMixin, ClusterMixin, BaseEstimator):
 
         self.init_cluster_centers_ = None
 
-        if isinstance(space, Euclidean):
-            self.mean_estimator = LinearMean(space=space)
-        else:
-            self.mean_estimator = FrechetMean(
-                space=space,
-                method="default",
-            ).set(max_iter=100, init_step_size=1.0)
+        self.mean_estimator = FrechetMean(space=space)
+        if isinstance(self.mean_estimator, FrechetMean):
+            self.mean_estimator.set(max_iter=100, init_step_size=1.0)
 
         self.cluster_centers_ = None
         self.labels_ = None
