1ddbb45f31d410353161591e072eef856f2da699
Daniel Ralston <drals787@gmail.com>
2024-02-16 17:40:01 -0800

    reformatted to adhear to flake styling

--- geomstats/information_geometry/statistical_metric.py
+++ geomstats/information_geometry/statistical_metric.py
@@ -1,45 +1,49 @@
-import geomstats.backend as gs
+"""Statistical metric to equip a statistical manifold."""
 
+import geomstats.backend as gs
 from geomstats.geometry.riemannian_metric import RiemannianMetric
+
 
 class StatisticalMetric(RiemannianMetric):
     """
     Defines statistical metric and connection induced from a divergence.
-    
-    Uses definitions provided in Nielson's An Elementary Introduction to Information Geometry, Theorem 4 on page 15
-    (https://arxiv.org/abs/1808.08271)
+
+    Uses definitions provided in Nielson's An Elementary Introduction to
+    Information Geometry, Theorem 4 on page 15 (https://arxiv.org/abs/1808.08271)
     """
+
     def __init__(self, dim, divergence):
         self.dim = dim
         self.divergence = self._unpack_tensor(divergence)
 
     def _unpack_tensor(self, func):
         def wrapper(tensor):
-            return func(tensor[...,:self.dim], tensor[...,self.dim:])
+            return func(tensor[..., : self.dim], tensor[..., self.dim :])
+
         return wrapper
 
-    def metric_matrix(self,base_point):
-        """Definition on line (51) on page 14"""
+    def metric_matrix(self, base_point):
+        """Definition on line (51) on page 14."""
         hess = gs.autodiff.hessian(self.divergence)
         base_point_pair = gs.concatenate([base_point, base_point])
-        return -1*hess(base_point_pair)[:self.dim, self.dim:]
-        
+        return -1 * hess(base_point_pair)[: self.dim, self.dim :]
+
     def divergence_christoffels(self, base_point):
-        """Definition on line (52) on page 14"""
+        """Definition on line (52) on page 14."""
         hess = gs.autodiff.hessian(self.divergence)
         jac_hess = gs.autodiff.jacobian(hess)
         base_point_pair = gs.concatenate([base_point, base_point])
-        return -1*jac_hess(base_point_pair)[:2, :2, 2:]
-    
+        return -1 * jac_hess(base_point_pair)[:2, :2, 2:]
+
     def dual_divergence_christoffels(self, base_point):
-        """Definition on line (53) on page 14"""
+        """Definition on line (53) on page 14."""
         hess = gs.autodiff.hessian(self.divergence)
         jac_hess = gs.autodiff.jacobian(hess)
         base_point_pair = gs.concatenate([base_point, base_point])
-        return -1*jac_hess(base_point_pair)[:2, 2:, 2:]
-    
+        return -1 * jac_hess(base_point_pair)[:2, 2:, 2:]
+
     def amari_divergence_tensor(self, base_point):
-        """Definition on line (42) on page 12"""
+        """Definition on line (42) on page 12."""
         divergence_christoffels = self.divergence_christoffels(base_point)
         dual_divergence_christoffels = self.dual_divergence_christoffels(base_point)
         return dual_divergence_christoffels - divergence_christoffels
--- tests/tests_geomstats/test_information_geometry/test_statistical_metric.py
+++ tests/tests_geomstats/test_information_geometry/test_statistical_metric.py
@@ -4,25 +4,32 @@ import geomstats.backend as gs
 from geomstats.information_geometry.statistical_metric import StatisticalMetric
 from geomstats.test.test_case import TestCase, autograd_only
 
+
 @autograd_only
+@pytest.mark.slow
 class TestStatisticalMetric(TestCase):
     """
-    Test StatisticalMetric class by verifying expected induced metric, connection, and dual connection properties in the case of Bregman divergence.
+    Test StatisticalMetric class by verifying expected induced metric, connection,
+    and dual connection properties in the case of Bregman divergence.
 
-    Using properties from Nielson's An Elementary Introduction to Information Geometry, Section 3.7 on page 14
-    (https://arxiv.org/abs/1808.08271)
+    Using properties from Nielson's An Elementary Introduction to
+    Information Geometry, Section 3.7 on page 14 (https://arxiv.org/abs/1808.08271)
     """
 
     def setup_method(self):
-
         def bregman_divergence(func):
             def _bregman_divergence(point, base_point):
                 grad_func = gs.autodiff.value_and_grad(func)
                 func_basepoint, grad_func_basepoint = grad_func(base_point)
-                bregman_div = func(point) - func_basepoint - gs.dot(point - base_point, grad_func_basepoint)
+                bregman_div = (
+                    func(point)
+                    - func_basepoint
+                    - gs.dot(point - base_point, grad_func_basepoint)
+                )
                 return bregman_div
-            return _bregman_divergence 
-        
+
+            return _bregman_divergence
+
         def potential_function(point):
             return gs.sum(point**4)
 
@@ -33,7 +40,6 @@ class TestStatisticalMetric(TestCase):
 
     def test_metric_matrix(self,):
         """Test equation (58) on page 15"""
-
         potential_hessian = gs.autodiff.hessian(self.potential_function)
         potential_hessian_base_point = potential_hessian(self.base_point)
         metric_matrix_base_point = self.stat_metric.metric_matrix(self.base_point)
@@ -41,19 +47,26 @@ class TestStatisticalMetric(TestCase):
 
     def test_divergence_christoffels(self,):
         """Test equation (59) on page 15"""
+        divergence_christoffels_base_point = self.stat_metric.divergence_christoffels(
+            self.base_point
+        )
+        self.assertAllClose(
+            divergence_christoffels_base_point,
+            gs.zeros(divergence_christoffels_base_point.shape),
+        )
 
-        divergence_christoffels_base_point = self.stat_metric.divergence_christoffels(self.base_point)
-        self.assertAllClose(divergence_christoffels_base_point, gs.zeros(divergence_christoffels_base_point.shape))
-    
     def test_dual_divergence_christoffels(self,):
         pass
 
     def test_amari_divergence_tensor(self,):
         """Test equation (60) on page 15"""
-
         potential_func_first = gs.autodiff.jacobian(self.potential_function)
         potential_func_second = gs.autodiff.jacobian(potential_func_first)
         potential_func_third = gs.autodiff.jacobian(potential_func_second)
         potential_func_third_base_point = potential_func_third(self.base_point)
-        amari_divergence_tensor_base_point = self.stat_metric.amari_divergence_tensor(self.base_point)
-        self.assertAllClose(amari_divergence_tensor_base_point, potential_func_third_base_point)
+        amari_divergence_tensor_base_point = self.stat_metric.amari_divergence_tensor(
+            self.base_point
+        )
+        self.assertAllClose(
+            amari_divergence_tensor_base_point, potential_func_third_base_point
+        )
