f54c49a2d35eafcf3f7c6bc5ab47e5750c03c481
johnharveymath <john.harvey.math@gmail.com>
2024-02-23 13:05:22 +0000

    Refactor so scaling lists are class variables.

--- geomstats/test_cases/geometry/scalar_product_metric.py
+++ geomstats/test_cases/geometry/scalar_product_metric.py
@@ -1,6 +1,5 @@
 from geomstats.geometry.scalar_product_metric import (
     ScalarProductMetric,
-    _get_scaling_factor,
     _wrap_attr,
 )
 from geomstats.test.test_case import TestCase
@@ -16,11 +15,11 @@ class WrapperTestCase(TestCase):
         self.assertAllClose(res, scaled_res / scale)
 
     def test_scaling_factor(self, func_name, scale, expected):
-        scaling_factor = _get_scaling_factor(func_name, scale)
+        scaling_factor = ScalarProductMetric._get_scaling_factor(func_name, scale)
         self.assertAllClose(scaling_factor, expected)
 
     def test_non_scaled(self, func_name, scale):
-        scaling_factor = _get_scaling_factor(func_name, scale)
+        scaling_factor = ScalarProductMetric._get_scaling_factor(func_name, scale)
         assert scaling_factor is None
 
 
