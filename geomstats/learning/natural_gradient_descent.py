275ff60a065d211b3e8d0c4e03644438a467f13d
Sammy Suliman <sammysuliman@protonmail.com>
2024-03-29 23:02:40 -0700

    overwrite ngd method

--- geomstats/learning/natural_gradient_descent.py
+++ geomstats/learning/natural_gradient_descent.py
@@ -46,22 +46,12 @@ class NaturalGradientDescent(Optimizer):
         super().__init__(params, defaults)
 
     def step(self):
-<<<<<<< HEAD
         """Performs one step of Natural Gradient Descent"""
         w_list = []  
         w_star_list = [] 
         for group in self.param_groups: # only 1 group for now
             for i, param in enumerate(group['params']):
                 if i % 2 == 0: # only want to capture the weight parameters
-=======
-        """Perform one step of Natural Gradient Descent."""
-        w_list = []
-        w_star_list = []
-        w_tilde_list = []
-        for group in self.param_groups:  # only 1 group for now
-            for i, param in enumerate(group["params"]):
-                if i % 2 == 0:  # only want to capture the weight parameters
->>>>>>> 78e13b8ed116e9862a3d89fbfa254d9bd6132fc5
                     param2 = param.detach().numpy()
                     w_list.append(param2)
                     # Append each bias value to the corresponding weight vector
@@ -80,7 +70,6 @@ class NaturalGradientDescent(Optimizer):
                 Z_list = []
                 for n_u, w in enumerate(weight_layer):
                     n = len(w)
-<<<<<<< HEAD
                     x = torch.randn(n) # approximate value of current node since we assume x ~ N(0, I)
                     w_star = w_star_list[layer_index][n_u]
                     A_00 = (1/2*gs.sqrt(2)) * math.erf(w_star[-1] / gs.sqrt(gs.dot(w,w))) # see Appendix II, eq. 107
@@ -124,53 +113,3 @@ class NaturalGradientDescent(Optimizer):
                             print('Y', Y)
                             print('Z', Z)
                             param.data.add_(d_p)
-=======
-                    x = torch.randn(
-                        n
-                    )  # approximate value of current node since we assume x ~ N(0, I)
-                    w_star = w_star_list[n_l][n_u]
-                    w_tilde = w_tilde_list[n_l][n_u]
-                    e_star_0 = np.zeros(n + 1)
-                    e_star_0[-1] = 1
-                    A_00 = (1 / 2 * gs.sqrt(2)) * math.erf(
-                        w_star[-1] / gs.sqrt(gs.dot(w, w))
-                    )  # see Appendix II, eq. 107
-                    A_0n = (1 / gs.sqrt(2)) * (
-                        math.exp((-1 / 2) * (w_star[-1] / gs.sqrt(gs.dot(w, w))) ** 2)
-                    )  # see Appendix II, eq. 109
-                    A_nn = ((1 / 2 * gs.sqrt(2)) * A_00) - (
-                        (w_star[-1] / gs.sqrt(gs.dot(w, w))) * A_0n
-                    )  # see Appendix II, eq. 110
-                    D = A_00 * A_nn - A_0n**2  # pp. 15, eq. 79
-                    X = (1 / D) * A_00 - (1 / A_00)  # pp. 15, eq. 78
-                    Y = (-1) * A_0n / D  # pp. 15, eq. 78
-                    Z = A_nn / D - (1 / A_00)  # pp. 15, eq. 78
-                    if param.grad is None:
-                        continue
-                    if i % 2 == 0:  # updating the gradients for the weights only
-                        d_p = (
-                            (-1)
-                            * param.grad.data
-                            * group["lr"]
-                            * (
-                                (1 / A_00) * x
-                                + (
-                                    X / gs.dot(w, w) * gs.dot(w, x)
-                                    + (Y / gs.sqrt(gs.dot(w, w))) * w
-                                )
-                            )
-                        )  # pp. 15, eq. 81
-                    else:  # updating the gradients for the bias only
-                        d_p = (
-                            (-1)
-                            * param.grad.data
-                            * group["lr"]
-                            * (
-                                (1 / A_00)
-                                + Z
-                                + (Y * gs.dot(w, x) / gs.sqrt(gs.dot(w, w)))
-                                * w_star[-1]
-                            )
-                        )  # pp. 15, eq. 82
-                    param.data.add_(d_p)
->>>>>>> 78e13b8ed116e9862a3d89fbfa254d9bd6132fc5
