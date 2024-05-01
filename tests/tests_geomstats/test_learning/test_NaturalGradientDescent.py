8a51e4e6f807085194df094c2ef0187093baaf45
Sammy Suliman <sammysuliman@protonmail.com>
2024-05-01 05:40:39 -0700

    Added new NGD algorithm

--- tests/tests_geomstats/test_learning/test_NaturalGradientDescent.py
+++ tests/tests_geomstats/test_learning/test_NaturalGradientDescent.py
@@ -1,14 +1,17 @@
 from datetime import datetime
-
 import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
-from natural_gradient_descent import NaturalGradientDescent
+import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
+from torchviz import make_dot
+import os
 
+from ComponentWiseNaturalGradientDescent import ComponentWiseNaturalGradientDescent
 
 def test_NaturalGradientDescent():
+    os.environ["PATH"] += os.pathsep + 'C:/Users/sammy/Downloads/windows_10_msbuild_Release_graphviz-10.0.1-win32.zip/Graphviz/bin'
     # Define transforms to preprocess the data
     transform = transforms.Compose(
         [
@@ -37,25 +40,52 @@ def test_NaturalGradientDescent():
         def __init__(self):
             super().__init__()
             self.fc1 = nn.Linear(784, 150, bias=True)
+            self.conv1 = nn.Conv2d(1, 1, kernel_size=3, stride=1, padding=1, bias=False)
             self.fc2 = nn.Linear(150, 10, bias=True)
+            image, _ = training_set[0]
+            self.image_size = image.size()
 
         def forward(self, x):
-            x = torch.relu(self.fc1(x))  # Must use ReLU activation see pp.
+         #   x = x.view(-1, self.image_size[0], self.image_size[1], self.image_size[2])
+            x = self.fc1(x)
+            x = torch.relu(x)
+            x = x.view(-1, self.image_size[0], x.shape[0], x.shape[1])
+            x = self.conv1(x)
+            x = torch.relu(x)
             x = self.fc2(x)
+            x = x.view(x.shape[2], x.shape[3])
             return x
+        
+    # Define a list to store the linear outputs of the first layer
+    activations = []
 
     # Create an instance of the 2-layer model
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = SimpleModel().to(device)
 
+    def hook(module, input, output):
+        # Save output of the layer
+        activations.append(output)
+
+    # Registering hook to layer1, layer2
+    hook_handle = model.fc1.register_forward_hook(hook)
+    hook_handle2 = model.conv1.register_forward_hook(hook)
+
     # Define loss function and optimizer
     criterion = nn.CrossEntropyLoss()
     parameters = list(model.parameters())
-    optimizer = NaturalGradientDescent(parameters, lr=0.01)
+
+    # Create an OrderedDict to store the layers in order
+    layers_dict = torch.nn.ModuleDict()
+
+    # Iterate through the modules and add them to the OrderedDict
+    for name, module in model.named_modules():
+        if name != "":
+            layers_dict[name] = module
+    optimizer = ComponentWiseNaturalGradientDescent(parameters, activations, layers_dict, lr=0.01)
 
     running_vloss = 0
     for i, vdata in enumerate(validation_loader):
-        print("szrdxghcjszrdxgj")
         vinputs, vlabels = vdata
         vinputs = vinputs.view(vinputs.size(0), -1)  # Flatten the input images
         voutputs = model(vinputs)
@@ -63,9 +93,6 @@ def test_NaturalGradientDescent():
         running_vloss += vloss
 
     def train_one_epoch(epoch_index, tb_writer):
-        print(
-            "WOW ANOTHER EPOCH !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1!!!!!!!!!!!!!!!!"
-        )
         running_loss = 0.0
         last_loss = 0.0
 
@@ -73,7 +100,6 @@ def test_NaturalGradientDescent():
         # iter(training_loader) so that we can track the batch
         # index and do some intra-epoch reporting
         for i, data in enumerate(training_loader):
-            print(i)
             # Every data instance is an input + label pair
             inputs, labels = data
             inputs = inputs.view(inputs.size(0), -1)  # Flatten the input images
@@ -82,19 +108,16 @@ def test_NaturalGradientDescent():
             optimizer.zero_grad()
 
             # Make predictions for this batch
-            print("weight size", model.fc1)
-            print("bias size", model.fc2)
-            print("input size", inputs.size())
             outputs = model(inputs)
-            print("outputs size", outputs.size())
-            #  print('labels', labels)
 
             # Compute the loss and its gradients
             loss = criterion(outputs, labels)
-            loss.backward()
-
+            loss.backward(retain_graph = True)
+            # Gradient Norm Clipping
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=0.5)
+            gradients = torch.autograd.grad(loss, model.parameters(), retain_graph=True)
             # Adjust learning weights
-            optimizer.step()
+            optimizer.step(gradients)
             # Gather data and report
             running_loss += loss.item()
             if i % 937 == 0:
@@ -119,7 +142,7 @@ def test_NaturalGradientDescent():
         print("EPOCH {}:".format(epoch_number + 1))
 
         # Make sure gradient tracking is on, and do a pass over the data
-        model.train(True)
+        model.train(True) 
         avg_loss = train_one_epoch(epoch_number, writer)
 
         running_vloss = 0.0
@@ -155,6 +178,5 @@ def test_NaturalGradientDescent():
             torch.save(model.state_dict(), model_path)
 
         epoch_number += 1
-
 
 test_NaturalGradientDescent()
