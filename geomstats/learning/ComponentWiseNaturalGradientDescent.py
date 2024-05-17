7b25c9ff99fdd858ab49b25caa8e4f09b163e05c
SammySuliman <96219622+SammySuliman@users.noreply.github.com>
2024-05-17 16:41:15 -0700

    Update ComponentWiseNaturalGradientDescent.py

--- geomstats/learning/ComponentWiseNaturalGradientDescent.py
+++ geomstats/learning/ComponentWiseNaturalGradientDescent.py
@@ -4,18 +4,18 @@ import geomstats.backend as gs
 
 
 class ComponentWiseNaturalGradientDescent:
-    """Implements Natural Gradient Descent using Van Sang et al. algorithm 
+    """Implements Natural Gradient Descent using Van Sang et al. algorithm
     for convolutional neural networks
     Parameters
     ----------
     params : array-like
         Array of all the layers of the CNN, consisting of
-        a vector of nodes, a vector of weights, 
-        and a bias scalar 
+        a vector of nodes, a vector of weights,
+        and a bias scalar
     activations : array-like
         The linear outputs of the c
     layers_dict : OrderedDict
-        Dictionary of all the layers of the CNN and their type 
+        Dictionary of all the layers of the CNN and their type
     lr : float
         The learning rate to update the weights
     gamma : float
@@ -28,6 +28,7 @@ class ComponentWiseNaturalGradientDescent:
     .. [2210.05268] Tran Van Sang, Mhd Irvan, Rie Shigetomi Yamaguchi, Toshiyuki Nakata (2022)
         Component-Wise Natural Gradient Descent -- An Efficient Neural Network Optimization
     """
+    
     def __init__(self, params, activations, layers_dict, lr=0.05, gamma=0.1, bias=True):
         self.defaults = dict(lr=lr, gamma=gamma, bias=bias)
         self.params = params
@@ -48,12 +49,37 @@ class ComponentWiseNaturalGradientDescent:
                 self.dense_params.append(p)
 
     def zero_grad(self):
+        """
+        Zero out the gradients of all model parameters.
+
+        This method sets the gradients of all model parameters to zero. It is typically
+        called before backpropagation to clear the gradients computed in the previous
+        iteration or batch. By zeroing out the gradients, we ensure that the gradients
+        from different iterations or batches do not accumulate.
+    
+        Returns:
+            None
+        """
+        
         for param in self.params:
             if param.grad is not None:
                 param.grad.detach_()
                 param.grad.zero_()
 
     def step(self, gradients):
+        '''
+        Update the parameters of the model using gradient descent.
+
+        Parameters
+        ----------
+        gradients : array-like
+            Array of the current gradients in the computation graph for
+            the weights and biases, used to update the parameters
+
+        Returns:
+            None
+        '''
+        
         for p in gradients:
             if p.dim() > 2:
                 self.conv_gradients.append(p)
@@ -72,7 +98,7 @@ class ComponentWiseNaturalGradientDescent:
         W_prev = self.activations[-1]
         A_prev = torch.relu(W_prev)
         d_act_L = gs.where((gs.matmul(layer_weight, A_prev.squeeze().T) + layer_bias.unsqueeze(1)) > 0,
-                            gs.array(1.0), gs.array(0.0))
+                  gs.array(1.0), gs.array(0.0))
         D_a = gs.matmul(layer_grad, gs.matmul(d_act_L, A_prev.squeeze()).T)
         l2 = len(self.dense_params) // 2
         l3 = len(self.conv_params)
@@ -84,7 +110,7 @@ class ComponentWiseNaturalGradientDescent:
                 layer_bias = self.dense_params[2*l2-1]
                 A_prev = self.dense_activations[l2-1]
                 d_act_l = gs.where((gs.matmul(layer_weight, A_prev.T) + layer_bias.unsqueeze(1)) > 0,
-                                    gs.array(1.0), gs.array(0.0))
+                          gs.array(1.0), gs.array(0.0))
                 D_s = gs.matmul(D_a, d_act_l)
                 D_a_grad = gs.empty_like(D_s)
                 D_a_grad = torch.autograd.grad(outputs=D_s, inputs=D_a, grad_outputs=D_a_grad)
