15399fae5c421756bf1b48eef6f981d7ff537c2a
Luís F. Pereira <luisfgper@gmail.com>
2024-09-05 18:40:14 +0200

    Add strictly lower triangular  matrices

--- geomstats/geometry/lower_triangular_matrices.py
+++ geomstats/geometry/lower_triangular_matrices.py
@@ -4,7 +4,7 @@ Lead author: Saiteja Utpala.
 """
 
 import geomstats.backend as gs
-from geomstats.geometry.base import MatrixVectorSpace
+from geomstats.geometry.base import LevelSet, MatrixVectorSpace
 from geomstats.geometry.matrices import Matrices, MatricesMetric
 
 
@@ -92,3 +92,120 @@ class LowerTriangularMatrices(MatrixVectorSpace):
             Symmetric matrix.
         """
         return gs.tril(point)
+
+
+class StrictlyLowerTriangularMatrices(LevelSet, MatrixVectorSpace):
+    r"""Strictly lower triangular matrices.
+
+    Set of lower triangular matrices with null diagonal:
+
+    .. math::
+
+        \operatorname{LT}^0(n)=\{L \in \operatorname{LT}(n)
+        \mid \operatorname{Diag}(L)=0\}
+
+    Parameters
+    ----------
+    n : int
+        Integer representing the shapes of the matrices: n x n.
+    equip : bool
+        If True, equip space with default metric.
+    """
+
+    def __init__(self, n, equip=True):
+        self.n = n
+        super().__init__(dim=int(n * (n - 1) / 2), equip=equip)
+
+    def _define_embedding_space(self):
+        """Define embedding space of the manifold.
+
+        Returns
+        -------
+        embedding_space : Manifold
+            Instance of Manifold.
+        """
+        return LowerTriangularMatrices(n=self.n)
+
+    @staticmethod
+    def default_metric():
+        """Metric to equip the space with if equip is True."""
+        return MatricesMetric
+
+    def submersion(self, point):
+        """Submersion that defines the manifold.
+
+        Parameters
+        ----------
+        point : array-like, shape=[..., n, n]
+
+        Returns
+        -------
+        submersed_point : array-like, shape=[..., n]
+        """
+        return Matrices.diagonal(point)
+
+    def tangent_submersion(self, vector, point):
+        """Tangent submersion.
+
+        Parameters
+        ----------
+        vector : array-like, shape=[..., n, n]
+        point : Ignored.
+
+        Returns
+        -------
+        submersed_vector : array-like, shape=[..., n]
+        """
+        submersed_vector = Matrices.diagonal(vector)
+        if point is not None and point.ndim > vector.ndim:
+            return gs.broadcast_to(submersed_vector, point.shape[:-1])
+
+        return submersed_vector
+
+    def _create_basis(self):
+        """Create a basis for the vector space.
+
+        Returns
+        -------
+        basis : array-like, shape=[dim, n, n]
+            Basis matrices of the space.
+        """
+        tril_idxs = gs.ravel_tril_indices(self.n, k=-1)
+        vector_bases = gs.cast(
+            gs.one_hot(tril_idxs, self.n * self.n),
+            dtype=gs.get_default_dtype(),
+        )
+        print(vector_bases.shape)
+        return gs.reshape(vector_bases, [-1, self.n, self.n])
+
+    @staticmethod
+    def basis_representation(matrix_representation):
+        """Compute the coefficients of matrices in the given basis.
+
+        Parameters
+        ----------
+        matrix_representation : array-like, shape=[..., n, n]
+            Matrix.
+
+        Returns
+        -------
+        vec : array-like, shape=[..., dim]
+            Vector.
+        """
+        return gs.tril_to_vec(matrix_representation, k=-1)
+
+    def projection(self, point):
+        """Project a point to the manifold.
+
+        Parameters
+        ----------
+        point: array-like, shape[..., n, n]
+            Point to project.
+
+        Returns
+        -------
+        proj_point: array-like, shape[..., n, n]
+            Projected point.
+        """
+        proj_point = self.embedding_space.projection(point)
+        return proj_point - gs.vec_to_diag(gs.diagonal(proj_point, axis1=-2, axis2=-1))
