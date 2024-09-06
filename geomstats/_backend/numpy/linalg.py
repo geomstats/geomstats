4380fca0b015ae1814cfaed1d4aa96af94f9753f
Luís F. Pereira <luisfgper@gmail.com>
2024-09-06 19:27:49 +0200

    Fix numpy backend for >1

--- geomstats/_backend/numpy/linalg.py
+++ geomstats/_backend/numpy/linalg.py
@@ -2,7 +2,7 @@
 
 import numpy as _np
 import scipy as _scipy
-from numpy.linalg import (  # NOQA
+from numpy.linalg import (
     cholesky,
     det,
     eig,
@@ -12,7 +12,6 @@ from numpy.linalg import (  # NOQA
     matrix_power,
     matrix_rank,
     norm,
-    solve,
     svd,
 )
 from scipy.linalg import expm
@@ -27,3 +26,33 @@ from .._shared_numpy.linalg import (
     solve_sylvester,
     sqrtm,
 )
+
+
+def solve(a, b):
+    """
+    Solve a linear matrix equation, or system of linear scalar equations.
+
+    Computes the "exact" solution, `x`, of the well-determined, i.e., full
+    rank, linear matrix equation `ax = b`.
+
+    Parameters
+    ----------
+    a : array-like, shape=[..., M, M]
+        Coefficient matrix.
+    b : array-like, shape=[..., M]
+        Ordinate or "dependent variable" values".
+
+    Returns
+    -------
+    x : array-like, shape=[..., M]
+        Solution to the system a x = b.
+    """
+    batch_shape = a.shape[:-2]
+    if batch_shape:
+        b = _np.expand_dims(b, axis=-1)
+
+    res = _np.linalg.solve(a, b)
+    if batch_shape:
+        return res[..., 0]
+
+    return res
