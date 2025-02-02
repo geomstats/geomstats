985a7e74125cfbd5b2c05bdfcb40ac8c171c4cf4
Luís F. Pereira <luisfgper@gmail.com>
2025-02-02 09:05:42 -0800

    Remove unnecessary xfails in Stiefel tests

--- tests/tests_geomstats/test_geometry/data/stiefel.py
+++ tests/tests_geomstats/test_geometry/data/stiefel.py
@@ -36,25 +36,6 @@ class StiefelCanonicalMetricTestData(RiemannianMetricTestData):
     fail_for_autodiff_exceptions = False
     fail_for_not_implemented_errors = False
 
-    xfails = (
-        "dist_is_log_norm",
-        "dist_is_positive",
-        "dist_triangle_inequality",
-        "dist_is_symmetric",
-        "exp_after_log",
-        "geodesic_bvp_belongs",
-        "geodesic_bvp_reverse",
-        "geodesic_boundary_points",
-        "log_after_exp",
-        "log_is_tangent",
-        "squared_dist_is_positive",
-        "squared_dist_is_symmetric",
-    )
-
-    tolerances = {
-        "log_is_tangent": {"atol": 1e-4},
-    }
-
     def retraction_vec_test_data(self):
         return self.generate_vec_data()
 
