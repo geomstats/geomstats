5b4bb3d644b606a7c467753d7d21390045002d5c
JIHYUN RYU_u <ktf10a@snu.ac.kr>
2025-09-02 15:39:25 +0900

    minor change4 : documentation check2

--- tests/tests_geomstats/test_learning/data/riemannian_robust_m_estimator.py
+++ tests/tests_geomstats/test_learning/data/riemannian_robust_m_estimator.py
@@ -5,27 +5,37 @@ from ._base import BaseEstimatorTestData
 
 
 class RobustMestimatorSOCoincideTestData(BaseEstimatorTestData):
+    """Test SO matrix/vector coincidence data"""
     def estimate_coincide_test_data(self):
+        """Test SO matrix/vector coincidence data"""
         return self.generate_random_data()
 
 
 class HuberMeanExtremeCTestData(BaseEstimatorTestData):
+    """Test huber limiting data"""
     def huber_extreme_c_test_data(self):
+        """Test huber limiting data"""
         return self.generate_random_data()
 
 
 class AutoGradientDescentTestData(TestData):
+    """Test autograd quality data"""
     def auto_grad_descent_same_as_explicit_grad_descent_test_data(self):
+        """Test autograd quality data"""
         return self.generate_random_data()
     
 
 class VarianceTestData(BaseEstimatorTestData):
+    """Test Variance quality data"""
     def variance_repeated_is_zero_test_data(self):
+        """Test Variance 0 quality data"""
         return self.generate_random_data()
 
 
 class VarianceEuclideanTestData(TestData):
+    """Test Euclidean Variance quality data."""
     def variance_test_data(self):
+        """Test Euclidean Variance quality data"""
         data = [
             dict(
                 points=gs.array([[1.0, 2.0], [2.0, 3.0], [3.0, 4.0], [4.0, 5.0]]),
@@ -38,15 +48,21 @@ class VarianceEuclideanTestData(TestData):
 
 
 class DiffStartingPointSameResultTestData(BaseEstimatorTestData):
+    """Test starting point invariance data"""
     def diff_starting_point_same_result_test_data(self):
+        """Test starting point invariance data"""
         return self.generate_random_data()
     
 
 class AutoGradientNotImplementedOnNumpyBackendTestData(BaseEstimatorTestData):
+    """Test autograd not working on numpy data"""
     def auto_gradient_not_implemented_on_numpy_backend_test_data(self):
+        """Test autograd not working on numpy data"""
         return self.generate_tests([{}])
       
 
 class SameMestimatorFunctionGivenByCustomAndExplicitTestData(BaseEstimatorTestData):
+    """Test custom function working data"""
     def same_m_estimator_function_given_by_custom_and_explicit_test_data(self):
+        """Test custom function working data"""
         return self.generate_random_data()
