c66725340e11ba160e6d1f985610251c47ebb4c6
Abby Bertics <abby.bertics@gmail.com>
2026-01-14 09:46:40 -0800

    Test breaking only for SO3 matrix point type

--- tests/tests_geomstats/test_learning/test_mds.py
+++ tests/tests_geomstats/test_learning/test_mds.py
@@ -6,6 +6,7 @@ import pytest
 from geomstats.geometry.hyperboloid import Hyperboloid
 from geomstats.geometry.hypersphere import Hypersphere
 from geomstats.geometry.spd_matrices import SPDMatrices
+from geomstats.geometry.special_orthogonal import SpecialOrthogonal
 from geomstats.learning.mds import MDS
 from geomstats.test.parametrizers import DataBasedParametrizer
 from geomstats.test_cases.learning.mds import (
@@ -19,7 +20,7 @@ from .data.mds import MDSTestData  # MDSEuclideanTestData, MDSSPDTestData,
     scope="class",
     params=[
         Hypersphere(dim=random.randint(3, 4)),
-        # SpecialOrthogonal(n=3, point_type="vector"),
+        SpecialOrthogonal(n=3, point_type="vector"),
         # SpecialOrthogonal(n=3, point_type="matrix"),
         SPDMatrices(3),
         Hyperboloid(dim=3),
